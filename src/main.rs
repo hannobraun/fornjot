5b60597822fa1f7b1e01a3d32f5fc46bb3e29b0e
Hanno Braun <hb@hannobraun.de>
2020-07-30 19:46:56 +0200

    Move renderer initialization to `Renderer`

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 mod renderer;
 
-use futures::executor::block_on;
 use winit::{
     event::{Event, KeyboardInput, VirtualKeyCode, WindowEvent},
     event_loop::{ControlFlow, EventLoop},
@@ -20,44 +19,7 @@ fn main() {
         .build(&event_loop)
         .unwrap();
 
-    let surface = wgpu::Surface::create(&window);
-
-    let adapter = block_on(wgpu::Adapter::request(
-        &wgpu::RequestAdapterOptions {
-            power_preference: wgpu::PowerPreference::Default,
-            compatible_surface: Some(&surface),
-        },
-        wgpu::BackendBit::VULKAN,
-    ))
-    .unwrap();
-
-    let (device, queue) =
-        block_on(adapter.request_device(&wgpu::DeviceDescriptor {
-            extensions: wgpu::Extensions {
-                anisotropic_filtering: false,
-            },
-            limits: wgpu::Limits::default(),
-        }));
-
-    let size = window.inner_size();
-
-    let swap_chain_desc = wgpu::SwapChainDescriptor {
-        usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
-        format: wgpu::TextureFormat::Bgra8UnormSrgb,
-        width: size.width,
-        height: size.height,
-        present_mode: wgpu::PresentMode::Mailbox,
-    };
-
-    let swap_chain = device.create_swap_chain(&surface, &swap_chain_desc);
-
-    let mut renderer = Renderer {
-        surface,
-        device,
-        queue,
-        swap_chain_desc,
-        swap_chain,
-    };
+    let mut renderer = Renderer::new(&window);
 
     event_loop.run(move |event, _, control_flow| match event {
         Event::WindowEvent {
--- src/renderer.rs
+++ src/renderer.rs
@@ -1,7 +1,53 @@
+use futures::executor::block_on;
+use winit::window::Window;
+
 pub struct Renderer {
     pub surface: wgpu::Surface,
     pub device: wgpu::Device,
     pub queue: wgpu::Queue,
     pub swap_chain_desc: wgpu::SwapChainDescriptor,
     pub swap_chain: wgpu::SwapChain,
+}
+
+impl Renderer {
+    pub fn new(window: &Window) -> Self {
+        let surface = wgpu::Surface::create(window);
+
+        let adapter = block_on(wgpu::Adapter::request(
+            &wgpu::RequestAdapterOptions {
+                power_preference: wgpu::PowerPreference::Default,
+                compatible_surface: Some(&surface),
+            },
+            wgpu::BackendBit::VULKAN,
+        ))
+        .unwrap();
+
+        let (device, queue) =
+            block_on(adapter.request_device(&wgpu::DeviceDescriptor {
+                extensions: wgpu::Extensions {
+                    anisotropic_filtering: false,
+                },
+                limits: wgpu::Limits::default(),
+            }));
+
+        let size = window.inner_size();
+
+        let swap_chain_desc = wgpu::SwapChainDescriptor {
+            usage: wgpu::TextureUsage::OUTPUT_ATTACHMENT,
+            format: wgpu::TextureFormat::Bgra8UnormSrgb,
+            width: size.width,
+            height: size.height,
+            present_mode: wgpu::PresentMode::Mailbox,
+        };
+
+        let swap_chain = device.create_swap_chain(&surface, &swap_chain_desc);
+
+        Self {
+            surface,
+            device,
+            queue,
+            swap_chain_desc,
+            swap_chain,
+        }
+    }
 }
