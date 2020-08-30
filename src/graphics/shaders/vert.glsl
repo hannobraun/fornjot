fe1924d7b85805612edb0225c7bcf9361f5fe58e
Hanno Braun <hb@hannobraun.de>
2020-08-31 00:14:38 +0200

    Rename variable

--- src/graphics/shaders/vert.glsl
+++ src/graphics/shaders/vert.glsl
@@ -5,9 +5,9 @@ layout(set = 0, binding = 0) uniform Locals {
     mat4 transform;
 };
 
-layout(location = 0) in vec3 pos;
+layout(location = 0) in vec3 position;
 
 
 void main() {
-    gl_Position = transform * vec4(pos, 1.0);
+    gl_Position = transform * vec4(position, 1.0);
 }
