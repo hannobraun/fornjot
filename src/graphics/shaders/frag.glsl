67769fc29eaec5bd494c1a99b9f90f35b9ed02d1
Hanno Braun <hb@hannobraun.de>
2020-08-31 11:43:46 +0200

    Implement simple shading

--- src/graphics/shaders/frag.glsl
+++ src/graphics/shaders/frag.glsl
@@ -1,9 +1,21 @@
 #version 450
 
 
+#define PI 3.14159265359
+
+
+layout(location = 1) in vec3 normal;
+
 layout(location = 0) out vec4 color;
 
 
 void main() {
-    color = vec4(1.0, 0.0, 0.0, 1.0);
+    vec3 light = vec3(0.0, 0.0, 1.0);
+
+    float angle = acos(dot(-light, normal));
+    float f_angle = (angle / (PI / 2.0));
+
+    float f_normal = 1.0 - f_angle;
+
+    color = vec4(vec3(1.0, 0.0, 0.0) * f_normal, 1.0);
 }
--- src/graphics/shaders/vert.glsl
+++ src/graphics/shaders/vert.glsl
@@ -6,8 +6,12 @@ layout(set = 0, binding = 0) uniform Locals {
 };
 
 layout(location = 0) in vec3 position;
+layout(location = 1) in vec3 normal;
+
+layout(location = 1) out vec3 normal_out;
 
 
 void main() {
     gl_Position = transform * vec4(position, 1.0);
+    normal_out = (transform * vec4(normal, 0.0)).xyz;
 }
