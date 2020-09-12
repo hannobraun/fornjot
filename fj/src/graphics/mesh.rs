35cb2a619a201a256409b36e916bb8e9c8b4c83b
Hanno Braun <hb@hannobraun.de>
2020-09-12 11:47:46 +0200

    Refactor

--- fj/src/graphics/mesh.rs
+++ fj/src/graphics/mesh.rs
@@ -1,3 +1,5 @@
+use std::convert::TryInto;
+
 use super::{Index, Vertex};
 
 pub struct Mesh {
@@ -11,5 +13,11 @@ impl Mesh {
             vertices: Vec::new(),
             indices: Vec::new(),
         }
+    }
+
+    pub fn vertex(&mut self, position: [f32; 3], normal: [f32; 3]) -> Index {
+        let i = self.vertices.len();
+        self.vertices.push(Vertex { position, normal });
+        i.try_into().unwrap()
     }
 }
--- models/cube/src/main.rs
+++ models/cube/src/main.rs
@@ -1,74 +1,68 @@
-use fj::Vertex;
-
+#[rustfmt::skip]
 fn main() {
     let mut mesh = fj::Mesh::new();
-    mesh.vertices.extend_from_slice(VERTICES);
 
-    #[rustfmt::skip]
+    // left
+    let i0  = mesh.vertex([-0.5, -0.5, -0.5], [-1.0,  0.0,  0.0]);
+    let i1  = mesh.vertex([-0.5,  0.5, -0.5], [-1.0,  0.0,  0.0]);
+    let i2  = mesh.vertex([-0.5, -0.5,  0.5], [-1.0,  0.0,  0.0]);
+    let i3  = mesh.vertex([-0.5,  0.5,  0.5], [-1.0,  0.0,  0.0]);
+
+    // right
+    let i4  = mesh.vertex([ 0.5, -0.5, -0.5], [ 1.0,  0.0,  0.0]);
+    let i5  = mesh.vertex([ 0.5,  0.5, -0.5], [ 1.0,  0.0,  0.0]);
+    let i6  = mesh.vertex([ 0.5, -0.5,  0.5], [ 1.0,  0.0,  0.0]);
+    let i7  = mesh.vertex([ 0.5,  0.5,  0.5], [ 1.0,  0.0,  0.0]);
+
+    // front
+    let i8  = mesh.vertex([-0.5, -0.5, -0.5], [ 0.0, -1.0,  0.0]);
+    let i9  = mesh.vertex([ 0.5, -0.5, -0.5], [ 0.0, -1.0,  0.0]);
+    let i10 = mesh.vertex([-0.5, -0.5,  0.5], [ 0.0, -1.0,  0.0]);
+    let i11 = mesh.vertex([ 0.5, -0.5,  0.5], [ 0.0, -1.0,  0.0]);
+
+    // back
+    let i12 = mesh.vertex([-0.5,  0.5, -0.5], [ 0.0,  1.0,  0.0]);
+    let i13 = mesh.vertex([ 0.5,  0.5, -0.5], [ 0.0,  1.0,  0.0]);
+    let i14 = mesh.vertex([-0.5,  0.5,  0.5], [ 0.0,  1.0,  0.0]);
+    let i15 = mesh.vertex([ 0.5,  0.5,  0.5], [ 0.0,  1.0,  0.0]);
+
+    // bottom
+    let i16 = mesh.vertex([-0.5, -0.5, -0.5], [ 0.0,  0.0, -1.0]);
+    let i17 = mesh.vertex([ 0.5, -0.5, -0.5], [ 0.0,  0.0, -1.0]);
+    let i18 = mesh.vertex([-0.5,  0.5, -0.5], [ 0.0,  0.0, -1.0]);
+    let i19 = mesh.vertex([ 0.5,  0.5, -0.5], [ 0.0,  0.0, -1.0]);
+
+    // top
+    let i20 = mesh.vertex([-0.5, -0.5,  0.5], [ 0.0,  0.0,  1.0]);
+    let i21 = mesh.vertex([ 0.5, -0.5,  0.5], [ 0.0,  0.0,  1.0]);
+    let i22 = mesh.vertex([-0.5,  0.5,  0.5], [ 0.0,  0.0,  1.0]);
+    let i23 = mesh.vertex([ 0.5,  0.5,  0.5], [ 0.0,  0.0,  1.0]);
+
     mesh.indices.extend_from_slice(&[
         // left
-        0, 2, 1,
-        1, 2, 3,
+        i0, i2, i1,
+        i1, i2, i3,
 
         // right
-        4, 5, 6,
-        5, 7, 6,
+        i4, i5, i6,
+        i5, i7, i6,
 
         // front
-        8, 9, 10,
-        9, 11, 10,
+        i8, i9, i10,
+        i9, i11, i10,
 
         // back
-        12, 14, 13,
-        13, 14, 15,
+        i12, i14, i13,
+        i13, i14, i15,
 
         // bottom
-        16, 18, 19,
-        16, 19, 17,
+        i16, i18, i19,
+        i16, i19, i17,
 
         // top
-        20, 21, 23,
-        20, 23, 22,
+        i20, i21, i23,
+        i20, i23, i22,
     ]);
 
     fj::run(mesh);
 }
-
-#[rustfmt::skip]
-pub const VERTICES: &[Vertex] = &[
-    // left
-    Vertex { position: [-0.5, -0.5, -0.5], normal: [-1.0,  0.0,  0.0] }, // 0
-    Vertex { position: [-0.5,  0.5, -0.5], normal: [-1.0,  0.0,  0.0] }, // 1
-    Vertex { position: [-0.5, -0.5,  0.5], normal: [-1.0,  0.0,  0.0] }, // 2
-    Vertex { position: [-0.5,  0.5,  0.5], normal: [-1.0,  0.0,  0.0] }, // 3
-
-    // right
-    Vertex { position: [ 0.5, -0.5, -0.5], normal: [ 1.0,  0.0,  0.0] }, // 4
-    Vertex { position: [ 0.5,  0.5, -0.5], normal: [ 1.0,  0.0,  0.0] }, // 5
-    Vertex { position: [ 0.5, -0.5,  0.5], normal: [ 1.0,  0.0,  0.0] }, // 6
-    Vertex { position: [ 0.5,  0.5,  0.5], normal: [ 1.0,  0.0,  0.0] }, // 7
-
-    // front
-    Vertex { position: [-0.5, -0.5, -0.5], normal: [ 0.0, -1.0,  0.0] }, // 8
-    Vertex { position: [ 0.5, -0.5, -0.5], normal: [ 0.0, -1.0,  0.0] }, // 9
-    Vertex { position: [-0.5, -0.5,  0.5], normal: [ 0.0, -1.0,  0.0] }, // 10
-    Vertex { position: [ 0.5, -0.5,  0.5], normal: [ 0.0, -1.0,  0.0] }, // 11
-
-    // back
-    Vertex { position: [-0.5,  0.5, -0.5], normal: [ 0.0,  1.0,  0.0] }, // 12
-    Vertex { position: [ 0.5,  0.5, -0.5], normal: [ 0.0,  1.0,  0.0] }, // 13
-    Vertex { position: [-0.5,  0.5,  0.5], normal: [ 0.0,  1.0,  0.0] }, // 14
-    Vertex { position: [ 0.5,  0.5,  0.5], normal: [ 0.0,  1.0,  0.0] }, // 15
-
-    // bottom
-    Vertex { position: [-0.5, -0.5, -0.5], normal: [ 0.0,  0.0, -1.0] }, // 16
-    Vertex { position: [ 0.5, -0.5, -0.5], normal: [ 0.0,  0.0, -1.0] }, // 17
-    Vertex { position: [-0.5,  0.5, -0.5], normal: [ 0.0,  0.0, -1.0] }, // 18
-    Vertex { position: [ 0.5,  0.5, -0.5], normal: [ 0.0,  0.0, -1.0] }, // 19
-
-    // top
-    Vertex { position: [-0.5, -0.5,  0.5], normal: [ 0.0,  0.0,  1.0] }, // 20
-    Vertex { position: [ 0.5, -0.5,  0.5], normal: [ 0.0,  0.0,  1.0] }, // 21
-    Vertex { position: [-0.5,  0.5,  0.5], normal: [ 0.0,  0.0,  1.0] }, // 22
-    Vertex { position: [ 0.5,  0.5,  0.5], normal: [ 0.0,  0.0,  1.0] }, // 23
-];
