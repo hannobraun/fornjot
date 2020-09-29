150edbd0647f1b4bcc800e7d73909d4b1b7b1656
Hanno Braun <hb@hannobraun.de>
2020-09-29 23:44:20 +0200

    Implement approximate comparisons of triangles

--- fj/src/geometry/triangle.rs
+++ fj/src/geometry/triangle.rs
@@ -56,10 +56,71 @@ impl From<&Triangle> for Array {
     }
 }
 
+impl approx::AbsDiffEq for Triangle {
+    type Epsilon = <f32 as approx::AbsDiffEq>::Epsilon;
+
+    fn default_epsilon() -> Self::Epsilon {
+        f32::default_epsilon()
+    }
+
+    fn abs_diff_eq(&self, other: &Triangle, epsilon: Self::Epsilon) -> bool {
+        [self.a, self.b, self.c]
+            .abs_diff_eq(&[other.a, other.b, other.c], epsilon)
+    }
+}
+
+impl approx::RelativeEq for Triangle {
+    fn default_max_relative() -> Self::Epsilon {
+        f32::default_max_relative()
+    }
+
+    fn relative_eq(
+        &self,
+        other: &Triangle,
+        epsilon: Self::Epsilon,
+        max_relative: Self::Epsilon,
+    ) -> bool {
+        [self.a, self.b, self.c].relative_eq(
+            &[other.a, other.b, other.c],
+            epsilon,
+            max_relative,
+        )
+    }
+}
+
 type Array = [[f32; 3]; 3];
 
 #[derive(Debug, PartialEq)]
 pub struct Triangles(pub Vec<Triangle>);
+
+impl approx::AbsDiffEq for Triangles {
+    type Epsilon = <Triangle as approx::AbsDiffEq>::Epsilon;
+
+    fn default_epsilon() -> Self::Epsilon {
+        Triangle::default_epsilon()
+    }
+
+    fn abs_diff_eq(&self, other: &Triangles, epsilon: Self::Epsilon) -> bool {
+        self.0.as_slice().abs_diff_eq(other.0.as_slice(), epsilon)
+    }
+}
+
+impl approx::RelativeEq for Triangles {
+    fn default_max_relative() -> Self::Epsilon {
+        Triangle::default_max_relative()
+    }
+
+    fn relative_eq(
+        &self,
+        other: &Triangles,
+        epsilon: Self::Epsilon,
+        max_relative: Self::Epsilon,
+    ) -> bool {
+        self.0
+            .as_slice()
+            .relative_eq(other.0.as_slice(), epsilon, max_relative)
+    }
+}
 
 #[cfg(test)]
 mod tests {
