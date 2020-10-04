60e7b1df178747c674d1b6aaa68c438000afa383
Hanno Braun <hb@hannobraun.de>
2020-10-04 19:12:02 +0200

    Reorder code

--- fj/src/geometry/primitives/circle.rs
+++ fj/src/geometry/primitives/circle.rs
@@ -9,22 +9,22 @@ pub struct Circle {
 }
 
 impl Circle {
+    pub fn from_radius(radius: f32) -> Self {
+        Self { radius }
+    }
+
     pub fn from_diameter(diameter: f32) -> Self {
         Self {
             radius: diameter / 2.0,
         }
     }
 
-    pub fn from_radius(radius: f32) -> Self {
-        Self { radius }
+    pub fn radius(&self) -> f32 {
+        self.radius
     }
 
     pub fn diameter(&self) -> f32 {
         self.radius * 2.0
-    }
-
-    pub fn radius(&self) -> f32 {
-        self.radius
     }
 
     pub fn to_mesh(&self, n: u16) -> Option<Mesh> {
@@ -76,11 +76,11 @@ mod tests {
 
     #[test]
     fn circle_should_be_created_from_diameter_and_radius() {
-        let circle = Circle::from_diameter(1.0);
-        assert_eq!(circle.radius(), 0.5);
-
         let circle = Circle::from_radius(1.0);
         assert_eq!(circle.diameter(), 2.0);
+
+        let circle = Circle::from_diameter(1.0);
+        assert_eq!(circle.radius(), 0.5);
     }
 
     #[test]
