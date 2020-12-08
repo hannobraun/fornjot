5b9be8c5890d9e59cf7b5979efe8efedc33cc239
Hanno Braun <hb@hannobraun.de>
2020-12-08 20:01:01 +0100

    Add `Vertex::new`

--- fj/src/geometry/trapezoidation/vertex.rs
+++ fj/src/geometry/trapezoidation/vertex.rs
@@ -4,6 +4,10 @@ use nalgebra::Point2;
 pub struct Vertex(pub Point2<f32>);
 
 impl Vertex {
+    pub fn new(x: f32, y: f32) -> Self {
+        Self(Point2::new(x, y))
+    }
+
     pub fn is_higher_than(&self, other: &Vertex) -> bool {
         // Higher-ness is primarily determined by y coordinate.
         if self.0.y > other.0.y {
@@ -35,14 +39,12 @@ impl Vertex {
 
 #[cfg(test)]
 mod tests {
-    use nalgebra::Point2;
-
     use super::Vertex;
 
     #[test]
     fn vertex_with_high_y_should_be_higher_than_vertex_with_low_y() {
-        let high = Vertex(Point2::new(0.0, 1.0));
-        let low = Vertex(Point2::new(0.0, 0.0));
+        let high = Vertex::new(0.0, 1.0);
+        let low = Vertex::new(0.0, 0.0);
 
         assert!(high.is_higher_than(&low));
         assert!(low.is_lower_than(&high));
@@ -50,8 +52,8 @@ mod tests {
 
     #[test]
     fn vertex_with_equal_y_but_left_x_should_be_higher_than_right_x() {
-        let high = Vertex(Point2::new(0.0, 0.0));
-        let low = Vertex(Point2::new(1.0, 0.0));
+        let high = Vertex::new(0.0, 0.0);
+        let low = Vertex::new(1.0, 0.0);
 
         assert!(high.is_higher_than(&low));
         assert!(low.is_lower_than(&high));
@@ -59,7 +61,7 @@ mod tests {
 
     #[test]
     fn vertex_should_not_be_higher_or_lower_than_equal_vertex() {
-        let vertex = Vertex(Point2::new(0.0, 0.0));
+        let vertex = Vertex::new(0.0, 0.0);
 
         assert_eq!(vertex.is_higher_than(&vertex), false);
         assert_eq!(vertex.is_lower_than(&vertex), false);
