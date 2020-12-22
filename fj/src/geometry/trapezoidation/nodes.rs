f0b15d80a90a68cb7f6316d2c5114de050b7c14a
Hanno Braun <hb@hannobraun.de>
2020-12-22 19:31:29 +0100

    Un-derive `Clone`/`Copy` from node types

--- fj/src/geometry/trapezoidation/nodes.rs
+++ fj/src/geometry/trapezoidation/nodes.rs
@@ -66,19 +66,19 @@ impl From<LeafId> for NodeId {
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub struct LeafId(pub NodeId);
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct Node<Branch, Leaf> {
     pub parent: Option<NodeId>,
     pub kind: NodeKind<Branch, Leaf>,
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub enum NodeKind<Branch, Leaf> {
     Branch(BranchNode<Branch>),
     Leaf(Leaf),
 }
 
-#[derive(Clone, Copy, Debug, PartialEq)]
+#[derive(Debug, PartialEq)]
 pub struct BranchNode<T> {
     pub above: NodeId,
     pub below: NodeId,
