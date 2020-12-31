f856442c556ebfc1949bf876932d8229cb1bf867
Hanno Braun <hb@hannobraun.de>
2020-12-31 19:33:45 +0100

    Move `Tree` to other module

---
+++ fj/src/geometry/trapezoidation/tree/layer_b_splitting.rs
@@ -0,0 +1,118 @@
+use crate::geometry::trapezoidation::{Edge, Trapezoid, Vertex};
+
+use super::layer_a_connections::{
+    Node, NodeId, Relation, Tree as InternalTree,
+};
+
+pub struct Tree {
+    nodes: InternalTree<Branch, Trapezoid>,
+    root: NodeId,
+}
+
+impl Tree {
+    pub fn new() -> Self {
+        let mut nodes = InternalTree::new();
+        let root = nodes.insert_leaf(Trapezoid::new());
+
+        Self { nodes, root }
+    }
+
+    pub fn root(&self) -> NodeId {
+        self.root
+    }
+
+    /// Split an existing trapezoid
+    ///
+    /// The provided branch will take its place in the tree. The branch will
+    /// have two children, the existing trapezoid and a new one.
+    pub fn split(&mut self, split_at: NodeId, split_with: Branch) -> NodeId {
+        if self.get(&split_at).branch().is_some() {
+            panic!("You can only split trapezoids, not branches");
+        }
+
+        // This is the new trapezoid.
+        let new_leaf_id = self.nodes.insert_leaf(Trapezoid::new());
+
+        // We're creating a leaf here, but we'll extend it into a branch in a
+        // moment.
+        let new_branch_id = self.nodes.insert_leaf(Trapezoid::new());
+
+        // Make the new leaf take the place of the one we're about to split,
+        // before transforming it into a branch.
+        let old_leaf_id = split_at;
+        self.nodes.replace_child(&old_leaf_id, &new_branch_id);
+
+        self.nodes.change_leaf_to_branch(
+            &new_branch_id,
+            split_with,
+            &old_leaf_id,
+            &new_leaf_id,
+        );
+
+        new_branch_id
+    }
+
+    pub fn get(&self, id: &NodeId) -> &Node<Branch, Trapezoid> {
+        self.nodes.get(id)
+    }
+
+    pub fn trapezoids(
+        &self,
+    ) -> impl Iterator<Item = (NodeId, &Trapezoid)> + '_ {
+        self.nodes.leafs()
+    }
+
+    pub fn parent_of(&self, id: &NodeId) -> Option<(NodeId, Relation)> {
+        self.nodes.parent_of(id)
+    }
+}
+
+#[derive(Clone, Copy, Debug, PartialEq)]
+pub enum Branch {
+    Edge(Edge),
+    Vertex(Vertex),
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::trapezoidation::{
+        tree::layer_a_connections::Relation, Vertex,
+    };
+
+    use super::{Branch, Tree};
+
+    #[test]
+    fn tree_should_start_with_a_single_root_leaf() {
+        let tree = Tree::new();
+
+        let leafs: Vec<_> = tree.trapezoids().collect();
+        assert_eq!(leafs.len(), 1);
+
+        let (root_id, _) = leafs[0];
+        assert_eq!(root_id, tree.root());
+    }
+
+    #[test]
+    fn tree_should_split_trapezoids() {
+        let mut tree = Tree::new();
+        let (original_root_id, _) = tree.trapezoids().next().unwrap();
+
+        let new_node = Branch::Vertex(Vertex::new(0.0, 0.0));
+        let new_root_id = tree.split(original_root_id, new_node);
+
+        let leafs: Vec<_> = tree.trapezoids().collect();
+        assert_eq!(leafs.len(), 2);
+
+        for (id, _) in leafs {
+            let (parent_id, relation) = tree.parent_of(&id).unwrap();
+
+            assert_eq!(parent_id, new_root_id);
+
+            if id == original_root_id {
+                assert_eq!(relation, Relation::Above);
+            } else {
+                assert_eq!(relation, Relation::Below);
+            }
+        }
+    }
+}
