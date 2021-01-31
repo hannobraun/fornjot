598eb39fe0f990cb6d7663e1f9ce83cf7144f1bc
Hanno Braun <hb@hannobraun.de>
2021-01-31 23:29:41 +0100

    Implement point query

--- fj/src/geometry/triangulation/trapezoidation/query/find_region.rs
+++ fj/src/geometry/triangulation/trapezoidation/query/find_region.rs
@@ -1,5 +1,113 @@
-// TASK: Implement point query (point -> trapezoid containing point):
-//       - Start at source, follow path to sink that identifies trapezoid.
-//       - At each non-sink, compare with point to decide which path to follow
-//         - At X node, whether point is left or right of segment
-//         - At Y node, whether point is below or above of segment
+use crate::geometry::triangulation::trapezoidation::{
+    point::{self, Point},
+    segment,
+};
+
+use super::graph::{Graph, Id, Node, X, Y};
+
+pub fn find_region<Region>(point: &Point, graph: &Graph<X, Y, Region>) -> Id {
+    let mut current_id = graph.source();
+
+    loop {
+        match graph.get(current_id) {
+            Node::X(X {
+                segment,
+                left,
+                right,
+            }) => match segment.relation_from_point(point) {
+                Some(segment::Relation::Left) => current_id = *left,
+                Some(segment::Relation::Right) => current_id = *right,
+                None => {
+                    // I don't think I have to handle this case, as it can only
+                    // happen if the tree is misshapen or we're getting invalid
+                    // input, meaning some other code would have to be buggy.
+                    // I'm not completely sure though, so please make up your
+                    // own mind if you happen to hit this panic.
+                    panic!(
+                        "No relation from point ({:?}) to segment ({:?})",
+                        point, segment
+                    )
+                }
+            },
+            Node::Y(Y {
+                point: p,
+                below,
+                above,
+            }) => match point.relation_to(p) {
+                Some(point::Relation::Below) => current_id = *below,
+                Some(point::Relation::Above) => current_id = *above,
+                None => {
+                    // TASK: Figure out, if we need to handle this case. I think
+                    //       it can happen as part of normal operations,
+                    //       whenever we're inserting a point that is already in
+                    //       the tree.
+                    todo!()
+                }
+            },
+            Node::Sink(_) => return current_id,
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::triangulation::trapezoidation::{
+        point::Point,
+        query::graph::{self, Node, X, Y},
+        segment::Segment,
+    };
+
+    use super::find_region;
+
+    type Graph = graph::Graph<X, Y, Region>;
+
+    #[derive(Default)]
+    struct Region(u64);
+
+    #[test]
+    fn find_region_should_find_root_region_if_none_other_exist() {
+        let graph = Graph::new();
+
+        let region = find_region(&Point::new(0.0, 0.0), &graph);
+        assert_eq!(region, graph.source());
+    }
+
+    #[test]
+    fn find_region_should_determine_if_point_is_left_or_right_of_x_node() {
+        let mut graph = Graph::new();
+
+        let left = graph.insert_sink(Region(1));
+        let right = graph.insert_sink(Region(2));
+
+        let node = Node::X(X {
+            segment: Segment::new(Point::new(1.0, 0.0), Point::new(1.0, 2.0))
+                .unwrap(),
+            left,
+            right,
+        });
+
+        graph.replace(graph.source(), node);
+
+        assert_eq!(find_region(&Point::new(0.0, 1.0), &graph), left);
+        assert_eq!(find_region(&Point::new(2.0, 1.0), &graph), right);
+    }
+
+    #[test]
+    fn find_region_should_determine_if_point_is_below_or_above_a_y_node() {
+        let mut graph = Graph::new();
+
+        let below = graph.insert_sink(Region(1));
+        let above = graph.insert_sink(Region(2));
+
+        let node = Node::Y(Y {
+            point: Point::new(0.0, 1.0),
+            below,
+            above,
+        });
+
+        graph.replace(graph.source(), node);
+
+        assert_eq!(find_region(&Point::new(0.0, 0.0), &graph), below);
+        assert_eq!(find_region(&Point::new(0.0, 2.0), &graph), above);
+    }
+}
