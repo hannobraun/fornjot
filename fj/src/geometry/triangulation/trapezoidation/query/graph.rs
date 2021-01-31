c2585d1fe6db84eb55840b6a542bf1889096fc9a
Hanno Braun <hb@hannobraun.de>
2021-01-31 13:03:29 +0100

    Add draft for query structure graph

--- fj/src/geometry/triangulation/trapezoidation/query/graph.rs
+++ fj/src/geometry/triangulation/trapezoidation/query/graph.rs
@@ -1,3 +1,37 @@
 //! Defines the point location query structure
 
-// TASK: Implement
+use std::collections::HashMap;
+
+use crate::geometry::triangulation::trapezoidation::{
+    point::Point, segment::Segment,
+};
+
+// TASK: Add single source on construction.
+// TASK: Implement behavior, as required by insertion and query code.
+pub struct Graph(HashMap<Id, Node>);
+
+pub struct Id(u32);
+
+pub enum Node {
+    NonSink(NonSink),
+    Sink(Sink),
+}
+
+pub enum NonSink {
+    X(X),
+    Y(Y),
+}
+
+pub struct X {
+    _segment: Segment,
+    _left: Id,
+    _right: Id,
+}
+
+pub struct Y {
+    _point: Point,
+    _below: Id,
+    _above: Id,
+}
+
+pub struct Sink;
