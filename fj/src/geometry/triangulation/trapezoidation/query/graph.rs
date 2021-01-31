8273cf7d308ea481f92c813f16d714a48308b14f
Hanno Braun <hb@hannobraun.de>
2021-01-31 16:02:55 +0100

    Derive traits for graph types

--- fj/src/geometry/triangulation/trapezoidation/query/graph.rs
+++ fj/src/geometry/triangulation/trapezoidation/query/graph.rs
@@ -15,28 +15,33 @@ pub struct Graph {
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash)]
 pub struct Id(u32);
 
+#[derive(Debug, PartialEq)]
 pub enum Node {
     NonSink(NonSink),
     Sink(Region),
 }
 
+#[derive(Debug, PartialEq)]
 pub enum NonSink {
     X(X),
     Y(Y),
 }
 
+#[derive(Debug, PartialEq)]
 pub struct X {
     pub segment: Segment,
     pub left: Id,
     pub right: Id,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Y {
     pub point: Point,
     pub below: Id,
     pub above: Id,
 }
 
+#[derive(Debug, PartialEq)]
 pub struct Region {
     pub left_segment: Option<Id>,
     pub right_segment: Option<Id>,
