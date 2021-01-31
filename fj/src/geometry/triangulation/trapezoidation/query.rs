e282f5780fd73389e86ae071b936b756f459f859
Hanno Braun <hb@hannobraun.de>
2021-01-31 13:05:31 +0100

    Add more specific tasks

--- fj/src/geometry/triangulation/trapezoidation/query.rs
+++ fj/src/geometry/triangulation/trapezoidation/query.rs
@@ -2,4 +2,15 @@
 
 pub mod graph;
 
-// TASK: Implement
+// TASK: Implement point query (point -> trapezoid containing point):
+//       - Start at source, follow path to sink that identifies trapezoid.
+//       - At each non-sink, compare with point to decide which path to follow
+//         - At X node, whether point is left or right of segment
+//         - At Y node, whether point is below or above of segment
+// TASK: Insertion:
+//       - Unless upper point is already inserted:
+//         - Find trapezoid that point is in
+//         - Add two new sink nodes
+//         - Replace trapezoid node with Y node that points to the new sinks
+//       - Unless lower point is already inserted, proceed as with upper point
+//       - "Thread" segment through trapezoidation; see paper, unclear right now
