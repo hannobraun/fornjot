c99ff6d3195dc4fb7edc79bf0c8a9cb49b06278d
Hanno Braun <hb@hannobraun.de>
2021-02-01 20:05:43 +0100

    Update task

--- fj/src/geometry/triangulation/trapezoidation/query/mod.rs
+++ fj/src/geometry/triangulation/trapezoidation/query/mod.rs
@@ -4,4 +4,5 @@ pub mod find_region;
 pub mod graph;
 pub mod insert;
 
-// TASK: Consider merging this module into its parent.
+// TASK: Consider merging this module into its parent. If you do this, consider
+//       renaming the module `find_region` to `query`.
