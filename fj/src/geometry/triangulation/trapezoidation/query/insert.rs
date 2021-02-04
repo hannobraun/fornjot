0ef7df70c25d54a4b38d679b08ded0e650f9f7a8
Hanno Braun <hb@hannobraun.de>
2021-02-04 21:27:20 +0100

    Update tasks

--- fj/src/geometry/triangulation/trapezoidation/query/insert.rs
+++ fj/src/geometry/triangulation/trapezoidation/query/insert.rs
@@ -7,6 +7,13 @@ use super::{
     graph::{Graph, Node, X, Y},
 };
 
+// TASK: Implement `insert`
+//       - Insert upper point
+//       - Insert lower point
+//       - Insert segment
+// TASK: Decide where to update region when inserting.
+// TASK: Merge all regions that have the same bounding segments.
+
 pub fn insert_point<Region>(point: Point, graph: &mut Graph<X, Y, Region>)
 where
     Region: Default,
@@ -31,31 +38,27 @@ where
     }
 }
 
-// TASK: Implement:
-//       - Insert upper point
-//       - Insert lower point
-//       - Insert segment:
-//         - Compare with each node
-//           - At Y node:
-//             - If above or below, follow respective path
-//             - If neither, follow _both_ paths
-//           - At X node: Check whether left or right, follow respective path
-//             - If segments overlap in y, "left" and "right" are unambiguously
-//               defined depending on whether the left or right horizontal
-//               extension of the overlapping endpoint hit the other segment.
-//               Just find an endpoint that is between the other segment's
-//               endpoints, find point on other segment with same y-coordinate,
-//               compare x-coordinate.
-//             - Endpoints of a segment can never be _on_ another segment,
-//               unless the initial polygon is self-intersecting, which we don't
-//               accept as valid input. (And the paper only allows segments with
-//               common endpoints, at most.)
-//             - But what if the segments don't overlap in y? There obviously
-//               are segments like that, but will we ever have to compare them
-//               when inserting into a valid tree? I don't know.
-//           - At Sink:
-//             Split region into left and right. Update bounding segments.
-//         - Once done, merge all regions that have the same bounding segments.
+// TASK: Implement `insert_segment`:
+//       - Compare with each node
+//         - At Y node:
+//           - If above or below, follow respective path
+//           - If neither, follow _both_ paths
+//         - At X node: Check whether left or right, follow respective path
+//           - If segments overlap in y, "left" and "right" are unambiguously
+//             defined depending on whether the left or right horizontal
+//             extension of the overlapping endpoint hit the other segment. Just
+//             find an endpoint that is between the other segment's endpoints,
+//             find point on other segment with same y-coordinate,
+//             compare x-coordinate.
+//           - Endpoints of a segment can never be _on_ another segment, unless
+//             the initial polygon is self-intersecting, which we don't accept
+//             as valid input. (And the paper only allows segments with common
+//             endpoints, at most.)
+//           - But what if the segments don't overlap in y? There obviously are
+//             segments like that, but will we ever have to compare them when
+//             inserting into a valid tree? I don't know.
+//         - At Sink:
+//           Split region into left and right. Update bounding segments.
 
 #[cfg(test)]
 mod tests {
