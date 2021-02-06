42ff7d25b2d5edb0d968d64b778839491955e152
Hanno Braun <hb@hannobraun.de>
2021-02-06 21:15:23 +0100

    Implement `insert_segment`

---
+++ fj/src/geometry/triangulation/trapezoidation/query/insert/insert_segment.rs
@@ -0,0 +1,102 @@
+use crate::geometry::triangulation::trapezoidation::{
+    query::{
+        find_regions_for_segment::find_regions_for_segment,
+        graph::{Graph, Node, X, Y},
+    },
+    segment::Segment,
+};
+
+pub fn insert_segment<Region>(segment: Segment, graph: &mut Graph<X, Y, Region>)
+where
+    Region: Default,
+{
+    for region in find_regions_for_segment(&segment, graph) {
+        let left = graph.insert_sink(Region::default());
+        let right = graph.insert_sink(Region::default());
+
+        graph.replace(
+            region,
+            Node::X(X {
+                segment,
+                left,
+                right,
+            }),
+        );
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::triangulation::trapezoidation::{
+        point::Point,
+        query::graph::{self, Node, X, Y},
+        segment::Segment,
+    };
+
+    use super::insert_segment;
+
+    type Graph = graph::Graph<X, Y, Region>;
+
+    #[derive(Clone, Copy, Debug, Default, Eq, PartialEq)]
+    struct Region(u64);
+
+    #[test]
+    fn insert_segment_should_split_the_region_the_segment_goes_through() {
+        let mut graph = Graph::new();
+
+        let segment_to_insert =
+            Segment::new(Point::new(0.0, 0.0), Point::new(0.0, 1.0)).unwrap();
+        insert_segment(segment_to_insert, &mut graph);
+
+        match graph.get(graph.source()) {
+            Node::X(X {
+                segment,
+                left,
+                right,
+            }) => {
+                let left = *left;
+                let right = *right;
+
+                // Point must have been inserted.
+                assert_eq!(segment, &segment_to_insert);
+
+                // Children should be sinks
+                assert_eq!(graph.get(left).is_sink(), true);
+                assert_eq!(graph.get(right).is_sink(), true);
+
+                // Children should be distinct
+                assert_ne!(left, right);
+
+                // Children should be new nodes
+                assert_ne!(graph.source(), left);
+                assert_ne!(graph.source(), right);
+            }
+            node => panic!("Unexpected node: {:?}", node),
+        }
+    }
+
+    #[test]
+    fn insert_segment_should_follow_right_path_at_x_node() {
+        let mut graph = Graph::new();
+
+        let region_left = Region(1);
+        let left = graph.insert_sink(region_left);
+        let right = graph.insert_sink(Region(2));
+
+        let node = Node::X(X {
+            segment: Segment::new(Point::new(0.0, 0.0), Point::new(0.0, 1.0))
+                .unwrap(),
+            left,
+            right,
+        });
+        graph.replace(graph.source(), node);
+
+        insert_segment(
+            Segment::new(Point::new(1.0, 0.0), Point::new(1.0, 1.0)).unwrap(),
+            &mut graph,
+        );
+
+        assert_eq!(graph.get(left), &Node::Sink(region_left));
+        assert_eq!(graph.get(right).is_x(), true);
+    }
+}
--- fj/src/geometry/triangulation/trapezoidation/query/insert/mod.rs
+++ fj/src/geometry/triangulation/trapezoidation/query/insert/mod.rs
@@ -1,6 +1,7 @@
 //! Insertion of segments into the graph
 
 pub mod insert_point;
+pub mod insert_segment;
 
 // TASK: Implement `insert`
 //       - Insert upper point
@@ -8,24 +9,3 @@ pub mod insert_point;
 //       - Insert segment
 // TASK: Decide where to update region when inserting.
 // TASK: After insertion, merge regions that have the same bounding segments.
-// TASK: Implement `insert_segment`:
-//       - Compare with each node
-//         - At Y node:
-//           - If above or below, follow respective path
-//           - If neither, follow _both_ paths
-//         - At X node: Check whether left or right, follow respective path
-//           - If segments overlap in y, "left" and "right" are unambiguously
-//             defined depending on whether the left or right horizontal
-//             extension of the overlapping endpoint hit the other segment. Just
-//             find an endpoint that is between the other segment's endpoints,
-//             find point on other segment with same y-coordinate,
-//             compare x-coordinate.
-//           - Endpoints of a segment can never be _on_ another segment, unless
-//             the initial polygon is self-intersecting, which we don't accept
-//             as valid input. (And the paper only allows segments with common
-//             endpoints, at most.)
-//           - But what if the segments don't overlap in y? There obviously are
-//             segments like that, but will we ever have to compare them when
-//             inserting into a valid tree? I don't know.
-//         - At Sink:
-//           Split region into left and right. Update bounding segments.
