28b37d50ff90c46ce2295fe7588af71ba47e986c
Hanno Braun <hb@hannobraun.de>
2021-02-06 20:47:49 +0100

    Improve signature of placeholder function

--- fj/src/geometry/triangulation/trapezoidation/query/find_regions_for_segment.rs
+++ fj/src/geometry/triangulation/trapezoidation/query/find_regions_for_segment.rs
@@ -1,12 +1,12 @@
 use crate::geometry::triangulation::trapezoidation::{
-    query::graph::{Graph, Id},
+    query::graph::{Graph, Id, X, Y},
     segment::Segment,
 };
 
 /// Find the regions that are split by the given segment
 pub fn find_regions_for_segment<Region>(
     _segment: &Segment,
-    _graph: &Graph,
+    _graph: &Graph<X, Y, Region>,
 ) -> Vec<Id> {
     // TASK: Implement
     todo!()
