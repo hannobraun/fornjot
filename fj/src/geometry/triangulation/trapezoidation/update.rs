e8b47a6ff70add51681cf464800b6981e26b931a
Hanno Braun <hb@hannobraun.de>
2021-02-08 19:11:04 +0100

    Add placeholder functions

--- fj/src/geometry/triangulation/trapezoidation/update.rs
+++ fj/src/geometry/triangulation/trapezoidation/update.rs
@@ -1,14 +1,20 @@
-// TASK: Implement function that updates all regions after an x node has been
-//       inserted:
-//       - Replace right segment of new left region.
-//       - Replace left segment of new right region.
-//       - Remove upper/lower boundary, if bounding point is on wrong side of
-//         the new segment. Mark affected regions for merging.
-//       - Update boundaries of upper and lower neighbors accordingly.
-//       - Merge all regions marked for merging that have the same left/right
-//         segment.
-// TASK: Implement function that updates all regions after a y node has been
-//       inserted:
-//       - Replace lower boundary of new upper region.
-//       - Replace upper boundary of new lower region.
-//       - Update lower neighbors.
+use super::{graph::Graph, ids::Id};
+
+pub fn update_after_x_split(_x: Id, _graph: &mut Graph) {
+    // TASK: Implement:
+    //       - Replace right segment of new left region.
+    //       - Replace left segment of new right region.
+    //       - Remove upper/lower boundary, if bounding point is on wrong side
+    //         of the new segment. Mark affected regions for merging.
+    //       - Update boundaries of upper and lower neighbors accordingly.
+    //       - Merge all regions marked for merging that have the same left/
+    //         right segment.
+}
+
+pub fn update_after_y_split(_y: Id, _graph: &mut Graph) {
+    // TASK: Implement:
+    //       - Replace lower boundary of new upper region.
+    //       - Replace upper boundary of new lower region.
+    //       - Update lower neighbors.
+    todo!()
+}
