976ba1824a356d9ef1e6320856e47ab173e4a8d4
Hanno Braun <hb@hannobraun.de>
2021-02-08 21:02:58 +0100

    Rename function

--- fj/src/geometry/triangulation/trapezoidation/update/y_split.rs
+++ fj/src/geometry/triangulation/trapezoidation/update/y_split.rs
@@ -1,6 +1,6 @@
 use crate::geometry::triangulation::trapezoidation::{graph::Graph, ids::Id};
 
-pub fn update_after_y_split(_y: Id, _graph: &mut Graph) {
+pub fn update(_y: Id, _graph: &mut Graph) {
     // TASK: Implement:
     //       - Replace lower boundary of new upper region.
     //       - Replace upper boundary of new lower region.
