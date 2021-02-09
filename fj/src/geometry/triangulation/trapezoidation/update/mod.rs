134e59f4339f2d862c4f0b5f53717f3d466286eb
Hanno Braun <hb@hannobraun.de>
2021-02-09 19:45:34 +0100

    Add top-level `update` function

--- fj/src/geometry/triangulation/trapezoidation/update/mod.rs
+++ fj/src/geometry/triangulation/trapezoidation/update/mod.rs
@@ -1,4 +1,17 @@
 pub mod x_split;
 pub mod y_split;
 
-// TASK: Add update function here that reads id and calls `x_split`/`y_split`?
+use super::{
+    graph::{Graph, Node},
+    ids::Id,
+};
+
+pub fn update(id: Id, graph: &mut Graph) {
+    match graph.get(id) {
+        Node::X(x) => x_split::update(*x, graph),
+        Node::Y(y) => y_split::update(*y, graph),
+        Node::Sink(_) => {
+            panic!("Split node can't be a sink.")
+        }
+    }
+}
