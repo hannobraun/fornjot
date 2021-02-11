00856164e02e654b283314e04ceceb0786d8e2e8
Hanno Braun <hb@hannobraun.de>
2021-02-11 19:44:23 +0100

    Change function parameters

--- fj/src/geometry/triangulation/trapezoidation/update/y_split.rs
+++ fj/src/geometry/triangulation/trapezoidation/update/y_split.rs
@@ -1,10 +1,12 @@
 use crate::geometry::triangulation::trapezoidation::{
-    graph::{Graph, Y},
+    graph::Graph,
     ids::Id,
     region::{BoundingRegions, HorizontalBoundary},
 };
 
-pub fn update(id_y: Id, y: Y, graph: &mut Graph) {
+pub fn update(id_y: Id, graph: &mut Graph) {
+    let y = graph.get(id_y).y().unwrap().clone();
+
     graph.get_mut(y.below).sink_mut().unwrap().upper_boundary =
         Some(HorizontalBoundary {
             point: y.point,
@@ -58,10 +60,9 @@ mod tests {
         let mut graph = Graph::new();
 
         let id_y = point::insert(Point::new(0.0, 0.0), &mut graph).unwrap();
-        let y = graph.get(id_y).y().unwrap().clone();
+        update(id_y, &mut graph);
 
-        update(id_y, y.clone(), &mut graph);
-
+        let y = graph.get(id_y).y().unwrap();
         assert_eq!(
             graph
                 .get(y.below)
@@ -96,17 +97,17 @@ mod tests {
 
         // Split original region horizontally.
         let id_y = point::insert(Point::new(0.0, 0.0), &mut graph).unwrap();
-        let y = graph.get(id_y).y().unwrap().clone();
-        update(id_y, y.clone(), &mut graph);
+        update(id_y, &mut graph);
 
+        let y = graph.get(id_y).y().unwrap();
         let lowest = y.below;
 
         // Now split the upper of those two regions again.
         let id_y = point::insert(Point::new(0.0, 1.0), &mut graph).unwrap();
-        let y = graph.get(id_y).y().unwrap().clone();
 
-        update(id_y, y.clone(), &mut graph);
+        update(id_y, &mut graph);
 
+        let y = graph.get(id_y).y().unwrap();
         assert_eq!(
             graph
                 .get(lowest)
