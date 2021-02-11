2496f5d1735e3de74f2262c3c6b2faa7ceb55912
Hanno Braun <hb@hannobraun.de>
2021-02-11 19:47:20 +0100

    Change function parameters

--- fj/src/geometry/triangulation/trapezoidation/update/x_split.rs
+++ fj/src/geometry/triangulation/trapezoidation/update/x_split.rs
@@ -1,11 +1,14 @@
-use crate::geometry::triangulation::trapezoidation::{
-    graph::{Graph, X},
-    ids::Id,
-};
+use crate::geometry::triangulation::trapezoidation::{graph::Graph, ids::Id};
 
-pub fn update(_id_x: Id, x: X, graph: &mut Graph) {
-    graph.get_mut(x.left).sink_mut().unwrap().right_segment = Some(x.segment);
-    graph.get_mut(x.right).sink_mut().unwrap().left_segment = Some(x.segment);
+pub fn update(ids: &[Id], graph: &mut Graph) {
+    for &id in ids {
+        let x = graph.get(id).x().unwrap().clone();
+
+        graph.get_mut(x.left).sink_mut().unwrap().right_segment =
+            Some(x.segment);
+        graph.get_mut(x.right).sink_mut().unwrap().left_segment =
+            Some(x.segment);
+    }
 
     // TASK: Implement:
     //       - Remove upper/lower boundary, if bounding point is on wrong side
@@ -38,10 +41,10 @@ mod tests {
         )
         .pop()
         .unwrap();
+
+        update(&[id_x], &mut graph);
+
         let x = graph.get(id_x).x().unwrap().clone();
-
-        update(id_x, x.clone(), &mut graph);
-
         assert_eq!(
             graph.get(x.left).sink().unwrap().right_segment.unwrap(),
             x.segment
