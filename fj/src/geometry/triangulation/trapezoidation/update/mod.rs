a4c85301c470b8e64f8d7821c1214c30bc25b740
Hanno Braun <hb@hannobraun.de>
2021-02-11 19:41:33 +0100

    Remove function
    Turns out that it's not going to be useful.

--- fj/src/geometry/triangulation/trapezoidation/update/mod.rs
+++ fj/src/geometry/triangulation/trapezoidation/update/mod.rs
@@ -1,17 +1,2 @@
 pub mod x_split;
 pub mod y_split;
-
-use super::{
-    graph::{Graph, Node},
-    ids::Id,
-};
-
-pub fn update(id: Id, graph: &mut Graph) {
-    match graph.get(id) {
-        Node::X(x) => x_split::update(id, x.clone(), graph),
-        Node::Y(y) => y_split::update(id, y.clone(), graph),
-        Node::Sink(_) => {
-            panic!("Split node can't be a sink.")
-        }
-    }
-}
