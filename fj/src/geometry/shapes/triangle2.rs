9bb1ff8ac05426a657ae6b35f54e386634cb6993
Hanno Braun <hb@hannobraun.de>
2021-02-22 23:18:11 +0100

    Add `Triangle2::new`

--- fj/src/geometry/shapes/triangle2.rs
+++ fj/src/geometry/shapes/triangle2.rs
@@ -5,3 +5,9 @@ pub struct Triangle2 {
     pub b: Point2<f32>,
     pub c: Point2<f32>,
 }
+
+impl Triangle2 {
+    pub fn new(a: Point2<f32>, b: Point2<f32>, c: Point2<f32>) -> Self {
+        Self { a, b, c }
+    }
+}
