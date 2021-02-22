a46f800ff5fc6696ab5846d76e922b9694916963
Hanno Braun <hb@hannobraun.de>
2021-02-22 23:13:54 +0100

    Rename module

--- fj/src/geometry/shapes/mod.rs
+++ fj/src/geometry/shapes/mod.rs
@@ -1,10 +1,10 @@
 pub mod circle;
 pub mod polygon;
-pub mod triangle;
+pub mod triangle3;
 pub mod triangles;
 pub mod vertex_chain;
 
 pub use self::{
-    circle::Circle, polygon::Polygon, triangle::Triangle3,
+    circle::Circle, polygon::Polygon, triangle3::Triangle3,
     triangles::Triangles, vertex_chain::VertexChain,
 };
