dd00d567427cfba9266265271c5a0f54278c1154
Hanno Braun <hb@hannobraun.de>
2021-02-22 22:56:07 +0100

    Add new module

---
+++ fj/src/geometry/triangulation/basic.rs
@@ -0,0 +1,7 @@
+//! Basic triangulation algorithm
+//!
+//! This is a brute-force algorithm that I've come up with myself, and that's
+//! designed to work with exactly the polygons I need it for right now, and not
+//! more.
+
+// TASK: Implement.
