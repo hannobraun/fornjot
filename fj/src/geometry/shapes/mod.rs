3e1a20b73c78fb83ea42b4272697ef6733e639c5
Hanno Braun <hb@hannobraun.de>
2021-02-22 23:16:08 +0100

    Define 2-dimensional triangle type

--- fj/src/geometry/shapes/mod.rs
+++ fj/src/geometry/shapes/mod.rs
@@ -1,10 +1,11 @@
 pub mod circle;
 pub mod polygon;
+pub mod triangle2;
 pub mod triangle3;
 pub mod triangles;
 pub mod vertex_chain;
 
 pub use self::{
-    circle::Circle, polygon::Polygon, triangle3::Triangle3,
-    triangles::Triangles, vertex_chain::VertexChain,
+    circle::Circle, polygon::Polygon, triangle2::Triangle2,
+    triangle3::Triangle3, triangles::Triangles, vertex_chain::VertexChain,
 };
---
+++ fj/src/geometry/shapes/triangle2.rs
@@ -0,0 +1,7 @@
+use nalgebra::Point2;
+
+pub struct Triangle2 {
+    pub a: Point2<f32>,
+    pub b: Point2<f32>,
+    pub c: Point2<f32>,
+}
