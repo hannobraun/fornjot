1cae332f3e352724cf6bf655a425371f2173c1a5
Hanno Braun <hb@hannobraun.de>
2021-03-04 22:36:17 +0100

    Implement `VertexChain::neighbors_of`

--- fj/src/geometry/shapes/vertex_chain.rs
+++ fj/src/geometry/shapes/vertex_chain.rs
@@ -55,6 +55,25 @@ impl VertexChain {
 
         edges
     }
+
+    /// Returns the neighbors of the given point
+    pub fn neighbors_of(&self, vertex: impl Into<Pnt2>) -> Option<Neighbors> {
+        let mut neighbors = IndexSet::new();
+
+        let index = self.0.get_index_of(&vertex.into())?;
+
+        let prev = index.overflowing_sub(1).0.clamp(0, self.0.len() - 1);
+        let next = index.overflowing_add(1).0 % self.0.len();
+
+        // Can't panic, as we did the whole clamping and remainder dance above.
+        let prev = *self.0.get_index(prev).unwrap();
+        let next = *self.0.get_index(next).unwrap();
+
+        neighbors.insert(prev);
+        neighbors.insert(next);
+
+        Some(Neighbors(neighbors))
+    }
 }
 
 impl From<&[Point2<f32>]> for VertexChain {
@@ -62,6 +81,14 @@ impl From<&[Point2<f32>]> for VertexChain {
         let points: IndexSet<_> =
             points.into_iter().map(|point| point.into()).collect();
         Self(points)
+    }
+}
+
+pub struct Neighbors(IndexSet<Pnt2>);
+
+impl Neighbors {
+    pub fn contains(&self, vertex: impl Into<Pnt2>) -> bool {
+        self.0.contains(&vertex.into())
     }
 }
 
@@ -108,5 +135,44 @@ mod tests {
             segments,
             vec![Segment::new(a, b), Segment::new(b, c), Segment::new(c, a)]
         );
+    }
+
+    #[test]
+    fn vertex_chain_should_return_neighbors_of_a_vertex() {
+        let a = Point2::new(0.0, 0.0);
+        let b = Point2::new(1.0, 0.0);
+        let c = Point2::new(1.0, 1.0);
+        let d = Point2::new(0.0, 1.0);
+
+        let mut vertex_chain = VertexChain::new();
+        vertex_chain.insert(a);
+        vertex_chain.insert(b);
+        vertex_chain.insert(c);
+        vertex_chain.insert(d);
+
+        let neighbors_of_a = vertex_chain.neighbors_of(a).unwrap();
+        let neighbors_of_b = vertex_chain.neighbors_of(b).unwrap();
+        let neighbors_of_c = vertex_chain.neighbors_of(c).unwrap();
+        let neighbors_of_d = vertex_chain.neighbors_of(d).unwrap();
+
+        assert!(!neighbors_of_a.contains(a));
+        assert!(neighbors_of_a.contains(b));
+        assert!(!neighbors_of_a.contains(c));
+        assert!(neighbors_of_a.contains(d));
+
+        assert!(neighbors_of_b.contains(a));
+        assert!(!neighbors_of_b.contains(b));
+        assert!(neighbors_of_b.contains(c));
+        assert!(!neighbors_of_b.contains(d));
+
+        assert!(!neighbors_of_c.contains(a));
+        assert!(neighbors_of_c.contains(b));
+        assert!(!neighbors_of_c.contains(c));
+        assert!(neighbors_of_c.contains(d));
+
+        assert!(neighbors_of_d.contains(a));
+        assert!(!neighbors_of_d.contains(b));
+        assert!(neighbors_of_d.contains(c));
+        assert!(!neighbors_of_d.contains(d));
     }
 }
