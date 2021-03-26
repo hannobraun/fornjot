10f67d92ed6384bf8664d6313faa08a782130522
Hanno Braun <hb@hannobraun.de>
2021-03-27 00:10:26 +0100

    Implement `Seg2::normalize`

--- fj/src/geometry/segment.rs
+++ fj/src/geometry/segment.rs
@@ -8,11 +8,43 @@ pub struct Seg2 {
     pub b: Pnt2,
 }
 
+impl Seg2 {
+    pub fn normalize(&self) -> Self {
+        if self.a <= self.b {
+            *self
+        } else {
+            Self {
+                a: self.b,
+                b: self.a,
+            }
+        }
+    }
+}
+
 impl From<Seg2> for Segment {
     fn from(segment: Seg2) -> Self {
         Self {
             a: segment.a.into(),
             b: segment.b.into(),
         }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::point::Pnt2;
+
+    use super::Seg2;
+
+    #[test]
+    fn it_should_normalize_a_segment() {
+        let a = Seg2 {
+            a: Pnt2::from_f32s(0.0, 0.0),
+            b: Pnt2::from_f32s(1.0, 1.0),
+        };
+        let b = Seg2 { a: a.b, b: a.a };
+
+        assert_eq!(a.normalize(), a);
+        assert_eq!(b.normalize(), a);
     }
 }
