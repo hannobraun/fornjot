f955a2ef9d48c01301968bcad706dc654eca962d
Hanno Braun <hanno@braun-odw.eu>
2021-04-12 10:42:51 +0000

    Add `PolygonData::contains_vertex`

--- fj/src/geometry/shapes/polygon/data.rs
+++ fj/src/geometry/shapes/polygon/data.rs
@@ -1,19 +1,18 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 
-use crate::geometry::segment::Seg2;
+use crate::geometry::{point::Pnt2, segment::Seg2};
 
 #[derive(Clone, Debug)]
 pub struct PolygonData {
     edges: HashSet<Seg2>,
-    // TASK: Add field `vertices` that contains all polygon vertices. This is
-    //       required to easily and efficiently query whether a vertex is part
-    //       of the polygon and will probably be useful for other things too.
+    vertices: Vertices,
 }
 
 impl PolygonData {
     pub fn new() -> Self {
         Self {
             edges: HashSet::new(),
+            vertices: Vertices::new(),
         }
     }
 
@@ -25,11 +24,94 @@ impl PolygonData {
         self.edges.is_empty()
     }
 
-    pub fn insert_edge(&mut self, edge: Seg2) {
-        self.edges.insert(edge);
+    pub fn contains_vertex(&self, vertex: &Pnt2) -> bool {
+        self.vertices.0.contains_key(vertex)
     }
 
-    pub fn retain_edges(&mut self, f: impl FnMut(&Seg2) -> bool) {
-        self.edges.retain(f);
+    pub fn insert_edge(&mut self, edge: Seg2) {
+        self.edges.insert(edge);
+
+        self.vertices.up(edge.a);
+        self.vertices.up(edge.b);
+    }
+
+    pub fn retain_edges(&mut self, mut f: impl FnMut(&Seg2) -> bool) {
+        let edges = &mut self.edges;
+        let vertices = &mut self.vertices;
+
+        edges.retain(|edge| {
+            let retain = f(edge);
+
+            if !retain {
+                vertices.down(edge.a);
+                vertices.down(edge.b);
+            }
+
+            retain
+        });
+    }
+}
+
+#[derive(Clone, Debug)]
+struct Vertices(HashMap<Pnt2, u32>);
+
+impl Vertices {
+    pub fn new() -> Self {
+        Self(HashMap::new())
+    }
+    pub fn up(&mut self, vertex: Pnt2) {
+        *self.0.entry(vertex).or_insert(0) += 1;
+    }
+
+    pub fn down(&mut self, vertex: Pnt2) {
+        *self.0.get_mut(&vertex).unwrap() -= 1;
+
+        if *self.0.get(&vertex).unwrap() == 0 {
+            self.0.remove(&vertex);
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::{point::Pnt2, segment::Seg2};
+
+    use super::PolygonData;
+
+    #[test]
+    fn insert_edge_should_update_vertices() {
+        let mut data = PolygonData::new();
+
+        let a = Pnt2::from_f32s(0.0, 0.0);
+        let b = Pnt2::from_f32s(1.0, 0.0);
+
+        assert_eq!(data.contains_vertex(&a), false);
+        assert_eq!(data.contains_vertex(&b), false);
+
+        data.insert_edge(Seg2::new(a, b));
+
+        assert_eq!(data.contains_vertex(&a), true);
+        assert_eq!(data.contains_vertex(&b), true);
+    }
+
+    #[test]
+    fn retain_edges_should_update_vertices() {
+        let mut data = PolygonData::new();
+
+        let a = Pnt2::from_f32s(0.0, 0.0);
+        let b = Pnt2::from_f32s(1.0, 0.0);
+        let c = Pnt2::from_f32s(0.0, 1.0);
+
+        let ab = Seg2::new(a, b);
+        let bc = Seg2::new(b, c);
+
+        data.insert_edge(ab);
+        data.insert_edge(bc);
+
+        data.retain_edges(|&edge| edge == ab);
+
+        assert_eq!(data.contains_vertex(&a), true);
+        assert_eq!(data.contains_vertex(&b), true);
+        assert_eq!(data.contains_vertex(&c), false);
     }
 }
