e0c41f1574a7689d2d031119b6178d7a3d329a43
Hanno Braun <hb@hannobraun.de>
2021-04-13 22:20:37 +0000

    Remove unused code

--- fj/src/geometry/shapes/mod.rs
+++ fj/src/geometry/shapes/mod.rs
@@ -2,9 +2,8 @@ pub mod circle;
 pub mod polygon;
 pub mod triangle3;
 pub mod triangles;
-pub mod vertex_chain;
 
 pub use self::{
     circle::Circle, polygon::Polygon, triangle3::Triangle3,
-    triangles::Triangles, vertex_chain::VertexChain,
+    triangles::Triangles,
 };
