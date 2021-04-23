7ab15764f4b8422fcf58e8cb5240da56afd6ed53
Hanno Braun <hb@hannobraun.de>
2021-04-23 18:03:42 +0000

    Remove unused code

--- fj/src/geometry/conversions/to_mesh.rs
+++ fj/src/geometry/conversions/to_mesh.rs
@@ -5,7 +5,7 @@ use nalgebra::Point3;
 use crate::geometry::{
     conversions::ToPolygon,
     triangulation::brute_force::{self, triangulate},
-    Mesh, Triangle3,
+    Mesh,
 };
 
 pub trait ToMesh {
@@ -67,43 +67,5 @@ where
         }
 
         Ok(())
-    }
-}
-
-impl ToMesh for &Triangle3 {
-    type Error = Infallible;
-
-    fn to_mesh(
-        self,
-        _tolerance: f32,
-        mesh: &mut Mesh,
-    ) -> Result<(), Self::Error> {
-        let i0 = mesh.vertex(self.a);
-        let i1 = mesh.vertex(self.b);
-        let i2 = mesh.vertex(self.c);
-
-        mesh.triangle(i0, i1, i2);
-
-        Ok(())
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use crate::geometry::{Mesh, Triangle3};
-
-    use crate::geometry::ToMesh as _;
-
-    #[test]
-    fn triangle_should_convert_to_mesh() {
-        let triangle =
-            Triangle3::new([0.0, 0.0, 0.0], [1.0, 0.0, 0.0], [0.0, 1.0, 0.0]);
-
-        let mut mesh = Mesh::new();
-        triangle.to_mesh(0.0, &mut mesh).unwrap();
-
-        let triangles = mesh.triangles();
-
-        assert_eq!(triangles.0, vec![triangle]);
     }
 }
