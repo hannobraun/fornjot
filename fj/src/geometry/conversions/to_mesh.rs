f3e8572928163e5f0f89a944c567e55a08439317
Hanno Braun <hb@hannobraun.de>
2021-04-23 18:10:20 +0000

    Extract polygon-mesh conversion to function

--- fj/src/geometry/conversions/to_mesh.rs
+++ fj/src/geometry/conversions/to_mesh.rs
@@ -29,26 +29,32 @@ where
     type Error = brute_force::InternalError;
 
     fn to_mesh(self, tolerance: f32) -> Result<Mesh, Self::Error> {
-        let mut mesh = Mesh::new();
-
-        let polygon = self.to_polygon(tolerance);
-        let triangles = triangulate(polygon)?;
-
-        for triangle in triangles {
-            let a_x: f32 = triangle.a.x.into();
-            let a_y: f32 = triangle.a.y.into();
-            let b_x: f32 = triangle.b.x.into();
-            let b_y: f32 = triangle.b.y.into();
-            let c_x: f32 = triangle.c.x.into();
-            let c_y: f32 = triangle.c.y.into();
-
-            let a = mesh.vertex(Point3::new(a_x, a_y, 0.0));
-            let b = mesh.vertex(Point3::new(b_x, b_y, 0.0));
-            let c = mesh.vertex(Point3::new(c_x, c_y, 0.0));
-
-            mesh.triangle(a, b, c);
-        }
-
-        Ok(mesh)
+        polygon_to_mesh(self, tolerance)
     }
+}
+
+fn polygon_to_mesh(
+    polygon: impl ToPolygon,
+    tolerance: f32,
+) -> Result<Mesh, brute_force::InternalError> {
+    let mut mesh = Mesh::new();
+    let polygon = polygon.to_polygon(tolerance);
+    let triangles = triangulate(polygon)?;
+
+    for triangle in triangles {
+        let a_x: f32 = triangle.a.x.into();
+        let a_y: f32 = triangle.a.y.into();
+        let b_x: f32 = triangle.b.x.into();
+        let b_y: f32 = triangle.b.y.into();
+        let c_x: f32 = triangle.c.x.into();
+        let c_y: f32 = triangle.c.y.into();
+
+        let a = mesh.vertex(Point3::new(a_x, a_y, 0.0));
+        let b = mesh.vertex(Point3::new(b_x, b_y, 0.0));
+        let c = mesh.vertex(Point3::new(c_x, c_y, 0.0));
+
+        mesh.triangle(a, b, c);
+    }
+
+    Ok(mesh)
 }
