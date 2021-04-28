ea395141506a32c87ae1036e9fd620e52fa3030b
Hanno Braun <hb@hannobraun.de>
2021-04-28 21:12:16 +0000

    Move `Mesh` to `shapes`

--- fj/src/geometry/mod.rs
+++ fj/src/geometry/mod.rs
@@ -1,11 +1,7 @@
 pub mod attributes;
 pub mod conversions;
-pub mod mesh;
 pub mod operations;
 pub mod shapes;
 pub mod triangulation;
 
-pub use self::{
-    mesh::Mesh,
-    shapes::{Circle, Sphere, Triangle3, Triangles},
-};
+pub use self::shapes::{Circle, Mesh, Sphere, Triangle3, Triangles};
