7618c466c6b6a5b69ed85ad180936484a0d3b3d4
Hanno Braun <hb@hannobraun.de>
2021-04-29 22:38:43 +0000

    Implement `Distance` for `Circle`

--- fj/src/geometry/attributes/distance.rs
+++ fj/src/geometry/attributes/distance.rs
@@ -1,6 +1,20 @@
 use nalgebra::Point;
 
+use crate::geometry::shapes::Circle;
+
 /// Provides a signed distance function
 pub trait Distance {
     fn distance(&self, point: Point<f32, 3>) -> f32;
+}
+
+impl Distance for Circle {
+    fn distance(&self, point: Point<f32, 3>) -> f32 {
+        let dist_2d = point.coords.xy().magnitude() - self.radius();
+
+        if dist_2d > 0.0 {
+            (dist_2d * dist_2d + point.z * point.z).sqrt()
+        } else {
+            point.z
+        }
+    }
 }
