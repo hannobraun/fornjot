296abeca65700cd9dbf7a72bac1be73613caab6f
Hanno Braun <hb@hannobraun.de>
2021-04-29 22:38:21 +0000

    Implement `Aabb` for `Circle`

--- fj/src/geometry/attributes/aabb.rs
+++ fj/src/geometry/attributes/aabb.rs
@@ -1,6 +1,18 @@
+use nalgebra::Point;
 use parry3d::bounding_volume::AABB;
+
+use crate::geometry::shapes::Circle;
 
 /// Axis-aligned bounding box
 pub trait Aabb {
     fn aabb(&self) -> AABB;
+}
+
+impl Aabb for Circle {
+    fn aabb(&self) -> AABB {
+        AABB {
+            mins: Point::<_, 3>::new(-self.radius(), -self.radius(), 0.0),
+            maxs: Point::<_, 3>::new(self.radius(), self.radius(), 0.0),
+        }
+    }
 }
