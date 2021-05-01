a3e8b9173635aa5b7f85ebd2490c62a643918eec
Hanno Braun <hanno@braun-odw.eu>
2021-05-01 19:03:58 +0000

    Implement `BoundingVolume` for `Difference`

--- fj/src/geometry/attributes/bounding_volume.rs
+++ fj/src/geometry/attributes/bounding_volume.rs
@@ -1,6 +1,6 @@
 use parry3d::bounding_volume::AABB;
 
-use crate::geometry::shapes;
+use crate::geometry::{operations, shapes};
 
 /// Axis-aligned bounding box
 pub trait BoundingVolume {
@@ -13,5 +13,18 @@ impl BoundingVolume for shapes::Cylinder {
             mins: [-self.radius, -self.radius, -self.height / 2.0].into(),
             maxs: [self.radius, self.radius, self.height / 2.0].into(),
         }
+    }
+}
+
+impl<A, B> BoundingVolume for operations::Difference<A, B>
+where
+    A: BoundingVolume,
+{
+    fn aabb(&self) -> AABB {
+        // Since `self.b` is subtracted from `self.a`, the bounding volume of
+        // the difference is not going to be bigger than that of `self.a`. Just
+        // taking the bounding volume from `self.a` is certainly not optimal,
+        // but good enough for now.
+        self.a.aabb()
     }
 }
