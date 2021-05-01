62d86223458c0de42c8db4677af9d82e0d61917a
Hanno Braun <hb@hannobraun.de>
2021-05-01 09:06:28 +0000

    Remove unused `BoundingVolume` implementation

--- fj/src/geometry/attributes/bounding_volume.rs
+++ fj/src/geometry/attributes/bounding_volume.rs
@@ -1,20 +1,10 @@
-use nalgebra::Point;
 use parry3d::bounding_volume::AABB;
 
-use crate::geometry::shapes::{Circle, Cylinder};
+use crate::geometry::shapes::Cylinder;
 
 /// Axis-aligned bounding box
 pub trait BoundingVolume {
     fn aabb(&self) -> AABB;
-}
-
-impl BoundingVolume for Circle {
-    fn aabb(&self) -> AABB {
-        AABB {
-            mins: Point::<_, 3>::new(-self.radius(), -self.radius(), 0.0),
-            maxs: Point::<_, 3>::new(self.radius(), self.radius(), 0.0),
-        }
-    }
 }
 
 impl BoundingVolume for Cylinder {
