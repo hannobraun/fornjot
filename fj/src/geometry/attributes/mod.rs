07cb5e851d505e8263580de568c30c505e49cc03
Hanno Braun <hb@hannobraun.de>
2021-05-01 09:03:31 +0000

    Rename module

--- fj/src/geometry/attributes/mod.rs
+++ fj/src/geometry/attributes/mod.rs
@@ -1,5 +1,7 @@
-pub mod aabb;
 pub mod boundary;
+pub mod bounding_volume;
 pub mod distance;
 
-pub use self::{aabb::BoundingVolume, boundary::Boundary, distance::Distance};
+pub use self::{
+    boundary::Boundary, bounding_volume::BoundingVolume, distance::Distance,
+};
