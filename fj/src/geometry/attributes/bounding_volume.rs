93b66b27ec001a4e75a03ba25a5754b8e2cc242f
Hanno Braun <hb@hannobraun.de>
2021-05-01 09:23:58 +0000

    Refactor

--- fj/src/geometry/attributes/bounding_volume.rs
+++ fj/src/geometry/attributes/bounding_volume.rs
@@ -1,13 +1,13 @@
 use parry3d::bounding_volume::AABB;
 
-use crate::geometry::shapes::Cylinder;
+use crate::geometry::shapes;
 
 /// Axis-aligned bounding box
 pub trait BoundingVolume {
     fn aabb(&self) -> AABB;
 }
 
-impl BoundingVolume for Cylinder {
+impl BoundingVolume for shapes::Cylinder {
     fn aabb(&self) -> AABB {
         AABB {
             mins: [-self.radius, -self.radius, -self.height / 2.0].into(),
