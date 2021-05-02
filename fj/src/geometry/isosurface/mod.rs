1a37aa90f36b42b11f173ac4eff19a9c25d9e1f0
Hanno Braun <hanno@braun-odw.eu>
2021-05-02 10:43:41 +0000

    Add task

--- fj/src/geometry/isosurface/mod.rs
+++ fj/src/geometry/isosurface/mod.rs
@@ -1,3 +1,6 @@
 pub mod grid_descriptor;
 
 pub use self::grid_descriptor::GridDescriptor;
+
+// TASK: Add `Grid` that uses `GridDescriptor` and a `Distance` implementation
+//       to populate a grid with values.
