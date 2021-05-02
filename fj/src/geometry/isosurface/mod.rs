6ddbdaad75cf48355eeb143e1b7995b4b2d6e09a
Hanno Braun <hanno@braun-odw.eu>
2021-05-02 11:23:41 +0000

    Re-export `GridIndex`

--- fj/src/geometry/isosurface/mod.rs
+++ fj/src/geometry/isosurface/mod.rs
@@ -1,6 +1,6 @@
 pub mod grid_descriptor;
 
-pub use self::grid_descriptor::GridDescriptor;
+pub use self::grid_descriptor::{GridDescriptor, GridIndex};
 
 // TASK: Add `Grid` that uses `GridDescriptor` and a `Distance` implementation
 //       to populate a grid with values.
