49f136d2ffb8f0db4b2eae9c49317fdbb527a14b
Hanno Braun <hb@hannobraun.de>
2021-05-04 18:50:25 +0000

    Implement `Grid::edges`

--- fj/src/geometry/isosurface/grid.rs
+++ fj/src/geometry/isosurface/grid.rs
@@ -1,4 +1,4 @@
-use std::{collections::BTreeMap, iter};
+use std::{array, collections::BTreeMap};
 
 use nalgebra::Point;
 
@@ -28,8 +28,22 @@ impl Grid {
 
     /// Returns iterator over all grid edges
     pub fn edges(&self) -> impl Iterator<Item = Edge> + '_ {
-        // TASK: Implement
-        iter::empty()
+        self._values
+            .iter()
+            .map(move |(&index, &value)| {
+                let next_z = [index[0], index[1], index[2] + 1];
+                let next_y = [index[0], index[1] + 1, index[2]];
+                let next_x = [index[0] + 1, index[1], index[2]];
+
+                [
+                    edge_to_next(index, value, next_z.into(), &self._values),
+                    edge_to_next(index, value, next_y.into(), &self._values),
+                    edge_to_next(index, value, next_x.into(), &self._values),
+                ]
+            })
+            .map(|edges| array::IntoIter::new(edges))
+            .flatten()
+            .filter_map(|edge| edge)
     }
 
     /// Returns the 4 neighboring cube centers of a grid edge
@@ -43,6 +57,23 @@ impl Grid {
     }
 }
 
+fn edge_to_next(
+    index: GridIndex,
+    value: f32,
+    next_index: GridIndex,
+    values: &BTreeMap<GridIndex, f32>,
+) -> Option<Edge> {
+    let &next_value = values.get(&next_index)?;
+
+    Some(Edge {
+        a: Value { index, value },
+        b: Value {
+            index: next_index,
+            value: next_value,
+        },
+    })
+}
+
 #[derive(Debug, PartialEq)]
 pub struct Edge {
     pub a: Value,
@@ -53,4 +84,96 @@ pub struct Edge {
 pub struct Value {
     pub index: GridIndex,
     pub value: f32,
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::{attributes::Distance, isosurface::GridDescriptor};
+
+    use super::Grid;
+
+    #[test]
+    fn edges_should_return_edges() {
+        let grid = Grid::from_descriptor(
+            &GridDescriptor {
+                min: [0.0, 0.0, 0.0].into(),
+                max: [1.0, 1.0, 1.0].into(),
+                resolution: 1.0,
+            },
+            Geometry,
+        );
+
+        let edges: Vec<_> = grid
+            .edges()
+            .map(|edge| (edge.a.index, edge.b.index))
+            .collect();
+
+        assert_eq!(
+            edges,
+            vec![
+                ([0, 0, 0], [0, 0, 1].into()),
+                ([0, 0, 0], [0, 1, 0].into()),
+                ([0, 0, 0], [1, 0, 0].into()),
+                ([0, 0, 1], [0, 0, 2].into()),
+                ([0, 0, 1], [0, 1, 1].into()),
+                ([0, 0, 1], [1, 0, 1].into()),
+                ([0, 0, 2], [0, 1, 2].into()),
+                ([0, 0, 2], [1, 0, 2].into()),
+                ([0, 1, 0], [0, 1, 1].into()),
+                ([0, 1, 0], [0, 2, 0].into()),
+                ([0, 1, 0], [1, 1, 0].into()),
+                ([0, 1, 1], [0, 1, 2].into()),
+                ([0, 1, 1], [0, 2, 1].into()),
+                ([0, 1, 1], [1, 1, 1].into()),
+                ([0, 1, 2], [0, 2, 2].into()),
+                ([0, 1, 2], [1, 1, 2].into()),
+                ([0, 2, 0], [0, 2, 1].into()),
+                ([0, 2, 0], [1, 2, 0].into()),
+                ([0, 2, 1], [0, 2, 2].into()),
+                ([0, 2, 1], [1, 2, 1].into()),
+                ([0, 2, 2], [1, 2, 2].into()),
+                ([1, 0, 0], [1, 0, 1].into()),
+                ([1, 0, 0], [1, 1, 0].into()),
+                ([1, 0, 0], [2, 0, 0].into()),
+                ([1, 0, 1], [1, 0, 2].into()),
+                ([1, 0, 1], [1, 1, 1].into()),
+                ([1, 0, 1], [2, 0, 1].into()),
+                ([1, 0, 2], [1, 1, 2].into()),
+                ([1, 0, 2], [2, 0, 2].into()),
+                ([1, 1, 0], [1, 1, 1].into()),
+                ([1, 1, 0], [1, 2, 0].into()),
+                ([1, 1, 0], [2, 1, 0].into()),
+                ([1, 1, 1], [1, 1, 2].into()),
+                ([1, 1, 1], [1, 2, 1].into()),
+                ([1, 1, 1], [2, 1, 1].into()),
+                ([1, 1, 2], [1, 2, 2].into()),
+                ([1, 1, 2], [2, 1, 2].into()),
+                ([1, 2, 0], [1, 2, 1].into()),
+                ([1, 2, 0], [2, 2, 0].into()),
+                ([1, 2, 1], [1, 2, 2].into()),
+                ([1, 2, 1], [2, 2, 1].into()),
+                ([1, 2, 2], [2, 2, 2].into()),
+                ([2, 0, 0], [2, 0, 1].into()),
+                ([2, 0, 0], [2, 1, 0].into()),
+                ([2, 0, 1], [2, 0, 2].into()),
+                ([2, 0, 1], [2, 1, 1].into()),
+                ([2, 0, 2], [2, 1, 2].into()),
+                ([2, 1, 0], [2, 1, 1].into()),
+                ([2, 1, 0], [2, 2, 0].into()),
+                ([2, 1, 1], [2, 1, 2].into()),
+                ([2, 1, 1], [2, 2, 1].into()),
+                ([2, 1, 2], [2, 2, 2].into()),
+                ([2, 2, 0], [2, 2, 1].into()),
+                ([2, 2, 1], [2, 2, 2].into()),
+            ]
+        );
+    }
+
+    struct Geometry;
+
+    impl Distance for Geometry {
+        fn distance(&self, _point: impl Into<nalgebra::Point<f32, 3>>) -> f32 {
+            0.0
+        }
+    }
 }
