b398020b8daf91962546a1dd417af1ef7cacdeb8
Hanno Braun <hb@hannobraun.de>
2021-05-04 20:20:58 +0000

    Implement `Grid::neighbors_of_edge`

--- fj/src/geometry/isosurface/grid.rs
+++ fj/src/geometry/isosurface/grid.rs
@@ -7,6 +7,7 @@ use crate::geometry::attributes::Distance;
 use super::{Edge, GridDescriptor, GridIndex, Value};
 
 pub struct Grid {
+    descriptor: GridDescriptor,
     values: BTreeMap<GridIndex, f32>,
 }
 
@@ -23,7 +24,7 @@ impl Grid {
             values.insert(index, value);
         }
 
-        Self { values }
+        Self { descriptor, values }
     }
 
     /// Returns iterator over all grid edges
@@ -49,11 +50,57 @@ impl Grid {
     /// Returns the 4 neighboring cube centers of a grid edge
     pub fn neighbors_of_edge(
         &self,
-        _a: GridIndex,
-        _b: GridIndex,
+        a: GridIndex,
+        b: GridIndex,
     ) -> [Point<f32, 3>; 4] {
-        // TASK: Implement
-        todo!()
+        let mut direction = [
+            b.x() as i32 - a.x() as i32,
+            b.y() as i32 - a.y() as i32,
+            b.z() as i32 - a.z() as i32,
+        ];
+
+        // Means the direction is left, front, or down. This check assumes that
+        // the direction is valid, which might not be the case at this point. If
+        // it's not, we'll catch it down in the `match` statement.
+        let start = if direction.contains(&-1) {
+            direction[0] = direction[0].abs();
+            direction[1] = direction[1].abs();
+            direction[2] = direction[2].abs();
+            b
+        } else {
+            a
+        };
+
+        let start = start
+            .to_coordinates(self.descriptor.min, self.descriptor.resolution);
+
+        let neighbors = match direction {
+            [0, 0, 1] => [
+                start + Point::<_, 3>::from([-0.5, -0.5, 0.5]).coords,
+                start + Point::<_, 3>::from([-0.5, 0.5, 0.5]).coords,
+                start + Point::<_, 3>::from([0.5, -0.5, 0.5]).coords,
+                start + Point::<_, 3>::from([0.5, 0.5, 0.5]).coords,
+            ],
+            [0, 1, 0] => [
+                start + Point::<_, 3>::from([-0.5, 0.5, -0.5]).coords,
+                start + Point::<_, 3>::from([-0.5, 0.5, 0.5]).coords,
+                start + Point::<_, 3>::from([0.5, 0.5, -0.5]).coords,
+                start + Point::<_, 3>::from([0.5, 0.5, 0.5]).coords,
+            ],
+            [1, 0, 0] => [
+                start + Point::<_, 3>::from([0.5, -0.5, -0.5]).coords,
+                start + Point::<_, 3>::from([0.5, -0.5, 0.5]).coords,
+                start + Point::<_, 3>::from([0.5, 0.5, -0.5]).coords,
+                start + Point::<_, 3>::from([0.5, 0.5, 0.5]).coords,
+            ],
+            direction => panic!(
+                "Invalid direction ({:?}).\
+                Only axis-aligned directions allowed.",
+                direction
+            ),
+        };
+
+        neighbors
     }
 }
 
@@ -154,6 +201,62 @@ mod tests {
                 ([2, 2, 0].into(), [2, 2, 1].into()),
                 ([2, 2, 1].into(), [2, 2, 2].into()),
             ]
+        );
+    }
+
+    #[test]
+    fn neighbors_of_edge_should_return_neighboring_grid_centers() {
+        let grid = Grid::from_descriptor(
+            GridDescriptor {
+                min: [0.0, 0.0, 0.0].into(),
+                max: [1.0, 1.0, 1.0].into(),
+                resolution: 1.0,
+            },
+            Geometry,
+        );
+
+        let x_neighbors = [
+            [1.0, 0.0, 0.0].into(),
+            [1.0, 0.0, 1.0].into(),
+            [1.0, 1.0, 0.0].into(),
+            [1.0, 1.0, 1.0].into(),
+        ];
+        let y_neighbors = [
+            [0.0, 1.0, 0.0].into(),
+            [0.0, 1.0, 1.0].into(),
+            [1.0, 1.0, 0.0].into(),
+            [1.0, 1.0, 1.0].into(),
+        ];
+        let z_neighbors = [
+            [0.0, 0.0, 1.0].into(),
+            [0.0, 1.0, 1.0].into(),
+            [1.0, 0.0, 1.0].into(),
+            [1.0, 1.0, 1.0].into(),
+        ];
+
+        assert_eq!(
+            grid.neighbors_of_edge([1, 1, 1].into(), [2, 1, 1].into()),
+            x_neighbors,
+        );
+        assert_eq!(
+            grid.neighbors_of_edge([2, 1, 1].into(), [1, 1, 1].into()),
+            x_neighbors,
+        );
+        assert_eq!(
+            grid.neighbors_of_edge([1, 1, 1].into(), [1, 2, 1].into()),
+            y_neighbors,
+        );
+        assert_eq!(
+            grid.neighbors_of_edge([1, 2, 1].into(), [1, 1, 1].into()),
+            y_neighbors,
+        );
+        assert_eq!(
+            grid.neighbors_of_edge([1, 1, 1].into(), [1, 1, 2].into()),
+            z_neighbors,
+        );
+        assert_eq!(
+            grid.neighbors_of_edge([1, 1, 2].into(), [1, 1, 1].into()),
+            z_neighbors,
         );
     }
 
