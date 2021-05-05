b49f1c6b00033c2c069deaff46b679fafca01470
Hanno Braun <hb@hannobraun.de>
2021-05-05 18:14:38 +0000

    Use isosurface extraction to create meshes

--- fj/src/geometry/conversions/to_mesh.rs
+++ fj/src/geometry/conversions/to_mesh.rs
@@ -1,12 +1,9 @@
 use std::convert::Infallible;
 
-use nalgebra::Point3;
-
 use crate::geometry::{
-    conversions::ToPolygon,
-    operations::linear_extrude::LinearExtrude,
-    shapes::{Mesh, Point},
-    triangulation::brute_force::{self, triangulate, InternalError},
+    attributes::{BoundingVolume, Distance},
+    isosurface::{self, Grid},
+    shapes::Mesh,
 };
 
 pub trait ToMesh {
@@ -25,73 +22,32 @@ impl ToMesh for Mesh {
 
 impl<T> ToMesh for T
 where
-    T: ToPolygon,
+    T: BoundingVolume + Distance,
 {
-    type Error = brute_force::InternalError;
+    type Error = Infallible;
 
-    fn to_mesh(self, tolerance: f32) -> Result<Mesh, Self::Error> {
+    fn to_mesh(self, resolution: f32) -> Result<Mesh, Self::Error> {
+        let aabb = self.aabb();
+        let grid_descriptor = isosurface::GridDescriptor {
+            min: aabb.mins,
+            max: aabb.maxs,
+            resolution,
+        };
+        let grid = Grid::from_descriptor(grid_descriptor, self);
+
         let mut mesh = Mesh::new();
 
-        let polygon = self.to_polygon(tolerance);
-        let triangles = triangulate(polygon)?;
+        for edge in grid.edges() {
+            if edge.at_surface() {
+                let [a, b, c, d] =
+                    grid.neighbors_of_edge(edge.a.index, edge.b.index);
 
-        for triangle in triangles {
-            let a_x: f32 = triangle.a.x.into();
-            let a_y: f32 = triangle.a.y.into();
-            let b_x: f32 = triangle.b.x.into();
-            let b_y: f32 = triangle.b.y.into();
-            let c_x: f32 = triangle.c.x.into();
-            let c_y: f32 = triangle.c.y.into();
-
-            mesh.triangle(
-                Point3::new(a_x, a_y, 0.0),
-                Point3::new(b_x, b_y, 0.0),
-                Point3::new(c_x, c_y, 0.0),
-            );
+                // TASK: Make sure that triangles face the right direction.
+                mesh.triangle(a, b, d);
+                mesh.triangle(b, c, d);
+            }
         }
 
         Ok(mesh)
-    }
-}
-
-impl<Sketch> ToMesh for LinearExtrude<Sketch>
-where
-    Sketch: ToPolygon,
-{
-    type Error = InternalError;
-
-    fn to_mesh(self, tolerance: f32) -> Result<Mesh, Self::Error> {
-        let sketch = self.sketch.to_polygon(tolerance);
-
-        let mut lower = sketch.clone().to_mesh(tolerance)?;
-        let upper = sketch.clone().to_mesh(tolerance)?;
-
-        // Triangles need to point down, which is the outside direction.
-        lower.invert_triangles();
-
-        // Merge meshes.
-        for [a, b, c] in upper.triangles() {
-            let a = Point::from_xyz(a.position.x, a.position.y, self.height);
-            let b = Point::from_xyz(b.position.x, b.position.y, self.height);
-            let c = Point::from_xyz(c.position.x, c.position.y, self.height);
-
-            lower.triangle(a, b, c);
-        }
-
-        // Build walls.
-        for edge in sketch.edges() {
-            lower.triangle(
-                Point::from_xyz(edge.a.x, edge.a.y, 0.0),
-                Point::from_xyz(edge.b.x, edge.b.y, 0.0),
-                Point::from_xyz(edge.a.x, edge.a.y, self.height),
-            );
-            lower.triangle(
-                Point::from_xyz(edge.a.x, edge.a.y, self.height),
-                Point::from_xyz(edge.b.x, edge.b.y, 0.0),
-                Point::from_xyz(edge.b.x, edge.b.y, self.height),
-            );
-        }
-
-        Ok(lower)
     }
 }
--- models/spacer/src/main.rs
+++ models/spacer/src/main.rs
@@ -1,11 +1,12 @@
 use fj::prelude::*;
 
 fn main() -> anyhow::Result<()> {
-    let outer = fj::Circle::from_radius(1.0);
-    let inner = fj::Circle::from_radius(0.5);
+    let height = 0.5;
 
-    let sketch = (outer, inner).difference();
-    let spacer = sketch.linear_extrude(0.5);
+    let outer = fj::Cylinder::new().with_radius(1.0).with_height(height);
+    let inner = fj::Cylinder::new().with_radius(0.5).with_height(height);
+
+    let spacer = (outer, inner).difference();
 
     fj::run(spacer)?;
     Ok(())
