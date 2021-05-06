3e77b0b95cb4694681836defad9e42337d2d905f
Hanno Braun <hb@hannobraun.de>
2021-05-06 17:02:26 +0000

    Remove unused code

--- fj/src/geometry/attributes/mod.rs
+++ fj/src/geometry/attributes/mod.rs
@@ -1,7 +1,4 @@
-pub mod boundary;
 pub mod bounding_volume;
 pub mod distance;
 
-pub use self::{
-    boundary::Boundary, bounding_volume::BoundingVolume, distance::Distance,
-};
+pub use self::{bounding_volume::BoundingVolume, distance::Distance};
--- fj/src/geometry/conversions/mod.rs
+++ fj/src/geometry/conversions/mod.rs
@@ -1,4 +1,3 @@
 pub mod to_mesh;
-pub mod to_polygon;
 
-pub use self::{to_mesh::ToMesh, to_polygon::ToPolygon};
+pub use self::to_mesh::ToMesh;
