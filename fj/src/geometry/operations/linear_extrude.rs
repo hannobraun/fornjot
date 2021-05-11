78da86179ffce936e68961f264e5bdd3182a4bb2
Hanno Braun <hb@hannobraun.de>
2021-05-11 21:14:16 +0000

    Add builder methods to `LinearExtrude`

--- fj/src/geometry/operations/linear_extrude.rs
+++ fj/src/geometry/operations/linear_extrude.rs
@@ -2,3 +2,15 @@ pub struct LinearExtrude<Sketch> {
     pub sketch: Sketch,
     pub height: f32,
 }
+
+impl<Sketch> LinearExtrude<Sketch> {
+    pub fn with_sketch(mut self, sketch: Sketch) -> Self {
+        self.sketch = sketch;
+        self
+    }
+
+    pub fn with_height(mut self, height: f32) -> Self {
+        self.height = height;
+        self
+    }
+}
