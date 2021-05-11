5b56e7563070f3ce1a9b64e962d5279daa1851be
Hanno Braun <hb@hannobraun.de>
2021-05-11 20:12:24 +0000

    Rename module

--- fj/src/geometry/isosurface/mod.rs
+++ fj/src/geometry/isosurface/mod.rs
@@ -2,12 +2,12 @@ pub mod edge;
 pub mod grid;
 pub mod grid_descriptor;
 pub mod grid_index;
-pub mod sdf_to_mesh;
+pub mod to_mesh;
 
 pub use self::{
     edge::{Edge, Value},
     grid::Grid,
     grid_descriptor::GridDescriptor,
     grid_index::GridIndex,
-    sdf_to_mesh::sdf_to_mesh,
+    to_mesh::sdf_to_mesh,
 };
