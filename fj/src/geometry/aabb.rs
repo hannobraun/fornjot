c1f4cab38b50e511d2675a557cd3dbb96da07d7d
Hanno Braun <hb@hannobraun.de>
2021-05-11 21:07:43 +0000

    Implement `Aabb<2>::extend`

--- fj/src/geometry/aabb.rs
+++ fj/src/geometry/aabb.rs
@@ -4,3 +4,12 @@ pub struct Aabb<const D: usize> {
     pub min: Point<f32, D>,
     pub max: Point<f32, D>,
 }
+
+impl Aabb<2> {
+    pub fn extend(self, min: f32, max: f32) -> Aabb<3> {
+        Aabb {
+            min: [self.min.x, self.min.y, min].into(),
+            max: [self.max.x, self.max.y, max].into(),
+        }
+    }
+}
