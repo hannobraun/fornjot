6aaa8f60c45ea136646060c5d7a32ccd5c0f091c
Hanno Braun <hb@hannobraun.de>
2021-05-11 21:12:58 +0000

    Implement `Distance` for `LinearExtrude`

--- fj/src/geometry/attributes/distance.rs
+++ fj/src/geometry/attributes/distance.rs
@@ -45,6 +45,24 @@ where
     }
 }
 
+impl<Sketch> Distance<3> for operations::LinearExtrude<Sketch>
+where
+    Sketch: Distance<2>,
+{
+    fn distance(&self, point: impl Into<Point<f32, 3>>) -> f32 {
+        let point = point.into();
+
+        let d_xy = self.sketch.distance(point.xy());
+        let d_z = point.z.abs() - self.height / 2.0;
+
+        if d_xy < 0.0 || d_z < 0.0 {
+            f32::max(d_xy, d_z)
+        } else {
+            f32::min(d_xy, d_z)
+        }
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use crate::geometry::shapes::Cylinder;
