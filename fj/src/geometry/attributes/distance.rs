a03f00b28a29c6e20942e93051e8ad052b066934
Hanno Braun <hanno@braun-odw.eu>
2021-05-16 07:22:17 +0000

    Move test

--- fj/src/geometry/attributes/distance.rs
+++ fj/src/geometry/attributes/distance.rs
@@ -4,26 +4,3 @@ use nalgebra::Point;
 pub trait Distance<const D: usize> {
     fn distance(&self, point: impl Into<Point<f32, D>>) -> f32;
 }
-
-#[cfg(test)]
-mod tests {
-    use crate::geometry::shapes::Cylinder;
-
-    use super::Distance as _;
-
-    #[test]
-    fn cylinder_should_return_distance() {
-        let cylinder = Cylinder::new().with_radius(0.5).with_height(1.0);
-
-        assert_eq!(cylinder.distance([0.0, 0.0, 0.0]), -0.5);
-        assert_eq!(cylinder.distance([0.25, 0.0, 0.0]), -0.25);
-        assert_eq!(cylinder.distance([0.0, 0.25, 0.0]), -0.25);
-        assert_eq!(cylinder.distance([0.0, 0.0, 0.25]), -0.25);
-
-        assert_eq!(cylinder.distance([1.0, 0.0, 0.0]), 0.5);
-        assert_eq!(cylinder.distance([0.0, 1.0, 0.0]), 0.5);
-        assert_eq!(cylinder.distance([0.0, 0.0, 1.0]), 0.5);
-        assert_eq!(cylinder.distance([0.0, 0.0, -1.0]), 0.5);
-        assert_eq!(cylinder.distance([1.0, 0.0, 2.0]), 0.5);
-    }
-}
--- fj/src/geometry/shapes/cylinder.rs
+++ fj/src/geometry/shapes/cylinder.rs
@@ -17,3 +17,24 @@ impl Cylinder {
         self
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use crate::geometry::{attributes::Distance as _, shapes::Cylinder};
+
+    #[test]
+    fn cylinder_should_return_distance() {
+        let cylinder = Cylinder::new().with_radius(0.5).with_height(1.0);
+
+        assert_eq!(cylinder.distance([0.0, 0.0, 0.0]), -0.5);
+        assert_eq!(cylinder.distance([0.25, 0.0, 0.0]), -0.25);
+        assert_eq!(cylinder.distance([0.0, 0.25, 0.0]), -0.25);
+        assert_eq!(cylinder.distance([0.0, 0.0, 0.25]), -0.25);
+
+        assert_eq!(cylinder.distance([1.0, 0.0, 0.0]), 0.5);
+        assert_eq!(cylinder.distance([0.0, 1.0, 0.0]), 0.5);
+        assert_eq!(cylinder.distance([0.0, 0.0, 1.0]), 0.5);
+        assert_eq!(cylinder.distance([0.0, 0.0, -1.0]), 0.5);
+        assert_eq!(cylinder.distance([1.0, 0.0, 2.0]), 0.5);
+    }
+}
