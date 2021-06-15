3264ef83d2ec3592204da8b96ce690e298e767bf
Hanno Braun <hb@hannobraun.de>
2021-06-15 15:19:24 +0000

    Update documentation

--- fj/src/export_3mf.rs
+++ fj/src/export_3mf.rs
@@ -4,9 +4,10 @@ use crate::Mesh;
 
 /// Export mesh to 3MF file
 ///
-/// See [3MF specification].
+/// See [3MF specification] and [Open Packaging Conventions Fundamentals].
 ///
 /// [3MF specification]: https://3mf.io/specification/
+/// [Open Packaging Conventions Fundamentals]: https://docs.microsoft.com/en-us/previous-versions/windows/desktop/opc/open-packaging-conventions-overview
 pub fn export_3mf(_mesh: &Mesh, path: PathBuf) -> anyhow::Result<()> {
     let _file = File::create(path)?;
 
