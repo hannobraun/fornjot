4545dfecf47d3a48d7304def47b0dc403bf37547
Hanno Braun <hb@hannobraun.de>
2021-06-15 15:09:42 +0000

    Add documentation

--- fj/src/export_3mf.rs
+++ fj/src/export_3mf.rs
@@ -2,6 +2,11 @@ use std::{fs::File, path::PathBuf};
 
 use crate::Mesh;
 
+/// Export mesh to 3MF file
+///
+/// See [3MF specification].
+///
+/// [3MF specification]: https://3mf.io/specification/
 pub fn export_3mf(_mesh: &Mesh, path: PathBuf) -> anyhow::Result<()> {
     let _file = File::create(path)?;
 
