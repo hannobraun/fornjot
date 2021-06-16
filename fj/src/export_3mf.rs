4118e8c042ad8e41dbb109fb2509854feb896c27
Hanno Braun <hb@hannobraun.de>
2021-06-16 13:59:59 +0000

    Link to standards document

--- fj/src/export_3mf.rs
+++ fj/src/export_3mf.rs
@@ -10,10 +10,10 @@ use crate::Mesh;
 
 /// Export mesh to 3MF file
 ///
-/// See [3MF specification] and [Open Packaging Conventions Fundamentals].
+/// See [3MF specification] and [Open Packaging Conventions].
 ///
 /// [3MF specification]: https://3mf.io/specification/
-/// [Open Packaging Conventions Fundamentals]: https://docs.microsoft.com/en-us/previous-versions/windows/desktop/opc/open-packaging-conventions-overview
+/// [Open Packaging Conventions]: https://standards.iso.org/ittf/PubliclyAvailableStandards/c061796_ISO_IEC_29500-2_2012.zip
 pub fn export_3mf(_mesh: &Mesh, path: PathBuf) -> Result<(), Error> {
     let name = path
         .file_stem()
