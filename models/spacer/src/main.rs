1f3a6934bafbbd2cb2f81c9c5b3f40d25a31a009
Hanno Braun <hb@hannobraun.de>
2021-06-17 12:59:57 +0000

    Refactor

--- models/spacer/src/main.rs
+++ models/spacer/src/main.rs
@@ -1,10 +1,12 @@
 use fj::prelude::*;
 
 fn main() -> anyhow::Result<()> {
+    let outer = 50.0;
+    let inner = 25.0;
     let height = 25.0;
 
-    let outer = fj::Cylinder::new().with_radius(50.0).with_height(height);
-    let inner = fj::Cylinder::new().with_radius(25.0).with_height(height);
+    let outer = fj::Cylinder::new().with_radius(outer).with_height(height);
+    let inner = fj::Cylinder::new().with_radius(inner).with_height(height);
 
     let spacer = (outer, inner).difference();
 
