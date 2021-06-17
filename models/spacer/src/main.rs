695607bb9f906b5b79632a9643bbc0451a77b1b1
Hanno Braun <hb@hannobraun.de>
2021-06-17 12:58:28 +0000

    Make spacer smaller

--- models/spacer/src/main.rs
+++ models/spacer/src/main.rs
@@ -1,10 +1,10 @@
 use fj::prelude::*;
 
 fn main() -> anyhow::Result<()> {
-    let height = 50.0;
+    let height = 25.0;
 
-    let outer = fj::Cylinder::new().with_radius(100.0).with_height(height);
-    let inner = fj::Cylinder::new().with_radius(50.0).with_height(height);
+    let outer = fj::Cylinder::new().with_radius(50.0).with_height(height);
+    let inner = fj::Cylinder::new().with_radius(25.0).with_height(height);
 
     let spacer = (outer, inner).difference();
 
