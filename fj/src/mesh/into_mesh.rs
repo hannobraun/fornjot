1410a268f5848e5d1dbf99448f2d4c1777b6d56c
Hanno Braun <hb@hannobraun.de>
2021-06-18 13:32:41 +0000

    Compute suitable resolution automatically

--- fj/src/mesh/into_mesh.rs
+++ fj/src/mesh/into_mesh.rs
@@ -6,11 +6,11 @@ use crate::geometry::{
 use super::Mesh;
 
 pub trait IntoMesh {
-    fn into_mesh(self, resolution: f32) -> Mesh;
+    fn into_mesh(self) -> Mesh;
 }
 
 impl IntoMesh for Mesh {
-    fn into_mesh(self, _: f32) -> Mesh {
+    fn into_mesh(self) -> Mesh {
         self
     }
 }
@@ -19,7 +19,8 @@ impl<T> IntoMesh for T
 where
     T: BoundingVolume<3> + Distance<3>,
 {
-    fn into_mesh(self, resolution: f32) -> Mesh {
+    fn into_mesh(self) -> Mesh {
+        let resolution = self.aabb().size().max() / 100.0;
         isosurface::to_mesh(&self, resolution)
     }
 }
