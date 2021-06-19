0b6126e4bce62f7425eee2198745c177912661f2
Hanno Braun <hanno@braun-odw.eu>
2021-06-19 09:11:36 +0000

    Add documentation

--- fj/src/geometry/isosurface/mod.rs
+++ fj/src/geometry/isosurface/mod.rs
@@ -1,3 +1,14 @@
+//! Simple isosurface extraction algorithm
+//!
+//! This algorithm is based on ideas from [Dual Contouring of Hermite Data], and
+//! might be similar to the Surface Nets algorithm.
+//!
+//! [Manifold Dual Contouring] might also become relevant, as that's one of the
+//! papers I'm looking at right now for improvements.
+//!
+//! [Dual Contouring of Hermite Data]: https://www.cse.wustl.edu/~taoju/research/dualContour.pdf
+//! [Manifold Dual Contouring]: https://people.engr.tamu.edu/schaefer/research/dualsimp_tvcg.pdf
+
 pub mod edge;
 pub mod grid;
 pub mod grid_descriptor;
