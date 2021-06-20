503aad9780c82d0f9a5bae8879b0075ac4c6c5a4
Hanno Braun <hanno@braun-odw.eu>
2021-06-20 11:17:43 +0000

    Remove unused code

--- fj/src/geometry/isosurface/grid/cell.rs
+++ fj/src/geometry/isosurface/grid/cell.rs
@@ -1,6 +1,4 @@
-use std::array;
-
-use nalgebra::{Point, Vector};
+use nalgebra::Point;
 
 use crate::geometry::isosurface::grid;
 
@@ -17,75 +15,4 @@ pub struct Cell {
     ///
     /// Required to compute the positions of the cell's vertices.
     pub resolution: f32,
-}
-
-impl Cell {
-    pub fn vertices(
-        &self,
-    ) -> impl Iterator<Item = (grid::Index, Point<f32, 3>)> {
-        array::IntoIter::new([
-            (
-                self.min_index + [0, 0, 0],
-                self.min_position + Vector::from([0.0, 0.0, 0.0]),
-            ),
-            (
-                self.min_index + [0, 0, 1],
-                self.min_position + Vector::from([0.0, 0.0, 1.0]),
-            ),
-            (
-                self.min_index + [0, 1, 0],
-                self.min_position + Vector::from([0.0, 1.0, 0.0]),
-            ),
-            (
-                self.min_index + [0, 1, 1],
-                self.min_position + Vector::from([0.0, 1.0, 1.0]),
-            ),
-            (
-                self.min_index + [1, 0, 0],
-                self.min_position + Vector::from([1.0, 0.0, 0.0]),
-            ),
-            (
-                self.min_index + [1, 0, 1],
-                self.min_position + Vector::from([1.0, 0.0, 1.0]),
-            ),
-            (
-                self.min_index + [1, 1, 0],
-                self.min_position + Vector::from([1.0, 1.0, 0.0]),
-            ),
-            (
-                self.min_index + [1, 1, 1],
-                self.min_position + Vector::from([1.0, 1.0, 1.0]),
-            ),
-        ])
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::Cell;
-
-    #[test]
-    fn vertices_should_compute_cell_vertices() {
-        let cell = Cell {
-            min_index: [0, 0, 0].into(),
-            min_position: [0.0, 0.0, 0.0].into(),
-            resolution: 1.0,
-        };
-
-        let vertices: Vec<_> = cell.vertices().collect();
-
-        assert_eq!(
-            vertices,
-            vec![
-                ([0, 0, 0].into(), [0.0, 0.0, 0.0].into()),
-                ([0, 0, 1].into(), [0.0, 0.0, 1.0].into()),
-                ([0, 1, 0].into(), [0.0, 1.0, 0.0].into()),
-                ([0, 1, 1].into(), [0.0, 1.0, 1.0].into()),
-                ([1, 0, 0].into(), [1.0, 0.0, 0.0].into()),
-                ([1, 0, 1].into(), [1.0, 0.0, 1.0].into()),
-                ([1, 1, 0].into(), [1.0, 1.0, 0.0].into()),
-                ([1, 1, 1].into(), [1.0, 1.0, 1.0].into()),
-            ]
-        );
-    }
 }
