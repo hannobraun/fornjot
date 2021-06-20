809c8eac72c4cc09c32330b44aa81fc3027f7ea4
Hanno Braun <hanno@braun-odw.eu>
2021-06-20 10:31:49 +0000

    Update documentation

--- fj/src/geometry/isosurface/grid/cell.rs
+++ fj/src/geometry/isosurface/grid/cell.rs
@@ -4,18 +4,18 @@ use nalgebra::{Point, Vector};
 
 use crate::geometry::isosurface::grid;
 
-/// A cube in a uniform grid used for isosurface extraction
+/// A cell in a uniform grid used for isosurface extraction
 #[derive(Debug, PartialEq)]
 pub struct Cell {
-    /// The index of the minimum (as defined by index ordering) cube vertex
+    /// The index of the minimum (as defined by index ordering) cell vertex
     pub min_index: grid::Index,
 
-    /// The position of the minimum (as defined by index ordering) cube vertex
+    /// The position of the minimum (as defined by index ordering) cell vertex
     pub min_position: Point<f32, 3>,
 
-    /// The resolution of the grid, i.e. the size of the cube
+    /// The resolution of the grid, i.e. the size of the cell
     ///
-    /// Required to compute the positions of the cube's vertices.
+    /// Required to compute the positions of the cell's vertices.
     pub resolution: f32,
 }
 
