4fd6c7a65652d7a29c675710b4396f71cef229af
Hanno Braun <hb@hannobraun.de>
2021-06-22 16:32:57 +0000

    Rename variable

--- fj/src/geometry/isosurface/grid/mod.rs
+++ fj/src/geometry/isosurface/grid/mod.rs
@@ -54,9 +54,9 @@ impl Grid {
             .filter_map(|(index, vertex)| {
                 // Compute distance of this vertex from the isosurface, and
                 // filter all points that aren't close to the surface.
-                let surface_point = isosurface.surface(vertex);
-                if surface_point.distance <= descriptor.resolution {
-                    Some((index, (vertex, surface_point.distance)))
+                let surface_sample = isosurface.surface(vertex);
+                if surface_sample.distance <= descriptor.resolution {
+                    Some((index, (vertex, surface_sample.distance)))
                 } else {
                     None
                 }
