c5acf67ad5d91857d9cc4a51affd3bd91b2a7b49
Hanno Braun <hb@hannobraun.de>
2021-06-24 16:11:22 +0000

    Refactor

--- fj/src/geometry/isosurface/grid/cell.rs
+++ fj/src/geometry/isosurface/grid/cell.rs
@@ -1,12 +1,12 @@
 use nalgebra::Point;
 
-use crate::geometry::isosurface::grid;
+use super::Index;
 
 /// A cell in a uniform grid used for isosurface extraction
 #[derive(Debug, PartialEq)]
 pub struct Cell {
     /// The index of the minimum (as defined by index ordering) cell vertex
-    pub min_index: grid::Index,
+    pub min_index: Index,
 
     /// The position of the minimum (as defined by index ordering) cell vertex
     pub min_position: Point<f32, 3>,
