fdb1eaa8ec281dc3f3ed33db16ced1a079169cd1
Hanno Braun <hb@hannobraun.de>
2021-06-24 16:05:17 +0000

    Remove unused code

--- fj/src/geometry/isosurface/grid/descriptor.rs
+++ fj/src/geometry/isosurface/grid/descriptor.rs
@@ -1,7 +1,6 @@
 use std::ops::Range;
 
 use itertools::Itertools as _;
-use nalgebra::Point;
 
 use crate::geometry::aabb::Aabb;
 
@@ -48,44 +47,11 @@ impl Descriptor {
 
         cells
     }
-
-    /// Compute the grid vertices
-    ///
-    /// The grid extends beyond `self.aabb`, so that the center of the outermost
-    /// grid cells are outside of, or on, the isosurface.
-    pub fn vertices(
-        &self,
-    ) -> impl Iterator<Item = (Index, Point<f32, 3>)> + '_ {
-        let min = self.aabb.min;
-        let max = self.aabb.max;
-
-        let indices_x = indices(min.x, max.x, self.resolution);
-        let indices_y = indices(min.y, max.y, self.resolution);
-        let indices_z = indices(min.z, max.z, self.resolution);
-
-        let indices = indices_x
-            .cartesian_product(indices_y)
-            .cartesian_product(indices_z)
-            .map(|((x, y), z)| Index::from([x, y, z]));
-
-        let vertices = indices.map(move |index| {
-            (index, index.to_coordinates(min, self.resolution))
-        });
-
-        vertices
-    }
 }
 
 fn cell_indices(min: f32, max: f32, resolution: f32) -> Range<usize> {
     let lower = 0;
     let upper = ((max - min) / resolution).ceil() as usize + 1;
-
-    lower..upper
-}
-
-fn indices(min: f32, max: f32, resolution: f32) -> Range<usize> {
-    let lower = 0;
-    let upper = ((max - min) / resolution).ceil() as usize + 2;
 
     lower..upper
 }
@@ -143,52 +109,6 @@ mod tests {
                     min_index: [1, 1, 1].into(),
                     min_position: [0.5, 0.5, 1.0].into(),
                 },
-            ]
-        );
-    }
-
-    #[test]
-    fn vertices_should_return_grid_vertices() {
-        let grid = Descriptor {
-            aabb: Aabb {
-                min: [0.0, 0.0, 0.5].into(),
-                max: [0.5, 1.0, 1.5].into(),
-            },
-            resolution: 1.0,
-        };
-
-        let vertices: Vec<_> = grid.vertices().collect();
-
-        assert_eq!(
-            vertices,
-            vec![
-                ([0, 0, 0].into(), [-0.5, -0.5, 0.0].into()),
-                ([0, 0, 1].into(), [-0.5, -0.5, 1.0].into()),
-                ([0, 0, 2].into(), [-0.5, -0.5, 2.0].into()),
-                ([0, 1, 0].into(), [-0.5, 0.5, 0.0].into()),
-                ([0, 1, 1].into(), [-0.5, 0.5, 1.0].into()),
-                ([0, 1, 2].into(), [-0.5, 0.5, 2.0].into()),
-                ([0, 2, 0].into(), [-0.5, 1.5, 0.0].into()),
-                ([0, 2, 1].into(), [-0.5, 1.5, 1.0].into()),
-                ([0, 2, 2].into(), [-0.5, 1.5, 2.0].into()),
-                ([1, 0, 0].into(), [0.5, -0.5, 0.0].into()),
-                ([1, 0, 1].into(), [0.5, -0.5, 1.0].into()),
-                ([1, 0, 2].into(), [0.5, -0.5, 2.0].into()),
-                ([1, 1, 0].into(), [0.5, 0.5, 0.0].into()),
-                ([1, 1, 1].into(), [0.5, 0.5, 1.0].into()),
-                ([1, 1, 2].into(), [0.5, 0.5, 2.0].into()),
-                ([1, 2, 0].into(), [0.5, 1.5, 0.0].into()),
-                ([1, 2, 1].into(), [0.5, 1.5, 1.0].into()),
-                ([1, 2, 2].into(), [0.5, 1.5, 2.0].into()),
-                ([2, 0, 0].into(), [1.5, -0.5, 0.0].into()),
-                ([2, 0, 1].into(), [1.5, -0.5, 1.0].into()),
-                ([2, 0, 2].into(), [1.5, -0.5, 2.0].into()),
-                ([2, 1, 0].into(), [1.5, 0.5, 0.0].into()),
-                ([2, 1, 1].into(), [1.5, 0.5, 1.0].into()),
-                ([2, 1, 2].into(), [1.5, 0.5, 2.0].into()),
-                ([2, 2, 0].into(), [1.5, 1.5, 0.0].into()),
-                ([2, 2, 1].into(), [1.5, 1.5, 1.0].into()),
-                ([2, 2, 2].into(), [1.5, 1.5, 2.0].into()),
             ]
         );
     }
