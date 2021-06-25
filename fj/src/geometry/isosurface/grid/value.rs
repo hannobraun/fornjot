27b622405a88f79f52e959cd2193d53e03a08490
Hanno Braun <hb@hannobraun.de>
2021-06-25 14:17:10 +0000

    Remove custom `Debug` implementation of `Value`
    It's getting in the way of my current refactoring. It can be re-added
    later, once it's needed again.

--- fj/src/geometry/isosurface/grid/value.rs
+++ fj/src/geometry/isosurface/grid/value.rs
@@ -1,20 +1,10 @@
-use std::fmt;
-
 use nalgebra::Point;
-
-use crate::geometry::util::DebugPoint;
 
 use super::Index;
 
-#[derive(Clone, Copy, PartialEq)]
+#[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Value {
     pub index: Index,
     pub point: Point<f32, 3>,
     pub value: f32,
-}
-
-impl fmt::Debug for Value {
-    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "{:?} ({:.2})", DebugPoint(self.point), self.value)
-    }
 }
