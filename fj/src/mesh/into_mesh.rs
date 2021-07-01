d35584e13af8a424f3e07f9519c2dfffa722d016
Hanno Braun <hb@hannobraun.de>
2021-07-01 14:10:08 +0000

    Add `into_mesh::WithResolution`

--- fj/src/mesh/into_mesh.rs
+++ fj/src/mesh/into_mesh.rs
@@ -24,3 +24,17 @@ where
         isosurface::to_mesh(&self, resolution)
     }
 }
+
+pub struct WithResolution<T> {
+    pub geometry: T,
+    pub resolution: f32,
+}
+
+impl<T> IntoMesh for WithResolution<T>
+where
+    T: BoundingVolume<3> + Surface<3>,
+{
+    fn into_mesh(self) -> Mesh {
+        isosurface::to_mesh(&self.geometry, self.resolution)
+    }
+}
