06bce82dfd9a95b6a0dc0eef3d387d94dcbb7c02
Hanno Braun <hanno@braun-odw.eu>
2021-07-05 09:48:56 +0000

    Remove unused trait

--- fj/src/model.rs
+++ fj/src/model.rs
@@ -5,26 +5,6 @@ use crate::geometry::{
 
 use super::Mesh;
 
-pub trait IntoMesh {
-    fn into_mesh(self) -> Mesh;
-}
-
-impl IntoMesh for Mesh {
-    fn into_mesh(self) -> Mesh {
-        self
-    }
-}
-
-impl<T> IntoMesh for T
-where
-    T: BoundingVolume<3> + Surface<3>,
-{
-    fn into_mesh(self) -> Mesh {
-        let resolution = self.aabb().size().max() / 100.0;
-        isosurface::to_mesh(&self, resolution)
-    }
-}
-
 impl<T> From<T> for Mesh
 where
     T: BoundingVolume<3> + Surface<3>,
@@ -38,15 +18,6 @@ where
 pub struct WithResolution<T> {
     pub geometry: T,
     pub resolution: f32,
-}
-
-impl<T> IntoMesh for WithResolution<T>
-where
-    T: BoundingVolume<3> + Surface<3>,
-{
-    fn into_mesh(self) -> Mesh {
-        isosurface::to_mesh(&self.geometry, self.resolution)
-    }
 }
 
 impl<T> From<WithResolution<T>> for Mesh
