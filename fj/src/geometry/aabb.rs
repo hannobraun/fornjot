17f8c2a55ac5419e83b5071fd9bceec4f3d0061c
Hanno Braun <hb@hannobraun.de>
2021-07-26 19:40:58 +0000

    Remove unused code

--- fj/src/geometry/aabb.rs
+++ fj/src/geometry/aabb.rs
@@ -88,34 +88,6 @@ impl Aabb<3> {
             [g, h],
         ]
     }
-
-    /// Partition the axis-aligned bounding box into 8 smaller boxes
-    pub fn partition(&self) -> [Self; 8] {
-        let size = self.size() / 2.0;
-
-        #[rustfmt::skip]
-        let [a, b, c, d, e, f, g, h]: [SVector<f32, 3>; 8] = [
-            [   0.0,    0.0,    0.0].into(),
-            [   0.0,    0.0, size.z].into(),
-            [   0.0, size.y,    0.0].into(),
-            [   0.0, size.y, size.z].into(),
-            [size.x,    0.0,    0.0].into(),
-            [size.x,    0.0, size.z].into(),
-            [size.x, size.y,    0.0].into(),
-            [size.x, size.y, size.z].into(),
-        ];
-
-        [
-            Self::from_min_and_size(self.min + a, size),
-            Self::from_min_and_size(self.min + b, size),
-            Self::from_min_and_size(self.min + c, size),
-            Self::from_min_and_size(self.min + d, size),
-            Self::from_min_and_size(self.min + e, size),
-            Self::from_min_and_size(self.min + f, size),
-            Self::from_min_and_size(self.min + g, size),
-            Self::from_min_and_size(self.min + h, size),
-        ]
-    }
 }
 
 impl<const D: usize> fmt::Debug for Aabb<D> {
@@ -189,48 +161,5 @@ mod tests {
                 [[1.0, 1.0, 0.0].into(), [1.0, 1.0, 1.0].into(),],
             ]
         );
-    }
-
-    #[test]
-    fn partition_should_partition_into_8_parts() {
-        let aabb = Aabb {
-            min: [0.0, 0.0, 0.0].into(),
-            max: [2.0, 2.0, 2.0].into(),
-        };
-
-        let a = Aabb {
-            min: [0.0, 0.0, 0.0].into(),
-            max: [1.0, 1.0, 1.0].into(),
-        };
-        let b = Aabb {
-            min: [0.0, 0.0, 1.0].into(),
-            max: [1.0, 1.0, 2.0].into(),
-        };
-        let c = Aabb {
-            min: [0.0, 1.0, 0.0].into(),
-            max: [1.0, 2.0, 1.0].into(),
-        };
-        let d = Aabb {
-            min: [0.0, 1.0, 1.0].into(),
-            max: [1.0, 2.0, 2.0].into(),
-        };
-        let e = Aabb {
-            min: [1.0, 0.0, 0.0].into(),
-            max: [2.0, 1.0, 1.0].into(),
-        };
-        let f = Aabb {
-            min: [1.0, 0.0, 1.0].into(),
-            max: [2.0, 1.0, 2.0].into(),
-        };
-        let g = Aabb {
-            min: [1.0, 1.0, 0.0].into(),
-            max: [2.0, 2.0, 1.0].into(),
-        };
-        let h = Aabb {
-            min: [1.0, 1.0, 1.0].into(),
-            max: [2.0, 2.0, 2.0].into(),
-        };
-
-        assert_eq!(aabb.partition(), [a, b, c, d, e, f, g, h]);
     }
 }
