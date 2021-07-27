24d2341a3dba1b3000e526e98c90bcc161b486cf
Hanno Braun <hb@hannobraun.de>
2021-07-27 20:30:00 +0000

    Document `attributes` module

--- fj/src/geometry/attributes.rs
+++ fj/src/geometry/attributes.rs
@@ -2,14 +2,31 @@ use nalgebra::{Point, SVector, Unit};
 
 use crate::geometry::aabb::Aabb;
 
-/// Provides a signed distance function
+/// Defines geometry that can be samples
+///
+/// The `D` parameter defines the dimensionality of the geometry (typically
+/// geometry would be 2- or 3-dimensional).
 pub trait Geometry<const D: usize> {
+    /// Sample the geometry at the specified point
+    ///
+    /// Returns a `Sample` value which describes, among other attributes, the
+    /// distance of the point from the surface.
     fn sample(&self, point: impl Into<Point<f32, D>>) -> Sample<D>;
 }
 
+/// The result of sampling geometry at a specific point
+///
+/// Returned by [`Geometry::sample`].
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Sample<const D: usize> {
+    /// The point at which the geometry was sampled
     pub point: Point<f32, D>,
+
+    /// The minimum distance of the point to the surface
+    ///
+    /// A positive value indicates that the point is outside of the object, a
+    /// negative value indicates that the point is inside. Either way, the
+    /// absolute value is equal to the distance of the point to the surface.
     pub distance: f32,
 
     // TASK: Remove normal from `SurfaceSample`.
@@ -20,7 +37,8 @@ pub struct Sample<const D: usize> {
     pub normal: Unit<SVector<f32, D>>,
 }
 
+/// Defines a bounding volume that encloses geometry
 pub trait BoundingVolume<const D: usize> {
-    /// Axis-aligned bounding box
+    /// Return the geometry's axis-aligned bounding box
     fn aabb(&self) -> Aabb<D>;
 }
