d25c5123f4cca41430c0162bc72ced4f03540ec9
Hanno Braun <hb@hannobraun.de>
2021-07-28 18:54:44 +0000

    Document `vertex`

--- fj/src/geometry/isosurface/grid/vertex.rs
+++ fj/src/geometry/isosurface/grid/vertex.rs
@@ -2,9 +2,15 @@ use nalgebra::Point;
 
 use super::Index;
 
+/// A vertex in an isosurface extraction grid
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Vertex {
+    /// The index that refers to this vertex
     pub index: Index,
+
+    /// The position of the vertex
     pub point: Point<f32, 3>,
+
+    /// The (signed) distance of the vertex to the closest surface point
     pub distance: f32,
 }
