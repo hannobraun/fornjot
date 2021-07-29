7b3ae5c0b6feeb56b18239478ae271a836d3526f
Hanno Braun <hb@hannobraun.de>
2021-07-29 18:48:18 +0000

    Document `model`

--- fj/src/model.rs
+++ fj/src/model.rs
@@ -7,10 +7,15 @@ use crate::geometry::{
 
 use super::Mesh;
 
+/// Implemented by Fornjot models
 pub trait Model {
+    /// The model parameters
     type Params: DeserializeOwned;
+
+    /// The type of the model's geometry
     type Ty: Into<Mesh>;
 
+    /// Instantiate the model
     fn instantiate(&self, params: Self::Params) -> Self::Ty;
 }
 
@@ -24,8 +29,18 @@ where
     }
 }
 
+/// Can be used to convert geometry to a mesh, using a specific resolution
+///
+/// There exists an `Into<Mesh>` implementation for geometry (i.e.
+/// implementations of [`BoundingVolume<3>`] and [`Geometry<3>`]) that
+/// automatically chooses a resolution based on the overall size of the
+/// geometry. For cases where this is not appropriate, this struct can be used
+/// to provide such a conversion, but with an explicitly provided resolution.
 pub struct WithResolution<T> {
+    /// The geometry to be converted into a [`Mesh`]
     pub geometry: T,
+
+    /// The resolution at which the geometry will be converted
     pub resolution: f32,
 }
 
