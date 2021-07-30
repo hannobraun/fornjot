0b2634c99bb4da2011ae3c7869eec207e4932c0f
Hanno Braun <hb@hannobraun.de>
2021-07-30 17:35:32 +0000

    Document `syntax`

--- fj/src/syntax.rs
+++ fj/src/syntax.rs
@@ -1,5 +1,14 @@
+//! Extension traits that provide simplified syntax for various operations
+//!
+//! Import the prelude (`use fj::prelude::*;`) to make these traits available to
+//! you.
+
 use crate::{geometry::operations, model};
 
+/// Provides convenient syntax for [`operations::Difference`]
+///
+/// This trait is implemented for tuples with two entries. The call
+/// `(a, b).difference()` will return the difference of `a` and `b`.
 pub trait Difference<A, B> {
     fn difference(self) -> operations::Difference<A, B>;
 }
@@ -13,6 +22,10 @@ impl<A, B> Difference<A, B> for (A, B) {
     }
 }
 
+/// Provides convenient syntax for [`operations::Sweep`]
+///
+/// This trait is implemented for all types. The call `sketch.sweep(distance)`
+/// will create a sweep of `sketch` over `distance`.
 pub trait Sweep<Sketch> {
     fn sweep(self, distance: f32) -> operations::Sweep<Sketch>;
 }
@@ -26,6 +39,11 @@ impl<Sketch> Sweep<Sketch> for Sketch {
     }
 }
 
+/// Provides convenient syntax for [`model::WithResolution`]
+///
+/// This trait is implemented for all types. The call `geometry.resolution(res)`
+/// will wrap `geometry` in a `model::WithResolution` struct, which can then be
+/// converted into a triangle mesh, using the resolution specified.
 pub trait Resolution: Sized {
     fn resolution(self, resolution: f32) -> model::WithResolution<Self> {
         model::WithResolution {
