1fb8e9a7bf0477d193a0394851b491af9ac2584d
Hanno Braun <hb@hannobraun.de>
2021-08-02 18:14:28 +0000

    Continue QEF solver implementation

--- fj/src/geometry/isosurface/qef.rs
+++ fj/src/geometry/isosurface/qef.rs
@@ -1,8 +1,12 @@
-use nalgebra::{Point, SVector};
+use nalgebra::{Matrix4, Point, SVector};
 
 pub fn find_best_point(
     _plains: &[(Point<f32, 3>, SVector<f32, 3>)],
 ) -> Point<f32, 3> {
+    // According to Dual Contouring: "The Secret Sauce", section 2.1, Solving
+    // QEFs, we start by initializing a 4x4 matrix to zero.
+    let _m = Matrix4::<f32>::zeros();
+
     // TASK: Implement.
     todo!()
 }
