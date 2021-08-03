39a47ca522867601ceefa4b743d96032698104e6
Hanno Braun <hb@hannobraun.de>
2021-08-03 20:24:17 +0000

    Fix typo in variable name

--- fj/src/geometry/isosurface/qef.rs
+++ fj/src/geometry/isosurface/qef.rs
@@ -1,7 +1,7 @@
 use nalgebra::{Matrix4, Point, SVector};
 
 pub fn find_best_point(
-    _plains: &[(Point<f32, 3>, SVector<f32, 3>)],
+    _planes: &[(Point<f32, 3>, SVector<f32, 3>)],
 ) -> Point<f32, 3> {
     // According to Dual Contouring: "The Secret Sauce", section 2.1, Solving
     // QEFs, we start by initializing a 4x4 matrix to zero.
