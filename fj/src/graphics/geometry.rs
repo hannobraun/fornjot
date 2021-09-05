8d4094fe160dcba6254401bae7a6dd12c4e0bb59
Hanno Braun <hanno@braun-odw.eu>
2021-09-05 08:35:32 +0000

    Refactor

--- fj/src/graphics/geometry.rs
+++ fj/src/graphics/geometry.rs
@@ -2,7 +2,7 @@ use std::convert::TryInto;
 
 use wgpu::util::DeviceExt;
 
-use super::mesh::Mesh;
+use super::{mesh::Mesh, vertex::Vertex};
 
 #[derive(Debug)]
 pub struct Geometry {
@@ -13,23 +13,30 @@ pub struct Geometry {
 
 impl Geometry {
     pub fn mesh(device: &wgpu::Device, mesh: &Mesh) -> Self {
+        Self::new(device, mesh.vertices(), mesh.indices())
+    }
+
+    fn new(
+        device: &wgpu::Device,
+        vertices: &[Vertex],
+        indices: &[u32],
+    ) -> Self {
         Geometry {
             vertex_buffer: device.create_buffer_init(
                 &wgpu::util::BufferInitDescriptor {
                     label: None,
-                    contents: bytemuck::cast_slice(mesh.vertices()),
+                    contents: bytemuck::cast_slice(vertices),
                     usage: wgpu::BufferUsages::VERTEX,
                 },
             ),
             index_buffer: device.create_buffer_init(
                 &wgpu::util::BufferInitDescriptor {
                     label: None,
-                    contents: bytemuck::cast_slice(mesh.indices()),
+                    contents: bytemuck::cast_slice(indices),
                     usage: wgpu::BufferUsages::INDEX,
                 },
             ),
-            num_indices: mesh
-                .indices()
+            num_indices: indices
                 .len()
                 .try_into()
                 .expect("`usize` couldn't be cast to `u32`"),
