d9af3ce400bbd57e59ca7336491eda86a5630b79
Hanno Braun <hanno@braun-odw.eu>
2021-09-07 07:33:53 +0000

    Update task

--- fj/src/graphics/mod.rs
+++ fj/src/graphics/mod.rs
@@ -5,6 +5,9 @@ mod renderer;
 mod shaders;
 mod transform;
 mod uniforms;
+// TASK: The name of this module (and the type within it) collides with
+//       `util::vertices`. One of the names should be change, and the one in
+//       `util` seems pretty much on point for what it does.
 mod vertices;
 
 pub use self::{
--- fj/src/util/mod.rs
+++ fj/src/util/mod.rs
@@ -1,5 +1,4 @@
 pub mod debug_point;
-// TASK: Find a better name for this.
 pub mod vertices;
 
 pub use self::{debug_point::DebugPoint, vertices::Vertices};
