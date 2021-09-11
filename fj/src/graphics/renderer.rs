406f506cf8ed135072bd4f96e61023133b035cb7
Hanno Braun <hanno@braun-odw.eu>
2021-09-11 16:09:25 +0000

    Add help text

--- fj/src/graphics/renderer.rs
+++ fj/src/graphics/renderer.rs
@@ -310,12 +310,12 @@ impl Renderer {
         encoder: &mut wgpu::CommandEncoder,
         view: &wgpu::TextureView,
     ) -> Result<(), String> {
-        // TASK: Remove.
         self.glyph_brush.queue(
             Section::new()
                 .with_screen_position((50.0, 50.0))
-                .add_text(text("test\n"))
-                .add_text(text("test\n")),
+                .add_text(text("Toggle model rendering with 1\n"))
+                .add_text(text("Toggle mesh rendering with 2\n"))
+                .add_text(text("Toggle grid rendering with 3\n")),
         );
 
         self.glyph_brush.draw_queued(
