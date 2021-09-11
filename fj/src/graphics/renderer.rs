6b114823a71486b7670f796e61dc3ea03a98ad33
Hanno Braun <hanno@braun-odw.eu>
2021-09-11 16:23:31 +0000

    Convert associated function into free function

--- fj/src/graphics/renderer.rs
+++ fj/src/graphics/renderer.rs
@@ -229,7 +229,7 @@ impl Renderer {
             );
         }
 
-        Self::draw_config_ui(
+        draw_config_ui(
             &mut self.glyph_brush,
             &self.device,
             &mut encoder,
@@ -306,37 +306,6 @@ impl Renderer {
             ),
         });
     }
-
-    fn draw_config_ui(
-        glyph_brush: &mut GlyphBrush<()>,
-        device: &wgpu::Device,
-        encoder: &mut wgpu::CommandEncoder,
-        view: &wgpu::TextureView,
-        surface_config: &wgpu::SurfaceConfiguration,
-    ) -> Result<(), String> {
-        // TASK: Update this to display the current configuration. Ideas:
-        //       - Display text like "X enabled/disabled (toggle with y)".
-        //       - Make text for disabled config semi-transparent.
-        glyph_brush.queue(
-            Section::new()
-                .with_screen_position((50.0, 50.0))
-                .add_text(text("Toggle model rendering with 1\n", true))
-                .add_text(text("Toggle mesh rendering with 2\n", true))
-                .add_text(text("Toggle grid rendering with 3\n", true)),
-        );
-
-        glyph_brush.draw_queued(
-            device,
-            // TASK: Put more thought into the staging belt's buffer size.
-            &mut StagingBelt::new(1024),
-            encoder,
-            &view,
-            surface_config.width,
-            surface_config.height,
-        )?;
-
-        Ok(())
-    }
 }
 
 #[derive(Debug)]
@@ -378,6 +347,37 @@ pub enum DrawError {
 
     #[error("Error drawing text")]
     Text(String),
+}
+
+fn draw_config_ui(
+    glyph_brush: &mut GlyphBrush<()>,
+    device: &wgpu::Device,
+    encoder: &mut wgpu::CommandEncoder,
+    view: &wgpu::TextureView,
+    surface_config: &wgpu::SurfaceConfiguration,
+) -> Result<(), String> {
+    // TASK: Update this to display the current configuration. Ideas:
+    //       - Display text like "X enabled/disabled (toggle with y)".
+    //       - Make text for disabled config semi-transparent.
+    glyph_brush.queue(
+        Section::new()
+            .with_screen_position((50.0, 50.0))
+            .add_text(text("Toggle model rendering with 1\n", true))
+            .add_text(text("Toggle mesh rendering with 2\n", true))
+            .add_text(text("Toggle grid rendering with 3\n", true)),
+    );
+
+    glyph_brush.draw_queued(
+        device,
+        // TASK: Put more thought into the staging belt's buffer size.
+        &mut StagingBelt::new(1024),
+        encoder,
+        &view,
+        surface_config.width,
+        surface_config.height,
+    )?;
+
+    Ok(())
 }
 
 fn text(text: &str, opaque: bool) -> Text {
