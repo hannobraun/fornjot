19efd8dc6d56f66c37ab813bba4901cafcabc2c0
Hanno Braun <hb@hannobraun.de>
2021-09-16 18:07:47 +0000

    Move tests

--- fj/src/geometry/attributes/surface_normal.rs
+++ fj/src/geometry/attributes/surface_normal.rs
@@ -60,3 +60,46 @@ where
         dir.normalize()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use nalgebra::{point, vector};
+
+    use crate::geometry::shapes::{Circle, Sphere};
+
+    use super::SurfaceNormal as _;
+
+    #[test]
+    fn normal_trait_should_be_implemented_for_2d_geometry() {
+        #[rustfmt::skip]
+        let expected = [
+            (point![-1.0,  0.0], vector![-1.0,  0.0]),
+            (point![ 1.0,  0.0], vector![ 1.0,  0.0]),
+            (point![ 0.0, -1.0], vector![ 0.0, -1.0]),
+            (point![ 0.0,  1.0], vector![ 0.0,  1.0]),
+        ];
+
+        let circle = Circle::new();
+        for (point, normal) in expected {
+            assert_eq!(circle.normal(point), normal);
+        }
+    }
+
+    #[test]
+    fn normal_trait_should_be_implemented_for_3d_geometry() {
+        #[rustfmt::skip]
+        let expected = [
+            (point![-1.0,  0.0,  0.0], vector![-1.0,  0.0,  0.0]),
+            (point![ 1.0,  0.0,  0.0], vector![ 1.0,  0.0,  0.0]),
+            (point![ 0.0, -1.0,  0.0], vector![ 0.0, -1.0,  0.0]),
+            (point![ 0.0,  1.0,  0.0], vector![ 0.0,  1.0,  0.0]),
+            (point![ 0.0,  0.0, -1.0], vector![ 0.0,  0.0, -1.0]),
+            (point![ 0.0,  0.0,  1.0], vector![ 0.0,  0.0,  1.0]),
+        ];
+
+        let sphere = Sphere::new();
+        for (point, normal) in expected {
+            assert_eq!(sphere.normal(point), normal);
+        }
+    }
+}
