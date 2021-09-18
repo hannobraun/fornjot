cad6bd23bd06c8217e9f99eab607fe891f0d6a80
Hanno Braun <hanno@braun-odw.eu>
2021-09-18 09:32:08 +0000

    Add shape being translated to `Translate`
    Seems obvious, but I still forgot it.

--- fj/src/geometry/operations/translate.rs
+++ fj/src/geometry/operations/translate.rs
@@ -4,7 +4,10 @@ use nalgebra::SVector;
 ///
 /// `D` defines the dimensionality of the translation. Typically, translations
 /// will be 1-, 2-, or 3-dimensional.
-pub struct Translate<const D: usize> {
+pub struct Translate<Shape, const D: usize> {
+    /// The shape being translated.
+    pub shape: Shape,
+
     /// The offset created by the translation
     pub offset: SVector<f32, D>,
 }
