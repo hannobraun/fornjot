9e7c7843fd578e0f802f1e8470db112980daca8c
Hanno Braun <hanno@braun-odw.eu>
2021-09-18 09:24:07 +0000

    Add `Vertex`

--- fj/src/geometry/shapes/mod.rs
+++ fj/src/geometry/shapes/mod.rs
@@ -2,8 +2,9 @@ pub mod circle;
 pub mod cylinder;
 pub mod hypersphere;
 pub mod sphere;
+pub mod vertex;
 
 pub use self::{
     circle::Circle, cylinder::Cylinder, hypersphere::Hypersphere,
-    sphere::Sphere,
+    sphere::Sphere, vertex::Vertex,
 };
---
+++ fj/src/geometry/shapes/vertex.rs
@@ -0,0 +1,6 @@
+/// A 0-dimensional vertex
+///
+/// By itself, this type is not very useful. You can apply meaning to it by
+/// using various operations on it, for example by sweeping it to create a line,
+/// or by giving it a position in space using a translation.
+pub struct Vertex;
