cf19b837b790905bc3ad703741643c1a904b7b46
Hanno Braun <hanno@braun-odw.eu>
2021-09-19 08:48:30 +0000

    Rename type parameter

--- fj/src/geometry/operations/translate.rs
+++ fj/src/geometry/operations/translate.rs
@@ -5,9 +5,9 @@ use nalgebra::SVector;
 /// `D` defines the dimensionality of the translation. Typically, translations
 /// will be 1-, 2-, or 3-dimensional.
 #[derive(Clone, Copy, Debug, PartialEq)]
-pub struct Translate<Shape, const D: usize> {
+pub struct Translate<T, const D: usize> {
     /// The shape being translated.
-    pub shape: Shape,
+    pub shape: T,
 
     /// The offset created by the translation
     pub offset: SVector<f32, D>,
