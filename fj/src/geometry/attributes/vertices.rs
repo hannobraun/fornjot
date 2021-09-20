9bb99b486a0f73cef20cf5801fe8d582cf892745
Hanno Braun <hanno@braun-odw.eu>
2021-09-20 17:42:57 +0000

    Implement `Vertices` for `Sweep`

--- fj/src/geometry/attributes/vertices.rs
+++ fj/src/geometry/attributes/vertices.rs
@@ -20,6 +20,22 @@ impl<const D: usize> Vertices<D> for shapes::Vertex {
     }
 }
 
+impl<T, const D: usize> Vertices<D> for operations::Sweep<T, SVector<f32, D>>
+where
+    T: Vertices<D>,
+{
+    fn vertices(&self) -> Vec<operations::Translate<shapes::Vertex, D>> {
+        let mut vertices = self.shape.vertices().clone();
+
+        for mut vertex in self.shape.vertices() {
+            vertex.offset += self.path;
+            vertices.push(vertex);
+        }
+
+        vertices
+    }
+}
+
 impl<T, const D: usize> Vertices<D> for operations::Translate<T, D>
 where
     T: Vertices<D>,
@@ -39,7 +55,10 @@ where
 mod tests {
     use nalgebra::vector;
 
-    use crate::{geometry::shapes::Vertex, prelude::*};
+    use crate::{
+        geometry::{operations::Translate, shapes::Vertex},
+        prelude::*,
+    };
 
     use super::Vertices;
 
@@ -60,6 +79,26 @@ mod tests {
     }
 
     #[test]
+    fn test_vertices_for_sweep() {
+        let vertices = MockVertices(vec![
+            Vertex.translate(vector![1., 2., 3.]),
+            Vertex.translate(vector![2., 3., 4.]),
+        ]);
+
+        let sweep = vertices.sweep(vector![3., 2., 1.]);
+
+        assert_eq!(
+            sweep.vertices(),
+            [
+                Vertex.translate(vector![1., 2., 3.]),
+                Vertex.translate(vector![2., 3., 4.]),
+                Vertex.translate(vector![4., 4., 4.]),
+                Vertex.translate(vector![5., 5., 5.]),
+            ],
+        );
+    }
+
+    #[test]
     fn test_vertices_for_translate_vertices() {
         let vertex_1d = Vertex.translate(vector![2.]);
         let vertex_2d = Vertex.translate(vector![2., 3.]);
@@ -68,5 +107,20 @@ mod tests {
         assert_eq!(vertex_1d.vertices(), [vertex_1d]);
         assert_eq!(vertex_2d.vertices(), [vertex_2d]);
         assert_eq!(vertex_3d.vertices(), [vertex_3d]);
+    }
+
+    struct MockVertices(Vec<Translate<Vertex, 3>>);
+
+    impl Vertices<3> for MockVertices {
+        fn vertices(
+            &self,
+        ) -> Vec<
+            crate::geometry::operations::Translate<
+                crate::geometry::shapes::Vertex,
+                3,
+            >,
+        > {
+            self.0.clone()
+        }
     }
 }
