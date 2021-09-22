a55f0832157e95889d10e467504912144941c766
Hanno Braun <hanno@braun-odw.eu>
2021-09-22 17:42:45 +0000

    Update module name

--- fj/src/geometry/shapes/mod.rs
+++ fj/src/geometry/shapes/mod.rs
@@ -1,14 +1,14 @@
 pub mod circle;
 pub mod cylinder;
+pub mod edge;
 pub mod hypersphere;
-pub mod line;
 pub mod parallelogram;
 pub mod sphere;
 pub mod toroid;
 pub mod vertex;
 
 pub use self::{
-    circle::Circle, cylinder::Cylinder, hypersphere::Hypersphere, line::Edge,
+    circle::Circle, cylinder::Cylinder, edge::Edge, hypersphere::Hypersphere,
     parallelogram::Parallelogram, sphere::Sphere, toroid::Toroid,
     vertex::Vertex,
 };
