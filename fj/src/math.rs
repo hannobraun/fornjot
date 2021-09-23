5d391538a16a7ba75df3ad44da47936f5b9af037
Hanno Braun <hanno@braun-odw.eu>
2021-09-23 16:42:42 +0000

    Add `math::Matrix`

--- fj/src/math.rs
+++ fj/src/math.rs
@@ -1,2 +1,8 @@
 pub type Point<const D: usize> = nalgebra::Point<f32, D>;
 pub type Vector<const D: usize> = nalgebra::SVector<f32, D>;
+pub type Matrix<const R: usize, const C: usize> = nalgebra::Matrix<
+    f32,
+    nalgebra::Const<R>,
+    nalgebra::Const<C>,
+    nalgebra::ArrayStorage<f32, R, C>,
+>;
