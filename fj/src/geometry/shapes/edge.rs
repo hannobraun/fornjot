7a158fd5e210518eda0902505dec440856a2cd42
Hanno Braun <hanno@braun-odw.eu>
2021-09-24 18:04:31 +0000

    Add `Edge::from_length`

--- fj/src/geometry/shapes/edge.rs
+++ fj/src/geometry/shapes/edge.rs
@@ -1,6 +1,17 @@
+use nalgebra::vector;
+
 use crate::{
     geometry::{operations::Sweep, shapes::Vertex},
     math::Vector,
 };
 
 pub type Edge = Sweep<Vertex, Vector<1>>;
+
+impl Edge {
+    pub fn from_length(length: f32) -> Self {
+        Sweep {
+            shape: Vertex,
+            path: vector![length],
+        }
+    }
+}
