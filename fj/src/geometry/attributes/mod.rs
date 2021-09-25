272ca2dd38a72054905dbc3080694132ec555d10
Hanno Braun <hanno@braun-odw.eu>
2021-09-25 10:06:03 +0000

    Update module name

--- fj/src/geometry/attributes/mod.rs
+++ fj/src/geometry/attributes/mod.rs
@@ -6,15 +6,15 @@
 pub mod bounding_volume;
 pub mod edges;
 pub mod signed_distance_field;
+pub mod surface_mesh;
 pub mod surface_normal;
-pub mod triangle_mesh;
 pub mod vertices;
 
 pub use self::{
     bounding_volume::{Aabb, BoundingVolume},
     edges::Edges,
     signed_distance_field::{Distance, SignedDistanceField},
+    surface_mesh::{Mesh, SurfaceMesh},
     surface_normal::SurfaceNormal,
-    triangle_mesh::{Mesh, SurfaceMesh},
     vertices::Vertices,
 };
