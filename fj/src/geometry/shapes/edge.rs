222a8d043de58702f644a1f1dd1083f8036e0bec
Hanno Braun <hanno@braun-odw.eu>
2021-09-27 20:27:30 +0000

    Add alternative API for constructinr `Edge`s

--- fj/src/geometry/shapes/edge.rs
+++ fj/src/geometry/shapes/edge.rs
@@ -8,6 +8,24 @@ use crate::{
 pub type Edge = Sweep<Vertex, Vector<1>>;
 
 impl Edge {
+    /// Create a new `Edge`
+    ///
+    /// The length is initially set to `1.0`.
+    pub fn new() -> Self {
+        Sweep {
+            shape: Vertex,
+            path: vector![1.0],
+        }
+    }
+
+    /// Update length
+    ///
+    /// Returns a copy of `self`, with the length replaced with `length`.
+    pub fn with_length(mut self, length: f32) -> Self {
+        self.path.x = length;
+        self
+    }
+
     pub fn from_length(length: f32) -> Self {
         Sweep {
             shape: Vertex,
