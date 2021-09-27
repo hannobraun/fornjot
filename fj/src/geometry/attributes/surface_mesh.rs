2e3304cb4391687a86c8716cf9f57beca4796077
Hanno Braun <hanno@braun-odw.eu>
2021-09-27 20:45:49 +0000

    Update `SurfaceMesh` to enable approximation

--- fj/src/geometry/attributes/surface_mesh.rs
+++ fj/src/geometry/attributes/surface_mesh.rs
@@ -3,5 +3,9 @@ use crate::geometry::shapes::Mesh;
 /// Compute a triangle mesh that approximates a shape's surface
 pub trait SurfaceMesh<const D: usize> {
     /// Compute surface mesh for shape
-    fn mesh(&self) -> Mesh<D>;
+    ///
+    /// If the surface mesh can only be approximated, `n` defines the number of
+    /// computational steps taken to compute the mesh.
+    // TASK: Replace `n` with a tolerance value.
+    fn mesh(&self, n: u32) -> Mesh<D>;
 }
