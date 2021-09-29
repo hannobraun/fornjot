fbea2f3c640840cde93f9b81e833a77290330515
Hanno Braun <hanno@braun-odw.eu>
2021-09-29 18:08:21 +0000

    Add WIP implementation of `SurfaceMesh`

--- fj/src/geometry/attributes/surface_mesh.rs
+++ fj/src/geometry/attributes/surface_mesh.rs
@@ -1,4 +1,7 @@
-use crate::geometry::shapes::Mesh;
+use crate::geometry::{
+    attributes::Edges,
+    shapes::{Mesh, Toroid},
+};
 
 /// Compute a triangle mesh that approximates a shape's surface
 pub trait SurfaceMesh<const D: usize> {
@@ -8,4 +11,14 @@ pub trait SurfaceMesh<const D: usize> {
     /// computational steps taken to compute the mesh.
     // TASK: Replace `n` with a tolerance value.
     fn surface_mesh(&self, n: u32) -> Mesh<D>;
+}
+
+impl<T> SurfaceMesh<3> for Toroid<T>
+where
+    T: Edges<2>,
+{
+    fn surface_mesh(&self, _n: u32) -> Mesh<3> {
+        // TASK: Implement.
+        todo!()
+    }
 }
