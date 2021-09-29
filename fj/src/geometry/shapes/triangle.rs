5306deba5f20e298e9e5e5953ed647ad11f08a26
Hanno Braun <hanno@braun-odw.eu>
2021-09-29 18:12:25 +0000

    Add placeholder method

--- fj/src/geometry/shapes/triangle.rs
+++ fj/src/geometry/shapes/triangle.rs
@@ -1,3 +1,14 @@
 use crate::math::Point;
 
 pub struct Triangle<const D: usize>(pub [Point<D>; 3]);
+
+impl<const D: usize> Triangle<D> {
+    /// Normalize the triangle
+    ///
+    /// Given two triangles that contain the same points, calling this method on
+    /// them will return the same triangle.
+    pub fn normalize(self) -> Self {
+        // TASK: Implement.
+        todo!()
+    }
+}
