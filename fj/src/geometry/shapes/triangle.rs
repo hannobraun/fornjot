1abfe8139dded813acb641c04ece233e92059aa7
Hanno Braun <hanno@braun-odw.eu>
2021-09-30 18:34:56 +0000

    Add `Triangle::new`

--- fj/src/geometry/shapes/triangle.rs
+++ fj/src/geometry/shapes/triangle.rs
@@ -3,6 +3,13 @@ use crate::math::Point;
 pub struct Triangle<const D: usize>(pub [Point<D>; 3]);
 
 impl<const D: usize> Triangle<D> {
+    /// Create a new `Triangle`
+    pub fn new(points: [Point<D>; 3]) -> Self {
+        // TASK: Make sure that points form a triangle.
+        // TASK: Normalize triangle.
+        Self(points)
+    }
+
     /// Normalize the triangle
     ///
     /// Given two triangles that contain the same points, calling this method on
