047d0e7c23d5f9e2ac468181d5d4e26758c0a0bd
Hanno Braun <hanno@braun-odw.eu>
2021-10-01 15:15:57 +0000

    Make sure triangle points are not collapsed

--- fj/src/geometry/shapes/triangle.rs
+++ fj/src/geometry/shapes/triangle.rs
@@ -5,6 +5,10 @@ pub struct Triangle<const D: usize>([Point<D>; 3]);
 impl<const D: usize> Triangle<D> {
     /// Create a new `Triangle`
     pub fn new([a, b, c]: [Point<D>; 3]) -> Option<Self> {
+        if a == b || a == c || b == c {
+            return None;
+        }
+
         // TASK: Make sure that points form a triangle.
         // TASK: Normalize triangle.
         Some(Self([a, b, c]))
@@ -26,7 +30,10 @@ mod tests {
     fn test_triangle_new() {
         let triangle =
             Triangle::new([point![0., 0.], point![0., 1.], point![1., 1.]]);
+        let collapsed_points =
+            Triangle::new([point![0., 0.], point![1., 1.], point![1., 1.]]);
 
         assert!(triangle.is_some());
+        assert!(collapsed_points.is_none());
     }
 }
