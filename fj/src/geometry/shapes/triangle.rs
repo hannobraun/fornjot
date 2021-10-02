18a3c4757e3f775d0fd3eaaffc9f26b32b30a785
Hanno Braun <hanno@braun-odw.eu>
2021-10-02 07:51:27 +0000

    Return `Result` from `Triangle::new`

--- fj/src/geometry/shapes/triangle.rs
+++ fj/src/geometry/shapes/triangle.rs
@@ -4,22 +4,27 @@ pub struct Triangle<const D: usize>([Point<D>; 3]);
 
 impl<const D: usize> Triangle<D> {
     /// Create a new `Triangle`
-    pub fn new(a: Point<D>, b: Point<D>, c: Point<D>) -> Option<Self> {
+    pub fn new(a: Point<D>, b: Point<D>, c: Point<D>) -> Result<Self, Error> {
         if a == b || a == c || b == c {
-            return None;
+            return Err(Error::CollapsedPoints);
         }
         if (b - a).normalize() == (c - b).normalize() {
-            return None;
+            return Err(Error::IsALineSegment);
         }
 
         // TASK: Normalize triangle.
-        Some(Self([a, b, c]))
+        Ok(Self([a, b, c]))
     }
 
     /// Return the points of the triangle
     pub fn points(&self) -> [Point<D>; 3] {
         self.0
     }
+}
+
+pub enum Error {
+    CollapsedPoints,
+    IsALineSegment,
 }
 
 #[cfg(test)]
@@ -37,8 +42,8 @@ mod tests {
         let collapsed_points =
             Triangle::new(point![0., 0.], point![1., 1.], point![1., 1.]);
 
-        assert!(triangle.is_some());
-        assert!(points_on_a_line.is_none());
-        assert!(collapsed_points.is_none());
+        assert!(triangle.is_ok());
+        assert!(points_on_a_line.is_err());
+        assert!(collapsed_points.is_err());
     }
 }
