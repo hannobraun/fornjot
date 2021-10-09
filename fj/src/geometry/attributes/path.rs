336ad9a7c0417957cc15cc813cb1e132b38fa7ef
Hanno Braun <hanno@braun-odw.eu>
2021-10-09 07:49:38 +0000

    Implement `Path` for `Vector` variants

--- fj/src/geometry/attributes/path.rs
+++ fj/src/geometry/attributes/path.rs
@@ -1,3 +1,5 @@
+use nalgebra::vector;
+
 use crate::math::Vector;
 
 /// Shapes that describe a path
@@ -5,4 +7,40 @@ use crate::math::Vector;
 /// `D` defines the dimension that the path is described in.
 pub trait Path<const D: usize> {
     fn path(&self) -> Vector<D>;
+}
+
+impl Path<1> for Vector<1> {
+    fn path(&self) -> Vector<1> {
+        *self
+    }
+}
+
+impl Path<2> for Vector<1> {
+    fn path(&self) -> Vector<2> {
+        vector![self.x, 0.0]
+    }
+}
+
+impl Path<3> for Vector<1> {
+    fn path(&self) -> Vector<3> {
+        vector![self.x, 0.0, 0.0]
+    }
+}
+
+impl Path<2> for Vector<2> {
+    fn path(&self) -> Vector<2> {
+        *self
+    }
+}
+
+impl Path<3> for Vector<2> {
+    fn path(&self) -> Vector<3> {
+        vector![self.x, self.y, 0.0]
+    }
+}
+
+impl Path<3> for Vector<3> {
+    fn path(&self) -> Vector<3> {
+        *self
+    }
 }
