9c9f4cc06848cc80e29d0e8bf55e4ee15cb0ea49
Hanno Braun <hanno@braun-odw.eu>
2021-10-10 12:15:15 +0000

    Add placeholder methods to support curved paths

--- fj/src/geometry/attributes/path.rs
+++ fj/src/geometry/attributes/path.rs
@@ -1,20 +1,41 @@
 use nalgebra::vector;
 
-use crate::math::Vector;
+use crate::math::{Point, Vector};
 
 /// Shapes that describe a path
 ///
 /// `D` defines the dimension that the path is described in.
 pub trait Path<const D: usize> {
-    // TASK: Extend this interface, so it can represent arbitrary curved paths.
-    //       Idea:
-    //       - `fn point_at(&self, s: f32) -> Point<D>; // 0.0 <= s <= 1.0`
-    //       - `fn next_s(&self, tolerance: f32) -> f32;`
-
     /// Return the vector that describes the path
     ///
     /// Only straight paths are supported right now.
     fn path(&self) -> Vector<D>;
+
+    /// Compute a point along the path
+    ///
+    /// `s` defines which point along the path should be computed. Typically, a
+    /// value of `0.0` defines the beginning of the path, `1.0` its end.
+    /// Implementations are allowed to return points for other values, too.
+    fn point_at(&self, _s: f32) -> Point<D> {
+        // TASK: Remove this default implementation.
+        todo!()
+    }
+
+    /// Compute the next point along the path
+    ///
+    /// Returns a float value that describes the point along the path, as well
+    /// as the point itself.
+    ///
+    /// Given a point `p1`, described by `from_s`, and a point `p2` returned by
+    /// this function, the following must hold for any point `p` on the path
+    /// between `p1` and `p2`:
+    ///
+    /// The distance of `p` to the line segment whose end points are `p1` and
+    /// `p2` must by equal to or smaller than `tolerance`.
+    fn next_point(&self, _from_s: f32, _tolerance: f32) -> (f32, Point<D>) {
+        // TASK: Remove this default implementation.
+        todo!()
+    }
 }
 
 impl Path<1> for Vector<1> {
