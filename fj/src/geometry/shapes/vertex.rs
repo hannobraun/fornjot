196275343748d562f78c9db872950092802634c0
Hanno Braun <hanno@braun-odw.eu>
2021-10-12 19:42:57 +0000

    Add method for better displaying vertices

--- fj/src/geometry/shapes/vertex.rs
+++ fj/src/geometry/shapes/vertex.rs
@@ -1,3 +1,7 @@
+use std::fmt;
+
+use crate::geometry::operations::Translate;
+
 /// A 0-dimensional vertex
 ///
 /// By itself, this type is not very useful. You can apply meaning to it by
@@ -5,3 +9,9 @@
 /// or by giving it a position in space using a translation.
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Vertex;
+
+impl<const D: usize> Translate<Vertex, D> {
+    pub fn display(&self) -> impl fmt::Display {
+        self.offset
+    }
+}
