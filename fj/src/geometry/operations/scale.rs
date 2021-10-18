a893bf5084f547c6e1040b216bb37b61fa5af952
Hanno Braun <hanno@braun-odw.eu>
2021-10-18 20:43:04 +0000

    Add task

--- fj/src/geometry/operations/scale.rs
+++ fj/src/geometry/operations/scale.rs
@@ -4,6 +4,8 @@ pub struct Scale<T> {
     /// The shape to scale
     pub shape: T,
 
+    // TASK: Convert this into an n-dimensional vector, to enable scaling the
+    //       shape along each dimension.
     /// The factor to scale the shape by
     pub factor: f32,
 }
