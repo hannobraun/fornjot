0224a45e957889da4721eb518fe4ef6a534ba82c
Hanno Braun <hanno@braun-odw.eu>
2021-10-18 20:42:29 +0000

    Add documentation

--- fj/src/geometry/operations/scale.rs
+++ fj/src/geometry/operations/scale.rs
@@ -1,6 +1,9 @@
 /// Scales a shape
 #[derive(Clone, Copy, Debug, PartialEq)]
 pub struct Scale<T> {
+    /// The shape to scale
     pub shape: T,
+
+    /// The factor to scale the shape by
     pub factor: f32,
 }
