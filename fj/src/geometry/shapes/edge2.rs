ae3d5313554472af1badec79c12120126f87eb35
Hanno Braun <hanno@braun-odw.eu>
2021-10-20 17:55:33 +0000

    Choose more compact definition for `Edge2`

--- fj/src/geometry/shapes/edge2.rs
+++ fj/src/geometry/shapes/edge2.rs
@@ -1,4 +1,4 @@
 use crate::geometry::{operations::Translate, shapes::Vertex};
 
 /// An edge, defined by two vertices
-pub type Edge2<const D: usize> = (Translate<Vertex, D>, Translate<Vertex, D>);
+pub type Edge2<const D: usize> = [Translate<Vertex, D>; 2];
