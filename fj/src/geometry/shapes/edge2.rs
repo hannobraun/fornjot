2a52bd07b8ab5252757b743ad55b4668c0bc4c62
Hanno Braun <hanno@braun-odw.eu>
2021-10-20 17:55:13 +0000

    Fix typo

--- fj/src/geometry/shapes/edge2.rs
+++ fj/src/geometry/shapes/edge2.rs
@@ -1,4 +1,4 @@
 use crate::geometry::{operations::Translate, shapes::Vertex};
 
-/// An edge, defines by two vertices
+/// An edge, defined by two vertices
 pub type Edge2<const D: usize> = (Translate<Vertex, D>, Translate<Vertex, D>);
