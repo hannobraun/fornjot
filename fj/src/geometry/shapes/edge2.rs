6a0090088d4d2fa9c5de437684332b8a9ac844d9
Hanno Braun <hanno@braun-odw.eu>
2021-10-21 17:52:28 +0000

    Implement conversion from vertex array to `Edge2`

--- fj/src/geometry/shapes/edge2.rs
+++ fj/src/geometry/shapes/edge2.rs
@@ -2,3 +2,12 @@ use crate::geometry::{operations::Translate, shapes::Vertex};
 
 /// An edge, defined by two vertices
 pub struct Edge2<const D: usize>(pub [Translate<Vertex, D>; 2]);
+
+impl<T, const D: usize> From<[T; 2]> for Edge2<D>
+where
+    T: Into<Translate<Vertex, D>>,
+{
+    fn from(vertices: [T; 2]) -> Self {
+        Self(vertices.map(|vertex| vertex.into()))
+    }
+}
