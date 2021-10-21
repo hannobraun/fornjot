8ed3823d21ef3e6e81860a522cd37d7705206858
Hanno Braun <hanno@braun-odw.eu>
2021-10-21 18:41:46 +0000

    Implement `SurfaceMesh` for `Toroid`

--- fj/src/geometry/attributes/surface_mesh.rs
+++ fj/src/geometry/attributes/surface_mesh.rs
@@ -1,6 +1,13 @@
-use crate::geometry::{
-    attributes::Edges2,
-    shapes::{Mesh, Toroid},
+use std::f32::consts::PI;
+
+use nalgebra::{point, vector, Rotation3};
+
+use crate::{
+    geometry::{
+        attributes::{Edges2, Vertices as _},
+        shapes::{Mesh, Toroid},
+    },
+    Triangle,
 };
 
 /// Compute a triangle mesh that approximates a shape's surface
@@ -17,9 +24,44 @@ impl<T> SurfaceMesh<3> for Toroid<T>
 where
     T: Edges2<2>,
 {
-    fn surface_mesh(&self, _n: u32) -> Mesh<3> {
-        // TASK: Implement.
-        todo!()
+    fn surface_mesh(&self, n: u32) -> Mesh<3> {
+        let edges = self.shape.edges();
+
+        let mut mesh = Mesh::new();
+        let mut angle = 0.;
+
+        for _ in 0..n {
+            let next_angle = angle + PI * 2. / n as f32;
+
+            let rot_curr = Rotation3::new(vector![0., angle, 0.]);
+            let rot_next = Rotation3::new(vector![0., next_angle, 0.]);
+
+            for edge in &edges {
+                let vertices = edge.vertices();
+
+                // An edge has exactly two vertices, so this isn't going to
+                // panic.
+                let v1 = vertices[0].offset;
+                let v2 = vertices[1].offset;
+
+                let v1 = point![v1.x, v1.y, 0.];
+                let v2 = point![v2.x, v2.y, 0.];
+
+                let a = rot_curr.transform_point(&v1);
+                let b = rot_curr.transform_point(&v2);
+                let c = rot_next.transform_point(&v2);
+                let d = rot_next.transform_point(&v1);
+
+                // TASK: Decide how to handle panics. An inner point could be on
+                //       the origin, then rotating it would do nothing.
+                mesh.triangle(Triangle::from_points([a, b, c]).unwrap());
+                mesh.triangle(Triangle::from_points([a, c, d]).unwrap());
+            }
+
+            angle = next_angle;
+        }
+
+        mesh
     }
 }
 
@@ -34,6 +76,7 @@ mod tests {
 
     use super::SurfaceMesh;
 
+    // TASK: Fix and un-ignore this test.
     #[test]
     #[ignore]
     fn triangle_mesh_for_toroid() {
