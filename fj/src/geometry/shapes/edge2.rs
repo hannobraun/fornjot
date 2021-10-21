dcfa9c829dfea8b56d10c6290e5994ec1e2b8e2e
Hanno Braun <hanno@braun-odw.eu>
2021-10-21 18:29:24 +0000

    Derive `Debug` for `Edge2`

--- fj/src/geometry/shapes/edge2.rs
+++ fj/src/geometry/shapes/edge2.rs
@@ -1,6 +1,7 @@
 use crate::geometry::{operations::Translate, shapes::Vertex};
 
 /// An edge, defined by two vertices
+#[derive(Debug)]
 pub struct Edge2<const D: usize>(pub [Translate<Vertex, D>; 2]);
 
 impl<T, const D: usize> From<[T; 2]> for Edge2<D>
