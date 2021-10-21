d19f63874adca109e4504bacc27ab4d1588c3754
Hanno Braun <hanno@braun-odw.eu>
2021-10-21 18:06:38 +0000

    Implement `Edges2` for `Polygon`

--- fj/src/geometry/attributes/edges2.rs
+++ fj/src/geometry/attributes/edges2.rs
@@ -1,4 +1,4 @@
-use crate::geometry::shapes::Edge2;
+use crate::geometry::shapes::{self, Edge2};
 
 /// The edges that make up a shape
 ///
@@ -6,4 +6,24 @@ use crate::geometry::shapes::Edge2;
 pub trait Edges2<const D: usize> {
     /// Return the edges of the shape
     fn edges(&self) -> Vec<Edge2<D>>;
+}
+
+impl<const D: usize, const N: usize> Edges2<D> for shapes::Polygon<D, N> {
+    fn edges(&self) -> Vec<Edge2<D>> {
+        let mut edges = Vec::new();
+
+        edges.extend(self.points().windows(2).map(|window| {
+            let a = window[0];
+            let b = window[0];
+
+            Edge2::from([a, b])
+        }));
+
+        edges.push(Edge2::from([
+            *self.points().last().unwrap(),
+            *self.points().first().unwrap(),
+        ]));
+
+        edges
+    }
 }
