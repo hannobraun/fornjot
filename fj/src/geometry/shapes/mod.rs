15b3cb063fb9ee381f31311bff5b5a1e815bbcb8
Hanno Braun <hanno@braun-odw.eu>
2021-10-22 16:24:34 +0000

    Remove `Parallelogram`

--- fj/src/geometry/shapes/mod.rs
+++ fj/src/geometry/shapes/mod.rs
@@ -4,7 +4,6 @@ pub mod edge;
 pub mod edge2;
 pub mod hypersphere;
 pub mod mesh;
-pub mod parallelogram;
 pub mod polygon;
 pub mod quad;
 pub mod sphere;
@@ -14,7 +13,6 @@ pub mod vertex;
 
 pub use self::{
     circle::Circle, cylinder::Cylinder, edge::Edge, edge2::Edge2,
-    hypersphere::Hypersphere, mesh::Mesh, parallelogram::Parallelogram,
-    polygon::Polygon, quad::Quad, sphere::Sphere, toroid::Toroid,
-    triangle::Triangle, vertex::Vertex,
+    hypersphere::Hypersphere, mesh::Mesh, polygon::Polygon, quad::Quad,
+    sphere::Sphere, toroid::Toroid, triangle::Triangle, vertex::Vertex,
 };
--- fj/src/geometry/shapes/parallelogram.rs
+++
@@ -1,6 +0,0 @@
-use crate::{
-    geometry::{operations::Sweep, shapes::Edge},
-    math::Vector,
-};
-
-pub type Parallelogram = Sweep<Edge, Vector<2>>;
