25ac2e4f9de6bf34f4bb77a9afabd00a9e48fa6c
Hanno Braun <hanno@braun-odw.eu>
2021-10-23 08:49:36 +0000

    Rename module

--- fj/src/threemf/mod.rs
+++ fj/src/threemf/mod.rs
@@ -15,7 +15,7 @@
 //! [3MF specification]: https://3mf.io/specification/
 //! [Open Packaging Conventions]: https://standards.iso.org/ittf/PubliclyAvailableStandards/c061796_ISO_IEC_29500-2_2012.zip
 
-pub mod export;
 pub mod mesh;
+pub mod write;
 
-pub use self::{export::export, mesh::TriangleMesh};
+pub use self::{mesh::TriangleMesh, write::export};
