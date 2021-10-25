4e7a488ef4e07541d44b24aac31f195f43ad9e0d
Hanno Braun <hanno@braun-odw.eu>
2021-10-25 19:24:36 +0000

    Add `operations::Rotate`

--- fj/src/geometry/operations/mod.rs
+++ fj/src/geometry/operations/mod.rs
@@ -1,10 +1,11 @@
 pub mod difference;
+pub mod rotate;
 pub mod scale;
 pub mod sweep;
 pub mod transform;
 pub mod translate;
 
 pub use self::{
-    difference::Difference, scale::Scale, sweep::Sweep, transform::Transform,
-    translate::Translate,
+    difference::Difference, rotate::Rotate, scale::Scale, sweep::Sweep,
+    transform::Transform, translate::Translate,
 };
