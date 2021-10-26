2a58f4e226a96f170d885f1d0f964cb4198887cc
Hanno Braun <hanno@braun-odw.eu>
2021-10-26 17:28:55 +0000

    Rotate spacer

--- models/spacer2/src/main.rs
+++ models/spacer2/src/main.rs
@@ -1,4 +1,7 @@
-use fj::geometry::attributes::SurfaceMesh as _;
+use std::f32::consts::FRAC_PI_2;
+
+use fj::{geometry::attributes::SurfaceMesh as _, prelude::*};
+use nalgebra::{vector, Rotation3};
 
 fn main() -> anyhow::Result<()> {
     let outer = 50.0;
@@ -12,6 +15,8 @@ fn main() -> anyhow::Result<()> {
         [inner, 0.0],
     ])?;
     let spacer = fj::Toroid::from_shape(cross_section);
+
+    let spacer = spacer.rotate(Rotation3::new(vector![FRAC_PI_2, 0., 0.]));
 
     fj::run_mesh(spacer.surface_mesh(360))
 }
