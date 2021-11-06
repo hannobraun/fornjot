9c002c845199eb7a189d7aef7ed5d0773e3122b7
Hanno Braun <hanno@braun-odw.eu>
2021-11-06 10:58:56 +0100

    Refactor

--- fj-ng/src/main.rs
+++ fj-ng/src/main.rs
@@ -1,10 +1,11 @@
 fn main() -> anyhow::Result<()> {
     unsafe {
         let lib = libloading::Library::new("model/target/debug/libmodel.so")?;
-        let func: libloading::Symbol<unsafe extern "C" fn()> =
-            lib.get(b"model")?;
+        let func: libloading::Symbol<ModelFn> = lib.get(b"model")?;
         func()
     }
 
     Ok(())
 }
+
+type ModelFn = unsafe extern "C" fn();
