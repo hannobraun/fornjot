d23287ab1da463372d5d845b0dec163a1cab01ab
Hanno Braun <hanno@braun-odw.eu>
2021-11-06 10:56:56 +0100

    Move host one directory up

--- fj-ng/src/main.rs
+++ fj-ng/src/main.rs
@@ -1,7 +1,6 @@
 fn main() -> anyhow::Result<()> {
     unsafe {
-        let lib =
-            libloading::Library::new("../model/target/debug/libmodel.so")?;
+        let lib = libloading::Library::new("model/target/debug/libmodel.so")?;
         let func: libloading::Symbol<unsafe extern "C" fn()> =
             lib.get(b"model")?;
         func()
