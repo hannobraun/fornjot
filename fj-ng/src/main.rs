69ed7912f175883b303bbbef9c743c52004e693d
Hanno Braun <hanno@braun-odw.eu>
2021-11-06 11:15:20 +0100

    Compile model before loading it

--- fj-ng/src/main.rs
+++ fj-ng/src/main.rs
@@ -1,4 +1,14 @@
+use std::process::Command;
+
 fn main() -> anyhow::Result<()> {
+    // This can be made a bit more contact using `ExitStatus::exit_ok`, once
+    // that is stable.
+    let status = Command::new("cargo")
+        .arg("build")
+        .args(["--manifest-path", "model/Cargo.toml"])
+        .status()?;
+    assert!(status.success());
+
     unsafe {
         let lib = libloading::Library::new("model/target/debug/libmodel.so")?;
         let func: libloading::Symbol<ModelFn> = lib.get(b"model")?;
