02f32272772aac89ae3580ccda934c8766012f13
Hanno Braun <hanno@braun-odw.eu>
2021-11-10 20:21:08 +0100

    Add dedicated function for creating triangle mesh

--- ng/src/main.rs
+++ ng/src/main.rs
@@ -1,3 +1,4 @@
+mod geometry;
 mod graphics;
 mod input;
 mod mesh;
@@ -12,10 +13,7 @@ use winit::{
     window::WindowBuilder,
 };
 
-use self::{
-    graphics::{DrawConfig, Renderer, Transform},
-    mesh::MeshMaker,
-};
+use self::graphics::{DrawConfig, Renderer, Transform};
 
 fn main() -> anyhow::Result<()> {
     // This can be made a bit more contact using `ExitStatus::exit_ok`, once
@@ -34,46 +32,7 @@ fn main() -> anyhow::Result<()> {
         func()
     };
 
-    let fj::Shape::Cube(cube) = model;
-
-    let mut mesh = MeshMaker::new();
-    let s = cube.size / 2.;
-
-    // Define a cube
-    let v0 = [-s, -s, -s];
-    let v1 = [-s, -s, s];
-    let v2 = [-s, s, -s];
-    let v3 = [-s, s, s];
-    let v4 = [s, -s, -s];
-    let v5 = [s, -s, s];
-    let v6 = [s, s, -s];
-    let v7 = [s, s, s];
-
-    // left
-    mesh.triangle([v0, v1, v2]);
-    mesh.triangle([v2, v1, v3]);
-
-    // right
-    mesh.triangle([v4, v6, v5]);
-    mesh.triangle([v6, v7, v5]);
-
-    // front
-    mesh.triangle([v0, v4, v1]);
-    mesh.triangle([v4, v5, v1]);
-
-    // back
-    mesh.triangle([v2, v3, v6]);
-    mesh.triangle([v6, v3, v7]);
-
-    // bottom
-    mesh.triangle([v0, v2, v6]);
-    mesh.triangle([v0, v6, v4]);
-
-    // top
-    mesh.triangle([v1, v5, v7]);
-    mesh.triangle([v1, v7, v3]);
-
-    let mesh = mesh.make();
+    let mesh = geometry::to_triangle_mesh(model);
 
     let event_loop = EventLoop::new();
     let window = WindowBuilder::new()
