e80568ea8357ce8daf6503100d6b74914f2ef5f1
Hanno Braun <hanno@braun-odw.eu>
2021-11-11 17:59:28 +0100

    Refactor

--- ng/src/geometry/to_mesh.rs
+++ ng/src/geometry/to_mesh.rs
@@ -14,7 +14,44 @@ impl ToMesh for fj::Shape {
 
 impl ToMesh for fj::Cube {
     fn to_mesh(&self) -> Mesh {
-        cube_to_triangle_mesh(self)
+        let mut mesh = MeshMaker::new();
+        let s = self.size / 2.;
+
+        // Define a cube
+        let v0 = [-s, -s, -s];
+        let v1 = [-s, -s, s];
+        let v2 = [-s, s, -s];
+        let v3 = [-s, s, s];
+        let v4 = [s, -s, -s];
+        let v5 = [s, -s, s];
+        let v6 = [s, s, -s];
+        let v7 = [s, s, s];
+
+        // left
+        mesh.triangle([v0, v1, v2]);
+        mesh.triangle([v2, v1, v3]);
+
+        // right
+        mesh.triangle([v4, v6, v5]);
+        mesh.triangle([v6, v7, v5]);
+
+        // front
+        mesh.triangle([v0, v4, v1]);
+        mesh.triangle([v4, v5, v1]);
+
+        // back
+        mesh.triangle([v2, v3, v6]);
+        mesh.triangle([v6, v3, v7]);
+
+        // bottom
+        mesh.triangle([v0, v2, v6]);
+        mesh.triangle([v0, v6, v4]);
+
+        // top
+        mesh.triangle([v1, v5, v7]);
+        mesh.triangle([v1, v7, v3]);
+
+        mesh.make()
     }
 }
 
@@ -22,45 +59,4 @@ pub fn shape_to_triangle_mesh(shape: &fj::Shape) -> Mesh {
     match shape {
         fj::Shape::Cube(cube) => cube.to_mesh(),
     }
-}
-
-fn cube_to_triangle_mesh(cube: &fj::Cube) -> Mesh {
-    let mut mesh = MeshMaker::new();
-    let s = cube.size / 2.;
-
-    // Define a cube
-    let v0 = [-s, -s, -s];
-    let v1 = [-s, -s, s];
-    let v2 = [-s, s, -s];
-    let v3 = [-s, s, s];
-    let v4 = [s, -s, -s];
-    let v5 = [s, -s, s];
-    let v6 = [s, s, -s];
-    let v7 = [s, s, s];
-
-    // left
-    mesh.triangle([v0, v1, v2]);
-    mesh.triangle([v2, v1, v3]);
-
-    // right
-    mesh.triangle([v4, v6, v5]);
-    mesh.triangle([v6, v7, v5]);
-
-    // front
-    mesh.triangle([v0, v4, v1]);
-    mesh.triangle([v4, v5, v1]);
-
-    // back
-    mesh.triangle([v2, v3, v6]);
-    mesh.triangle([v6, v3, v7]);
-
-    // bottom
-    mesh.triangle([v0, v2, v6]);
-    mesh.triangle([v0, v6, v4]);
-
-    // top
-    mesh.triangle([v1, v5, v7]);
-    mesh.triangle([v1, v7, v3]);
-
-    mesh.make()
 }
