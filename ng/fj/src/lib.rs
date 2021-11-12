933bd0246a53ecbe89d47256f707b2c76eb81009
Hanno Braun <hanno@braun-odw.eu>
2021-11-12 21:25:52 +0100

    Re-order code

--- ng/fj/src/lib.rs
+++ ng/fj/src/lib.rs
@@ -5,26 +5,11 @@ pub enum Shape {
     Shape3d(Shape3d),
 }
 
-impl<T> From<T> for Shape
-where
-    T: Into<Shape3d>,
-{
-    fn from(shape: T) -> Self {
-        Self::Shape3d(shape.into())
-    }
-}
-
 /// A 2-dimensional shape
 #[derive(Debug)]
 #[repr(C)]
 pub enum Shape2d {
     Square(Square),
-}
-
-impl From<Square> for Shape2d {
-    fn from(square: Square) -> Self {
-        Self::Square(square)
-    }
 }
 
 /// A 3-dimensional shape
@@ -35,9 +20,12 @@ pub enum Shape3d {
     Cube(Cube),
 }
 
-impl From<Cube> for Shape3d {
-    fn from(cube: Cube) -> Self {
-        Self::Cube(cube)
+impl<T> From<T> for Shape
+where
+    T: Into<Shape3d>,
+{
+    fn from(shape: T) -> Self {
+        Self::Shape3d(shape.into())
     }
 }
 
@@ -49,10 +37,22 @@ pub struct Square {
     pub size: f32,
 }
 
+impl From<Square> for Shape2d {
+    fn from(square: Square) -> Self {
+        Self::Square(square)
+    }
+}
+
 /// A cube
 #[derive(Debug)]
 #[repr(C)]
 pub struct Cube {
     /// The side length of the cube
     pub size: f32,
+}
+
+impl From<Cube> for Shape3d {
+    fn from(cube: Cube) -> Self {
+        Self::Cube(cube)
+    }
 }
