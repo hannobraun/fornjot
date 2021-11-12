1257a9d04c20617c87b89b84d20b1caae24fde44
Hanno Braun <hanno@braun-odw.eu>
2021-11-12 21:55:06 +0100

    Add `shape_3d` module

--- ng/fj/src/lib.rs
+++ ng/fj/src/lib.rs
@@ -1,6 +1,7 @@
 mod shape_2d;
+mod shape_3d;
 
-pub use self::shape_2d::*;
+pub use self::{shape_2d::*, shape_3d::*};
 
 /// A shape
 #[derive(Debug)]
@@ -8,38 +9,4 @@ pub use self::shape_2d::*;
 pub enum Shape {
     Shape2d(Shape2d),
     Shape3d(Shape3d),
-}
-
-/// A 3-dimensional shape
-#[derive(Debug)]
-#[repr(C)]
-pub enum Shape3d {
-    /// A cube
-    Cube(Cube),
-}
-
-impl From<Shape3d> for Shape {
-    fn from(shape: Shape3d) -> Self {
-        Self::Shape3d(shape.into())
-    }
-}
-
-/// A cube
-#[derive(Debug)]
-#[repr(C)]
-pub struct Cube {
-    /// The side length of the cube
-    pub size: f32,
-}
-
-impl From<Cube> for Shape {
-    fn from(shape: Cube) -> Self {
-        Self::Shape3d(Shape3d::Cube(shape))
-    }
-}
-
-impl From<Cube> for Shape3d {
-    fn from(shape: Cube) -> Self {
-        Self::Cube(shape)
-    }
 }
---
+++ ng/fj/src/shape_3d.rs
@@ -0,0 +1,35 @@
+use crate::Shape;
+
+/// A 3-dimensional shape
+#[derive(Debug)]
+#[repr(C)]
+pub enum Shape3d {
+    /// A cube
+    Cube(Cube),
+}
+
+impl From<Shape3d> for Shape {
+    fn from(shape: Shape3d) -> Self {
+        Self::Shape3d(shape.into())
+    }
+}
+
+/// A cube
+#[derive(Debug)]
+#[repr(C)]
+pub struct Cube {
+    /// The side length of the cube
+    pub size: f32,
+}
+
+impl From<Cube> for Shape {
+    fn from(shape: Cube) -> Self {
+        Self::Shape3d(Shape3d::Cube(shape))
+    }
+}
+
+impl From<Cube> for Shape3d {
+    fn from(shape: Cube) -> Self {
+        Self::Cube(shape)
+    }
+}
