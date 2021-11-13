2c790bb94bb8da34f33d547e1a01d50b1641e642
Hanno Braun <hanno@braun-odw.eu>
2021-11-13 12:29:23 +0100

    Model cube as the sweep of a square

--- ng/model/src/lib.rs
+++ ng/model/src/lib.rs
@@ -1,5 +1,8 @@
 #[no_mangle]
 pub extern "C" fn model() -> fj::Shape {
-    let cube = fj::Cube { size: 1.0 };
+    let cube = fj::Sweep {
+        shape: fj::Square { size: 1.0 }.into(),
+        length: 1.0,
+    };
     cube.into()
 }
