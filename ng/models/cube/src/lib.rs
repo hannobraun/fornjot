e61adbb4afa7a58d8391cdc100f2b9e978b68614
Hanno Braun <hanno@braun-odw.eu>
2021-11-13 12:49:41 +0100

    Refactor

--- ng/models/cube/src/lib.rs
+++ ng/models/cube/src/lib.rs
@@ -1,8 +1,11 @@
 #[no_mangle]
 pub extern "C" fn model() -> fj::Shape {
+    let square = fj::Square { size: 1.0 }.into();
+
     let cube = fj::Sweep {
-        shape: fj::Square { size: 1.0 }.into(),
+        shape: square,
         length: 1.0,
     };
+
     cube.into()
 }
