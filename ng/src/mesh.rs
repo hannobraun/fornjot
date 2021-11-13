294ce402296c9cba688eaf3a5e6703f466580ed8
Hanno Braun <hanno@braun-odw.eu>
2021-11-13 11:34:34 +0100

    Remove obsolete task

--- ng/src/mesh.rs
+++ ng/src/mesh.rs
@@ -1,12 +1,3 @@
-// TASK: Consider simplifying this module:
-//       - Do away with indices. Just convert `Mesh` into a `Vec<Triangle>`,
-//         basically.
-//       - Merge `Mesh` and `MeshMaker`.
-//
-//       I don't think the complexity here is pulling its weight. All it does is
-//       save a bit of RAM. And `graphics::Vertices` is basically duplicating
-//       the the logic here anyway, because it uses a different vertex type.
-
 /// An index that refers to a vertex
 ///
 /// See [`Mesh`].
