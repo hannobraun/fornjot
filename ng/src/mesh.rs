9d84e91b09e3e765eef9c7226ab9bbd029b9d96c
Hanno Braun <hanno@braun-odw.eu>
2021-11-13 09:45:05 +0100

    Add task

--- ng/src/mesh.rs
+++ ng/src/mesh.rs
@@ -4,6 +4,15 @@ use decorum::R32;
 
 use crate::math::Point;
 
+// TASK: Consider simplifying this module:
+//       - Do away with indices. Just convert `Mesh` into a `Vec<Triangle>`,
+//         basically.
+//       - Merge `Mesh` and `MeshMaker`.
+//
+//       I don't think the complexity here is pulling its weight. All it does is
+//       save a bit of RAM. And `graphics::Vertices` is basically duplicating
+//       the the logic here anyway, because it uses a different vertex type.
+
 /// A triangle mesh
 #[derive(Debug)]
 pub struct Mesh {
