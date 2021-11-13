02fdd9d586daa3ad3f293584c86603967599fa20
Hanno Braun <hanno@braun-odw.eu>
2021-11-13 10:13:56 +0100

    Add dedicated module for `Sweep`'s `Vertices impl

--- ng/src/geometry/vertices/mod.rs
+++ ng/src/geometry/vertices/mod.rs
@@ -1,4 +1,4 @@
-use nalgebra::vector;
+mod sweep;
 
 use crate::math::Point;
 
@@ -79,20 +79,5 @@ impl Vertices for fj::Cube {
         ];
 
         v.map(|coord| coord.into()).to_vec()
-    }
-}
-
-impl Vertices for fj::Sweep {
-    type Vertices = Vec<Point>;
-
-    fn vertices(&self) -> Self::Vertices {
-        let mut vertices = Vec::new();
-
-        for vertex in self.shape.vertices() {
-            vertices.push(vertex);
-            vertices.push(vertex + vector![0.0, 0.0, self.length]);
-        }
-
-        vertices
     }
 }
---
+++ ng/src/geometry/vertices/sweep.rs
@@ -0,0 +1,18 @@
+use nalgebra::vector;
+
+use crate::math::Point;
+
+impl super::Vertices for fj::Sweep {
+    type Vertices = Vec<Point>;
+
+    fn vertices(&self) -> Self::Vertices {
+        let mut vertices = Vec::new();
+
+        for vertex in self.shape.vertices() {
+            vertices.push(vertex);
+            vertices.push(vertex + vector![0.0, 0.0, self.length]);
+        }
+
+        vertices
+    }
+}
