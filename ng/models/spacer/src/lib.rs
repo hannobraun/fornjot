b7a8078a2bed5983d0911ab8c3c258027b7ac741
Hanno Braun <hanno@braun-odw.eu>
2021-11-13 16:51:11 +0100

    Flesh out spacer model some more

--- ng/models/spacer/src/lib.rs
+++ ng/models/spacer/src/lib.rs
@@ -1,5 +1,9 @@
 #[no_mangle]
 pub extern "C" fn model() -> fj::Shape {
-    // TASK: Implement.
-    todo!()
+    let circle = fj::Circle { radius: 1.0 };
+
+    // TASK: Make hole in circle.
+    // TASK: Extrude 2D shape into 3D shape..
+
+    circle.into()
 }
