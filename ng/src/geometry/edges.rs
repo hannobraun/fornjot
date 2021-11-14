965a9bfa506221e940612c0d5be401c064bf26f1
Hanno Braun <hanno@braun-odw.eu>
2021-11-14 12:36:41 +0100

    Add trait `Edges`

---
+++ ng/src/geometry/edges.rs
@@ -0,0 +1,14 @@
+use crate::math::Point;
+
+/// Access the edges of a shape
+pub trait Edges {
+    /// Compute line segments to approximate the shape's edges
+    ///
+    /// `tolerance` defines how far these line segments are allowed to deviate
+    /// from the actual edges of the shape.
+    ///
+    /// This method presents a weird API right now, as it just returns all the
+    /// segments, not distinguishing which edge they approximate. This design is
+    /// simple and in line with current use cases, but not expected to last.
+    fn segments(&self, tolerance: f32) -> Vec<[Point; 2]>;
+}
--- ng/src/geometry/mod.rs
+++ ng/src/geometry/mod.rs
@@ -1,3 +1,4 @@
 pub mod bounding_volume;
+pub mod edges;
 pub mod faces;
 pub mod vertices;
