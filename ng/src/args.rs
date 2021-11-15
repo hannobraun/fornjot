ce00629bca70e4a4bfa88e0938964cc1355887e4
Hanno Braun <hanno@braun-odw.eu>
2021-11-15 20:53:34 +0100

    Add `--export` parameter

--- ng/src/args.rs
+++ ng/src/args.rs
@@ -1,9 +1,15 @@
+use std::path::PathBuf;
+
 /// Fornjot - Experimental CAD System - Host Application
 #[derive(clap::Parser)]
 pub struct Args {
     /// The model to open
     #[clap(short, long, default_value = "cube")]
     pub model: String,
+
+    /// Export model to this path
+    #[clap(short, long)]
+    pub export: Option<PathBuf>,
 }
 
 impl Args {
