85bd08df55ef96287004d77c811866b21efa5e15
Hanno Braun <hanno@braun-odw.eu>
2021-11-18 19:50:43 +0100

    Refactor

--- ng/src/geometry/vertices/sweep.rs
+++ ng/src/geometry/vertices/sweep.rs
@@ -7,7 +7,7 @@ use crate::math::Point;
 use super::Vertices;
 
 impl Vertices for fj::Sweep {
-    type Vertices = SweepVertices;
+    type Vertices = Vec<Point>;
 
     fn vertices(&self) -> Self::Vertices {
         // TASK Implement.
