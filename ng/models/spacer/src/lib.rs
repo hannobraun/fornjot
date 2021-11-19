18fbf6f00c49896c306c380d68e42eb41c31ce8c
Hanno Braun <hanno@braun-odw.eu>
2021-11-19 18:09:27 +0100

    Complete spacer model
    It still crashes when run, due to lacking support in the host, but the
    code should be complete as-is.

--- ng/models/spacer/src/lib.rs
+++ ng/models/spacer/src/lib.rs
@@ -1,9 +1,12 @@
 #[no_mangle]
 pub extern "C" fn model() -> fj::Shape {
     let outer_edge = fj::Circle { radius: 1.0 };
+    let inner_edge = fj::Circle { radius: 0.5 };
 
-    // TASK: Make hole in circle.
-    let footprint = outer_edge;
+    let footprint = fj::Difference {
+        a: outer_edge.into(),
+        b: inner_edge.into(),
+    };
 
     let spacer = fj::Sweep {
         shape: footprint.into(),
