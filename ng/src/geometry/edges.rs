8e0dbe142b88e690933704a521b0b2b5753d0097
Hanno Braun <hanno@braun-odw.eu>
2021-11-20 17:36:50 +0100

    Update documentation

--- ng/src/geometry/edges.rs
+++ ng/src/geometry/edges.rs
@@ -9,12 +9,10 @@ use crate::{
 pub trait Edges {
     /// Compute vertices to approximate the shape's edges
     ///
+    /// Returns a `Vec` that contains a `Vec<Point>` for each edge of the shape.
+    ///
     /// `tolerance` defines how far the implicit line segments between those
     /// vertices are allowed to deviate from the actual edges of the shape.
-    ///
-    /// This method presents a weird API right now, as it just returns all the
-    /// segments, not distinguishing which edge they approximate. This design is
-    /// simple and in line with current use cases, but not expected to last.
     fn edge_vertices(&self, tolerance: f64) -> Vec<Vec<Point>>;
 
     /// Compute line segments to approximate the shape's edges
