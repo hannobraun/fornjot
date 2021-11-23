169a2894e241648aa8fc8ebf78479ebb2d532363
Hanno Braun <hanno@braun-odw.eu>
2021-11-23 20:41:26 +0100

    Update documentation

--- src/input/zoom.rs
+++ src/input/zoom.rs
@@ -16,6 +16,10 @@ impl Zoom {
         }
     }
 
+    /// Push an input delta from the mouse wheel or track pad
+    ///
+    /// Expects the delta to be normalized, so using the mouse wheel and track
+    /// pad lead to the same zoom feel.
     pub fn push_input_delta(&mut self, delta: f32, now: Instant) {
         let new_event = delta * 0.1;
 
@@ -31,9 +35,8 @@ impl Zoom {
         self.events.push_back((now, new_event));
     }
 
+    /// Discard zoom events that fall out of the zoom input time window
     pub fn discard_old_events(&mut self, now: Instant) {
-        // Discard all zoom input events that fall out of the zoom input time
-        // window.
         const ZOOM_INPUT_WINDOW: Duration = Duration::from_millis(500);
         while let Some((time, _)) = self.events.front() {
             if now.duration_since(*time) > ZOOM_INPUT_WINDOW {
