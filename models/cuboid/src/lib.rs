61d7a732738c8fcbefdb48ca91e75e51a8117319
Hanno Braun <hanno@braun-odw.eu>
2021-11-25 12:04:08 +0100

    Update default values of cuboid parameters

--- models/cuboid/src/lib.rs
+++ models/cuboid/src/lib.rs
@@ -2,8 +2,8 @@ use std::collections::HashMap;
 
 #[no_mangle]
 pub extern "C" fn model(args: &HashMap<String, String>) -> fj::Shape {
-    let x = args.get("x").unwrap_or(&"1.0".to_owned()).parse().unwrap();
-    let y = args.get("y").unwrap_or(&"1.0".to_owned()).parse().unwrap();
+    let x = args.get("x").unwrap_or(&"3.0".to_owned()).parse().unwrap();
+    let y = args.get("y").unwrap_or(&"2.0".to_owned()).parse().unwrap();
     let z = args.get("z").unwrap_or(&"1.0".to_owned()).parse().unwrap();
 
     let rectangle = fj::Rectangle { x, y }.into();
