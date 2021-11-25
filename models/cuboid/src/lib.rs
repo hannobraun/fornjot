d4071fb55110ad04855763724578120ce5cba16a
Hanno Braun <hanno@braun-odw.eu>
2021-11-25 12:02:46 +0100

    Split `width` parameter of cuboid into `x`, `y`

--- models/cuboid/src/lib.rs
+++ models/cuboid/src/lib.rs
@@ -2,14 +2,11 @@ use std::collections::HashMap;
 
 #[no_mangle]
 pub extern "C" fn model(args: &HashMap<String, String>) -> fj::Shape {
-    let width = args
-        .get("width")
-        .unwrap_or(&"1.0".to_owned())
-        .parse()
-        .unwrap();
+    let x = args.get("x").unwrap_or(&"1.0".to_owned()).parse().unwrap();
+    let y = args.get("y").unwrap_or(&"1.0".to_owned()).parse().unwrap();
     let z = args.get("z").unwrap_or(&"1.0".to_owned()).parse().unwrap();
 
-    let rectangle = fj::Rectangle { x: width, y: width }.into();
+    let rectangle = fj::Rectangle { x, y }.into();
 
     let cuboid = fj::Sweep {
         shape: rectangle,
