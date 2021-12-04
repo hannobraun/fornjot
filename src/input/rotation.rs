aacfa426da3774d504258d267852ae92e95cc406
Hanno Braun <hanno@braun-odw.eu>
2021-12-04 11:28:11 +0100

    Refactor

--- src/input/rotation.rs
+++ src/input/rotation.rs
@@ -1,19 +1,13 @@
-use nalgebra::{Rotation3, Unit};
+use nalgebra::Rotation3;
 
-use crate::graphics::Camera;
+use crate::{graphics::Camera, math::Vector};
 
 pub struct Rotation;
 
 impl Rotation {
     pub fn apply(&self, angle_x: f64, angle_y: f64, camera: &mut Camera) {
-        let rot_x = Rotation3::from_axis_angle(
-            &Unit::new_unchecked([1.0, 0.0, 0.0].into()),
-            angle_x,
-        );
-        let rot_y = Rotation3::from_axis_angle(
-            &Unit::new_unchecked([0.0, 1.0, 0.0].into()),
-            angle_y,
-        );
+        let rot_x = Rotation3::from_axis_angle(&Vector::x_axis(), angle_x);
+        let rot_y = Rotation3::from_axis_angle(&Vector::y_axis(), angle_y);
 
         camera.rotation = rot_y * rot_x * camera.rotation;
     }
