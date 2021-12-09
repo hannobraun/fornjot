699613b2d1e376ebd561bc906a8390d07b5e687e
Hanno Braun <hanno@braun-odw.eu>
2021-12-09 19:49:45 +0100

    Implement `<fj::Sketch as Shape>::edges`

--- src/geometry/shapes/sketch.rs
+++ src/geometry/shapes/sketch.rs
@@ -2,7 +2,7 @@ use parry3d_f64::bounding_volume::AABB;
 
 use crate::{
     geometry::{
-        edges::Edges,
+        edges::{Edge, Edges},
         faces::{triangulate, Faces},
         Shape,
     },
@@ -21,8 +21,30 @@ impl Shape for fj::Sketch {
     }
 
     fn edges(&self) -> Edges {
-        // TASK: Implement.
-        todo!()
+        let v = match self.vertices() {
+            vertices if vertices.is_empty() => vertices,
+            mut vertices => {
+                // Add the first vertex at the end again, to close the loop.
+                //
+                // This can't panic. This `match` expression makes sure that
+                // there are vertices.
+                vertices.push(vertices[0]);
+                vertices
+            }
+        };
+
+        let mut edges = Edges::new();
+        for window in v.windows(2) {
+            // Can't panic, we passed `2` to `windows`.
+            //
+            // Can be cleaned up, once `array_windows` is stable.
+            let a = window[0];
+            let b = window[1];
+
+            edges.0.push(Edge::line_segment(a, b));
+        }
+
+        edges
     }
 
     fn vertices(&self) -> Vec<Point> {
