acc761edae181a020516c0fe99be4816e1ffee26
Hanno Braun <hanno@braun-odw.eu>
2021-12-09 20:17:29 +0100

    Replace `Rectangle` with `Sketch` in cuboid model

--- models/cuboid/src/lib.rs
+++ models/cuboid/src/lib.rs
@@ -2,11 +2,17 @@ use std::collections::HashMap;
 
 #[no_mangle]
 pub extern "C" fn model(args: &HashMap<String, String>) -> fj::Shape {
-    let x = args.get("x").unwrap_or(&"3.0".to_owned()).parse().unwrap();
-    let y = args.get("y").unwrap_or(&"2.0".to_owned()).parse().unwrap();
-    let z = args.get("z").unwrap_or(&"1.0".to_owned()).parse().unwrap();
+    let x: f64 = args.get("x").unwrap_or(&"3.0".to_owned()).parse().unwrap();
+    let y: f64 = args.get("y").unwrap_or(&"2.0".to_owned()).parse().unwrap();
+    let z: f64 = args.get("z").unwrap_or(&"1.0".to_owned()).parse().unwrap();
 
-    let rectangle = fj::Rectangle { x, y };
+    #[rustfmt::skip]
+    let rectangle = fj::Sketch::from_points(vec![
+        [-x / 2., -y / 2.],
+        [ x / 2., -y / 2.],
+        [ x / 2.,  y / 2.],
+        [-x / 2.,  y / 2.],
+    ]);
 
     let cuboid = fj::Sweep {
         shape: rectangle.into(),
