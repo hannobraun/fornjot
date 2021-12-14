6ed4bad1172372e4c53872cf02a1c44ad85584c2
Hanno Braun <hanno@braun-odw.eu>
2021-12-14 20:06:19 +0100

    Implement `<fj::Union as Shape>::bounding_volume`

--- src/geometry/shapes/union.rs
+++ src/geometry/shapes/union.rs
@@ -1,4 +1,4 @@
-use parry3d_f64::bounding_volume::AABB;
+use parry3d_f64::bounding_volume::{BoundingVolume as _, AABB};
 
 use crate::{
     geometry::{edges::Edges, faces::Faces, Shape},
@@ -7,8 +7,10 @@ use crate::{
 
 impl Shape for fj::Union {
     fn bounding_volume(&self) -> AABB {
-        // TASK: Implement.
-        todo!()
+        let a = self.a.bounding_volume();
+        let b = self.b.bounding_volume();
+
+        a.merged(&b)
     }
 
     fn faces(&self, _tolerance: f64) -> Faces {
