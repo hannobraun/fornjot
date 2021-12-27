f16724da5264f82f570e1f7f76dcf778a912f242
Hanno Braun <hanno@braun-odw.eu>
2021-12-27 10:13:52 +0100

    Derive `Debug` for `Curve`

--- src/kernel/geometry/curves.rs
+++ src/kernel/geometry/curves.rs
@@ -14,6 +14,7 @@ use crate::math::Point;
 ///
 /// This distinction is not observed here, but moving things into that direction
 /// is the intention.
+#[derive(Debug)]
 pub enum Curve {
     /// A circle
     Circle(Circle),
@@ -42,6 +43,7 @@ impl Curve {
 ///
 /// It might be better to define a circle using two points: The center, and
 /// the "zero" point on the circumference.
+#[derive(Debug)]
 pub struct Circle {
     /// The radius of the circle
     pub radius: f64,
@@ -97,6 +99,7 @@ impl Circle {
 }
 
 /// A line, defined by two points
+#[derive(Debug)]
 pub struct Line {
     /// One point defining the line
     pub a: Point,
