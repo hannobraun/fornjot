47fc8589802363ef143b12c9af3aba119f3ecac9
Hanno Braun <hanno@braun-odw.eu>
2022-01-01 14:26:08 +0100

    Add field `rays` to `DebugInfo`

--- src/debug.rs
+++ src/debug.rs
@@ -1,10 +1,15 @@
+use parry3d_f64::query::Ray;
+
 /// Debug info from the CAD kernel that can be visualized
 ///
 /// At this point, this is a placeholder that will be filled with life later.
-pub struct DebugInfo;
+pub struct DebugInfo {
+    /// Rays being used during face triangulation
+    pub rays: Vec<Ray>,
+}
 
 impl DebugInfo {
     pub fn new() -> Self {
-        Self
+        Self { rays: Vec::new() }
     }
 }
