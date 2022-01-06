a2f5c26d22fb931b2d8382d2f72697747a73c989
Hanno Braun <hanno@braun-odw.eu>
2022-01-06 18:13:51 +0100

    Add enum `Surface`
    Right now, the surface that a face is a section of is implicit, and
    always the X-Y plane. Hence, this new `Surface` enum's only variant is
    that plane.

--- src/kernel/geometry/mod.rs
+++ src/kernel/geometry/mod.rs
@@ -1,3 +1,7 @@
 pub mod curves;
+pub mod surfaces;
 
-pub use self::curves::{Circle, Curve, Line};
+pub use self::{
+    curves::{Circle, Curve, Line},
+    surfaces::Surface,
+};
---
+++ src/kernel/geometry/surfaces.rs
@@ -0,0 +1,10 @@
+/// A two-dimensional shape
+// TASK: Un-suppress warning.
+#[allow(unused)]
+pub enum Surface {
+    /// The X-Y plane
+    ///
+    /// This will be replaced with a more general plane representation in due
+    /// time.
+    XYPlane,
+}
