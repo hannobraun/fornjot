6eabfe7b7e9a42c5ac6ed3bcbecd716d48278d09
Hanno Braun <hanno@braun-odw.eu>
2022-01-15 20:53:44 +0100

    Improve doc comment

--- src/kernel/geometry/curves/line.rs
+++ src/kernel/geometry/curves/line.rs
@@ -3,7 +3,10 @@ use crate::math::Point;
 /// A line, defined by two points
 #[derive(Clone, Debug)]
 pub struct Line {
-    /// One point defining the line
+    /// The origin point of the line
+    ///
+    /// The point on the plane that is the origin of the 1-dimensional curve
+    /// coordinate system.
     pub origin: Point<3>,
 
     /// The other point defining the line
