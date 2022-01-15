647ddc10eec73fe41299d1321c3ce2598b9aec1e
Hanno Braun <hanno@braun-odw.eu>
2022-01-15 20:46:26 +0100

    Move `curves::Circle` to dedicated module

--- src/kernel/geometry/curves/mod.rs
+++ src/kernel/geometry/curves/mod.rs
@@ -1,4 +1,6 @@
-use std::f64::consts::PI;
+mod circle;
+
+pub use self::circle::Circle;
 
 use crate::math::Point;
 
@@ -28,66 +30,6 @@ impl Curve {
         match self {
             Curve::Circle(circle) => circle.approx_vertices(tolerance, out),
             Curve::Line(Line { a, b }) => out.extend([*a, *b]),
-        }
-    }
-}
-
-/// A circle
-///
-/// This representation quite limited, for two reasons:
-/// - It doesn't define the center point of the circle. For that reason,
-///   only circles centered on the origin are supported at this point.
-/// - It doesn't define where the circle begins. For the purposes of
-///   defining an arc on the circle, the zero angle will implicitly be to
-///   the right.
-///
-/// It might be better to define a circle using two points: The center, and
-/// the "zero" point on the circumference.
-#[derive(Clone, Debug)]
-pub struct Circle {
-    /// The radius of the circle
-    pub radius: f64,
-}
-
-impl Circle {
-    pub fn approx_vertices(&self, tolerance: f64, out: &mut Vec<Point<3>>) {
-        let angle_to_point = |angle: f64| {
-            let (sin, cos) = angle.sin_cos();
-
-            let x = cos * self.radius;
-            let y = sin * self.radius;
-
-            [x, y, 0.].into()
-        };
-
-        // To approximate the circle, we use a regular polygon for which
-        // the circle is the circumscribed circle. The `tolerance`
-        // parameter is the maximum allowed distance between the polygon
-        // and the circle. This is the same as the difference between
-        // the circumscribed circle and the incircle.
-        //
-        // Let's figure which regular polygon we need to use, by just
-        // trying out some of them until we find one whose maximum error
-        // is less than or equal to the tolerance.
-        let mut n = 3;
-        loop {
-            let incircle_radius = self.radius * (PI / n as f64).cos();
-            let maximum_error = self.radius - incircle_radius;
-
-            if maximum_error <= tolerance {
-                break;
-            }
-
-            n += 1;
-
-            // TASK: Log a warning, if `n` is becoming unreasonably large. This
-            //       is either a bug, or the consequence of an unreasonably low
-            //       value for `tolerance`.
-        }
-
-        for i in 0..n {
-            let angle = 2. * PI / n as f64 * i as f64;
-            out.push(angle_to_point(angle));
         }
     }
 }
