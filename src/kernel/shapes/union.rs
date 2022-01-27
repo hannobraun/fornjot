cc9a612acfb5138eccb8d1f521f9fae3809adce7
Hanno Braun <hanno@braun-odw.eu>
2022-01-27 16:38:05 +0100

    Replace `TASK` with comment referencing issue

--- src/kernel/shapes/union.rs
+++ src/kernel/shapes/union.rs
@@ -21,8 +21,11 @@ impl Shape for fj::Union {
         let a = self.a.faces(tolerance, debug_info);
         let b = self.b.faces(tolerance, debug_info);
 
-        // TASK: This doesn't create a true union, as it doesn't eliminate,
-        //       merge, or split faces.
+        // This doesn't create a true union, as it doesn't eliminate, merge, or
+        // split faces.
+        //
+        // See issue:
+        // https://github.com/hannobraun/Fornjot/issues/42
         let mut faces = Vec::new();
         faces.extend(a.0);
         faces.extend(b.0);
