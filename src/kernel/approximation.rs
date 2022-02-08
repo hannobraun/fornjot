564e2a4bcc933351861be87452d06de483b5427a
Hanno Braun <hanno@braun-odw.eu>
2022-02-08 14:44:21 +0100

    Update documentation of `Approximation`

--- src/kernel/approximation.rs
+++ src/kernel/approximation.rs
@@ -2,8 +2,18 @@ use parry3d_f64::shape::Segment;
 
 use crate::math::Point;
 
-/// An approximation of one or more edges
+/// An approximation of an edge, multiple edges, or a face
 pub struct Approximation {
+    /// All points that make up the approximation
+    ///
+    /// These could be actual vertices from the model, points that approximate
+    /// an edge, or points that approximate a face.
     pub points: Vec<Point<3>>,
+
+    /// Segments that approximate edges
+    ///
+    /// Every approximation will involve edges, typically, and these are
+    /// approximated by these segments. All the points of these segments will
+    /// also be available in the `points` field of this struct.
     pub segments: Vec<Segment>,
 }
