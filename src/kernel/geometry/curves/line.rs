d72424bc92a5bfd782ee69c4244114509dac9fc0
Hanno Braun <hanno@braun-odw.eu>
2022-02-10 17:34:32 +0100

    Add `Line::point_model_to_curve`

--- src/kernel/geometry/curves/line.rs
+++ src/kernel/geometry/curves/line.rs
@@ -1,4 +1,6 @@
 use approx::AbsDiffEq;
+#[cfg(test)]
+use nalgebra::point;
 use parry3d_f64::math::Isometry;
 
 use crate::math::Point;
@@ -25,6 +27,27 @@ impl Line {
             a: transform.transform_point(&self.a),
             b: transform.transform_point(&self.b),
         }
+    }
+
+    /// Convert a point in model coordinates to curve coordinates
+    ///
+    /// Ignores the distance of the point to the line, meaning points on the
+    /// line will be converted to the curve coordinates of their projection on
+    /// the line.
+    ///
+    /// This is done to make this method robust against floating point accuracy
+    /// issues. Callers are advised to be careful about the points they pass, as
+    /// the point not being on the line, intended or not, will not result in an
+    /// error.
+    #[cfg(test)]
+    pub fn point_model_to_curve(&self, point: &Point<3>) -> Point<1> {
+        let p = point - self.a;
+        let d = self.b - self.a;
+
+        // scalar projection
+        let t = p.dot(&d.normalize());
+
+        point![t]
     }
 }
 
@@ -73,5 +96,18 @@ mod tests {
             },
             epsilon = 1e-8,
         );
+    }
+
+    #[test]
+    fn test_point_model_to_curve() {
+        let line = Line {
+            a: point![1., 0., 0.],
+            b: point![2., 0., 0.],
+        };
+
+        assert_eq!(line.point_model_to_curve(&point![0., 0., 0.]), point![-1.]);
+        assert_eq!(line.point_model_to_curve(&point![1., 0., 0.]), point![0.]);
+        assert_eq!(line.point_model_to_curve(&point![2., 0., 0.]), point![1.]);
+        assert_eq!(line.point_model_to_curve(&point![3., 0., 0.]), point![2.]);
     }
 }
