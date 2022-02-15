2cb6ac4bf9c29f3d56ab2b24d4a1a6d5caa1928a
Hanno Braun <hanno@braun-odw.eu>
2022-02-15 16:22:37 +0100

    Remove unused code

--- src/kernel/geometry/surfaces/mod.rs
+++ src/kernel/geometry/surfaces/mod.rs
@@ -1,7 +1,6 @@
-pub mod plane;
 pub mod swept;
 
-pub use self::{plane::Plane, swept::Swept};
+pub use self::swept::Swept;
 
 use nalgebra::vector;
 use parry3d_f64::math::Isometry;
@@ -13,9 +12,6 @@ use super::{points::SurfacePoint, Curve, Line};
 /// A two-dimensional shape
 #[derive(Clone, Debug, PartialEq)]
 pub enum Surface {
-    /// A plane
-    Plane(Plane),
-
     /// A swept curve
     Swept(Swept),
 }
@@ -36,7 +32,6 @@ impl Surface {
     #[must_use]
     pub fn transform(self, transform: &Isometry<f64>) -> Self {
         match self {
-            Self::Plane(surface) => Self::Plane(surface.transform(transform)),
             Self::Swept(surface) => Self::Swept(surface.transform(transform)),
         }
     }
@@ -44,7 +39,6 @@ impl Surface {
     /// Convert a point in model coordinates to surface coordinates
     pub fn point_model_to_surface(&self, point_3d: Point<3>) -> SurfacePoint {
         let point_2d = match self {
-            Self::Plane(surface) => surface.point_model_to_surface(point_3d),
             Self::Swept(surface) => surface.point_model_to_surface(&point_3d),
         };
 
@@ -57,7 +51,6 @@ impl Surface {
     /// Convert a point in surface coordinates to model coordinates
     pub fn point_surface_to_model(&self, point: &Point<2>) -> Point<3> {
         match self {
-            Self::Plane(surface) => surface.point_surface_to_model(point),
             Self::Swept(surface) => surface.point_surface_to_model(point),
         }
     }
@@ -65,7 +58,6 @@ impl Surface {
     /// Convert a vector in surface coordinates to model coordinates
     pub fn vector_surface_to_model(&self, vector: &Vector<2>) -> Vector<3> {
         match self {
-            Self::Plane(surface) => surface.vector_surface_to_model(vector),
             Self::Swept(surface) => surface.vector_surface_to_model(vector),
         }
     }
