e889f1961c94b8d3a398639984dd1cf9524e1023
Hanno Braun <hanno@braun-odw.eu>
2022-02-17 20:25:46 +0100

    Run `cargo fmt`
    Seems like I missed doing this in the last pull request.

--- src/camera.rs
+++ src/camera.rs
@@ -46,18 +46,14 @@ impl Camera {
             // To do that, first compute the model's highest point, as well as
             // the furthest point from the origin, in x and y.
             let highest_point = aabb.max.z;
-            let furthest_point = [
-                aabb.min.x.abs(),
-                aabb.max.x,
-                aabb.min.y.abs(),
-                aabb.max.y,
-            ]
-            .into_iter()
-            .reduce(|a, b| f64::max(a, b))
-            // `reduce` can only return `None`, if there are no items in
-            // the iterator. And since we're creating an array full of
-            // items above, we know this can't panic.
-            .unwrap();
+            let furthest_point =
+                [aabb.min.x.abs(), aabb.max.x, aabb.min.y.abs(), aabb.max.y]
+                    .into_iter()
+                    .reduce(|a, b| f64::max(a, b))
+                    // `reduce` can only return `None`, if there are no items in
+                    // the iterator. And since we're creating an array full of
+                    // items above, we know this can't panic.
+                    .unwrap();
 
             // The actual furthest point is not far enough. We don't want the
             // model to fill the whole screen.
