c5a051ebec66ce30ba6ca60f9376a8feb7df7d10
Hanno Braun <hanno@braun-odw.eu>
2022-02-17 17:02:35 +0100

    Remove redundant variable

--- src/kernel/geometry/curves/line.rs
+++ src/kernel/geometry/curves/line.rs
@@ -57,8 +57,7 @@ impl Line {
 
     /// Convert a vector on the curve into model coordinates
     pub fn vector_curve_to_model(&self, point: &Vector<1>) -> Vector<3> {
-        let t = point.t();
-        self.direction * t
+        self.direction * point.t()
     }
 }
 
