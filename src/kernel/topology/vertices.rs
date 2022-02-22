263e825fbb0f0f93c86cb645db1a310c066b052b
Hanno Braun <hanno@braun-odw.eu>
2022-02-22 16:08:21 +0100

    Move documentation on vertex uniqueness

--- src/kernel/topology/vertices.rs
+++ src/kernel/topology/vertices.rs
@@ -14,6 +14,22 @@ pub struct Vertices(pub Vec<Vertex<3>>);
 ///
 /// Points, on the other hand, might be used to approximate a shape for various
 /// purposes, without presenting any deeper truth about the shape's structure.
+///
+/// # Uniqueness
+///
+/// You **MUST NOT** construct a new instance of `Vertex` that represents an
+/// already existing vertex. If there already exists a vertex and you need a
+/// `Vertex` instance to refer to it, acquire one by copying or converting the
+/// existing `Vertex` instance.
+///
+/// Every time you create a `Vertex` instance, you might do so using a point you
+/// have computed. When doing this for an existing vertex, you run the risk of
+/// computing a slightly different point, due to floating point accuracy issues.
+/// The resulting `Vertex` will then no longer be equal to the existing `Vertex`
+/// instance that refers to the same vertex, which will cause bugs.
+///
+/// This can be prevented outright by never creating a new `Vertex` instance
+/// for an existing vertex. Hence why this is strictly forbidden.
 #[derive(Clone, Copy, Debug, Eq, Hash, PartialEq, Ord, PartialOrd)]
 pub struct Vertex<const D: usize> {
     location: Point<D>,
@@ -34,20 +50,9 @@ impl Vertex<3> {
     /// representation of a vertex. If you need a vertex of different
     /// dimensionality, use a conversion method.
     ///
-    /// This method **MUST NOT** be used to construct a new instance of `Vertex`
-    /// that represents an already existing vertex. If there already exists a
-    /// vertex and you need a `Vertex` instance to refer to it, acquire one by
-    /// copying or converting the existing `Vertex` instance.
-    ///
-    /// Every time you create a `Vertex` instance, you might do so using a point
-    /// you have computed. When doing this for an existing vertex, you run the
-    /// risk of computing a slightly different point, due to floating point
-    /// accuracy issues. The resulting `Vertex` will then no longer be equal to
-    /// the existing `Vertex` instance that refers to the same vertex, which
-    /// will surely cause bugs.
-    ///
-    /// This can be prevented outright by never creating a new `Vertex` instance
-    /// for an existing vertex. Hence why this is strictly forbidden.
+    /// You **MUST NOT** use this method to construct a new instance of `Vertex`
+    /// that represents an already existing vertex. See documentation of
+    /// [`Vertex`] for more information.
     pub fn create_at(location: Point<3>) -> Self {
         Self {
             location,
