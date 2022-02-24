439aaac2049685da3864aa6b167c92f1e0b84ee6
Hanno Braun <hanno@braun-odw.eu>
2022-02-24 16:49:33 +0100

    Make sweep operation slightly more correct
    By sweeping each cycle separately, the number of cases where the
    algorithm is producing wrong data is reduced.
    
    Previously, it was basically only correct, if there was a single edge in
    the original shape. Now, it's at least correct, if there's a single edge
    per cycle.

--- src/kernel/shapes/sweep.rs
+++ src/kernel/shapes/sweep.rs
@@ -34,36 +34,39 @@ impl Shape for fj::Sweep {
         let top_faces = original_faces
             .transform(&Isometry::translation(0.0, 0.0, self.length).into());
 
-        // This will only work correctly, if the original shape consists of one
-        // edge. If there are more, this will create some kind of weird face
-        // chimera, a single face to represent all the side faces.
-        //
-        // It'll be even worse, if the original shape consists of multiple
-        // faces.
-        let approx = Approximation::for_edges(&self.shape.edges(), tolerance);
+        let mut side_faces = Vec::new();
+        for cycle in self.shape.edges().cycles {
+            let approx = Approximation::for_cycle(&cycle, tolerance);
 
-        let mut quads = Vec::new();
-        for segment in approx.segments {
-            let [v0, v1] = segment.points();
-            let [v3, v2] = {
-                let segment = Transform::translation(0., 0., self.length)
-                    .transform_segment(&segment);
-                segment.points()
-            };
+            // This will only work correctly, if the cycle consists of one edge.
+            // If there are more, this will create some kind of weird face
+            // chimera, a single face to represent all the side faces.
 
-            quads.push([v0, v1, v2, v3]);
-        }
+            let mut quads = Vec::new();
+            for segment in approx.segments {
+                let [v0, v1] = segment.points();
+                let [v3, v2] = {
+                    let segment = Transform::translation(0., 0., self.length)
+                        .transform_segment(&segment);
+                    segment.points()
+                };
 
-        let mut side_face = Vec::new();
-        for [v0, v1, v2, v3] in quads {
-            side_face.push([v0, v1, v2].into());
-            side_face.push([v0, v2, v3].into());
+                quads.push([v0, v1, v2, v3]);
+            }
+
+            let mut side_face = Vec::new();
+            for [v0, v1, v2, v3] in quads {
+                side_face.push([v0, v1, v2].into());
+                side_face.push([v0, v2, v3].into());
+            }
+
+            side_faces.push(Face::Triangles(side_face));
         }
 
         let mut faces = Vec::new();
         faces.extend(bottom_faces.0);
         faces.extend(top_faces.0);
-        faces.push(Face::Triangles(side_face));
+        faces.extend(side_faces);
 
         Faces(faces)
     }
