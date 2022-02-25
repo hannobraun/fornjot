2a40557b31a12aa0e71ba35ae7be6c0c3b802a1e
Hanno Braun <hanno@braun-odw.eu>
2022-02-25 18:12:16 +0100

    Contain creation of `vertices` in a single block
    This will make merging `vertices` into `to_shape` easier.

--- src/kernel/shapes/sketch.rs
+++ src/kernel/shapes/sketch.rs
@@ -16,38 +16,40 @@ use super::ToShape;
 
 impl ToShape for fj::Sketch {
     fn to_shape(&self, _: Scalar, _: &mut DebugInfo) -> Shape {
-        let vertices = match self.vertices() {
-            vertices if vertices.0.is_empty() => vertices.0,
-            vertices => {
-                let mut vertices = vertices.0;
+        let edges = {
+            let vertices = match self.vertices() {
+                vertices if vertices.0.is_empty() => vertices.0,
+                vertices => {
+                    let mut vertices = vertices.0;
 
-                // Add the first vertex at the end again, to close the loop.
+                    // Add the first vertex at the end again, to close the loop.
+                    //
+                    // This can't panic. This `match` expression makes sure that
+                    // there are vertices.
+                    vertices.push(vertices[0]);
+                    vertices
+                }
+            };
+
+            let mut edges = Vec::new();
+            for window in vertices.windows(2) {
+                // Can't panic, we passed `2` to `windows`.
                 //
-                // This can't panic. This `match` expression makes sure that
-                // there are vertices.
-                vertices.push(vertices[0]);
-                vertices
+                // Can be cleaned up, once `array_windows` is stable.
+                let a = window[0];
+                let b = window[1];
+
+                let line = Curve::Line(Line {
+                    origin: *a.location(),
+                    direction: Vector::from(*b.location() - *a.location()),
+                });
+                let edge = Edge::new(line, Some([a, b]));
+
+                edges.push(edge);
             }
+
+            Edges::single_cycle(edges)
         };
-
-        let mut edges = Vec::new();
-        for window in vertices.windows(2) {
-            // Can't panic, we passed `2` to `windows`.
-            //
-            // Can be cleaned up, once `array_windows` is stable.
-            let a = window[0];
-            let b = window[1];
-
-            let line = Curve::Line(Line {
-                origin: *a.location(),
-                direction: Vector::from(*b.location() - *a.location()),
-            });
-            let edge = Edge::new(line, Some([a, b]));
-
-            edges.push(edge);
-        }
-
-        let edges = Edges::single_cycle(edges);
 
         let face = Face::Face {
             edges: edges.clone(),
