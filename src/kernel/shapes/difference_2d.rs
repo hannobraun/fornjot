f64568ae18f1f00c739d7cf49aa1492eee1c3ddd
Hanno Braun <hanno@braun-odw.eu>
2022-02-25 17:49:29 +0100

    Contain creation of `faces` in a block
    This is going to help with merging `edges` into `to_shape`.

--- src/kernel/shapes/difference_2d.rs
+++ src/kernel/shapes/difference_2d.rs
@@ -19,46 +19,48 @@ impl ToShape for fj::Difference2d {
         let mut a = self.a.to_shape(tolerance, debug_info);
         let mut b = self.b.to_shape(tolerance, debug_info);
 
-        let (a, b) = if a.faces.0.len() == 1 && b.faces.0.len() == 1 {
-            // Can't panic. We just checked that length of `a` and `b` is 1.
-            (a.faces.0.pop().unwrap(), b.faces.0.pop().unwrap())
-        } else {
-            // See issue:
-            // https://github.com/hannobraun/Fornjot/issues/95
-            todo!(
-                "The 2-dimensional difference operation only supports one face \
-                in each operand."
-            );
-        };
+        let faces = {
+            let (a, b) = if a.faces.0.len() == 1 && b.faces.0.len() == 1 {
+                // Can't panic. We just checked that length of `a` and `b` is 1.
+                (a.faces.0.pop().unwrap(), b.faces.0.pop().unwrap())
+            } else {
+                // See issue:
+                // https://github.com/hannobraun/Fornjot/issues/95
+                todo!(
+                    "The 2-dimensional difference operation only supports one \
+                    face in each operand."
+                );
+            };
 
-        let (a, b, surface_a, surface_b) = match (a, b) {
-            (
-                Face::Face {
-                    edges: a,
-                    surface: surface_a,
-                },
-                Face::Face {
-                    edges: b,
-                    surface: surface_b,
-                },
-            ) => (a, b, surface_a, surface_b),
-            _ => {
-                // None of the 2D types still use the triangles representation.
-                unreachable!()
+            let (a, b, surface_a, surface_b) = match (a, b) {
+                (
+                    Face::Face {
+                        edges: a,
+                        surface: surface_a,
+                    },
+                    Face::Face {
+                        edges: b,
+                        surface: surface_b,
+                    },
+                ) => (a, b, surface_a, surface_b),
+                _ => {
+                    // None of the 2D types still use the triangles representation.
+                    unreachable!()
+                }
+            };
+
+            if surface_a != surface_b {
+                // Panicking is not great, but as long as we don't have a real
+                // error handling mechanism, it will do.
+                panic!("Trying to subtract sketches with different surfaces.")
             }
+            let surface = surface_a;
+
+            let mut edges = a;
+            edges.cycles.extend(b.cycles);
+
+            Faces(vec![Face::Face { edges, surface }])
         };
-
-        if surface_a != surface_b {
-            // Panicking is not great, but as long as we don't have a real error
-            // handling mechanism, it will do.
-            panic!("Trying to subtract sketches with different surfaces.")
-        }
-        let surface = surface_a;
-
-        let mut edges = a;
-        edges.cycles.extend(b.cycles);
-
-        let faces = Faces(vec![Face::Face { edges, surface }]);
 
         Shape { faces }
     }
