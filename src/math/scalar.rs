18e3b2d52da0c6484376babb56fcaf7255f69315
Hanno Braun <hanno@braun-odw.eu>
2022-03-02 17:38:29 +0100

    Implement `num_traits::Float` for `Scalar`

--- src/math/scalar.rs
+++ src/math/scalar.rs
@@ -258,6 +258,12 @@ impl num_traits::Num for Scalar {
     }
 }
 
+impl num_traits::NumCast for Scalar {
+    fn from<T: num_traits::ToPrimitive>(n: T) -> Option<Self> {
+        Some(Self::from_f64(<f64 as num_traits::NumCast>::from(n)?))
+    }
+}
+
 impl num_traits::Signed for Scalar {
     fn abs(&self) -> Self {
         self.0.abs().into()
@@ -277,6 +283,235 @@ impl num_traits::Signed for Scalar {
 
     fn is_negative(&self) -> bool {
         <f64 as num_traits::Signed>::is_negative(&self.0)
+    }
+}
+
+impl num_traits::ToPrimitive for Scalar {
+    fn to_i64(&self) -> Option<i64> {
+        self.0.to_i64()
+    }
+
+    fn to_u64(&self) -> Option<u64> {
+        self.0.to_u64()
+    }
+}
+
+impl num_traits::Float for Scalar {
+    fn nan() -> Self {
+        panic!("`Scalar` can not represent NaN")
+    }
+
+    fn infinity() -> Self {
+        Self::from_f64(f64::infinity())
+    }
+
+    fn neg_infinity() -> Self {
+        Self::from_f64(f64::neg_infinity())
+    }
+
+    fn neg_zero() -> Self {
+        Self::from_f64(f64::neg_zero())
+    }
+
+    fn min_value() -> Self {
+        Self::from_f64(f64::min_value())
+    }
+
+    fn min_positive_value() -> Self {
+        Self::from_f64(f64::min_positive_value())
+    }
+
+    fn max_value() -> Self {
+        Self::from_f64(f64::max_value())
+    }
+
+    fn is_nan(self) -> bool {
+        self.0.is_nan()
+    }
+
+    fn is_infinite(self) -> bool {
+        self.0.is_infinite()
+    }
+
+    fn is_finite(self) -> bool {
+        self.0.is_finite()
+    }
+
+    fn is_normal(self) -> bool {
+        self.0.is_normal()
+    }
+
+    fn classify(self) -> std::num::FpCategory {
+        self.0.classify()
+    }
+
+    fn floor(self) -> Self {
+        Self::from_f64(self.0.floor())
+    }
+
+    fn ceil(self) -> Self {
+        Self::from_f64(self.0.ceil())
+    }
+
+    fn round(self) -> Self {
+        Self::from_f64(self.0.round())
+    }
+
+    fn trunc(self) -> Self {
+        Self::from_f64(self.0.trunc())
+    }
+
+    fn fract(self) -> Self {
+        Self::from_f64(self.0.fract())
+    }
+
+    fn abs(self) -> Self {
+        Self::from_f64(self.0.abs())
+    }
+
+    fn signum(self) -> Self {
+        Self::from_f64(self.0.signum())
+    }
+
+    fn is_sign_positive(self) -> bool {
+        self.0.is_sign_positive()
+    }
+
+    fn is_sign_negative(self) -> bool {
+        self.0.is_sign_negative()
+    }
+
+    fn mul_add(self, a: Self, b: Self) -> Self {
+        Self::from_f64(self.0.mul_add(a.0, b.0))
+    }
+
+    fn recip(self) -> Self {
+        Self::from_f64(self.0.recip())
+    }
+
+    fn powi(self, n: i32) -> Self {
+        Self::from_f64(self.0.powi(n))
+    }
+
+    fn powf(self, n: Self) -> Self {
+        Self::from_f64(self.0.powf(n.0))
+    }
+
+    fn sqrt(self) -> Self {
+        Self::from_f64(self.0.sqrt())
+    }
+
+    fn exp(self) -> Self {
+        Self::from_f64(self.0.exp())
+    }
+
+    fn exp2(self) -> Self {
+        Self::from_f64(self.0.exp2())
+    }
+
+    fn ln(self) -> Self {
+        Self::from_f64(self.0.ln())
+    }
+
+    fn log(self, base: Self) -> Self {
+        Self::from_f64(self.0.log(base.0))
+    }
+
+    fn log2(self) -> Self {
+        Self::from_f64(self.0.log2())
+    }
+
+    fn log10(self) -> Self {
+        Self::from_f64(self.0.log10())
+    }
+
+    fn max(self, other: Self) -> Self {
+        Self::from_f64(self.0.max(other.0))
+    }
+
+    fn min(self, other: Self) -> Self {
+        Self::from_f64(self.0.min(other.0))
+    }
+
+    fn abs_sub(self, other: Self) -> Self {
+        (self - other).abs()
+    }
+
+    fn cbrt(self) -> Self {
+        Self::from_f64(self.0.cbrt())
+    }
+
+    fn hypot(self, other: Self) -> Self {
+        Self::from_f64(self.0.hypot(other.0))
+    }
+
+    fn sin(self) -> Self {
+        Self::from_f64(self.0.sin())
+    }
+
+    fn cos(self) -> Self {
+        Self::from_f64(self.0.cos())
+    }
+
+    fn tan(self) -> Self {
+        Self::from_f64(self.0.tan())
+    }
+
+    fn asin(self) -> Self {
+        Self::from_f64(self.0.asin())
+    }
+
+    fn acos(self) -> Self {
+        Self::from_f64(self.0.acos())
+    }
+
+    fn atan(self) -> Self {
+        Self::from_f64(self.0.atan())
+    }
+
+    fn atan2(self, other: Self) -> Self {
+        Self::from_f64(self.0.atan2(other.0))
+    }
+
+    fn sin_cos(self) -> (Self, Self) {
+        let (sin, cos) = self.0.sin_cos();
+        (Self::from_f64(sin), Self::from_f64(cos))
+    }
+
+    fn exp_m1(self) -> Self {
+        Self::from_f64(self.0.exp_m1())
+    }
+
+    fn ln_1p(self) -> Self {
+        Self::from_f64(self.0.ln_1p())
+    }
+
+    fn sinh(self) -> Self {
+        Self::from_f64(self.0.sinh())
+    }
+
+    fn cosh(self) -> Self {
+        Self::from_f64(self.0.cosh())
+    }
+
+    fn tanh(self) -> Self {
+        Self::from_f64(self.0.tanh())
+    }
+
+    fn asinh(self) -> Self {
+        Self::from_f64(self.0.asinh())
+    }
+
+    fn acosh(self) -> Self {
+        Self::from_f64(self.0.acosh())
+    }
+
+    fn atanh(self) -> Self {
+        Self::from_f64(self.0.atanh())
+    }
+
+    fn integer_decode(self) -> (u64, i16, i8) {
+        self.0.integer_decode()
     }
 }
 
