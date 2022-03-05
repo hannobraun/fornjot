0a54030c2d082eb42afc2a4b68c58e5f0e5958b4
Hanno Braun <hanno@braun-odw.eu>
2022-03-05 12:37:03 +0100

    Update doc comments

--- src/kernel/shape/edges.rs
+++ src/kernel/shape/edges.rs
@@ -32,7 +32,7 @@ impl Edges {
         }
     }
 
-    /// Construct an edge
+    /// Create an edge
     ///
     /// If vertices are provided in `vertices`, they must be on `curve`.
     ///
@@ -40,6 +40,12 @@ impl Edges {
     /// they are not on the curve, this will result in their projection being
     /// converted into curve coordinates, which is likely not the caller's
     /// intention.
+    ///
+    /// # Implementation notes
+    ///
+    /// Right now this is just an overly complicated constructor for `Edge`. In
+    /// the future, it can add the edge to the proper internal data structures,
+    /// and validate any constraints that apply to edge creation.
     pub fn create(
         &mut self,
         curve: Curve,
@@ -48,7 +54,10 @@ impl Edges {
         Edge { curve, vertices }
     }
 
-    /// Construct an edge that is a line segment
+    /// Create a line segment
+    ///
+    /// Calls [`Edges::create`] internally, and inherits its limitations and
+    /// requirements.
     pub fn create_line_segment(&mut self, vertices: [Vertex; 2]) -> Edge {
         self.create(
             Curve::Line(Line::from_points(
@@ -59,6 +68,9 @@ impl Edges {
     }
 
     /// Create a circle
+    ///
+    /// Calls [`Edges::create`] internally, and inherits its limitations and
+    /// requirements.
     pub fn create_circle(&mut self, radius: Scalar) -> Edge {
         self.create(
             Curve::Circle(Circle {
