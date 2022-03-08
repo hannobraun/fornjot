3c43f7dd6d28c6968dc6eaf580ddb062ccb2368c
Hanno Braun <hanno@braun-odw.eu>
2022-03-08 14:33:15 +0100

    Remove outdated documentation
    Vertex uniqueness is now checked in `Vertices::add`, so the caller
    doesn't have to keep track of this during construction.

--- src/kernel/topology/vertices.rs
+++ src/kernel/topology/vertices.rs
@@ -8,22 +8,6 @@ use crate::math::Point;
 ///
 /// Points, on the other hand, might be used to approximate a shape for various
 /// purposes, without presenting any deeper truth about the shape's structure.
-///
-/// # Uniqueness
-///
-/// You **MUST NOT** construct a new instance of `Vertex` that represents an
-/// already existing vertex. If there already exists a vertex and you need a
-/// `Vertex` instance to refer to it, acquire one by copying or converting the
-/// existing `Vertex` instance.
-///
-/// Every time you create a `Vertex` instance, you might do so using a point you
-/// have computed. When doing this for an existing vertex, you run the risk of
-/// computing a slightly different point, due to floating point accuracy issues.
-/// The resulting `Vertex` will then no longer be equal to the existing `Vertex`
-/// instance that refers to the same vertex, which will cause bugs.
-///
-/// This can be prevented outright by never creating a new `Vertex` instance
-/// for an existing vertex. Hence why this is strictly forbidden.
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub struct Vertex(Point<3>);
 
