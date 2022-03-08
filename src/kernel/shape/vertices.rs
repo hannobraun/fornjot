5abffdc4cc365d319798c017b6e57e0029ec46c2
Hanno Braun <hanno@braun-odw.eu>
2022-03-08 16:25:50 +0100

    Add `Vertices::all`

--- src/kernel/shape/vertices.rs
+++ src/kernel/shape/vertices.rs
@@ -57,6 +57,14 @@ impl Vertices<'_> {
 
         handle
     }
+
+    /// Access iterator over all vertices
+    ///
+    /// The caller must not make any assumptions about the order of vertices.
+    #[allow(unused)]
+    pub fn all(&self) -> impl Iterator<Item = Handle<Vertex>> + '_ {
+        self.vertices.iter().map(|storage| storage.handle())
+    }
 }
 
 #[cfg(test)]
