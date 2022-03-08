43f7778927ae37ba29bf733b3a6281afc2c62778
Hanno Braun <hanno@braun-odw.eu>
2022-03-08 13:16:00 +0100

    Refactor

--- src/kernel/shapes/union.rs
+++ src/kernel/shapes/union.rs
@@ -10,8 +10,8 @@ impl ToShape for fj::Union {
     fn to_shape(&self, tolerance: Scalar, debug_info: &mut DebugInfo) -> Shape {
         let mut shape = Shape::new();
 
-        let a = self.a.to_shape(tolerance, debug_info).faces;
-        let b = self.b.to_shape(tolerance, debug_info).faces;
+        let a = self.a.to_shape(tolerance, debug_info).faces.0;
+        let b = self.b.to_shape(tolerance, debug_info).faces.0;
 
         // This doesn't create a true union, as it doesn't eliminate, merge, or
         // split faces.
@@ -19,8 +19,8 @@ impl ToShape for fj::Union {
         // See issue:
         // https://github.com/hannobraun/Fornjot/issues/42
         let mut faces = Vec::new();
-        faces.extend(a.0);
-        faces.extend(b.0);
+        faces.extend(a);
+        faces.extend(b);
 
         shape.faces = Faces(faces);
 
