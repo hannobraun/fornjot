755ae05982b66a5f65ca3be8aa3c4490fa40d6fd
Hanno Braun <hanno@braun-odw.eu>
2022-03-10 14:56:06 +0100

    Derive `thiserror::Error` for `ValidationError`

--- src/kernel/shape/mod.rs
+++ src/kernel/shape/mod.rs
@@ -109,12 +109,13 @@ impl Shape {
 pub type ValidationResult<T> = Result<Handle<T>, ValidationError>;
 
 /// An error that can occur during a validation
-#[derive(Debug)]
+#[derive(Debug, thiserror::Error)]
 pub enum ValidationError {
     /// Structural validation failed
     ///
     /// Structural validation verifies, that all the object that an object
     /// refers to are already part of the shape.
+    #[error("Structural validation failed")]
     #[allow(unused)]
     Structural,
 
@@ -123,6 +124,7 @@ pub enum ValidationError {
     /// Uniqueness validation checks, that an object is unique. Uniqueness is
     /// only required for topological objects, as there's no harm in geometric
     /// objects being duplicated.
+    #[error("Uniqueness validation failed")]
     #[allow(unused)]
     Uniqueness,
 
@@ -131,6 +133,7 @@ pub enum ValidationError {
     /// Geometric validation checks, that various geometric constraints of an
     /// object are upheld. For example, edges or faces might not be allowed to
     /// intersect.
+    #[error("Geometric validation failed")]
     #[allow(unused)]
     Geometric,
 }
