570b5ea777d2b0247d33cd3bc0c61f5d27d8221f
Hanno Braun <hanno@braun-odw.eu>
2022-03-10 19:39:56 +0100

    Update test name

--- src/kernel/shape/vertices.rs
+++ src/kernel/shape/vertices.rs
@@ -74,7 +74,7 @@ mod tests {
     const MIN_DISTANCE: f64 = 5e-7;
 
     #[test]
-    fn add() -> anyhow::Result<()> {
+    fn add_vertex() -> anyhow::Result<()> {
         let mut shape = Shape::new().with_min_distance(MIN_DISTANCE);
 
         let point = shape.geometry().add_point(Point::from([0., 0., 0.]))?;
