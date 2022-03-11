f1934bd9e0d8a01b3a72f84eea3be7437fd3798d
Hanno Braun <hanno@braun-odw.eu>
2022-03-11 16:38:11 +0100

    Consolidate duplicated code

--- src/kernel/shapes/union.rs
+++ src/kernel/shapes/union.rs
@@ -10,20 +10,16 @@ impl ToShape for fj::Union {
     fn to_shape(&self, tolerance: Scalar, debug_info: &mut DebugInfo) -> Shape {
         let mut shape = Shape::new();
 
-        let mut a = self.a.to_shape(tolerance, debug_info);
-        let mut b = self.b.to_shape(tolerance, debug_info);
+        let a = self.a.to_shape(tolerance, debug_info);
+        let b = self.b.to_shape(tolerance, debug_info);
 
         // This doesn't create a true union, as it doesn't eliminate, merge, or
         // split faces.
         //
         // See issue:
         // https://github.com/hannobraun/Fornjot/issues/42
-        for face in a.topology().faces() {
-            shape.topology().add_face(face.get().clone()).unwrap();
-        }
-        for face in b.topology().faces() {
-            shape.topology().add_face(face.get().clone()).unwrap();
-        }
+        copy_shape(a, &mut shape);
+        copy_shape(b, &mut shape);
 
         shape
     }
@@ -33,5 +29,11 @@ impl ToShape for fj::Union {
         let b = self.b.bounding_volume();
 
         a.merged(&b)
+    }
+}
+
+fn copy_shape(mut orig: Shape, target: &mut Shape) {
+    for face in orig.topology().faces() {
+        target.topology().add_face(face.get().clone()).unwrap();
     }
 }
