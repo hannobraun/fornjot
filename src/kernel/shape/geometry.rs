695bf09892f620b8af1df6e716a6f271a7d460be
Hanno Braun <hanno@braun-odw.eu>
2022-03-11 13:49:05 +0100

    Document geometry (non-)validation

--- src/kernel/shape/geometry.rs
+++ src/kernel/shape/geometry.rs
@@ -6,6 +6,20 @@ use crate::{
 use super::handle::{Handle, Storage};
 
 /// API to access a shape's geometry
+///
+/// Other than topology, geometry doesn't need to be validated. Hence adding
+/// geometry is infallible.
+///
+/// There are several reasons for this:
+/// - Geometry doesn't refer to other objects, so structural validation doesn't
+///   apply.
+/// - There simply no reason that geometry needs to be unique. In addition, it's
+///   probably quite hard to rule out generating duplicate geometry. Think about
+///   line segment edges that are on identical lines, but are created
+///   separately.
+/// - Geometric validation doesn't apply either. It simply doesn't matter, if
+///   curves or surfaces intersect, for example, as long as they don't do that
+///   where an edge or face is defined.
 pub struct Geometry;
 
 impl Geometry {
