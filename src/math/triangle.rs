e057635bc66b04dafee4be63b5d06ff0fa85467e
Daniel Egger <daniel@eggers-club.de>
2022-03-12 14:27:51 +0100

    Allow Triangles to carry a color and transfer that to the vertices during meshing
    This moves the hardcoded color specification (still red with full
    opacity) one level closer to the actual model.
    
    Signed-off-by: Daniel Egger <daniel@eggers-club.de>

--- src/math/triangle.rs
+++ src/math/triangle.rs
@@ -2,12 +2,20 @@ use super::{Point, Scalar};
 
 /// A triangle
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
-pub struct Triangle<const D: usize>([Point<D>; 3]);
+pub struct Triangle<const D: usize> {
+    points: [Point<D>; 3],
+    color: [u8; 4],
+}
 
 impl<const D: usize> Triangle<D> {
     /// Access the triangle's points
     pub fn points(&self) -> [Point<D>; 3] {
-        self.0
+        self.points
+    }
+
+    /// Return the specified color of the triangle in RGBA
+    pub fn color(&self) -> [u8; 4] {
+        self.color
     }
 }
 
@@ -27,7 +35,10 @@ impl<const D: usize> From<[Point<D>; 3]> for Triangle<D> {
 
         // A triangle is not valid if it doesn't span any area
         if area != Scalar::from(0.0) {
-            Self(points)
+            Self {
+                points,
+                color: [255, 0, 0, 255],
+            }
         } else {
             panic!("Invalid Triangle specified");
         }
@@ -71,5 +82,14 @@ mod tests {
         let b = Point::from([1.0, 1.0, 1.0]);
         let c = Point::from([2.0, 2.0, 2.0]);
         let _triangle = Triangle::from([a, b, c]);
+    }
+
+    #[test]
+    fn triangle_default_color() {
+        let a = Point::from([0.0, 0.0]);
+        let b = Point::from([1.0, 1.0]);
+        let c = Point::from([1.0, 2.0]);
+        let triangle = Triangle::from([a, b, c]);
+        assert_eq!(triangle.color(), [255, 0, 0, 255]);
     }
 }
