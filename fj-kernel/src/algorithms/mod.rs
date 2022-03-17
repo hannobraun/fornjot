0cebb309db347ccd8acfcafbf46aca64f871694a
Hanno Braun <hanno@braun-odw.eu>
2022-03-17 17:09:20 +0100

    Make modules in `algorihms` private
    This simplifies the public API.

--- fj-kernel/src/algorithms/mod.rs
+++ fj-kernel/src/algorithms/mod.rs
@@ -1,6 +1,6 @@
-pub mod approximation;
-pub mod sweep;
-pub mod triangulation;
+mod approximation;
+mod sweep;
+mod triangulation;
 
 pub use self::{
     approximation::Approximation, sweep::sweep_shape,
