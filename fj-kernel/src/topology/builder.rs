180ddcfc24dc995baeae739c3510b7689e27a26f
Hanno Braun <hanno@braun-odw.eu>
2022-03-24 16:29:04 +0100

    In `VertexBuilder`, re-use objects, if they exists
    This makes the builder APIs more flexible, allowing for methods that
    build a cycle out of points, for example.

--- fj-kernel/src/topology/builder.rs
+++ fj-kernel/src/topology/builder.rs
@@ -23,8 +23,8 @@ impl<'r> VertexBuilder<'r> {
         self,
         point: impl Into<Point<3>>,
     ) -> ValidationResult<Vertex> {
-        let point = self.shape.insert(point.into())?;
-        let vertex = self.shape.insert(Vertex { point })?;
+        let point = self.shape.get_handle_or_insert(point.into())?;
+        let vertex = self.shape.get_handle_or_insert(Vertex { point })?;
 
         Ok(vertex)
     }
