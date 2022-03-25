3c67279e917d83d745d2456cf3a4bb8786f0a7db
Hanno Braun <hanno@braun-odw.eu>
2022-03-25 16:37:00 +0100

    Remove lifetime from `shape::Iter`
    I'm working on some changes, and `Iter`'s lifetime is really getting in
    the way of progress here. I think dumbing `Iter` down, at the cost of an
    allocation, is the right move for now.

--- fj-kernel/src/shape/iter.rs
+++ fj-kernel/src/shape/iter.rs
@@ -1,37 +1,38 @@
-use super::{
-    handle::Handle,
-    stores::{self, Store},
-};
+use super::{handle::Handle, stores::Store};
 
 /// An iterator over geometric or topological objects
 ///
 /// Returned by various methods of the [`Shape`] API.
-pub struct Iter<'r, T> {
-    inner: stores::Iter<'r, T>,
+pub struct Iter<T> {
+    elements: Vec<Handle<T>>,
 }
 
-impl<'r, T> Iter<'r, T> {
-    pub(super) fn new(store: &'r Store<T>) -> Self {
+impl<T> Iter<T> {
+    pub(super) fn new(store: &Store<T>) -> Self {
+        // The allocation here is unfortunate, but I think it's fine for now. If
+        // this turns into a performance issue, it should be possible to avoid
+        // it by adding methods to `Store`, that are geared at implementing
+        // iterators.
         Self {
-            inner: store.iter(),
+            elements: store.iter().collect(),
         }
     }
 
     /// Convert this iterator over handles into an iterator over values
     ///
     /// This is a convenience method, for cases where no `Handle` is needed.
-    pub fn values(self) -> impl Iterator<Item = T> + 'r
+    pub fn values(self) -> impl Iterator<Item = T>
     where
         T: Clone,
     {
-        self.inner.map(|handle| handle.get().clone())
+        self.elements.into_iter().map(|handle| handle.get())
     }
 }
 
-impl<T> Iterator for Iter<'_, T> {
+impl<T> Iterator for Iter<T> {
     type Item = Handle<T>;
 
     fn next(&mut self) -> Option<Self::Item> {
-        self.inner.next()
+        self.elements.pop()
     }
 }
