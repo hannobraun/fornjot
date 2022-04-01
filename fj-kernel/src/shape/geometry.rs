360927a97990edf7f7ba5e5fe75142d0df69e2b8
Hanno Braun <hanno@braun-odw.eu>
2022-04-01 17:18:24 +0200

    Remove obsolete documentation

--- fj-kernel/src/shape/geometry.rs
+++ fj-kernel/src/shape/geometry.rs
@@ -11,20 +11,6 @@ use super::{
 };
 
 /// API to access a shape's geometry
-///
-/// Other than topology, geometry doesn't need to be validated. Hence adding
-/// geometry is infallible.
-///
-/// There are several reasons for this:
-/// - Geometry doesn't refer to other objects, so structural validation doesn't
-///   apply.
-/// - There simply no reason that geometry needs to be unique. In addition, it's
-///   probably quite hard to rule out generating duplicate geometry. Think about
-///   line segment edges that are on identical lines, but are created
-///   separately.
-/// - Geometric validation doesn't apply either. It simply doesn't matter, if
-///   curves or surfaces intersect, for example, as long as they don't do that
-///   where an edge or face is defined.
 pub struct Geometry<'r> {
     pub(super) points: &'r mut Points,
     pub(super) curves: &'r mut Curves,
