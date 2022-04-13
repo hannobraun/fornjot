ab4d83bf345da8b561fe504a81e1edb8ab555530
Hanno Braun <hanno@braun-odw.eu>
2022-04-13 16:25:55 +0200

    Add initial version of validator
    This initial version just exports each model, failing if there's an
    error. Eventually, it should also validate the exported 3MF file. But
    the current version is already useful, and can replace the manually
    specified model export steps in the CI build.

---
+++ tools/validator/src/main.rs
@@ -0,0 +1,27 @@
+use std::{fs, process::Command};
+
+use anyhow::{anyhow, bail};
+
+fn main() -> anyhow::Result<()> {
+    for model in fs::read_dir("models")? {
+        let model = model?;
+        let model = model.file_name().into_string().map_err(|err| {
+            anyhow!("Failed to convert directory name to `String`: {:?}", err)
+        })?;
+
+        let export_file = format!("{model}.3mf");
+
+        let exit_status = Command::new("cargo")
+            .arg("run")
+            .arg("--")
+            .args(["--model", &model])
+            .args(["--export", &export_file])
+            .status()?;
+
+        if !exit_status.success() {
+            bail!("Exporting model failed with error status: {}", exit_status);
+        }
+    }
+
+    Ok(())
+}
