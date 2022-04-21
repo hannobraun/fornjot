5311d5599b9fb619d60ddc78770e6bb5f0e85064
Hanno Braun <hanno@braun-odw.eu>
2022-04-21 21:44:20 +0200

    Rename `approximation` module to `approx`
    The new name is not so horribly long, and also matches the names of the
    types it contains.

--- crates/fj-kernel/src/algorithms/mod.rs
+++ crates/fj-kernel/src/algorithms/mod.rs
@@ -3,12 +3,12 @@
 //! Algorithmic code is collected in this module, to keep other modules focused
 //! on their respective purpose.
 
-mod approximation;
+mod approx;
 mod sweep;
 mod triangulation;
 
 pub use self::{
-    approximation::{CycleApprox, FaceApprox, Tolerance},
+    approx::{CycleApprox, FaceApprox, Tolerance},
     sweep::sweep_shape,
     triangulation::triangulate,
 };
