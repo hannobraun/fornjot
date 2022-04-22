6fcb02ffaec9c9594e5b30c7f5ff82b126ba171a
Hanno Braun <hanno@braun-odw.eu>
2022-04-22 13:49:53 +0200

    Move `CycleApprox` to dedicated module

--- crates/fj-kernel/src/algorithms/approx/mod.rs
+++ crates/fj-kernel/src/algorithms/approx/mod.rs
@@ -1,12 +1,13 @@
+mod cycles;
 mod edges;
+
+pub use self::cycles::CycleApprox;
 
 use std::collections::HashSet;
 
-use fj_math::{Point, Scalar, Segment};
+use fj_math::{Point, Scalar};
 
-use crate::topology::{Cycle, Face};
-
-use self::edges::approximate_edge;
+use crate::topology::Face;
 
 /// An approximation of a [`Face`]
 #[derive(Debug, PartialEq)]
@@ -77,61 +78,6 @@ impl FaceApprox {
             exterior,
             interiors,
         }
-    }
-}
-
-/// An approximation of a [`Cycle`]
-#[derive(Debug, Eq, PartialEq, Hash)]
-pub struct CycleApprox {
-    /// The points that approximate the cycle
-    pub points: Vec<Point<3>>,
-}
-
-impl CycleApprox {
-    /// Compute the approximation of a cycle
-    ///
-    /// `tolerance` defines how far the approximation is allowed to deviate from
-    /// the actual face.
-    pub fn new(cycle: &Cycle, tolerance: Tolerance) -> Self {
-        let mut points = Vec::new();
-
-        for edge in cycle.edges() {
-            let mut edge_points = Vec::new();
-            edge.curve().approx(tolerance, &mut edge_points);
-
-            points.extend(approximate_edge(edge_points, edge.vertices()));
-        }
-
-        points.dedup();
-
-        Self { points }
-    }
-
-    /// Construct the segments that approximate the cycle
-    pub fn segments(&self) -> Vec<Segment<3>> {
-        let mut segments = Vec::new();
-
-        for segment in self.points.windows(2) {
-            // This can't panic, as we passed `2` to `windows`. Can be cleaned
-            // up, once `array_windows` is stable.
-            let p0 = segment[0];
-            let p1 = segment[1];
-
-            segments.push(Segment::from([p0, p1]));
-        }
-
-        segments
-    }
-}
-
-impl<T, P> From<T> for CycleApprox
-where
-    T: IntoIterator<Item = P>,
-    P: Into<Point<3>>,
-{
-    fn from(points: T) -> Self {
-        let points = points.into_iter().map(Into::into).collect();
-        Self { points }
     }
 }
 
