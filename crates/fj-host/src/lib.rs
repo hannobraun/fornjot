d4cfcb073aefb8df14862f717926f706f4df9502
Hanno Braun <hanno@braun-odw.eu>
2022-05-03 14:51:21 +0200

    Update top-level documentation of `fj-host`

--- crates/fj-host/src/lib.rs
+++ crates/fj-host/src/lib.rs
@@ -1,7 +1,17 @@
-//! Plugin host for Fornjot models
+//! # Fornjot Model Host
 //!
-//! Fornjot models are loaded into the Fornjot application as plugins. This
-//! crate contains the code required to host plugins.
+//! This library is part of the [Fornjot] ecosystem. Fornjot is an open-source,
+//! code-first CAD application; and collection of libraries that make up the CAD
+//! application, but can be used independently.
+//!
+//! This library is an internal component of Fornjot. It is not relevant to end
+//! users that just want to create CAD models.
+//!
+//! The purpose of this library is to load Fornjot models and watch them for
+//! changes. Fornjot models are basically plugins that can be loaded into a CAD
+//! application. This library is the host for these model plugins.
+//!
+//! [Fornjot]: https://www.fornjot.app/
 
 #![deny(missing_docs)]
 
