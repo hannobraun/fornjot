e984681095f3e70fa0de6441ec47b6400968e6d2
Hanno Braun <hanno@braun-odw.eu>
2022-05-03 14:53:00 +0200

    Update top-level documentation of `fj-interop`

--- crates/fj-interop/src/lib.rs
+++ crates/fj-interop/src/lib.rs
@@ -1,4 +1,16 @@
-//! Data types for interoperation within the Fornjot ecosystem
+//! # Fornjot Interop Types
+//!
+//! This library is part of the [Fornjot] ecosystem. Fornjot is an open-source,
+//! code-first CAD application; and collection of libraries that make up the CAD
+//! application, but can be used independently.
+//!
+//! This library is an internal component of Fornjot. It is not relevant to end
+//! users that just want to create CAD models.
+//!
+//! The purpose of this library is to define types that allow other components
+//! of the Fornjot ecosystem to interoperate, without depending on each other.
+//!
+//! [Fornjot]: https://www.fornjot.app/
 
 #![deny(missing_docs)]
 
