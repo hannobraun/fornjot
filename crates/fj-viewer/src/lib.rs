4a997568cf78bf7670fd0210876506ee9a3ead16
Hanno Braun <hanno@braun-odw.eu>
2022-05-03 15:18:57 +0200

    Update top-level documentation of `fj-viewer`

--- crates/fj-viewer/src/lib.rs
+++ crates/fj-viewer/src/lib.rs
@@ -1,6 +1,16 @@
-//! Fornjot Model Viewer
+//! # Fornjot Model Viewer
 //!
-//! A model viewer which allows basic navigation and rendering of generated models.
+//! This library is part of the [Fornjot] ecosystem. Fornjot is an open-source,
+//! code-first CAD application; and collection of libraries that make up the CAD
+//! application, but can be used independently.
+//!
+//! This library is an internal component of Fornjot. It is not relevant to end
+//! users that just want to create CAD models.
+//!
+//! This library provides a model viewer which allows basic navigation and
+//! rendering of generated models.
+//!
+//! [Fornjot]: https://www.fornjot.app/
 
 #![warn(missing_docs)]
 
