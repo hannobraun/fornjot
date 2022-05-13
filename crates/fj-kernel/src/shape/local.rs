6b49299931b73f4af52459b7fa429b627e65d645
Hanno Braun <hanno@braun-odw.eu>
2022-05-13 16:07:23 +0200

    Add `LocalForm`
    This is a generic version of `EdgeVertex`, which this is going to
    replace.

---
+++ crates/fj-kernel/src/shape/local.rs
@@ -0,0 +1,46 @@
+use std::hash::{Hash, Hasher};
+
+use super::{Handle, Object};
+
+/// A reference to an object, which includes a local form
+///
+/// This type is used by topological objects to reference other objects, while
+/// also keeping track of a local representation of that object, which is often
+/// more appropriate for various tasks.
+///
+/// # Equality
+///
+/// Since `LocalForm` is used by topological objects, its equality is defined in
+/// terms that are useful to those objects. Two instances of `LocalForm` are
+/// equal, if both the local and the canonical forms are equal. The equality of
+/// the handle that refers to the canonical form is disregarded.
+#[derive(Clone, Debug, Eq, Ord, PartialOrd)]
+pub struct LocalForm<Local, Canonical: Object> {
+    /// The local form of the referenced object
+    pub local: Local,
+
+    /// The canonical form of the referenced object
+    pub canonical: Handle<Canonical>,
+}
+
+impl<Local, Canonical: Object> PartialEq for LocalForm<Local, Canonical>
+where
+    Local: PartialEq,
+    Canonical: PartialEq,
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.local == other.local
+            && self.canonical.get() == other.canonical.get()
+    }
+}
+
+impl<Local, Canonical: Object> Hash for LocalForm<Local, Canonical>
+where
+    Local: Hash,
+    Canonical: Hash,
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.local.hash(state);
+        self.canonical.get().hash(state);
+    }
+}
--- crates/fj-kernel/src/shape/mod.rs
+++ crates/fj-kernel/src/shape/mod.rs
@@ -3,12 +3,14 @@
 //! See [`Shape`], which is the main entry point to this API.
 
 mod api;
+mod local;
 mod object;
 mod stores;
 mod validate;
 
 pub use self::{
     api::Shape,
+    local::LocalForm,
     object::Object,
     stores::{Handle, Iter},
     validate::{StructuralIssues, ValidationError, ValidationResult},
