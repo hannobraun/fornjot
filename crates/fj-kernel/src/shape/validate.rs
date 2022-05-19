7eb825f2ed59830f7b4024dc0297c647d4553977
Hanno Braun <hanno@braun-odw.eu>
2022-05-19 15:33:26 +0200

    Implement `fmt::Display` for `StructuralIssues`

--- crates/fj-kernel/src/shape/validate.rs
+++ crates/fj-kernel/src/shape/validate.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::{collections::HashSet, fmt};
 
 use fj_math::{Point, Scalar};
 
@@ -314,4 +314,43 @@ pub struct StructuralIssues {
 
     /// Missing cycles found in face validation
     pub missing_cycles: HashSet<Handle<Cycle<3>>>,
+}
+
+impl fmt::Display for StructuralIssues {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        writeln!(f, "Structural issues found:")?;
+
+        if let Some(point) = &self.missing_point {
+            writeln!(f, "- Missing point: {:?}", point.get())?;
+        }
+        if let Some(curve) = &self.missing_curve {
+            writeln!(f, "- Missing curve: {:?}", curve.get())?;
+        }
+        if !self.missing_vertices.is_empty() {
+            writeln!(f, "- Missing vertices:")?;
+
+            for vertex in &self.missing_vertices {
+                writeln!(f, "  - {:?}", vertex.get())?;
+            }
+        }
+        if !self.missing_edges.is_empty() {
+            writeln!(f, "- Missing edges:")?;
+
+            for edge in &self.missing_edges {
+                writeln!(f, "  - {}", edge.get())?;
+            }
+        }
+        if let Some(surface) = &self.missing_surface {
+            writeln!(f, "- Missing surface: {:?}", surface.get())?;
+        }
+        if !self.missing_cycles.is_empty() {
+            writeln!(f, "- Missing cycles:")?;
+
+            for cycle in &self.missing_cycles {
+                writeln!(f, "  - {:?}", cycle.get())?;
+            }
+        }
+
+        Ok(())
+    }
 }
