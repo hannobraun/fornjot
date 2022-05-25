a7fc50441334ea91a22b248c4ca18e8d3df45628
Hanno Braun <hanno@braun-odw.eu>
2022-05-25 13:13:56 +0200

    Simplify group operation

--- crates/fj-operations/src/group.rs
+++ crates/fj-operations/src/group.rs
@@ -18,8 +18,8 @@ impl ToShape for fj::Group {
         let a = self.a.to_shape(tolerance, debug_info)?;
         let b = self.b.to_shape(tolerance, debug_info)?;
 
-        copy_shape(a, &mut shape);
-        copy_shape(b, &mut shape);
+        shape.merge_shape(&a)?;
+        shape.merge_shape(&b)?;
 
         Ok(shape)
     }
@@ -29,11 +29,5 @@ impl ToShape for fj::Group {
         let b = self.b.bounding_volume();
 
         a.merged(&b)
-    }
-}
-
-fn copy_shape(orig: Shape, target: &mut Shape) {
-    for face_orig in orig.faces() {
-        target.merge(face_orig.get()).unwrap();
     }
 }
