7aebd1c0fc1bfd852f9f5c20fefcb2e56104a6a8
Hanno Braun <hanno@braun-odw.eu>
2022-06-02 13:53:40 +0200

    Add more convenient API to `Mapping`

--- crates/fj-kernel/src/shape/mapping.rs
+++ crates/fj-kernel/src/shape/mapping.rs
@@ -33,6 +33,90 @@ impl Mapping {
         }
     }
 
+    /// Access the point mapped from the provided point
+    ///
+    /// # Panics
+    ///
+    /// Panics, if `object` can not be found in the mapping.
+    pub fn point(&self, object: &Handle<Point<3>>) -> Handle<Point<3>> {
+        self.points
+            .get(object)
+            .expect("Could not find point in mapping")
+            .clone()
+    }
+
+    /// Access the curve mapped from the provided curve
+    ///
+    /// # Panics
+    ///
+    /// Panics, if `object` can not be found in the mapping.
+    pub fn curve(&self, object: &Handle<Curve<3>>) -> Handle<Curve<3>> {
+        self.curves
+            .get(object)
+            .expect("Could not find curve in mapping")
+            .clone()
+    }
+
+    /// Access the surface mapped from the provided surface
+    ///
+    /// # Panics
+    ///
+    /// Panics, if `object` can not be found in the mapping.
+    pub fn surface(&self, object: &Handle<Surface>) -> Handle<Surface> {
+        self.surfaces
+            .get(object)
+            .expect("Could not find surface in mapping")
+            .clone()
+    }
+
+    /// Access the vertex mapped from the provided vertex
+    ///
+    /// # Panics
+    ///
+    /// Panics, if `object` can not be found in the mapping.
+    pub fn vertex(&self, object: &Handle<Vertex>) -> Handle<Vertex> {
+        self.vertices
+            .get(object)
+            .expect("Could not find vertex in mapping")
+            .clone()
+    }
+
+    /// Access the edge mapped from the provided edge
+    ///
+    /// # Panics
+    ///
+    /// Panics, if `object` can not be found in the mapping.
+    pub fn edge(&self, object: &Handle<Edge<3>>) -> Handle<Edge<3>> {
+        self.edges
+            .get(object)
+            .expect("Could not find edge in mapping")
+            .clone()
+    }
+
+    /// Access the cycle mapped from the provided cycle
+    ///
+    /// # Panics
+    ///
+    /// Panics, if `object` can not be found in the mapping.
+    pub fn cycle(&self, object: &Handle<Cycle<3>>) -> Handle<Cycle<3>> {
+        self.cycles
+            .get(object)
+            .expect("Could not find vertex in mapping")
+            .clone()
+    }
+
+    /// Access the face mapped from the provided face
+    ///
+    /// # Panics
+    ///
+    /// Panics, if `object` can not be found in the mapping.
+    pub fn face(&self, object: &Handle<Face>) -> Handle<Face> {
+        self.faces
+            .get(object)
+            .expect("Could not find face in mapping")
+            .clone()
+    }
+
     /// Access iterator over the mapped points
     pub fn points(&self) -> &OneMapping<Point<3>> {
         &self.points
