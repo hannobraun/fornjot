432d89f6dffd139d799b9b2c6077ab0c0bc1de3d
Hanno Braun <hanno@braun-odw.eu>
2022-06-02 13:55:34 +0200

    Remove obsolete methods

--- crates/fj-kernel/src/shape/mapping.rs
+++ crates/fj-kernel/src/shape/mapping.rs
@@ -116,41 +116,6 @@ impl Mapping {
             .expect("Could not find face in mapping")
             .clone()
     }
-
-    /// Access iterator over the mapped points
-    pub fn points(&self) -> &OneMapping<Point<3>> {
-        &self.points
-    }
-
-    /// Access iterator over the mapped curves
-    pub fn curves(&self) -> &OneMapping<Curve<3>> {
-        &self.curves
-    }
-
-    /// Access iterator over the mapped surfaces
-    pub fn surfaces(&self) -> &OneMapping<Surface> {
-        &self.surfaces
-    }
-
-    /// Access iterator over the mapped vertices
-    pub fn vertices(&self) -> &OneMapping<Vertex> {
-        &self.vertices
-    }
-
-    /// Access iterator over the mapped edges
-    pub fn edges(&self) -> &OneMapping<Edge<3>> {
-        &self.edges
-    }
-
-    /// Access iterator over the mapped cycles
-    pub fn cycles(&self) -> &OneMapping<Cycle<3>> {
-        &self.cycles
-    }
-
-    /// Access iterator over the mapped faces
-    pub fn faces(&self) -> &OneMapping<Face> {
-        &self.faces
-    }
 }
 
 pub type OneMapping<T> = HashMap<Handle<T>, Handle<T>>;
