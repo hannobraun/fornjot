831a18a29804aedd7f3663a2d2bce51e1ec8126d
Hanno Braun <hanno@braun-odw.eu>
2022-06-14 14:33:35 +0200

    Update documentation of `geometry` module

--- crates/fj-kernel/src/geometry/mod.rs
+++ crates/fj-kernel/src/geometry/mod.rs
@@ -1,9 +1,4 @@
-//! Geometry objects
-//!
-//! Simplifying a bit, geometry is responsible for where things are, but now how
-//! they are related. The types in this module are referred to by the types in
-//! [`crate::topology`], which are responsible for defining how objects are
-//! related.
+//! Miscellaneous geometry code
 
 mod points;
 
