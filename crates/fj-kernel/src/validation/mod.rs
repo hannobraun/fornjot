42a8293a7853feb4418885d0bddf9fa0abe7867c
Hanno Braun <hanno@braun-odw.eu>
2022-06-20 14:19:04 +0200

    Add new validation infrastructure
    Nothing is actually validated yet using the new infrastructure. It's
    just a placeholder, and as a next step, in can be hooked into all the
    places that require validation. The actual validation code can be
    migrated from the old infrastructure later, piece by piece.

--- crates/fj-kernel/src/validation/mod.rs
+++ crates/fj-kernel/src/validation/mod.rs
@@ -24,10 +24,71 @@
 //!
 //! [`Shape`]: crate::shape::Shape
 
+use std::ops::Deref;
+
+use fj_math::Scalar;
+
 use crate::{
     objects::{Curve, Cycle, Edge, Surface, Vertex},
-    shape::{CoherenceIssues, Handle, StructuralIssues, UniquenessIssues},
+    shape::{
+        CoherenceIssues, Handle, Shape, StructuralIssues, UniquenessIssues,
+    },
 };
+
+/// Validate the given [`Shape`]
+pub fn validate(
+    shape: Shape,
+    _: &Config,
+) -> Result<Validated<Shape>, ValidationError> {
+    Ok(Validated(shape))
+}
+
+/// Configuration required for the validation process
+#[derive(Debug, Clone, Copy)]
+pub struct Config {
+    /// The minimum distance between distinct objects
+    ///
+    /// Objects whose distance is less than the value defined in this field, are
+    /// considered identical.
+    pub distinct_min_distance: Scalar,
+
+    /// The maximum distance between identical objects
+    ///
+    /// Objects that are considered identical might still have a distance
+    /// between them, due to inaccuracies of the numerical representation. If
+    /// that distance is less than the one defined in this field, can not be
+    /// considered identical.
+    pub identical_max_distance: Scalar,
+}
+
+impl Default for Config {
+    fn default() -> Self {
+        Self {
+            distinct_min_distance: Scalar::from_f64(5e-7), // 0.5 µm,
+            identical_max_distance: Scalar::from_f64(5e-14),
+        }
+    }
+}
+
+/// Wrapper around an object that indicates the object has been validated
+///
+/// Returned by implementations of `Validate`.
+pub struct Validated<T>(T);
+
+impl<T> Validated<T> {
+    /// Consume this instance of `Validated` and return the wrapped object
+    pub fn into_inner(self) -> T {
+        self.0
+    }
+}
+
+impl<T> Deref for Validated<T> {
+    type Target = T;
+
+    fn deref(&self) -> &Self::Target {
+        &self.0
+    }
+}
 
 /// An error that can occur during a validation
 #[allow(clippy::large_enum_variant)]
