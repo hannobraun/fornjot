3df2877f3bf6070c1a46e8a9e6d1698de7b7958b
Hanno Braun <hanno@braun-odw.eu>
2022-06-20 17:10:18 +0200

    Remove unused code

--- crates/fj-kernel/src/shape/api.rs
+++ crates/fj-kernel/src/shape/api.rs
@@ -14,7 +14,6 @@ use super::{
 #[derive(Clone, Debug)]
 pub struct Shape {
     distinct_min_distance: Scalar,
-    identical_max_distance: Scalar,
 
     stores: Stores,
 }
@@ -27,16 +26,6 @@ impl Shape {
             // similarly named constant. Unfortunately `Scalar::from_f64` can't
             // be `const` yet.
             distinct_min_distance: Scalar::from_f64(5e-7), // 0.5 µm
-
-            // This value was chosen pretty arbitrarily. Seems small enough to
-            // catch errors. If it turns out it's too small (because it produces
-            // false positives due to floating-point accuracy issues), we can
-            // adjust it.
-            //
-            // This should be defined in an associated constant, so API users
-            // can see what the default is. Unfortunately, `Scalar::from_f64`
-            // can't be `const` yet.
-            identical_max_distance: Scalar::from_f64(5e-14),
 
             stores: Stores {
                 curves: Store::new(),
@@ -77,17 +66,6 @@ impl Shape {
         distinct_min_distance: impl Into<Scalar>,
     ) -> Self {
         self.distinct_min_distance = distinct_min_distance.into();
-        self
-    }
-
-    /// Override the maximum distance between objects considered identical
-    ///
-    /// Used for geometric validation.
-    pub fn with_identical_max_distance(
-        mut self,
-        identical_max_distance: impl Into<Scalar>,
-    ) -> Self {
-        self.identical_max_distance = identical_max_distance.into();
         self
     }
 
