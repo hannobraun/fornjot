85c944e195b4698719cd7f5b79be457422db8e42
Hanno Braun <hanno@braun-odw.eu>
2022-06-28 15:54:31 +0200

    Update variable name

--- crates/fj-operations/src/sketch.rs
+++ crates/fj-operations/src/sketch.rs
@@ -16,12 +16,12 @@ impl ToShape for fj::Sketch {
         _: Tolerance,
         _: &mut DebugInfo,
     ) -> Result<Validated<Vec<Face>>, ValidationError> {
-        let mut shape = Shape::new();
+        let mut tmp = Shape::new();
 
         let surface = Surface::xy_plane();
         let points = self.to_points().into_iter().map(Point::from);
 
-        let sketch = Face::builder(surface, &mut shape)
+        let sketch = Face::builder(surface, &mut tmp)
             .with_exterior_polygon(points)
             .with_color(self.color())
             .build()
