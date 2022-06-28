503cbe18ae694bea3e6b0e84b98fb52bc489d34f
Hanno Braun <hanno@braun-odw.eu>
2022-06-28 16:37:52 +0200

    Refactor

--- crates/fj-operations/src/transform.rs
+++ crates/fj-operations/src/transform.rs
@@ -18,8 +18,7 @@ impl ToShape for fj::Transform {
         let shape = self.shape.to_shape(config, tolerance, debug_info)?;
         let shape = shape.into_inner();
 
-        let transform = make_transform(self);
-        let faces = transform_shape(&shape, &transform);
+        let faces = transform_shape(&shape, &make_transform(self));
 
         let mut target = Shape::new();
         for face in faces {
