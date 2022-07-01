9176814b7f97eb4d73a0d49eafecb957a984fe69
follower <follower@rancidbacon.com>
2022-07-02 06:45:53 +1200

    Replace UI with full demo UI from original proof-of-concept.

--- crates/fj-viewer/src/graphics/renderer.rs
+++ crates/fj-viewer/src/graphics/renderer.rs
@@ -376,8 +376,157 @@ impl Renderer {
         let egui_input = self.egui.winit_state.take_egui_input(window);
         self.egui.context.begin_frame(egui_input);
 
+        fn get_bbox_size_text(aabb: &Aabb<3>) -> String {
+            /* Render size of model bounding box */
+            let bbsize = aabb.size().components;
+            let info = format!(
+                "Model bounding box size:\n{:0.1} {:0.1} {:0.1}",
+                bbsize[0].into_f32(),
+                bbsize[1].into_f32(),
+                bbsize[2].into_f32()
+            );
+            info
+        }
+
         egui::SidePanel::left("fj-left-panel").show(&self.egui.context, |ui| {
-            ui.label("Fornjot").on_hover_text_at_pointer("with egui!");
+            ui.add_space(16.0);
+
+            ui.group(|ui| {
+                ui.checkbox(&mut config.draw_model, "Render model")
+                    .on_hover_text_at_pointer("Toggle with 1");
+                ui.checkbox(&mut config.draw_mesh, "Render mesh")
+                    .on_hover_text_at_pointer("Toggle with 2");
+                ui.checkbox(&mut config.draw_debug, "Render debug")
+                    .on_hover_text_at_pointer("Toggle with 3");
+                ui.checkbox(
+                    &mut self.egui.options.show_original_ui,
+                    "Render original UI",
+                );
+                ui.add_space(16.0);
+                ui.strong(get_bbox_size_text(&self.geometries.aabb));
+            });
+
+            ui.add_space(16.0);
+
+            {
+                ui.group(|ui| {
+                    ui.checkbox(
+                        &mut self.egui.options.show_settings_ui,
+                        "Show egui settings UI",
+                    );
+                    if self.egui.options.show_settings_ui {
+                        self.egui.context.settings_ui(ui);
+                    }
+                });
+
+                ui.add_space(16.0);
+
+                ui.group(|ui| {
+                    ui.checkbox(
+                        &mut self.egui.options.show_inspection_ui,
+                        "Show egui inspection UI",
+                    );
+                    if self.egui.options.show_inspection_ui {
+                        ui.indent("indent-inspection-ui", |ui| {
+                            self.egui.context.inspection_ui(ui);
+                        });
+                    }
+                });
+            }
+
+            ui.add_space(16.0);
+
+            {
+                //
+                // Originally this was only meant to be a simple demonstration
+                // of the `egui` `trace!()` macro...
+                //
+                // ...but it seems the trace feature can't be enabled
+                // separately from the layout debug feature, which all
+                // gets a bit messy...
+                //
+                // ...so, this instead shows one possible way to implement
+                // "trace only" style debug text on hover.
+                //
+                ui.group(|ui| {
+                    let label_text = format!(
+                        "Show debug text demo.{}",
+                        if self.egui.options.show_debug_text_example {
+                            " (Hover me.)"
+                        } else {
+                            ""
+                        }
+                    );
+
+                    ui.style_mut().wrap = Some(false);
+
+                    if ui
+                        .checkbox(
+                            &mut self.egui.options.show_debug_text_example,
+                            label_text,
+                        )
+                        .hovered()
+                    {
+                        if self.egui.options.show_debug_text_example {
+                            let hover_pos = ui
+                                .input()
+                                .pointer
+                                .hover_pos()
+                                .unwrap_or_default();
+                            ui.painter().debug_text(
+                                hover_pos,
+                                egui::Align2::LEFT_TOP,
+                                egui::Color32::DEBUG_COLOR,
+                                format!("{:#?}", &config),
+                            );
+                        }
+                    }
+                });
+            }
+
+            ui.add_space(16.0);
+
+            {
+                //
+                // Demonstration of the `egui` layout debug functionality.
+                //
+                ui.group(|ui| {
+                    //
+
+                    if ui
+                        .checkbox(
+                            &mut self.egui.options.show_layout_debug_on_hover,
+                            "Show layout debug on hover.",
+                        )
+                        .changed()
+                    {
+                        ui.ctx().set_debug_on_hover(
+                            self.egui.options.show_layout_debug_on_hover,
+                        );
+                    }
+
+                    ui.scope(|ui| {
+                        if self.egui.options.show_trace {
+                            egui::trace!(ui, format!("{:?}", &config));
+                        }
+                    });
+
+                    ui.indent("indent-show-trace", |ui| {
+                        ui.set_enabled(
+                            self.egui.options.show_layout_debug_on_hover,
+                        );
+
+                        ui.checkbox(
+                            &mut self.egui.options.show_trace,
+                            "Also show egui trace.",
+                        );
+
+                        //
+                    });
+                });
+            }
+
+            ui.add_space(16.0);
         });
 
         // End the UI frame. We could now handle the output and draw the UI with the backend.
