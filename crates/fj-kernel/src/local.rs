5ecf133bf9205ff9c5d2cfcc43598c84ff6559da
Hanno Braun <hanno@braun-odw.eu>
2022-07-01 17:37:48 +0200

    Update documentation of `Local`

--- crates/fj-kernel/src/local.rs
+++ crates/fj-kernel/src/local.rs
@@ -2,11 +2,15 @@
 
 use crate::objects::Curve;
 
-/// A reference to an object, which includes a local form
+/// A wrapper around the local and global forms of a type
 ///
-/// This type is used by topological objects to reference other objects, while
-/// also keeping track of a local representation of that object, which is often
-/// more appropriate for various tasks.
+/// The local form is whatever representation of the value that is most
+/// appropriate in a given local context, which might be a curve or surface. The
+/// global form is the global 3D form of the same value.
+///
+/// The purpose of storing both forms is to be able to losslessly convert
+/// between them. Even if this conversion can be computed on the fly, it might
+/// be lossy due to floating point accuracy issues.
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub struct Local<T: LocalForm> {
     local: T,
@@ -14,20 +18,20 @@ pub struct Local<T: LocalForm> {
 }
 
 impl<T: LocalForm> Local<T> {
-    /// Construct a new instance of `LocalForm`
+    /// Construct a new instance
     ///
-    /// It is the caller's responsibility to make sure that the local and
-    /// canonical forms passed to this method actually match.
+    /// It is the caller's responsibility to make sure that the local and global
+    /// forms passed into this constructor match.
     pub fn new(local: T, global: T::GlobalForm) -> Self {
         Self { local, global }
     }
 
-    /// Access the local form of the referenced object
+    /// Access the local form of the value
     pub fn local(&self) -> T {
         self.local
     }
 
-    /// Access the canonical form of the referenced object
+    /// Access the global form of the value
     pub fn global(&self) -> T::GlobalForm {
         self.global
     }
