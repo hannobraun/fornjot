98576a0a6b01ad7915b8bc6a05de60466446ebd8
Hanno Braun <hanno@braun-odw.eu>
2022-07-13 15:42:04 +0200

    Convert free function into method

--- crates/fj-kernel/src/algorithms/intersection/curve_face.rs
+++ crates/fj-kernel/src/algorithms/intersection/curve_face.rs
@@ -7,87 +7,93 @@ use crate::objects::{Curve, Face};
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub struct CurveFaceIntersections(Vec<[Scalar; 2]>);
 
-/// Determine the intersection between a [`Curve`] and a [`Face`]
-///
-/// Returns a list of intersections in curve coordinates.
-pub fn curve_face(curve: &Curve<2>, face: &Face) -> CurveFaceIntersections {
-    let line = match curve {
-        Curve::Line(line) => line,
-        _ => todo!("Curve-face intersection only supports lines"),
-    };
+impl CurveFaceIntersections {
+    /// Determine the intersection between a [`Curve`] and a [`Face`]
+    ///
+    /// Returns a list of intersections in curve coordinates.
+    pub fn curve_face(curve: &Curve<2>, face: &Face) -> CurveFaceIntersections {
+        let line = match curve {
+            Curve::Line(line) => line,
+            _ => todo!("Curve-face intersection only supports lines"),
+        };
 
-    let face_as_polygon = face
-        .exteriors()
-        .chain(face.interiors())
-        .flat_map(|cycle| {
-            let edges: Vec<_> = cycle.edges().collect();
-            edges
-        })
-        .map(|edge| {
-            let line = match edge.curve.local() {
-                Curve::Line(line) => line,
-                _ => todo!("Curve-face intersection only supports polygons"),
-            };
+        let face_as_polygon = face
+            .exteriors()
+            .chain(face.interiors())
+            .flat_map(|cycle| {
+                let edges: Vec<_> = cycle.edges().collect();
+                edges
+            })
+            .map(|edge| {
+                let line = match edge.curve.local() {
+                    Curve::Line(line) => line,
+                    _ => {
+                        todo!("Curve-face intersection only supports polygons")
+                    }
+                };
 
-            let vertices = match edge.vertices() {
-                Some(vertices) => vertices,
-                None => todo!(
-                    "Curve-face intersection does not support faces with \
+                let vertices = match edge.vertices() {
+                    Some(vertices) => vertices,
+                    None => todo!(
+                        "Curve-face intersection does not support faces with \
                     continuous edges"
-                ),
+                    ),
+                };
+
+                (line, vertices)
+            });
+
+        let mut intersections = Vec::new();
+
+        for (edge_line, vertices) in face_as_polygon {
+            let vertices = vertices.map(|vertex| {
+                edge_line.point_from_line_coords(vertex.position())
+            });
+            let segment = Segment::from_points(vertices);
+
+            let ray = Ray {
+                origin: line.origin.to_na(),
+                dir: line.direction.to_na(),
+            };
+            let ray_inv = Ray {
+                origin: line.origin.to_na(),
+                dir: -line.direction.to_na(),
             };
 
-            (line, vertices)
-        });
+            let result =
+                segment
+                    .to_parry()
+                    .cast_local_ray(&ray, f64::INFINITY, false);
+            let result_inv = segment.to_parry().cast_local_ray(
+                &ray_inv,
+                f64::INFINITY,
+                false,
+            );
 
-    let mut intersections = Vec::new();
-
-    for (edge_line, vertices) in face_as_polygon {
-        let vertices = vertices
-            .map(|vertex| edge_line.point_from_line_coords(vertex.position()));
-        let segment = Segment::from_points(vertices);
-
-        let ray = Ray {
-            origin: line.origin.to_na(),
-            dir: line.direction.to_na(),
-        };
-        let ray_inv = Ray {
-            origin: line.origin.to_na(),
-            dir: -line.direction.to_na(),
-        };
-
-        let result =
-            segment
-                .to_parry()
-                .cast_local_ray(&ray, f64::INFINITY, false);
-        let result_inv =
-            segment
-                .to_parry()
-                .cast_local_ray(&ray_inv, f64::INFINITY, false);
-
-        if let Some(result) = result {
-            intersections.push(Scalar::from(result));
+            if let Some(result) = result {
+                intersections.push(Scalar::from(result));
+            }
+            if let Some(result_inv) = result_inv {
+                intersections.push(-Scalar::from(result_inv));
+            }
         }
-        if let Some(result_inv) = result_inv {
-            intersections.push(-Scalar::from(result_inv));
-        }
+
+        assert!(intersections.len() % 2 == 0);
+
+        intersections.sort();
+
+        // Can be cleaned up, once `array_chunks` is stable:
+        // https://doc.rust-lang.org/std/primitive.slice.html#method.array_chunks
+        let intersections = intersections
+            .chunks(2)
+            .map(|chunk| {
+                // Can't panic, as we passed `2` to `windows`.
+                [chunk[0], chunk[1]]
+            })
+            .collect();
+
+        CurveFaceIntersections(intersections)
     }
-
-    assert!(intersections.len() % 2 == 0);
-
-    intersections.sort();
-
-    // Can be cleaned up, once `array_chunks` is stable:
-    // https://doc.rust-lang.org/std/primitive.slice.html#method.array_chunks
-    let intersections = intersections
-        .chunks(2)
-        .map(|chunk| {
-            // Can't panic, as we passed `2` to `windows`.
-            [chunk[0], chunk[1]]
-        })
-        .collect();
-
-    CurveFaceIntersections(intersections)
 }
 
 #[cfg(test)]
@@ -130,6 +136,6 @@ mod tests {
             .map(|interval: [f64; 2]| interval.map(Scalar::from))
             .collect();
         let expected = CurveFaceIntersections(expected);
-        assert_eq!(super::curve_face(&curve, &face), expected);
+        assert_eq!(CurveFaceIntersections::curve_face(&curve, &face), expected);
     }
 }
--- crates/fj-kernel/src/algorithms/intersection/mod.rs
+++ crates/fj-kernel/src/algorithms/intersection/mod.rs
@@ -5,7 +5,7 @@ mod line_segment;
 mod surface_surface;
 
 pub use self::{
-    curve_face::{curve_face, CurveFaceIntersections},
+    curve_face::CurveFaceIntersections,
     line_segment::{line_segment, LineSegmentIntersection},
     surface_surface::surface_surface,
 };
