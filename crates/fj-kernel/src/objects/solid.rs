802d880d1774659c32f82dc832d66e69b7a1a6de
Hanno Braun <hanno@braun-odw.eu>
2022-07-15 12:29:29 +0200

    Add `Solid::cube_from_edge_length`
    This constructor should be well-suited for use in tests.

--- crates/fj-kernel/src/objects/solid.rs
+++ crates/fj-kernel/src/objects/solid.rs
@@ -1,4 +1,8 @@
-use super::Face;
+use fj_math::Scalar;
+
+use crate::algorithms::TransformObject;
+
+use super::{Face, Surface};
 
 /// A 3-dimensional shape
 ///
@@ -16,6 +20,31 @@ impl Solid {
     pub fn from_faces(faces: impl IntoIterator<Item = Face>) -> Self {
         let faces = faces.into_iter().collect();
         Self { faces }
+    }
+
+    /// Create a cube from the length of its edges
+    pub fn cube_from_edge_length(edge_length: impl Into<Scalar>) -> Self {
+        // Let's define a short-hand for half the edge length. We're going to
+        // need it a lot.
+        let h = edge_length.into() / 2.;
+
+        let points = [[-h, -h], [h, -h], [h, h], [-h, h]];
+
+        const Z: Scalar = Scalar::ZERO;
+        let planes = [
+            Surface::xy_plane().translate([Z, Z, -h]), // bottom
+            Surface::xy_plane().translate([Z, Z, h]),  // top
+            Surface::xz_plane().translate([Z, -h, Z]), // front
+            Surface::xz_plane().translate([Z, h, Z]),  // back
+            Surface::yz_plane().translate([-h, Z, Z]), // left
+            Surface::yz_plane().translate([h, Z, Z]),  // right
+        ];
+
+        let faces = planes.map(|plane| {
+            Face::builder(plane).with_exterior_polygon(points).build()
+        });
+
+        Solid::from_faces(faces)
     }
 
     /// Access the solid's faces
