8305deb2a729d41cb234a46dbb09bba5c7fe971e
Hanno Braun <hanno@braun-odw.eu>
2022-07-22 12:46:08 +0200

    Implement `Default` for `Color`

--- crates/fj-interop/src/mesh.rs
+++ crates/fj-interop/src/mesh.rs
@@ -109,3 +109,10 @@ pub struct Triangle {
 /// RGBA color
 #[derive(Clone, Copy, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
 pub struct Color(pub [u8; 4]);
+
+impl Default for Color {
+    fn default() -> Self {
+        // The default color is red. This is an arbitrary choice.
+        Self([255, 0, 0, 255])
+    }
+}
