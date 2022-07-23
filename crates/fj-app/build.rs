fa72e7344afed34ed0fad0f84af2b23cbeacf3ca
Michael-F-Bryan <michaelfbryan@gmail.com>
2022-07-23 22:43:06 +0800

    Re-run the build script whenever the commit changes

--- crates/fj-app/build.rs
+++ crates/fj-app/build.rs
@@ -1,12 +1,14 @@
-use std::process::{Command, Output, Stdio};
+use std::{
+    path::PathBuf,
+    process::{Command, Output, Stdio},
+};
 
 fn main() {
     println!("cargo:rustc-env=FJ_VERSION_STRING={}", version_string());
 }
 
 fn version_string() -> String {
-    let pkg_version = std::env::var("CARGO_PKG_VERSION")
-        .expect("The $CARGO_PKG_VERSION variable wasn't set");
+    let pkg_version = std::env::var("CARGO_PKG_VERSION").unwrap();
     let commit = git_description();
 
     let official_release = std::env::var("FJ_OFFICIAL_RELEASE").is_ok();
@@ -22,31 +24,50 @@ fn version_string() -> String {
     }
 }
 
+/// Try to get the current git commit.
+///
+/// This may fail if `git` isn't installed (unlikely) or if the `.git/` folder
+/// isn't accessible (more likely than you think). This typically happens when
+/// we're building just the `fj-app` crate in a Docker container or when
+/// someone is installing from crates.io via `cargo install`.
 fn git_description() -> Option<String> {
-    // Note: it's okay for this to fail to start if git isn't installed (e.g.
-    // because we're building in a docker container), but any errors returned by
-    // git itself should fail the build.
+    let mut cmd = Command::new("git");
+    cmd.args(["describe", "--always", "--dirty=-modified"])
+        .stdout(Stdio::piped())
+        .stderr(Stdio::piped());
 
     let Output {
         status,
         stdout,
         stderr,
-    } = Command::new("git")
-        .args(["describe", "--always", "--dirty=-modified"])
-        .stdout(Stdio::piped())
-        .stderr(Stdio::piped())
-        .output()
-        .ok()?;
+    } = cmd.output().ok()?;
 
     let stdout = String::from_utf8_lossy(&stdout);
 
     if !status.success() {
+        // Not sure if anyone will ever see this, but it could be helpful for
+        // troubleshooting.
+        eprintln!("Command failed: {cmd:?}");
         let stderr = String::from_utf8_lossy(&stderr);
         eprintln!("---- Stdout ----");
         eprintln!("{stdout}");
         eprintln!("---- Stderr ----");
         eprintln!("{stderr}");
-        panic!("Git exited unsuccessfully");
+        return None;
+    }
+
+    // Make sure we re-run whenever the current commit changes
+    let crate_dir = PathBuf::from(std::env::var("CARGO_MANIFEST_DIR").unwrap());
+    let project_root = crate_dir.ancestors().nth(2).unwrap();
+    let head_file = project_root.join(".git").join("HEAD");
+    println!("cargo:rerun-if-changed={}", head_file.display());
+
+    if let Ok(contents) = std::fs::read_to_string(&head_file) {
+        let (_, branch) = contents
+            .split_once(' ')
+            .expect(".git/HEAD should point to a valid head file");
+        let commit_hash_file = project_root.join(".git").join(branch.trim());
+        println!("cargo:rerun-if-changed={}", commit_hash_file.display());
     }
 
     Some(stdout.trim().to_string())
