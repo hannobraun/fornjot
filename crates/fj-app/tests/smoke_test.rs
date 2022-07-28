0877cebdbd0f1e57aa534f5b68700ae4d10f4b63
Michael-F-Bryan <michaelfbryan@gmail.com>
2022-07-28 23:06:58 +0800

    Run all of our demo models during the smoke tests

--- crates/fj-app/tests/smoke_test.rs
+++ crates/fj-app/tests/smoke_test.rs
@@ -1,25 +1,55 @@
 use std::{
+    fs::File,
     path::Path,
     process::{Command, Output, Stdio},
 };
 
-use tempfile::NamedTempFile;
+use stl::BinaryStlFile;
+use tempfile::TempDir;
 
 const FJ_APP: &str = env!("CARGO_BIN_EXE_fj-app");
 
 #[test]
-fn spacer_model() {
+fn spacer() {
+    let BinaryStlFile { header, triangles } = execute_model("spacer");
+    assert_eq!(header.num_triangles, 42);
+    assert_eq!(header.num_triangles as usize, triangles.len());
+}
+
+#[test]
+fn test() {
+    let BinaryStlFile { header, triangles } = execute_model("test");
+    assert_eq!(header.num_triangles, 42);
+    assert_eq!(header.num_triangles as usize, triangles.len());
+}
+
+#[test]
+fn cuboid() {
+    let BinaryStlFile { header, triangles } = execute_model("cuboid");
+    assert_eq!(header.num_triangles, 42);
+    assert_eq!(header.num_triangles as usize, triangles.len());
+}
+
+#[test]
+fn star() {
+    let BinaryStlFile { header, triangles } = execute_model("star");
+    assert_eq!(header.num_triangles, 42);
+    assert_eq!(header.num_triangles as usize, triangles.len());
+}
+
+fn execute_model(name: &str) -> BinaryStlFile {
     let project_root = Path::new(env!("CARGO_MANIFEST_DIR"))
         .ancestors()
         .nth(2)
         .unwrap();
-    let dest = NamedTempFile::new().unwrap();
+    let temp = TempDir::new().unwrap();
+    let dest = temp.path().join("output.stl");
 
     let mut cmd = Command::new(FJ_APP);
     cmd.arg("--model")
-        .arg("cuboid")
+        .arg(name)
         .arg("--export")
-        .arg(dest.path())
+        .arg(&dest)
         .env("RUST_BACKTRACE", "1")
         .env("RUST_LOG", "debug")
         .stdout(Stdio::piped())
@@ -41,4 +71,7 @@ fn spacer_model() {
         println!("{stderr}");
         panic!("`{cmd:?}` failed with exit code {:?}", status.code());
     }
+
+    let mut f = File::open(&dest).unwrap();
+    stl::read_stl(&mut f).unwrap()
 }
