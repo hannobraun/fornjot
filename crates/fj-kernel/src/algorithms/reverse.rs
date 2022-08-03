4d72398cbcbf6c3db39991b6ae4cdba9fb2c2eed
Hanno Braun <hanno@braun-odw.eu>
2022-08-03 17:11:10 +0200

    Refactor
    This just serves to minimize the diff of an upcoming commit.

--- crates/fj-kernel/src/algorithms/reverse.rs
+++ crates/fj-kernel/src/algorithms/reverse.rs
@@ -34,10 +34,11 @@ fn reverse_local_coordinates_in_cycle<'r>(
 
                         CurveKind::Circle(Circle { center, a, b })
                     }
-                    CurveKind::Line(Line { origin, direction }) => {
-                        let origin = Point::from([origin.u, -origin.v]);
+                    CurveKind::Line(line) => {
+                        let origin =
+                            Point::from([line.origin.u, -line.origin.v]);
                         let direction =
-                            Vector::from([direction.u, -direction.v]);
+                            Vector::from([line.direction.u, -line.direction.v]);
 
                         CurveKind::Line(Line::from_origin_and_direction(
                             origin, direction,
