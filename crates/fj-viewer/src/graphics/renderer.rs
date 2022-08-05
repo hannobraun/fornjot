d3ba1ca34173fced80713bf292a53b674e8853f6
Devan Looches <devan.looches@gmail.com>
2022-08-05 09:55:21 -0700

    Add scrollable/stretchable area with full error

--- crates/fj-viewer/src/graphics/renderer.rs
+++ crates/fj-viewer/src/graphics/renderer.rs
@@ -425,13 +425,24 @@ impl Renderer {
             );
             info
         }
+        
+        
+        egui::SidePanel::right("fj-right-panel").show(
+            &self.egui.context,
+            |ui| {
+                ui.group(|ui| {
+                    egui::ScrollArea::vertical()
+                        .max_height(f32::INFINITY)
+                        .max_width(f32::INFINITY)
+                        .show(ui, |ui| {
+                            ui.label(format!("Status:\n{}", status.status()))
+                        });
+                })
+            },
+        );
 
         let line_drawing_available = self.is_line_drawing_available();
         egui::SidePanel::left("fj-left-panel").show(&self.egui.context, |ui| {
-            ui.add_space(16.0);
-
-            ui.label(format!("Status Report:\n{}", status.status()));
-
             ui.add_space(16.0);
 
             ui.group(|ui| {
