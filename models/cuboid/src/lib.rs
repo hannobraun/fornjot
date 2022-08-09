c1ef9f506bde614e2369cb0c1f2efb182edfbe1f
Michael-F-Bryan <michaelfbryan@gmail.com>
2022-08-10 01:06:11 +0800

    Put the #[rustfmt::skip] back now that I don't need to run "cargo expand"

--- models/cuboid/src/lib.rs
+++ models/cuboid/src/lib.rs
@@ -4,11 +4,12 @@ pub fn model(
     #[param(default = 2.0)] y: f64,
     #[param(default = 1.0)] z: f64,
 ) -> fj::Shape {
+    #[rustfmt::skip]
     let rectangle = fj::Sketch::from_points(vec![
         [-x / 2., -y / 2.],
-        [x / 2., -y / 2.],
-        [x / 2., y / 2.],
-        [-x / 2., y / 2.],
+        [ x / 2., -y / 2.],
+        [ x / 2.,  y / 2.],
+        [-x / 2.,  y / 2.],
     ])
     .with_color([100, 255, 0, 200]);
 
