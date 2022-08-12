9e4dda12842d2d9f844f48d07d716664d3d3e1ee
Hanno Braun <hanno@braun-odw.eu>
2022-08-12 13:23:08 +0200

    Add `Intersect` trait

--- crates/fj-kernel/src/algorithms/intersection/mod.rs
+++ crates/fj-kernel/src/algorithms/intersection/mod.rs
@@ -13,3 +13,17 @@ pub use self::{
     line_segment::LineSegmentIntersection,
     surface_surface::SurfaceSurfaceIntersection,
 };
+
+/// Compute the intersection between a tuple of objects
+///
+/// # Implementation Note
+///
+/// This trait is newer than most of the intersection algorithms in this module.
+/// Most of them don't support it yet.
+pub trait Intersect {
+    /// The type that describes the intersection between the objects in `Self`
+    type Intersection;
+
+    /// Compute the intersection between a tuple of objects
+    fn intersect(&self) -> Option<Self::Intersection>;
+}
