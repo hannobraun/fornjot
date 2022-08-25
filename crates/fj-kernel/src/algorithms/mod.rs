55ec4b404ff0a3a30bb07942d2505f3e5da99f88
Hanno Braun <hanno@braun-odw.eu>
2022-08-25 15:48:25 +0200

    Make module public

--- crates/fj-kernel/src/algorithms/mod.rs
+++ crates/fj-kernel/src/algorithms/mod.rs
@@ -3,11 +3,11 @@
 //! Algorithmic code is collected in this module, to keep other modules focused
 //! on their respective purpose.
 
-mod reverse;
 mod triangulate;
 
 pub mod approx;
 pub mod intersect;
+pub mod reverse;
 pub mod sweep;
 pub mod transform;
 
