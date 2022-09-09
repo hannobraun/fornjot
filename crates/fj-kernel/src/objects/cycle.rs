1c51c019251ad70ff62b2865050c23207459dd27
Hanno Braun <hanno@braun-odw.eu>
2022-09-09 12:09:46 +0200

    Add `Cycle::winding`
    This is another building block for defining face orientation by exterior
    winding.

--- crates/fj-kernel/src/objects/cycle.rs
+++ crates/fj-kernel/src/objects/cycle.rs
@@ -1,3 +1,5 @@
+use fj_math::{Scalar, Winding};
+
 use crate::builder::CycleBuilder;
 
 use super::{HalfEdge, Surface};
@@ -90,6 +92,66 @@ impl Cycle {
     /// Access the half-edges that make up the cycle
     pub fn half_edges(&self) -> impl Iterator<Item = &HalfEdge> + '_ {
         self.half_edges.iter()
+    }
+
+    /// Indicate the cycle's winding, assuming a right-handed coordinate system
+    pub fn winding(&self) -> Winding {
+        // The cycle could be made up of one or two circles. If that is the
+        // case, the winding of the cycle is determined by the winding of the
+        // first circle.
+        if self.half_edges.len() < 3 {
+            let first = self
+                .half_edges()
+                .next()
+                .expect("Invalid cycle: expected at least one half-edge");
+
+            let [a, b] = first.vertices();
+            let edge_direction_positive = a.position() < b.position();
+
+            let circle = match first.curve().kind() {
+                super::CurveKind::Circle(circle) => circle,
+                super::CurveKind::Line(_) => unreachable!(
+                    "Invalid cycle: less than 3 edges, but not all are circles"
+                ),
+            };
+            let cross_positive = circle.a().cross(&circle.b()) > Scalar::ZERO;
+
+            if edge_direction_positive == cross_positive {
+                return Winding::Ccw;
+            } else {
+                return Winding::Cw;
+            }
+        }
+
+        // Now that we got the special case out of the way, we can treat the
+        // cycle as a polygon:
+        // https://stackoverflow.com/a/1165943
+
+        let mut sum = Scalar::ZERO;
+
+        for half_edge in self.half_edges.windows(2) {
+            // Can't panic, as we passed `2` to `windows`.
+            //
+            // Can be cleaned up, once `array_windows` is stable:
+            // https://doc.rust-lang.org/std/primitive.slice.html#method.array_windows
+            let [a, b] = [half_edge[0], half_edge[1]];
+
+            let [a, b] = [a, b].map(|half_edge| {
+                let [vertex, _] = half_edge.vertices();
+                vertex.surface_form().position()
+            });
+
+            sum += (b.u - a.u) * (b.v + a.v);
+        }
+
+        if sum > Scalar::ZERO {
+            return Winding::Cw;
+        }
+        if sum < Scalar::ZERO {
+            return Winding::Ccw;
+        }
+
+        unreachable!("Encountered invalid cycle: {self:#?}");
     }
 
     /// Consume the cycle and return its half-edges
