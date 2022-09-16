856325c707e87026398e9f9bcf710d26f19d5a8d
Hanno Braun <hanno@braun-odw.eu>
2022-09-16 16:13:15 +0200

    Add `Stores`
    This is just a placeholder, so it can be added everywhere it's going to
    be required. It can be filled with life later.

--- crates/fj-kernel/src/stores/mod.rs
+++ crates/fj-kernel/src/stores/mod.rs
@@ -3,3 +3,14 @@
 mod store;
 
 pub use self::store::{Handle, Iter, Reservation, Store};
+
+/// The available object stores
+#[derive(Debug, Default)]
+pub struct Stores {}
+
+impl Stores {
+    /// Construct a new instance of `Stores`
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
