630eff122dd007d1eee815ef29664bf3645d00fb
Hanno Braun <hanno@braun-odw.eu>
2022-09-19 16:13:01 +0200

    Inline `Sweep` implementation of `GlobalCurve`
    I'm working on changes to `GlobalCurve`. Once those changes come to
    fruition, it will no longer be able to support that `Sweep`
    implementation.

--- crates/fj-kernel/src/algorithms/sweep/curve.rs
+++ crates/fj-kernel/src/algorithms/sweep/curve.rs
@@ -1,7 +1,7 @@
 use fj_math::Vector;
 
 use crate::{
-    objects::{Curve, GlobalCurve, Surface},
+    objects::{Curve, Surface},
     stores::Stores,
 };
 
@@ -10,15 +10,7 @@ use super::Sweep;
 impl Sweep for Curve {
     type Swept = Surface;
 
-    fn sweep(self, path: impl Into<Vector<3>>, stores: &Stores) -> Self::Swept {
-        self.global_form().sweep(path, stores)
-    }
-}
-
-impl Sweep for GlobalCurve {
-    type Swept = Surface;
-
     fn sweep(self, path: impl Into<Vector<3>>, _: &Stores) -> Self::Swept {
-        Surface::new(self.path(), path.into())
+        Surface::new(self.global_form().path(), path)
     }
 }
