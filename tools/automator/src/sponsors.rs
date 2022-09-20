a90ee271131d22cdb05708800864b25cfac8461f
Hanno Braun <hanno@braun-odw.eu>
2022-09-20 13:03:50 +0200

    Add types to encode query result

--- tools/automator/src/sponsors.rs
+++ tools/automator/src/sponsors.rs
@@ -1,7 +1,8 @@
+use chrono::{DateTime, Utc};
 use octocrab::Octocrab;
 
 pub async fn query_sponsors(octocrab: &Octocrab) -> anyhow::Result<()> {
-    let response: serde_json::Value = octocrab
+    let response: QueryResult = octocrab
         .graphql(
             "query {
                 viewer {
@@ -33,7 +34,49 @@ pub async fn query_sponsors(octocrab: &Octocrab) -> anyhow::Result<()> {
         )
         .await?;
 
-    println!("{response}");
+    println!("{:#?}", response.data.viewer.sponsors.nodes);
 
     Ok(())
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct QueryResult {
+    pub data: QueryResultData,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct QueryResultData {
+    pub viewer: QueryResultViewer,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct QueryResultViewer {
+    pub sponsors: QueryResultSponsorsNodes,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct QueryResultSponsorsNodes {
+    pub nodes: Vec<QueryResultSponsorsNode>,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct QueryResultSponsorsNode {
+    pub login: String,
+
+    #[serde(rename = "sponsorshipForViewerAsSponsorable")]
+    pub sponsorship_for_viewer_as_sponsorable: QueryResultSponsorable,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct QueryResultSponsorable {
+    #[serde(rename = "createdAt")]
+    pub created_at: DateTime<Utc>,
+
+    pub tier: QueryResultSponsorableTier,
+}
+
+#[derive(Debug, serde::Deserialize)]
+pub struct QueryResultSponsorableTier {
+    #[serde(rename = "monthlyPriceInDollars")]
+    pub monthly_price_in_dollars: u32,
 }
