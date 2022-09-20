3718664d957326527d108f9e059f73b25a4d8022
Hanno Braun <hanno@braun-odw.eu>
2022-09-20 12:44:56 +0200

    Add GraphQL query for sponsors

--- tools/automator/src/run.rs
+++ tools/automator/src/run.rs
@@ -17,8 +17,37 @@ pub async fn run() -> anyhow::Result<()> {
                 .context("Failed to create release announcement")?;
         }
         Args::Sponsors => {
-            let response: serde_json::Value =
-                octocrab.graphql("query { viewer { login }}").await?;
+            let response: serde_json::Value = octocrab
+                .graphql(
+                    "query {
+                        viewer {
+                            sponsors(first: 100) {
+                                nodes {
+                                    __typename
+                                    ... on User {
+                                        login
+                                        sponsorshipForViewerAsSponsorable {
+                                            createdAt
+                                            tier {
+                                                monthlyPriceInDollars
+                                            }
+                                        }
+                                    }
+                                    ... on Organization {
+                                        login
+                                        sponsorshipForViewerAsSponsorable {
+                                            createdAt
+                                            tier {
+                                                monthlyPriceInDollars
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }",
+                )
+                .await?;
             println!("{response}");
 
             todo!("Querying sponsors is not supported yet.")
