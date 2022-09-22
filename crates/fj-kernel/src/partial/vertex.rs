4c5fd94fed379d2d58098f70d6a88bf34f1558b1
Hanno Braun <hanno@braun-odw.eu>
2022-09-22 13:30:28 +0200

    Update doc comments

--- crates/fj-kernel/src/partial/vertex.rs
+++ crates/fj-kernel/src/partial/vertex.rs
@@ -5,62 +5,66 @@ use crate::{
     objects::{Curve, GlobalVertex, SurfaceVertex, Vertex},
 };
 
-/// API for building a [`Vertex`]
+/// A partial [`Vertex`]
 ///
-/// Also see [`Vertex::partial`].
+/// See [`crate::partial`] for more information.
 #[derive(Default)]
 pub struct PartialVertex {
     /// The position of the [`Vertex`] on the [`Curve`]
     ///
-    /// Must be provided to the builder before [`PartialVertex::build`] is
-    /// called.
+    /// Must be provided before [`PartialVertex::build`] is called.
     pub position: Option<Point<1>>,
 
     /// The curve that the [`Vertex`] is defined in
     ///
-    /// Must be provided to the builder before [`PartialVertex::build`] is
-    /// called.
+    /// Must be provided before [`PartialVertex::build`] is called.
     pub curve: Option<Curve>,
 
     /// The surface form of the [`Vertex`]
     ///
-    /// Can be provided to the builder, if already available, or computed from
-    /// the position on the [`Curve`].
+    /// Can be provided, if already available, or computed from the position on
+    /// the [`Curve`].
     pub surface_form: Option<SurfaceVertex>,
 
     /// The global form of the [`Vertex`]
     ///
-    /// Can be provided to the builder, if already available, or acquired
-    /// through the surface form.
+    /// Can be provided, if already available, or acquired through the surface
+    /// form.
     pub global_form: Option<GlobalVertex>,
 }
 
 impl PartialVertex {
-    /// Build the [`Vertex`] with the provided position
+    /// Provide a position for the partial vertex
     pub fn with_position(mut self, position: impl Into<Point<1>>) -> Self {
         self.position = Some(position.into());
         self
     }
 
-    /// Build the [`Vertex`] with the provided curve
+    /// Provide a curve for the partial vertex
     pub fn with_curve(mut self, curve: Curve) -> Self {
         self.curve = Some(curve);
         self
     }
 
-    /// Build the [`Vertex`] with the provided surface form
+    /// Provide a surface form for the partial vertex
     pub fn with_surface_form(mut self, surface_form: SurfaceVertex) -> Self {
         self.surface_form = Some(surface_form);
         self
     }
 
-    /// Build the [`Vertex`] with the provided global form
+    /// Provide a global form for the partial vertex
     pub fn with_global_form(mut self, global_form: GlobalVertex) -> Self {
         self.global_form = Some(global_form);
         self
     }
 
-    /// Finish building the [`Vertex`]
+    /// Build a full [`Vertex`] from the partial vertex
+    ///
+    /// # Panics
+    ///
+    /// Panics, if no position has been provided.
+    ///
+    /// Panics, if no curve has been provided.
     pub fn build(self) -> Vertex {
         let position = self
             .position
