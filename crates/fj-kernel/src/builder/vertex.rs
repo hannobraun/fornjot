24be0e55a3b402a1dbfd18c24b6dcbc0ca88022e
Hanno Braun <hanno@braun-odw.eu>
2022-09-22 14:07:55 +0200

    Add full builder API to `GlobalVertexBuilder`

--- crates/fj-kernel/src/builder/vertex.rs
+++ crates/fj-kernel/src/builder/vertex.rs
@@ -6,9 +6,18 @@ use crate::objects::{Curve, GlobalVertex, Surface};
 ///
 /// Also see [`GlobalVertex::builder`].
 #[derive(Default)]
-pub struct GlobalVertexBuilder;
+pub struct GlobalVertexBuilder {
+    /// The position of the [`GlobalVertex`]
+    pub position: Option<Point<3>>,
+}
 
 impl GlobalVertexBuilder {
+    /// Provide a position
+    pub fn with_position(mut self, position: impl Into<Point<3>>) -> Self {
+        self.position = Some(position.into());
+        self
+    }
+
     /// Build a [`GlobalVertex`] from a curve and a position on that curve
     pub fn build_from_curve_and_position(
         self,
@@ -26,6 +35,15 @@ impl GlobalVertexBuilder {
         position: impl Into<Point<2>>,
     ) -> GlobalVertex {
         let position = surface.point_from_surface_coords(position);
+        GlobalVertex::from_position(position)
+    }
+
+    /// Build a full [`GlobalVertex`]
+    pub fn build(self) -> GlobalVertex {
+        let position = self
+            .position
+            .expect("Can't build a `GlobalVertex` without a position");
+
         GlobalVertex::from_position(position)
     }
 }
