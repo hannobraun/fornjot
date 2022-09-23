4a72095c162ed94d481447ecb1c857feb2275e5a
Hanno Braun <hanno@braun-odw.eu>
2022-09-23 12:14:35 +0200

    Add `MaybePartial` enum

--- crates/fj-kernel/src/partial/mod.rs
+++ crates/fj-kernel/src/partial/mod.rs
@@ -38,6 +38,34 @@ use crate::{
     stores::{Handle, Stores},
 };
 
+/// Either a partial object or a full one
+#[derive(Clone, Copy, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
+pub enum MaybePartial<T: HasPartialForm> {
+    /// A full object
+    Full(T),
+
+    /// A partial object
+    Partial(T::PartialForm),
+}
+
+impl<T: HasPartialForm> MaybePartial<T> {
+    /// Return the full object, either directly or by building it
+    pub fn into_full(self, stores: &Stores) -> T {
+        match self {
+            Self::Partial(partial) => T::from_partial(partial, stores),
+            Self::Full(full) => full,
+        }
+    }
+
+    /// Return the partial object, either directly or via conversion
+    pub fn into_partial(self) -> T::PartialForm {
+        match self {
+            Self::Partial(partial) => partial,
+            Self::Full(full) => full.into(),
+        }
+    }
+}
+
 /// Implemented for types that are partial objects
 ///
 /// # Implementation Note
@@ -65,6 +93,18 @@ macro_rules! impl_traits {
                     -> Self
                 {
                     partial.build(stores)
+                }
+            }
+
+            impl From<$full> for MaybePartial<$full> {
+                fn from(full: $full) -> Self {
+                    Self::Full(full)
+                }
+            }
+
+            impl From<$partial> for MaybePartial<$full> {
+                fn from(partial: $partial) -> Self {
+                    Self::Partial(partial)
                 }
             }
         )*
