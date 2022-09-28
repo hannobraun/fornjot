b153bd74460b5348ffd0a89d324b50888b466f03
Hanno Braun <hanno@braun-odw.eu>
2022-09-28 15:01:05 +0200

    Impl `TransformObject` for `PartialSurfaceVertex`

--- crates/fj-kernel/src/algorithms/transform/vertex.rs
+++ crates/fj-kernel/src/algorithms/transform/vertex.rs
@@ -2,7 +2,7 @@ use fj_math::Transform;
 
 use crate::{
     objects::{GlobalVertex, SurfaceVertex, Vertex},
-    partial::PartialGlobalVertex,
+    partial::{PartialGlobalVertex, PartialSurfaceVertex},
     stores::Stores,
 };
 
@@ -35,6 +35,25 @@ impl TransformObject for GlobalVertex {
     fn transform(self, transform: &Transform, _: &Stores) -> Self {
         let position = transform.transform_point(&self.position());
         Self::from_position(position)
+    }
+}
+
+impl TransformObject for PartialSurfaceVertex {
+    fn transform(self, transform: &Transform, stores: &Stores) -> Self {
+        let surface = self
+            .surface
+            .map(|surface| surface.transform(transform, stores));
+        let global_form = self
+            .global_form
+            .map(|global_form| global_form.transform(transform, stores));
+
+        // Don't need to transform `self.position`, as that is in surface
+        // coordinates and thus transforming the surface takes care of it.
+        Self {
+            position: self.position,
+            surface,
+            global_form,
+        }
     }
 }
 
