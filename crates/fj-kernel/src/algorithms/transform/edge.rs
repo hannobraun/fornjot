5ec0558b98b5bcf4e651c5be3d7ce82326a6f7e5
Hanno Braun <hanno@braun-odw.eu>
2022-10-05 13:43:34 +0200

    Implement `TransformObject` for `PartialHalfEdge`

--- crates/fj-kernel/src/algorithms/transform/edge.rs
+++ crates/fj-kernel/src/algorithms/transform/edge.rs
@@ -2,7 +2,7 @@ use fj_math::Transform;
 
 use crate::{
     objects::{GlobalEdge, HalfEdge},
-    partial::{HasPartial, PartialGlobalEdge},
+    partial::{HasPartial, PartialGlobalEdge, PartialHalfEdge},
     stores::Stores,
 };
 
@@ -42,6 +42,43 @@ impl TransformObject for GlobalEdge {
             .map(|vertex| vertex.transform(transform, stores));
 
         Self::new(curve, vertices)
+    }
+}
+
+impl TransformObject for PartialHalfEdge {
+    fn transform(self, transform: &Transform, stores: &Stores) -> Self {
+        let curve = self
+            .curve
+            .clone()
+            .map(|curve| curve.transform(transform, stores));
+        let vertices = self.vertices.clone().map(|vertices| {
+            vertices.map(|vertex| {
+                let vertex = vertex.into_partial().transform(transform, stores);
+                let vertex = match &curve {
+                    Some(curve) => vertex.with_curve(curve.clone()),
+                    None => vertex,
+                };
+                vertex.into()
+            })
+        });
+        let global_form = self.global_form.map(|global_form| {
+            let global_form =
+                global_form.into_partial().transform(transform, stores);
+
+            let curve = curve.as_ref().and_then(|curve| curve.global_form());
+            let global_form = match curve {
+                Some(curve) => global_form.with_curve(curve),
+                None => global_form,
+            };
+
+            global_form.into()
+        });
+
+        Self {
+            curve,
+            vertices,
+            global_form,
+        }
     }
 }
 
