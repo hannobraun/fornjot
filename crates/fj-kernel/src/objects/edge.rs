514bc5576dadd296844ded9d7e5efaa3a92bfaa0
Hanno Braun <hanno@braun-odw.eu>
2022-10-07 15:13:41 +0200

    Return more information from constructor

--- crates/fj-kernel/src/objects/edge.rs
+++ crates/fj-kernel/src/objects/edge.rs
@@ -39,6 +39,10 @@ impl HalfEdge {
 
         let curve = a.curve();
 
+        let (vertices_in_normalized_order, _) = VerticesInNormalizedOrder::new(
+            [&a, &b].map(|vertex| vertex.global_form().clone()),
+        );
+
         // Make sure `curve` and `vertices` match `global_form`.
         assert_eq!(
             curve.global_form().id(),
@@ -47,9 +51,7 @@ impl HalfEdge {
             the half-edge's global form"
         );
         assert_eq!(
-            &VerticesInNormalizedOrder::new(
-                [&a, &b].map(|vertex| vertex.global_form().clone())
-            ),
+            &vertices_in_normalized_order,
             global_form.vertices(),
             "The global forms of a half-edge's vertices must match the \
             vertices of the half-edge's global form"
@@ -130,7 +132,7 @@ impl GlobalEdge {
         vertices: [Handle<GlobalVertex>; 2],
     ) -> Self {
         let curve = curve.into();
-        let vertices = VerticesInNormalizedOrder::new(vertices);
+        let (vertices, _) = VerticesInNormalizedOrder::new(vertices);
         Self { curve, vertices }
     }
 
@@ -166,10 +168,14 @@ pub struct VerticesInNormalizedOrder([Handle<GlobalVertex>; 2]);
 impl VerticesInNormalizedOrder {
     /// Construct a new instance of `VerticesInNormalizedOrder`
     ///
-    /// The provided vertices can be in any order.
-    pub fn new([a, b]: [Handle<GlobalVertex>; 2]) -> Self {
-        let vertices = if a < b { [a, b] } else { [b, a] };
-        Self(vertices)
+    /// The provided vertices can be in any order. The returned `bool` value
+    /// indicates whether the normalization changed the order of the vertices.
+    pub fn new([a, b]: [Handle<GlobalVertex>; 2]) -> (Self, bool) {
+        if a < b {
+            (Self([a, b]), false)
+        } else {
+            (Self([b, a]), true)
+        }
     }
 
     /// Access the vertices
