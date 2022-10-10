2268b4af550c11986bf8f91b0c36cb6ab33bd6a1
Hanno Braun <hanno@braun-odw.eu>
2022-10-10 19:54:09 +0200

    Rename arguments

--- crates/fj-kernel/src/objects/curve.rs
+++ crates/fj-kernel/src/objects/curve.rs
@@ -19,9 +19,9 @@ impl Curve {
         surface: Handle<Surface>,
         path: SurfacePath,
         global_form: impl Into<HandleWrapper<GlobalCurve>>,
-        stores: &Objects,
+        objects: &Objects,
     ) -> Handle<Self> {
-        stores.curves.insert(Self {
+        objects.curves.insert(Self {
             surface,
             path,
             global_form: global_form.into(),
@@ -50,7 +50,7 @@ pub struct GlobalCurve;
 
 impl GlobalCurve {
     /// Construct a new instance of `Handle` and add it to the store
-    pub fn new(stores: &Objects) -> Handle<Self> {
-        stores.global_curves.insert(GlobalCurve)
+    pub fn new(objects: &Objects) -> Handle<Self> {
+        objects.global_curves.insert(GlobalCurve)
     }
 }
