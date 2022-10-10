aecf689fd72db51b0dbfe2652c2c75ca60b71d42
Hanno Braun <hanno@braun-odw.eu>
2022-10-10 19:41:23 +0200

    Rename arguments

--- crates/fj-kernel/src/algorithms/transform/face.rs
+++ crates/fj-kernel/src/algorithms/transform/face.rs
@@ -8,20 +8,20 @@ use crate::{
 use super::TransformObject;
 
 impl TransformObject for Face {
-    fn transform(self, transform: &Transform, stores: &Objects) -> Self {
-        let surface = self.surface().clone().transform(transform, stores);
+    fn transform(self, transform: &Transform, objects: &Objects) -> Self {
+        let surface = self.surface().clone().transform(transform, objects);
         let exterior = self
             .exterior()
             .to_partial()
-            .transform(transform, stores)
+            .transform(transform, objects)
             .with_surface(Some(surface.clone()))
-            .build(stores);
+            .build(objects);
         let interiors = self.interiors().map(|cycle| {
             cycle
                 .to_partial()
-                .transform(transform, stores)
+                .transform(transform, objects)
                 .with_surface(Some(surface.clone()))
-                .build(stores)
+                .build(objects)
         });
 
         let color = self.color();
@@ -33,11 +33,11 @@ impl TransformObject for Face {
 }
 
 impl TransformObject for Faces {
-    fn transform(self, transform: &Transform, stores: &Objects) -> Self {
+    fn transform(self, transform: &Transform, objects: &Objects) -> Self {
         let mut faces = Faces::new();
         faces.extend(
             self.into_iter()
-                .map(|face| face.transform(transform, stores)),
+                .map(|face| face.transform(transform, objects)),
         );
         faces
     }
