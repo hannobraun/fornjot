fbc193ceae45bcd784aadc672aa69278a0f295cc
Hanno Braun <hanno@braun-odw.eu>
2022-10-10 19:39:33 +0200

    Rename argument

--- crates/fj-kernel/src/algorithms/transform/cycle.rs
+++ crates/fj-kernel/src/algorithms/transform/cycle.rs
@@ -5,17 +5,17 @@ use crate::{objects::Objects, partial::PartialCycle};
 use super::TransformObject;
 
 impl TransformObject for PartialCycle {
-    fn transform(self, transform: &Transform, stores: &Objects) -> Self {
+    fn transform(self, transform: &Transform, objects: &Objects) -> Self {
         let surface = self
             .surface
             .clone()
-            .map(|surface| surface.transform(transform, stores));
+            .map(|surface| surface.transform(transform, objects));
         let half_edges = self
             .half_edges
             .into_iter()
             .map(|edge| {
                 edge.into_partial()
-                    .transform(transform, stores)
+                    .transform(transform, objects)
                     .with_surface(surface.clone())
                     .into()
             })
