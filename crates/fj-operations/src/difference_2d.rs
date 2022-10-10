da5cf263a179739956703fad1dfb8eb6aa2e3ee9
Hanno Braun <hanno@braun-odw.eu>
2022-10-10 19:58:45 +0200

    Rename argument

--- crates/fj-operations/src/difference_2d.rs
+++ crates/fj-operations/src/difference_2d.rs
@@ -19,7 +19,7 @@ impl Shape for fj::Difference2d {
     fn compute_brep(
         &self,
         config: &ValidationConfig,
-        stores: &Objects,
+        objects: &Objects,
         planes: &Planes,
         debug_info: &mut DebugInfo,
     ) -> Result<Validated<Self::Brep>, ValidationError> {
@@ -36,7 +36,7 @@ impl Shape for fj::Difference2d {
         // - https://doc.rust-lang.org/std/primitive.array.html#method.try_map
         let [a, b] = self.shapes();
         let [a, b] = [a, b].map(|shape| {
-            shape.compute_brep(config, stores, planes, debug_info)
+            shape.compute_brep(config, objects, planes, debug_info)
         });
         let [a, b] = [a?, b?];
 
