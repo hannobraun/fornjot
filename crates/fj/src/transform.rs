0c0c6c155f2e812986ff5b162fd4cbbbe99fff35
Hanno Braun <hanno@braun-odw.eu>
2022-10-11 14:55:46 +0200

    Document convenient syntax for `fj::Transform`

--- crates/fj/src/transform.rs
+++ crates/fj/src/transform.rs
@@ -2,6 +2,19 @@ use crate::{Angle, Shape};
 
 /// A transformed 3-dimensional shape
 ///
+/// # Examples
+///
+/// Convenient syntax for this operation is available through [`crate::syntax`].
+///
+/// ``` rust
+/// # let shape = fj::Sketch::from_points(vec![[0., 0.], [1., 0.], [0., 1.]]);
+/// use fj::syntax::*;
+///
+/// // `shape` can be anything that converts to `fj::Shape`
+/// let rotated = shape.rotate([0., 0., 1.], fj::Angle::from_rev(0.5));
+/// let translated = shape.translate([1., 2., 3.]);
+/// ```
+///
 /// # Limitations
 ///
 /// Transformations are currently limited to a rotation, followed by a
