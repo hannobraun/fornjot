be9a2f980955d7f1dc728b1e20830f651359eb3c
Hanno Braun <hanno@braun-odw.eu>
2022-10-13 16:58:56 +0200

    Update function name to latest nomenclature

--- crates/fj-kernel/src/partial/objects/edge.rs
+++ crates/fj-kernel/src/partial/objects/edge.rs
@@ -73,7 +73,7 @@ impl PartialHalfEdge {
     }
 
     /// Update the partial half-edge with the given from vertex
-    pub fn with_to_vertex(
+    pub fn with_front_vertex(
         mut self,
         vertex: Option<impl Into<MaybePartial<Vertex>>>,
     ) -> Self {
