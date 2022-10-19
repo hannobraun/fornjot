74c5f950e89c20030e7d06028d345b59d34ba4b3
Hanno Braun <hanno@braun-odw.eu>
2022-10-19 13:37:19 +0200

    Check version before loading model

--- crates/fj/src/version.rs
+++ crates/fj/src/version.rs
@@ -1,5 +1,7 @@
 //! API for checking compatibility between the Fornjot app and a model
 
+use core::slice;
+
 /// The Fornjot package version
 ///
 /// Can be used to check for compatibility between a model and the Fornjot app
@@ -16,3 +18,37 @@ pub static VERSION_PKG: &str = env!("FJ_VERSION_PKG");
 /// Can be used to check for compatibility between a model and the Fornjot app
 /// that runs it.
 pub static VERSION_FULL: &str = env!("FJ_VERSION_FULL");
+
+/// C-ABI-compatible representation of a version
+///
+/// Used by the Fornjot application to check for compatibility between a model
+/// and the app.
+#[repr(C)]
+pub struct RawVersion {
+    /// The pointer to the `str`
+    pub ptr: *const u8,
+
+    /// The length of the `str`
+    pub len: usize,
+}
+
+impl RawVersion {
+    /// Convert the `RawVersion` into a string
+    ///
+    /// # Safety
+    ///
+    /// Must be a `RawVersion` returned from one of the hidden version functions
+    /// in this module.
+    pub unsafe fn as_str(&self) -> &str {
+        let slice = slice::from_raw_parts(self.ptr, self.len);
+        std::str::from_utf8(slice).unwrap()
+    }
+}
+
+#[no_mangle]
+extern "C" fn version_pkg() -> RawVersion {
+    RawVersion {
+        ptr: VERSION_PKG.as_ptr(),
+        len: VERSION_PKG.len(),
+    }
+}
