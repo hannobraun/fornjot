58dadc211da35cbff458cb64f5b96e78ec1d048b
Hanno Braun <hanno@braun-odw.eu>
2022-10-21 13:49:19 +0200

    Add `ArrayExt`

---
+++ crates/fj-interop/src/ext.rs
@@ -0,0 +1,42 @@
+//! Extension traits for standard library types
+
+/// Extension trait for arrays
+pub trait ArrayExt<T, const N: usize> {
+    /// Stable replacement for `each_ref`
+    ///
+    /// <https://doc.rust-lang.org/std/primitive.array.html#method.each_ref>
+    fn each_ref_ext(&self) -> [&T; N];
+
+    /// Stable replacement for `zip`
+    ///
+    /// <https://doc.rust-lang.org/std/primitive.array.html#method.zip>
+    fn zip_ext<U>(self, rhs: [U; N]) -> [(T, U); N];
+}
+
+impl<T> ArrayExt<T, 2> for [T; 2] {
+    fn each_ref_ext(&self) -> [&T; 2] {
+        let [a, b] = self;
+        [a, b]
+    }
+
+    fn zip_ext<U>(self, rhs: [U; 2]) -> [(T, U); 2] {
+        let [a, b] = self;
+        let [c, d] = rhs;
+
+        [(a, c), (b, d)]
+    }
+}
+
+impl<T> ArrayExt<T, 4> for [T; 4] {
+    fn each_ref_ext(&self) -> [&T; 4] {
+        let [a, b, c, d] = self;
+        [a, b, c, d]
+    }
+
+    fn zip_ext<U>(self, rhs: [U; 4]) -> [(T, U); 4] {
+        let [a, b, c, d] = self;
+        let [e, f, g, h] = rhs;
+
+        [(a, e), (b, f), (c, g), (d, h)]
+    }
+}
