c874a9cef216c422ab01abbf326c71fae556dc97
Hanno Braun <hanno@braun-odw.eu>
2022-11-09 11:54:40 +0100

    Update documentation of `validate`

--- crates/fj-kernel/src/validate/mod.rs
+++ crates/fj-kernel/src/validate/mod.rs
@@ -1,18 +1,4 @@
-//! Infrastructure for validating shapes
-//!
-//! Validation enforces various constraints about shapes and the objects that
-//! constitute them. These constraints fall into 4 categories:
-//!
-//! - **Coherence:** Local forms of objects must be consistent with their
-//!   canonical forms.
-//! - **Geometric:** Comprises various object-specific constraints, for example
-//!   edges or faces might not be allowed to intersect.
-//! - **Structural:** All other objects that an object references must be part
-//!   of the same shape.
-//! - **Uniqueness:** Objects within a shape must be unique.
-//!
-//! Please note that not all of these validation categories are fully
-//! implemented, as of this writing.
+//! Infrastructure for validating objects
 
 mod curve;
 mod cycle;
@@ -36,6 +22,8 @@ use std::convert::Infallible;
 use fj_math::Scalar;
 
 /// Validate an object
+///
+/// This trait is used automatically when inserting an object into a store.
 pub trait Validate: Sized {
     /// The error that validation of the implementing type can result in
     type Error: Into<ValidationError>;
