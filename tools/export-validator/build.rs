a7d96de6ed294f3271029229eb0e984aa655b521
Martin Kavík <martin@kavik.cz>
2022-11-13 06:25:58 +0100

    Run export-validator on Windows

--- tools/export-validator/build.rs
+++ tools/export-validator/build.rs
@@ -1,4 +1,8 @@
-use std::{env, path::PathBuf};
+use anyhow::bail;
+use std::{
+    env, fs,
+    path::{Path, PathBuf},
+};
 
 fn main() -> anyhow::Result<()> {
     let manifest_dir = env::var("CARGO_MANIFEST_DIR")?;
@@ -14,6 +18,15 @@ fn main() -> anyhow::Result<()> {
     // And this is necessary, so the linked library is found at runtime.
     if cfg!(target_family = "unix") {
         println!("cargo:rustc-link-arg=-Wl,-rpath,{}", libs_dir.display());
+    }
+    if cfg!(target_family = "windows") {
+        let out_dir = env::var("OUT_DIR")?;
+        // TODO: Refactor once possible: https://github.com/rust-lang/cargo/issues/9661
+        let Some(bin_dir) = Path::new(&out_dir).ancestors().nth(3) else {
+            bail!("Failed to get bin_dir from this out_dir: {out_dir}");
+        };
+        // Note: Other rpath alternatives: https://ibob.bg/blog/2018/12/16/windows-rpath/
+        fs::copy(libs_dir.join("lib3mf.dll"), bin_dir.join("lib3mf.dll"))?;
     }
 
     Ok(())
