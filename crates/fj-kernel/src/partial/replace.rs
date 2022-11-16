3c9ebb8b73ff914fcb53aaf445538afdaf45c1e8
Hanno Braun <hanno@braun-odw.eu>
2022-11-16 15:42:15 +0100

    Implement `Replace` for arrays

--- crates/fj-kernel/src/partial/replace.rs
+++ crates/fj-kernel/src/partial/replace.rs
@@ -5,3 +5,16 @@ pub trait Replace<T> {
     /// Recursively replace the referenced object
     fn replace(&mut self, object: Handle<T>) -> &mut Self;
 }
+
+impl<T, R, const N: usize> Replace<T> for [R; N]
+where
+    R: Replace<T>,
+{
+    fn replace(&mut self, object: Handle<T>) -> &mut Self {
+        for item in self.iter_mut() {
+            item.replace(object.clone());
+        }
+
+        self
+    }
+}
