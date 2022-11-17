280a7f2a8af494997c56739c1e65aad99fa2c8db
Hanno Braun <hanno@braun-odw.eu>
2022-11-17 13:02:34 +0100

    Add dedicated model template to `fj-app` directory
    The current system for including the model template doesn't work with
    `cargo install`, as the `models/` directory is not included in the
    `fj-app` crate.

---
+++ crates/fj-app/model-template/README.md
@@ -0,0 +1,3 @@
+# Fornjot - Model Template
+
+This template is compiled into the `fj-app` binary, where it is used to generate new models.
---
+++ crates/fj-app/model-template/src/lib.rs
@@ -0,0 +1,40 @@
+use std::f64::consts::PI;
+
+#[fj::model]
+pub fn model(
+    #[param(default = 5, min = 3)] num_points: u64,
+    #[param(default = 1.0, min = 1.0)] r1: f64,
+    #[param(default = 2.0, min = 2.0)] r2: f64,
+    #[param(default = 1.0)] h: f64,
+) -> fj::Shape {
+    let num_vertices = num_points * 2;
+    let vertex_iter = (0..num_vertices).map(|i| {
+        let angle =
+            fj::Angle::from_rad(2. * PI / num_vertices as f64 * i as f64);
+        let radius = if i % 2 == 0 { r1 } else { r2 };
+        (angle, radius)
+    });
+
+    // Now that we got that iterator prepared, generating the vertices is just a
+    // bit of trigonometry.
+    let mut outer = Vec::new();
+    let mut inner = Vec::new();
+    for (angle, radius) in vertex_iter {
+        let (sin, cos) = angle.rad().sin_cos();
+
+        let x = cos * radius;
+        let y = sin * radius;
+
+        outer.push([x, y]);
+        inner.push([x / 2., y / 2.]);
+    }
+
+    let outer = fj::Sketch::from_points(outer);
+    let inner = fj::Sketch::from_points(inner);
+
+    let footprint = fj::Difference2d::from_shapes([outer.into(), inner.into()]);
+
+    let star = fj::Sweep::from_path(footprint.into(), [0., 0., h]);
+
+    star.into()
+}
