d1585b8e842e7685033ee59eab771a4483c6cdcd
Hanno Braun <hanno@braun-odw.eu>
2022-11-25 13:35:16 +0100

    Add `Services`

--- crates/fj-kernel/src/services/mod.rs
+++ crates/fj-kernel/src/services/mod.rs
@@ -5,7 +5,28 @@
 mod objects;
 mod service;
 
+use crate::objects::Objects;
+
 pub use self::{
     objects::ServiceObjectsExt,
     service::{Service, State},
 };
+
+/// The kernel services
+#[derive(Default)]
+pub struct Services {
+    /// The objects service
+    ///
+    /// Allows for inserting objects into a store after they were created.
+    ///
+    /// [`ServiceObjectsExt`] is available to provide a convenient API around
+    /// this service.
+    pub objects: Service<Objects>,
+}
+
+impl Services {
+    /// Construct an instance of `Services`
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
