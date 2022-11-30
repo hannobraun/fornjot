36920429a055a779fa2a3c96a2036092bacde433
Hanno Braun <hanno@braun-odw.eu>
2022-11-30 12:15:13 +0100

    Fix variable name

--- crates/fj-kernel/src/algorithms/transform/solid.rs
+++ crates/fj-kernel/src/algorithms/transform/solid.rs
@@ -15,10 +15,10 @@ impl TransformObject for Handle<Solid> {
         objects: &mut Service<Objects>,
         cache: &mut TransformCache,
     ) -> Self {
-        let faces = self
+        let shells = self
             .shells()
             .cloned()
             .map(|shell| shell.transform_with_cache(transform, objects, cache));
-        Solid::builder().with_shells(faces).build(objects)
+        Solid::builder().with_shells(shells).build(objects)
     }
 }
