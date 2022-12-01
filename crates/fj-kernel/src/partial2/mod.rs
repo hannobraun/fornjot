9af75b2ec63e779a912a0d6be76d9af3bf2f845d
Hanno Braun <hanno@braun-odw.eu>
2022-12-01 12:59:29 +0100

    Add `partial2` module

---
+++ crates/fj-kernel/src/partial2/mod.rs
@@ -0,0 +1,11 @@
+//! Partially defined objects
+//!
+//! This module contains types that mirror the full object types from
+//! [`crate::objects`], only the types from this module can be defined only
+//! partially, with the non-defined parts being inferred when a full object is
+//! constructed.
+//!
+//! # Implementation Note
+//!
+//! This API was created as a replacement for the [original partial object
+//! API][crate::partial]. This is still a work in progress.
