e141c2361a76d2c8073741bc680f0424013fa51d
Hanno Braun <hanno@braun-odw.eu>
2022-12-14 09:43:25 +0100

    Add `CycleBuilder::add_half_edge`

--- crates/fj-kernel/src/builder/cycle.rs
+++ crates/fj-kernel/src/builder/cycle.rs
@@ -2,7 +2,7 @@ use fj_interop::ext::ArrayExt;
 use fj_math::Point;
 
 use crate::{
-    objects::{Surface, SurfaceVertex},
+    objects::{HalfEdge, Surface, SurfaceVertex},
     partial::{Partial, PartialCycle, PartialHalfEdge, PartialSurfaceVertex},
 };
 
@@ -15,6 +15,17 @@ pub trait CycleBuilder {
         surface: impl Into<Partial<Surface>>,
         points: impl IntoIterator<Item = impl Into<Point<2>>>,
     ) -> Self;
+
+    /// Add a new half-edge to the cycle
+    ///
+    /// Creates a half-edge and adds it to the cycle. The new half-edge is
+    /// connected to the front vertex of the last half-edge , and the back
+    /// vertex of the first edge, making sure the half-edges actually form a
+    /// cycle.
+    ///
+    /// If this is the first half-edge being added, it is connected to itself,
+    /// meaning its front and back vertices are the same.
+    fn add_half_edge(&mut self) -> Partial<HalfEdge>;
 }
 
 impl CycleBuilder for PartialCycle {
@@ -119,5 +130,54 @@ impl CycleBuilder for PartialCycle {
         cycle.half_edges.push(Partial::from_partial(half_edge));
 
         cycle
+    }
+
+    fn add_half_edge(&mut self) -> Partial<HalfEdge> {
+        let mut new_half_edge = Partial::<HalfEdge>::new();
+
+        let (first_half_edge, mut last_half_edge) =
+            match self.half_edges.first() {
+                Some(first_half_edge) => {
+                    let first_half_edge = first_half_edge.clone();
+                    let last_half_edge = self
+                        .half_edges
+                        .last()
+                        .cloned()
+                        .unwrap_or_else(|| first_half_edge.clone());
+
+                    (first_half_edge, last_half_edge)
+                }
+                None => (new_half_edge.clone(), new_half_edge.clone()),
+            };
+
+        let shared_surface =
+            first_half_edge.read().curve().read().surface.clone();
+
+        {
+            let shared_surface_vertex =
+                new_half_edge.read().back().read().surface_form.clone();
+
+            let mut last_half_edge = last_half_edge.write();
+            last_half_edge.curve().write().surface = shared_surface.clone();
+            last_half_edge.front_mut().write().surface_form =
+                shared_surface_vertex;
+
+            last_half_edge.infer_global_form();
+        }
+
+        {
+            let shared_surface_vertex =
+                first_half_edge.read().back().read().surface_form.clone();
+
+            let mut new_half_edge = new_half_edge.write();
+
+            new_half_edge.curve().write().surface = shared_surface;
+            new_half_edge.front_mut().write().surface_form =
+                shared_surface_vertex;
+            new_half_edge.infer_global_form();
+        }
+
+        self.half_edges.push(new_half_edge.clone());
+        new_half_edge
     }
 }
