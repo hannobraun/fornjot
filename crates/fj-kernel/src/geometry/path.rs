a77c13ba160d5a64de9673e9ff9ce0bc65641e53
Hanno Braun <hanno@braun-odw.eu>
2022-12-16 16:13:50 +0100

    Refactor

--- crates/fj-kernel/src/geometry/path.rs
+++ crates/fj-kernel/src/geometry/path.rs
@@ -44,7 +44,8 @@ impl SurfacePath {
 
     /// Construct a line from two points
     pub fn line_from_points(points: [impl Into<Point<2>>; 2]) -> Self {
-        Self::Line(Line::from_points(points))
+        let line = Line::from_points(points);
+        Self::Line(line)
     }
 
     /// Convert a point on the path into surface coordinates
@@ -103,7 +104,8 @@ impl GlobalPath {
 
     /// Construct a line from two points
     pub fn line_from_points(points: [impl Into<Point<3>>; 2]) -> Self {
-        Self::Line(Line::from_points(points))
+        let line = Line::from_points(points);
+        Self::Line(line)
     }
 
     /// Access the origin of the path's coordinate system
