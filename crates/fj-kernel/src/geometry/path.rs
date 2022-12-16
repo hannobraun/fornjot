223802eadd5683c5a86202d7840c346b658dfc8f
Hanno Braun <hanno@braun-odw.eu>
2022-12-16 16:24:36 +0100

    Update doc comments

--- crates/fj-kernel/src/geometry/path.rs
+++ crates/fj-kernel/src/geometry/path.rs
@@ -43,6 +43,8 @@ impl SurfacePath {
     }
 
     /// Construct a line from two points
+    ///
+    /// Also returns the coordinates of the points on the path.
     pub fn line_from_points(
         points: [impl Into<Point<2>>; 2],
     ) -> (Self, [Point<1>; 2]) {
@@ -105,6 +107,8 @@ impl GlobalPath {
     }
 
     /// Construct a line from two points
+    ///
+    /// Also returns the coordinates of the points on the path.
     pub fn line_from_points(
         points: [impl Into<Point<3>>; 2],
     ) -> (Self, [Point<1>; 2]) {
