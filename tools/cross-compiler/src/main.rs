9de2a88a6788adaa95777585dc1a72841735d136
Hanno Braun <hanno@braun-odw.eu>
2022-12-21 11:57:16 +0100

    Add list of cross-compile targets

--- tools/cross-compiler/src/main.rs
+++ tools/cross-compiler/src/main.rs
@@ -10,6 +10,7 @@ use std::process::Command;
 use anyhow::anyhow;
 
 fn main() -> anyhow::Result<()> {
+    let targets = ["wasm32-unknown-unknown"];
     let crates = [
         "fj",
         "fj-export",
@@ -21,20 +22,22 @@ fn main() -> anyhow::Result<()> {
         "fj-viewer",
     ];
 
-    for crate_ in crates {
-        let mut command = Command::new("cargo");
-        command
-            .arg("build")
-            .arg("--all-features")
-            .args(["--target", "wasm32-unknown-unknown"])
-            .args(["-p", crate_])
-            .env("RUSTFLAGS", "-D warnings");
+    for target in targets {
+        for crate_ in crates {
+            let mut command = Command::new("cargo");
+            command
+                .arg("build")
+                .arg("--all-features")
+                .args(["--target", target])
+                .args(["-p", crate_])
+                .env("RUSTFLAGS", "-D warnings");
 
-        println!("Running {command:?}");
-        let status = command.status()?;
+            println!("Running {command:?}");
+            let status = command.status()?;
 
-        if !status.success() {
-            return Err(anyhow!("Cargo exited with error code: {status}"));
+            if !status.success() {
+                return Err(anyhow!("Cargo exited with error code: {status}"));
+            }
         }
     }
 
