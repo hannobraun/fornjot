cd4bd4d3b6464e1895822fc3001aab94d279a697
Hanno Braun <hanno@braun-odw.eu>
2023-01-06 13:36:35 +0100

    Remove unused builder methods
    The unit tests that used these methods no longer exist, and as I found
    out, that code was very dodgy to begin with. It worked in the contexts
    where it was used, but it probably wouldn't have worked in real code, as
    it didn't correctly take care of face orientation.
    
    Pretty sure those cubes wouldn't have made it through the 3MF validation
    testing.

--- crates/fj-kernel/src/builder/solid.rs
+++ crates/fj-kernel/src/builder/solid.rs
@@ -1,31 +1,9 @@
-use fj_math::Scalar;
-
-use crate::{
-    objects::Objects,
-    partial::{Partial, PartialShell, PartialSolid},
-    services::Service,
-};
-
-use super::ShellBuilder;
+use crate::partial::PartialSolid;
 
 /// Builder API for [`PartialSolid`]
 pub trait SolidBuilder {
-    /// Add a cube with the given edge length to the solid
-    fn with_cube_from_edge_length(
-        &mut self,
-        edge_length: impl Into<Scalar>,
-        objects: &mut Service<Objects>,
-    );
+    // No methods are currently defined. This trait serves as a placeholder, to
+    // make it clear where to add such methods, once necessary.
 }
 
-impl SolidBuilder for PartialSolid {
-    fn with_cube_from_edge_length(
-        &mut self,
-        edge_length: impl Into<Scalar>,
-        objects: &mut Service<Objects>,
-    ) {
-        let shell =
-            PartialShell::create_cube_from_edge_length(edge_length, objects);
-        self.shells.push(Partial::from_partial(shell));
-    }
-}
+impl SolidBuilder for PartialSolid {}
