0899b353d4371c9eb7f61ca2ca0dbe33f23a188f
Hanno Braun <hanno@braun-odw.eu>
2023-01-08 11:26:16 +0100

    Update `CurveBuilder` documentation

--- crates/fj-kernel/src/builder/curve.rs
+++ crates/fj-kernel/src/builder/curve.rs
@@ -5,18 +5,26 @@ use crate::{geometry::path::SurfacePath, partial::PartialCurve};
 /// Builder API for [`PartialCurve`]
 pub trait CurveBuilder {
     /// Update partial curve to represent the u-axis of the surface it is on
+    ///
+    /// Returns the updated path.
     fn update_as_u_axis(&mut self) -> SurfacePath;
 
     /// Update partial curve to represent the v-axis of the surface it is on
+    ///
+    /// Returns the updated path.
     fn update_as_v_axis(&mut self) -> SurfacePath;
 
     /// Update partial curve to be a circle, from the provided radius
+    ///
+    /// Returns the updated path.
     fn update_as_circle_from_radius(
         &mut self,
         radius: impl Into<Scalar>,
     ) -> SurfacePath;
 
     /// Update partial curve to be a circle, from the provided radius
+    ///
+    /// Returns the updated path.
     fn update_as_circle_from_center_and_radius(
         &mut self,
         center: impl Into<Point<2>>,
@@ -24,6 +32,8 @@ pub trait CurveBuilder {
     ) -> SurfacePath;
 
     /// Update partial curve to be a line, from the provided points
+    ///
+    /// Returns the updated path.
     fn update_as_line_from_points(
         &mut self,
         points: [impl Into<Point<2>>; 2],
