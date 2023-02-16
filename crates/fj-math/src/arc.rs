dc2bfaa0bd105a84ae071d0eb542f243617c2097
Hanno Braun <hanno@braun-odw.eu>
2023-02-16 12:55:09 +0100

    Refactor

--- crates/fj-math/src/arc.rs
+++ crates/fj-math/src/arc.rs
@@ -34,10 +34,11 @@ impl Arc {
         let radius = distance_between_endpoints
             / (2. * (angle_rad.abs().into_f64() / 2.).sin());
 
+        let clockwise_turn = angle_rad <= Scalar::ZERO;
         let more_than_half_turn = angle_rad.abs() > Scalar::PI;
 
         let center = {
-            let f = match (angle_rad <= Scalar::ZERO, more_than_half_turn) {
+            let f = match (clockwise_turn, more_than_half_turn) {
                 (false, false) => Scalar::ONE,
                 (false, true) => -Scalar::ONE,
                 (true, false) => -Scalar::ONE,
