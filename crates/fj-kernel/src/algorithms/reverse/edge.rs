fd1ab4a6d61dede84ea0ea96e45028f1a6afbcb0
Hanno Braun <hanno@braun-odw.eu>
2023-02-27 16:40:09 +0100

    Refactor

--- crates/fj-kernel/src/algorithms/reverse/edge.rs
+++ crates/fj-kernel/src/algorithms/reverse/edge.rs
@@ -11,12 +11,16 @@ use super::Reverse;
 
 impl Reverse for Handle<HalfEdge> {
     fn reverse(self, objects: &mut Service<Objects>) -> Self {
-        let vertices = {
-            let [a, b] = self
-                .boundary()
-                .zip_ext(self.surface_vertices().map(Clone::clone));
+        let boundary = {
+            let [a, b] = self.boundary();
             [b, a]
         };
+        let surface_vertices = {
+            let [a, b] = self.surface_vertices().map(Clone::clone);
+            [b, a]
+        };
+
+        let vertices = boundary.zip_ext(surface_vertices);
 
         HalfEdge::new(self.curve(), vertices, self.global_form().clone())
             .insert(objects)
