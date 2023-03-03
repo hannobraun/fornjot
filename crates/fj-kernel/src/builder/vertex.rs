63a574d8b4e9bce8ace72a6ce13b59db99bacbc3
Hanno Braun <hanno@braun-odw.eu>
2023-03-03 16:43:26 +0100

    Remove `VertexBuilder`

--- crates/fj-kernel/src/builder/vertex.rs
+++
@@ -1,9 +0,0 @@
-use crate::partial::PartialVertex;
-
-/// Builder API for [`PartialVertex`]
-pub trait VertexBuilder {
-    // No methods are currently defined. This trait serves as a placeholder, to
-    // make it clear where to add such methods, once necessary.
-}
-
-impl VertexBuilder for PartialVertex {}
