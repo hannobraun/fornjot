78a9675059bd27fff759e75715ce68c8d9c23601
Hanno Braun <hanno@braun-odw.eu>
2023-03-09 11:52:37 +0100

    Add `FullOrPartial`

--- crates/fj-kernel/src/partial/mod.rs
+++ crates/fj-kernel/src/partial/mod.rs
@@ -22,3 +22,33 @@ pub use self::{
     traits::{HasPartial, PartialObject},
     wrapper::{FullToPartialCache, Partial},
 };
+
+use crate::storage::Handle;
+
+/// Either a full or a partial object
+///
+/// # Implementation Note
+///
+/// This enum temporarily exists to aid in the transition towards a unified
+/// object system. Issue:
+/// <https://github.com/hannobraun/Fornjot/issues/1570>
+#[derive(Clone, Debug)]
+pub enum FullOrPartial<T: HasPartial + 'static> {
+    /// A full object
+    Full(Handle<T>),
+
+    /// A partial object
+    Partial(Partial<T>),
+}
+
+impl<T: HasPartial> From<Handle<T>> for FullOrPartial<T> {
+    fn from(object: Handle<T>) -> Self {
+        Self::Full(object)
+    }
+}
+
+impl<T: HasPartial> From<Partial<T>> for FullOrPartial<T> {
+    fn from(object: Partial<T>) -> Self {
+        Self::Partial(object)
+    }
+}
