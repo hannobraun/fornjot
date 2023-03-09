0a68de0e947d45606b3e3b9a768cbe0c95125704
Hanno Braun <hanno@braun-odw.eu>
2023-03-09 12:11:40 +0100

    Simplify `PartialHalfEdge` construction

--- crates/fj-kernel/src/algorithms/approx/edge.rs
+++ crates/fj-kernel/src/algorithms/approx/edge.rs
@@ -377,12 +377,8 @@ mod tests {
         let mut services = Services::new();
 
         let surface = services.objects.surfaces.xz_plane();
-        let half_edge = {
-            let half_edge =
-                PartialHalfEdge::make_circle(1., &mut services.objects);
-
-            half_edge.build(&mut services.objects)
-        };
+        let half_edge = PartialHalfEdge::make_circle(1., &mut services.objects)
+            .build(&mut services.objects);
 
         let tolerance = 1.;
         let approx = (&half_edge, surface.deref()).approx(tolerance);
