8e7c1008229247019ac5df19ca0ab633a000235a
Hanno Braun <hanno@braun-odw.eu>
2023-03-15 16:57:05 +0100

    Remove unused builder method

--- crates/fj-kernel/src/builder/cycle.rs
+++ crates/fj-kernel/src/builder/cycle.rs
@@ -28,17 +28,6 @@ pub trait CycleBuilder: Sized {
         objects: &mut Service<Objects>,
     ) -> (Self, Handle<HalfEdge>);
 
-    /// Update cycle as a polygon from the provided points
-    fn update_as_polygon_from_points<P, Ps>(
-        self,
-        points: Ps,
-        objects: &mut Service<Objects>,
-    ) -> Self
-    where
-        P: Into<Point<2>>,
-        Ps: IntoIterator<Item = P>,
-        Ps::IntoIter: Clone + ExactSizeIterator;
-
     /// Connect the cycles to the provided half-edges
     ///
     /// Assumes that the provided half-edges, once translated into local
@@ -64,31 +53,6 @@ impl CycleBuilder for Cycle {
         let cycle =
             Cycle::new(self.half_edges().cloned().chain([half_edge.clone()]));
         (cycle, half_edge)
-    }
-
-    fn update_as_polygon_from_points<P, Ps>(
-        mut self,
-        points: Ps,
-        objects: &mut Service<Objects>,
-    ) -> Self
-    where
-        P: Into<Point<2>>,
-        Ps: IntoIterator<Item = P>,
-        Ps::IntoIter: Clone + ExactSizeIterator,
-    {
-        points
-            .into_iter()
-            .map(Into::into)
-            .circular_tuple_windows()
-            .for_each(|(start, end)| {
-                let half_edge =
-                    HalfEdgeBuilder::line_segment([start, end], None);
-
-                let (cycle, _) = self.clone().add_half_edge(half_edge, objects);
-                self = cycle;
-            });
-
-        self
     }
 
     fn connect_to_edges<O>(
