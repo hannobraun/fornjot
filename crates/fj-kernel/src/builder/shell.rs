bdddf7100a462bcfcc3b10106f517db95f8bd7e6
Hanno Braun <hanno@braun-odw.eu>
2023-03-22 12:00:56 +0100

    Make more use of `FaceBuilder::triangle`

--- crates/fj-kernel/src/builder/shell.rs
+++ crates/fj-kernel/src/builder/shell.rs
@@ -1,12 +1,11 @@
 use fj_math::Point;
 
 use crate::{
-    insert::Insert,
-    objects::{Cycle, Face, Objects, Shell},
+    objects::{Objects, Shell},
     services::Service,
 };
 
-use super::{FaceBuilder, HalfEdgeBuilder, SurfaceBuilder};
+use super::FaceBuilder;
 
 /// Builder API for [`Shell`]
 pub struct ShellBuilder {}
@@ -21,88 +20,18 @@ impl ShellBuilder {
 
         let (bottom, [ab, bc, ca]) =
             FaceBuilder::triangle([a, b, c], [None, None, None], objects);
-        let (front, [_, bd, da]) = {
-            let surface =
-                SurfaceBuilder::plane_from_points([a, b, d]).insert(objects);
-            let (exterior, global_edges) = {
-                let half_edges =
-                    [([a, b], Some(ab)), ([b, d], None), ([d, a], None)].map(
-                        |(points, global_form)| {
-                            let mut builder =
-                            HalfEdgeBuilder::line_segment_from_global_points(
-                                points, &surface, None,
-                            );
-
-                            if let Some(global_form) = global_form {
-                                builder = builder.with_global_form(global_form);
-                            }
-
-                            builder.build(objects).insert(objects)
-                        },
-                    );
-
-                let cycle = Cycle::new(half_edges.clone()).insert(objects);
-
-                let global_edges = half_edges
-                    .map(|half_edges| half_edges.global_form().clone());
-
-                (cycle, global_edges)
-            };
-
-            let face = Face::new(surface, exterior, [], None).insert(objects);
-
-            (face, global_edges)
-        };
-        let (left, [_, _, dc]) = {
-            let surface =
-                SurfaceBuilder::plane_from_points([c, a, d]).insert(objects);
-            let (exterior, global_edges) = {
-                let half_edges =
-                    [([c, a], Some(ca)), ([a, d], Some(da)), ([d, c], None)]
-                        .map(|(points, global_form)| {
-                            let mut builder =
-                            HalfEdgeBuilder::line_segment_from_global_points(
-                                points, &surface, None,
-                            );
-
-                            if let Some(global_form) = global_form {
-                                builder = builder.with_global_form(global_form);
-                            }
-
-                            builder.build(objects).insert(objects)
-                        });
-
-                let cycle = Cycle::new(half_edges.clone()).insert(objects);
-
-                let global_edges =
-                    half_edges.map(|half_edge| half_edge.global_form().clone());
-
-                (cycle, global_edges)
-            };
-
-            let face = Face::new(surface, exterior, [], None).insert(objects);
-
-            (face, global_edges)
-        };
-        let back_right = {
-            let surface =
-                SurfaceBuilder::plane_from_points([b, c, d]).insert(objects);
-            let exterior = {
-                let half_edges = [([b, c], bc), ([c, d], dc), ([d, b], bd)]
-                    .map(|(points, global_form)| {
-                        HalfEdgeBuilder::line_segment_from_global_points(
-                            points, &surface, None,
-                        )
-                        .with_global_form(global_form)
-                        .build(objects)
-                        .insert(objects)
-                    });
-
-                Cycle::new(half_edges).insert(objects)
-            };
-
-            Face::new(surface, exterior, [], None).insert(objects)
-        };
+        let (front, [_, bd, da]) =
+            FaceBuilder::triangle([a, b, d], [Some(ab), None, None], objects);
+        let (left, [_, _, dc]) = FaceBuilder::triangle(
+            [c, a, d],
+            [Some(ca), Some(da), None],
+            objects,
+        );
+        let (back_right, _) = FaceBuilder::triangle(
+            [b, c, d],
+            [Some(bc), Some(dc), Some(bd)],
+            objects,
+        );
 
         Shell::new([bottom, front, left, back_right])
     }
