7645e74ce95c8768217b23bcd3762b408eafe8be
Hanno Braun <hanno@braun-odw.eu>
2023-03-22 10:31:22 +0100

    Add new `HalfEdgeBuilder` method

--- crates/fj-kernel/src/builder/edge.rs
+++ crates/fj-kernel/src/builder/edge.rs
@@ -4,7 +4,7 @@ use fj_math::{Arc, Point, Scalar};
 use crate::{
     geometry::curve::Curve,
     insert::Insert,
-    objects::{GlobalEdge, HalfEdge, Objects, Vertex},
+    objects::{GlobalEdge, HalfEdge, Objects, Surface, Vertex},
     services::Service,
     storage::Handle,
 };
@@ -74,6 +74,17 @@ impl HalfEdgeBuilder {
         );
 
         Self::new(curve, boundary)
+    }
+
+    /// Create a line segment from global points
+    pub fn line_segment_from_global_points(
+        points_global: [impl Into<Point<3>>; 2],
+        surface: &Surface,
+        boundary: Option<[Point<1>; 2]>,
+    ) -> Self {
+        let points_surface = points_global
+            .map(|point| surface.geometry().project_global_point(point));
+        Self::line_segment(points_surface, boundary)
     }
 
     /// Build the half-edge with a specific start vertex
