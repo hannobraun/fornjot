d8f38710fd0e8d413263bffdf9c0940265f4097e
Hanno Braun <hanno@braun-odw.eu>
2023-03-23 14:40:36 +0100

    Add `UpdateShell::remove_face`

--- crates/fj-kernel/src/operations/update/shell.rs
+++ crates/fj-kernel/src/operations/update/shell.rs
@@ -11,6 +11,9 @@ pub trait UpdateShell {
         handle: &Handle<Face>,
         f: impl FnMut(&Handle<Face>) -> Handle<Face>,
     ) -> Shell;
+
+    /// Remove a face from the shell
+    fn remove_face(&self, handle: &Handle<Face>) -> Shell;
 }
 
 impl UpdateShell for Shell {
@@ -26,6 +29,16 @@ impl UpdateShell for Shell {
                 face.clone()
             }
         });
+
+        Shell::new(faces)
+    }
+
+    fn remove_face(&self, handle: &Handle<Face>) -> Shell {
+        let faces = self
+            .faces()
+            .into_iter()
+            .filter(|face| face.id() == handle.id())
+            .cloned();
 
         Shell::new(faces)
     }
