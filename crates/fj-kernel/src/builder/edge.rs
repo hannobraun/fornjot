b3c6ae9de8e74a01a4edf25281a72c6f0c473f75
Hanno Braun <hanno@braun-odw.eu>
2023-03-30 12:24:19 +0200

    Remove unused `HalfEdgeBuilder` methods

--- crates/fj-kernel/src/builder/edge.rs
+++ crates/fj-kernel/src/builder/edge.rs
@@ -17,28 +17,6 @@ pub struct HalfEdgeBuilder {
 }
 
 impl HalfEdgeBuilder {
-    /// Create an instance of `HalfEdgeBuilder`
-    pub fn new(curve: Curve, boundary: [Point<1>; 2]) -> Self {
-        Self {
-            curve,
-            boundary,
-            start_vertex: None,
-            global_form: None,
-        }
-    }
-
-    /// Build the half-edge with a specific start vertex
-    pub fn with_start_vertex(mut self, start_vertex: Handle<Vertex>) -> Self {
-        self.start_vertex = Some(start_vertex);
-        self
-    }
-
-    /// Build the half-edge with a specific global form
-    pub fn with_global_form(mut self, global_form: Handle<GlobalEdge>) -> Self {
-        self.global_form = Some(global_form);
-        self
-    }
-
     /// Build the half-edge
     pub fn build(self, objects: &mut Service<Objects>) -> HalfEdge {
         HalfEdge::new(
