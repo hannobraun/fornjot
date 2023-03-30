00d5bfb86d452b8385d4e36e0b1ef993902af14c
Hanno Braun <hanno@braun-odw.eu>
2023-03-30 11:35:03 +0200

    Remove unused `HalfEdgeBuilder::arc`

--- crates/fj-kernel/src/builder/edge.rs
+++ crates/fj-kernel/src/builder/edge.rs
@@ -1,5 +1,5 @@
 use fj_interop::ext::ArrayExt;
-use fj_math::{Arc, Point, Scalar};
+use fj_math::Point;
 
 use crate::{
     geometry::curve::Curve,
@@ -26,31 +26,6 @@ impl HalfEdgeBuilder {
             start_vertex: None,
             global_form: None,
         }
-    }
-
-    /// Create an arc
-    ///
-    /// # Panics
-    ///
-    /// Panics if the given angle is not within the range (-2pi, 2pi) radians.
-    pub fn arc(
-        start: impl Into<Point<2>>,
-        end: impl Into<Point<2>>,
-        angle_rad: impl Into<Scalar>,
-    ) -> Self {
-        let angle_rad = angle_rad.into();
-        if angle_rad <= -Scalar::TAU || angle_rad >= Scalar::TAU {
-            panic!("arc angle must be in the range (-2pi, 2pi) radians");
-        }
-
-        let arc = Arc::from_endpoints_and_angle(start, end, angle_rad);
-
-        let curve =
-            Curve::circle_from_center_and_radius(arc.center, arc.radius);
-        let boundary =
-            [arc.start_angle, arc.end_angle].map(|coord| Point::from([coord]));
-
-        Self::new(curve, boundary)
     }
 
     /// Create a line segment
