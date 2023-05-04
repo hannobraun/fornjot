da5bd11057ebe669202bfa4667ebfa1bc193aee7
Hanno Braun <hanno@braun-odw.eu>
2023-05-04 11:20:36 +0200

    Make `Insert` more flexible

--- crates/fj-kernel/src/operations/insert.rs
+++ crates/fj-kernel/src/operations/insert.rs
@@ -12,15 +12,23 @@ use crate::{
 /// This is the only primitive operation that is directly understood by
 /// `Service<Objects>`. All other operations are built on top of it.
 pub trait Insert: Sized {
+    /// The type of `Self`, once it has been inserted
+    ///
+    /// Usually this is just `Handle<Self>`, but there are some more complex
+    /// cases where this type needs to be customized.
+    type Inserted;
+
     /// Insert the object into its respective store
-    fn insert(self, services: &mut Services) -> Handle<Self>;
+    fn insert(self, services: &mut Services) -> Self::Inserted;
 }
 
 macro_rules! impl_insert {
     ($($ty:ty, $store:ident;)*) => {
         $(
             impl Insert for $ty {
-                fn insert(self, services: &mut Services) -> Handle<Self> {
+                type Inserted = Handle<Self>;
+
+                fn insert(self, services: &mut Services) -> Self::Inserted {
                     let handle = services.objects.$store.reserve();
                     let object = (handle.clone(), self).into();
                     services.insert_object(object);
