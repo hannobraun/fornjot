c43aa318820553d785eb99581eeb60e64b72c6bd
Hanno Braun <hanno@braun-odw.eu>
2023-05-30 12:01:56 +0200

    Upgrade to wgpu 0.16.1

--- crates/fj-viewer/src/graphics/texture.rs
+++ crates/fj-viewer/src/graphics/texture.rs
@@ -1,5 +1,4 @@
 use image::{GenericImageView, ImageError};
-use std::num::NonZeroU32;
 
 #[derive(Debug)]
 pub struct Texture {
@@ -61,8 +60,8 @@ impl Texture {
             &rgba,
             wgpu::ImageDataLayout {
                 offset: 0,
-                bytes_per_row: NonZeroU32::new(4 * dimensions.0),
-                rows_per_image: NonZeroU32::new(dimensions.1),
+                bytes_per_row: Some(4 * dimensions.0),
+                rows_per_image: Some(dimensions.1),
             },
             size,
         );
