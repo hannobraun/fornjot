0ea99f87ee3da3c57e0feef250f44b3604d3df13
Hanno Braun <hanno@braun-odw.eu>
2023-05-30 13:08:21 +0200

    Use `BuildCycle` to simplify `cuboid`

--- models/cuboid/src/lib.rs
+++ models/cuboid/src/lib.rs
@@ -1,44 +1,27 @@
 use fj_kernel::{
     algorithms::sweep::Sweep,
     geometry::region::Region,
-    objects::{Cycle, HalfEdge, Sketch, Solid},
-    operations::{BuildCycle, BuildHalfEdge, Insert, UpdateCycle},
+    objects::{Cycle, Sketch, Solid},
+    operations::{BuildCycle, Insert},
     services::Services,
     storage::Handle,
 };
-use fj_math::{Point, Vector};
-use itertools::Itertools;
+use fj_math::Vector;
 
 pub fn cuboid(x: f64, y: f64, z: f64) -> Handle<Solid> {
     let mut services = Services::new();
 
     let sketch = {
-        let exterior = {
-            #[rustfmt::skip]
-            let rectangle = [
+        let exterior = Cycle::polygon(
+            [
                 [-x / 2., -y / 2.],
-                [ x / 2., -y / 2.],
-                [ x / 2.,  y / 2.],
-                [-x / 2.,  y / 2.],
-            ];
-
-            let mut cycle = Cycle::empty();
-
-            let segments = rectangle
-                .into_iter()
-                .map(Point::from)
-                .circular_tuple_windows();
-
-            for (start, end) in segments {
-                let half_edge =
-                    HalfEdge::line_segment([start, end], None, &mut services)
-                        .insert(&mut services);
-
-                cycle = cycle.add_half_edges([half_edge]);
-            }
-
-            cycle.insert(&mut services)
-        };
+                [x / 2., -y / 2.],
+                [x / 2., y / 2.],
+                [-x / 2., y / 2.],
+            ],
+            &mut services,
+        )
+        .insert(&mut services);
 
         let region = Region::new(exterior, Vec::new(), None);
 
