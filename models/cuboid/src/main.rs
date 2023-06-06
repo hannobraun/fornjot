e8e3c9d5732b2055c43534c535194b0531ee410d
Hanno Braun <hanno@braun-odw.eu>
2023-06-06 11:07:34 +0200

    Simplify code in `cuboid`

--- models/cuboid/src/main.rs
+++ models/cuboid/src/main.rs
@@ -1,11 +1,11 @@
-use fj::{core::algorithms::approx::Tolerance, handle_model};
+use fj::handle_model;
 
 fn main() -> anyhow::Result<()> {
     let cuboid = cuboid::cuboid(3., 2., 1.);
 
     // The tolerance makes no difference for this model, as there aren't any
     // curves.
-    let tolerance = Tolerance::from_scalar(1.)?;
+    let tolerance = 1.;
     handle_model(cuboid, tolerance)?;
 
     Ok(())
