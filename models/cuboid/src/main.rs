da5fa66c2abe3b1edc4f03e83247ec38eda3b380
Hanno Braun <hanno@braun-odw.eu>
2023-06-06 11:08:18 +0200

    Simplify return value of `cuboid::main`

--- models/cuboid/src/main.rs
+++ models/cuboid/src/main.rs
@@ -1,6 +1,6 @@
 use fj::handle_model;
 
-fn main() -> anyhow::Result<()> {
+fn main() -> Result<(), fj::Error> {
     let cuboid = cuboid::cuboid(3., 2., 1.);
 
     // The tolerance makes no difference for this model, as there aren't any
