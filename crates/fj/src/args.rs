cbcb031ae8a8461ce28add316eeaeb16a92e0602
Hanno Braun <hanno@braun-odw.eu>
2023-06-06 13:11:53 +0200

    Add `--tolerance` argument to model CLI

--- crates/fj/src/args.rs
+++ crates/fj/src/args.rs
@@ -1,4 +1,7 @@
-use std::path::PathBuf;
+use std::{num::ParseFloatError, path::PathBuf, str::FromStr};
+
+use fj_core::algorithms::approx::{InvalidTolerance, Tolerance};
+use fj_math::Scalar;
 
 /// Standardized CLI for Fornjot models
 #[derive(clap::Parser)]
@@ -6,6 +9,10 @@ pub struct Args {
     /// Export model to this path
     #[arg(short, long, value_name = "PATH")]
     pub export: Option<PathBuf>,
+
+    /// How much the export can deviate from the original model
+    #[arg(short, long, value_parser = parse_tolerance)]
+    pub tolerance: Option<Tolerance>,
 }
 
 impl Args {
@@ -16,4 +23,21 @@ impl Args {
     pub fn parse() -> Self {
         <Self as clap::Parser>::parse()
     }
+}
+
+fn parse_tolerance(input: &str) -> Result<Tolerance, ArgsError> {
+    let tolerance = f64::from_str(input)?;
+    let tolerance = Scalar::from_f64(tolerance);
+    let tolerance = Tolerance::from_scalar(tolerance)?;
+
+    Ok(tolerance)
+}
+
+#[derive(Debug, thiserror::Error)]
+pub enum ArgsError {
+    #[error("Error parsing tolerance")]
+    ParseTolerance(#[from] ParseFloatError),
+
+    #[error(transparent)]
+    InvalidTolerance(#[from] InvalidTolerance),
 }
