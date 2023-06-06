5b8c3a18aea2b970559c1d138f5275e3967c99a1
Hanno Braun <hanno@braun-odw.eu>
2023-06-06 12:48:43 +0200

    Update order of struct fields
    The mesh is much more integral to the model than the AABB, so it makes
    sense to list it first.

--- crates/fj-interop/src/model.rs
+++ crates/fj-interop/src/model.rs
@@ -7,9 +7,9 @@ use crate::mesh::Mesh;
 /// An approximated model
 #[derive(Clone, Debug)]
 pub struct Model {
-    /// The axis-aligned bounding box of the model
-    pub aabb: Aabb<3>,
-
     /// The triangle mesh that approximates the model
     pub mesh: Mesh<Point<3>>,
+
+    /// The axis-aligned bounding box of the model
+    pub aabb: Aabb<3>,
 }
