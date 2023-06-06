88ff9f6c6e4df79f84a751e77bf690d43a18fb5e
Hanno Braun <hanno@braun-odw.eu>
2023-06-06 11:34:15 +0200

    Update doc comment

--- crates/fj-window/src/run.rs
+++ crates/fj-window/src/run.rs
@@ -18,7 +18,7 @@ use winit::{
 
 use crate::window::{self, Window};
 
-/// Initializes a model viewer for a given model and enters its process loop.
+/// Display the provided mesh in a window that processes input
 pub fn display(mesh: Mesh<Point<3>>, invert_zoom: bool) -> Result<(), Error> {
     let event_loop = EventLoop::new();
     let window = Window::new(&event_loop)?;
