c9b767b0a455fe05b2ab4b39503dcbe42c6933ef
Hanno Braun <hanno@braun-odw.eu>
2023-06-12 12:29:43 +0200

    Replace `BuildSketch::polygon`
    Having `UpdateSketch::add_polygon` instead is more flexible, providing
    an API to add new regions to the sketch.

--- crates/fj-core/src/operations/update/sketch.rs
+++ crates/fj-core/src/operations/update/sketch.rs
@@ -1,6 +1,32 @@
-use crate::objects::Sketch;
+use fj_math::Point;
+
+use crate::{
+    geometry::region::Region,
+    objects::{Cycle, Sketch},
+    operations::{BuildCycle, Insert},
+    services::Services,
+};
 
 /// Update a [`Sketch`]
-pub trait UpdateSketch {}
+pub trait UpdateSketch {
+    /// Add a polygon to the sketch
+    fn add_polygon<P, Ps>(&self, points: Ps, services: &mut Services) -> Self
+    where
+        P: Into<Point<2>>,
+        Ps: IntoIterator<Item = P>,
+        Ps::IntoIter: Clone + ExactSizeIterator;
+}
 
-impl UpdateSketch for Sketch {}
+impl UpdateSketch for Sketch {
+    fn add_polygon<P, Ps>(&self, points: Ps, services: &mut Services) -> Self
+    where
+        P: Into<Point<2>>,
+        Ps: IntoIterator<Item = P>,
+        Ps::IntoIter: Clone + ExactSizeIterator,
+    {
+        let exterior = Cycle::polygon(points, services).insert(services);
+        let region = Region::new(exterior, Vec::new(), None);
+
+        Sketch::new(self.regions().cloned().chain([region]))
+    }
+}
