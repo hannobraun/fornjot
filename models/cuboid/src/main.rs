e4efdb0c36f7afb0bf72351459a47dd802791f28
Hanno Braun <hanno@braun-odw.eu>
2023-06-12 11:42:44 +0200

    Update variable name

--- models/cuboid/src/main.rs
+++ models/cuboid/src/main.rs
@@ -1,7 +1,7 @@
 use fj::handle_model;
 
 fn main() -> fj::Result {
-    let cuboid = cuboid::cuboid(3., 2., 1.);
-    handle_model(cuboid)?;
+    let model = cuboid::cuboid(3., 2., 1.);
+    handle_model(model)?;
     Ok(())
 }
