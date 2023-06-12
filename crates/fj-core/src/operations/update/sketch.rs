0467a6643bdea4f187509d8ae889288ef3c4db33
Hanno Braun <hanno@braun-odw.eu>
2023-06-12 12:32:44 +0200

    Add `UpdateSketch::add_region`

--- crates/fj-core/src/operations/update/sketch.rs
+++ crates/fj-core/src/operations/update/sketch.rs
@@ -9,6 +9,9 @@ use crate::{
 
 /// Update a [`Sketch`]
 pub trait UpdateSketch {
+    /// Add a region to the sketch
+    fn add_region(&self, region: Region) -> Self;
+
     /// Add a polygon to the sketch
     fn add_polygon<P, Ps>(&self, points: Ps, services: &mut Services) -> Self
     where
@@ -18,6 +21,10 @@ pub trait UpdateSketch {
 }
 
 impl UpdateSketch for Sketch {
+    fn add_region(&self, region: Region) -> Self {
+        Sketch::new(self.regions().cloned().chain([region]))
+    }
+
     fn add_polygon<P, Ps>(&self, points: Ps, services: &mut Services) -> Self
     where
         P: Into<Point<2>>,
@@ -25,8 +32,6 @@ impl UpdateSketch for Sketch {
         Ps::IntoIter: Clone + ExactSizeIterator,
     {
         let exterior = Cycle::polygon(points, services).insert(services);
-        let region = Region::new(exterior, Vec::new(), None);
-
-        Sketch::new(self.regions().cloned().chain([region]))
+        self.add_region(Region::new(exterior, Vec::new(), None))
     }
 }
