6a69d248b245983e0a6e69f20521ae9789b11e6d
Hanno Braun <hanno@braun-odw.eu>
2023-06-13 13:52:43 +0200

    Remove redundant `UpdateSketch::add_polygon`

--- crates/fj-core/src/operations/update/sketch.rs
+++ crates/fj-core/src/operations/update/sketch.rs
@@ -1,9 +1,5 @@
-use fj_math::Point;
-
 use crate::{
     objects::{Region, Sketch},
-    operations::{BuildRegion, Insert},
-    services::Services,
     storage::Handle,
 };
 
@@ -11,27 +7,10 @@ use crate::{
 pub trait UpdateSketch {
     /// Add a region to the sketch
     fn add_region(&self, region: Handle<Region>) -> Self;
-
-    /// Add a polygon to the sketch
-    fn add_polygon<P, Ps>(&self, points: Ps, services: &mut Services) -> Self
-    where
-        P: Into<Point<2>>,
-        Ps: IntoIterator<Item = P>,
-        Ps::IntoIter: Clone + ExactSizeIterator;
 }
 
 impl UpdateSketch for Sketch {
     fn add_region(&self, region: Handle<Region>) -> Self {
         Sketch::new(self.regions().cloned().chain([region]))
-    }
-
-    fn add_polygon<P, Ps>(&self, points: Ps, services: &mut Services) -> Self
-    where
-        P: Into<Point<2>>,
-        Ps: IntoIterator<Item = P>,
-        Ps::IntoIter: Clone + ExactSizeIterator,
-    {
-        let region = Region::polygon(points, services).insert(services);
-        self.add_region(region)
     }
 }
