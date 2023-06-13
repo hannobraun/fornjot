1c3f87e283f74726ecc9aef4050442daee716fa1
Hanno Braun <hanno@braun-odw.eu>
2023-06-13 12:33:23 +0200

    Add `UpdateFace::update_region`

--- crates/fj-core/src/operations/update/face.rs
+++ crates/fj-core/src/operations/update/face.rs
@@ -8,6 +8,9 @@ use crate::{
 
 /// Update a [`Face`]
 pub trait UpdateFace {
+    /// Replace the region of the face
+    fn update_region(&self, f: impl FnOnce(&Region) -> Handle<Region>) -> Self;
+
     /// Update the exterior of the face
     fn update_exterior(
         &self,
@@ -22,6 +25,11 @@ pub trait UpdateFace {
 }
 
 impl UpdateFace for Face {
+    fn update_region(&self, f: impl FnOnce(&Region) -> Handle<Region>) -> Self {
+        let region = f(self.region());
+        Face::new(self.surface().clone(), region.clone_object())
+    }
+
     fn update_exterior(
         &self,
         f: impl FnOnce(&Handle<Cycle>) -> Handle<Cycle>,
@@ -50,6 +58,34 @@ impl UpdateFace for Face {
 }
 
 impl<const D: usize> UpdateFace for Polygon<D> {
+    fn update_region(&self, f: impl FnOnce(&Region) -> Handle<Region>) -> Self {
+        let face = self.face.update_region(f);
+        let edges = array::from_fn(|i| {
+            face.region()
+                .exterior()
+                .nth_half_edge(i)
+                .expect("Operation should not have changed length of cycle")
+                .clone()
+        });
+        let vertices = array::from_fn(|i| {
+            // The duplicated code here is unfortunate, but unless we get a
+            // stable `array::each_ref` and something like `array::unzip`, I'm
+            // not sure how to avoid it.
+            face.region()
+                .exterior()
+                .nth_half_edge(i)
+                .expect("Operation should not have changed length of cycle")
+                .start_vertex()
+                .clone()
+        });
+
+        Polygon {
+            face,
+            edges,
+            vertices,
+        }
+    }
+
     fn update_exterior(
         &self,
         f: impl FnOnce(&Handle<Cycle>) -> Handle<Cycle>,
