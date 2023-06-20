c848dbec9c52a5511d180406ff986368fef14b5d
Hanno Braun <hanno@braun-odw.eu>
2023-06-20 10:25:03 +0200

    Update documentation of `algorithms` module

--- crates/fj-core/src/algorithms/mod.rs
+++ crates/fj-core/src/algorithms/mod.rs
@@ -1,7 +1,14 @@
-//! Collection of algorithms that are used by the kernel
+//! Collection of algorithms that operate on geometry
 //!
 //! Algorithmic code is collected in this module, to keep other modules focused
 //! on their respective purpose.
+//!
+//! # Implementation Note
+//!
+//! This module exists in a bit of an in-between state, as some of the things
+//! that are still here are probably better places in the [`operations`] module.
+//!
+//! [`operations`]: crate::operations
 
 pub mod approx;
 pub mod bounding_volume;
