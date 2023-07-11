fce6f724bb23005821f0ad0144805a5c25aea415
Hanno Braun <hanno@braun-odw.eu>
2023-07-11 09:32:32 +0200

    Mark `Reverse::reverse` as `#[must_use]`

--- crates/fj-core/src/operations/reverse/mod.rs
+++ crates/fj-core/src/operations/reverse/mod.rs
@@ -8,5 +8,6 @@ mod face;
 /// Reverse the direction/orientation of an object
 pub trait Reverse {
     /// Reverse the direction/orientation of the object
+    #[must_use]
     fn reverse(&self, services: &mut Services) -> Self;
 }
