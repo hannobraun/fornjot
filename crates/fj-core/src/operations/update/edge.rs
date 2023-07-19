85e6d1e451d9a119c8d561cac045bda76c1fea39
Hanno Braun <hanno@braun-odw.eu>
2023-07-19 08:52:41 +0200

    Add `UpdateHalfEdge::replace_curve`

--- crates/fj-core/src/operations/update/edge.rs
+++ crates/fj-core/src/operations/update/edge.rs
@@ -1,10 +1,14 @@
 use crate::{
-    objects::{GlobalEdge, HalfEdge, Vertex},
+    objects::{Curve, GlobalEdge, HalfEdge, Vertex},
     storage::Handle,
 };
 
 /// Update a [`HalfEdge`]
 pub trait UpdateHalfEdge {
+    /// Replace the curve of the half-edge
+    #[must_use]
+    fn replace_curve(&self, curve: Handle<Curve>) -> Self;
+
     /// Replace the start vertex of the half-edge
     #[must_use]
     fn replace_start_vertex(&self, start_vertex: Handle<Vertex>) -> Self;
@@ -15,6 +19,16 @@ pub trait UpdateHalfEdge {
 }
 
 impl UpdateHalfEdge for HalfEdge {
+    fn replace_curve(&self, curve: Handle<Curve>) -> Self {
+        HalfEdge::new(
+            self.path(),
+            self.boundary(),
+            curve,
+            self.start_vertex().clone(),
+            self.global_form().clone(),
+        )
+    }
+
     fn replace_start_vertex(&self, start_vertex: Handle<Vertex>) -> Self {
         HalfEdge::new(
             self.path(),
