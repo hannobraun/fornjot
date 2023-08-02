e4d8df2ef831a08443b5dc747140d646949d064b
Hanno Braun <hanno@braun-odw.eu>
2023-08-02 11:38:31 +0200

    Implement `Reverse` for `Polygon`

--- crates/fj-core/src/operations/reverse/face.rs
+++ crates/fj-core/src/operations/reverse/face.rs
@@ -1,4 +1,10 @@
-use crate::{objects::Face, operations::Insert, services::Services};
+use std::borrow::Borrow;
+
+use crate::{
+    objects::Face,
+    operations::{Insert, IsInsertedNo, IsInsertedYes, Polygon},
+    services::Services,
+};
 
 use super::Reverse;
 
@@ -6,5 +12,21 @@ impl Reverse for Face {
     fn reverse(&self, services: &mut Services) -> Self {
         let region = self.region().reverse(services).insert(services);
         Face::new(self.surface().clone(), region)
+    }
+}
+
+impl<const D: usize> Reverse for Polygon<D, IsInsertedNo> {
+    fn reverse(&self, services: &mut Services) -> Self {
+        let face = self.face.borrow().reverse(services);
+        self.replace_face(face)
+    }
+}
+
+impl<const D: usize> Reverse for Polygon<D, IsInsertedYes> {
+    fn reverse(&self, services: &mut Services) -> Self {
+        let face: &Face = self.face.borrow();
+        let face = face.reverse(services).insert(services);
+
+        self.replace_face(face)
     }
 }
