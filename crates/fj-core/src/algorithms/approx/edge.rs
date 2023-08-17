2a59bab67be08506bab74d42af9d006709ae0e68
Hanno Braun <hanno@braun-odw.eu>
2023-08-17 10:39:59 +0200

    Update documentation of `CurveApproxSegment`

--- crates/fj-core/src/algorithms/approx/edge.rs
+++ crates/fj-core/src/algorithms/approx/edge.rs
@@ -279,15 +279,20 @@ impl EdgeCache {
     }
 }
 
-/// An approximation of a [`GlobalEdge`]
+/// A segment of a curve approximation
+///
+/// A curve is potentially infinite (at least its local coordinate space is
+/// infinite, even if the curve itself isn't; a circle is an example of that).
+/// This means a curve can only be approximated locally, at a number of
+/// segments. This struct represents on such segment.
 #[derive(Clone, Debug, Eq, PartialEq, Hash, Ord, PartialOrd)]
 struct CurveApproxSegment {
-    /// The points that approximate the edge
+    /// The points that approximate the curve segment
     points: Vec<ApproxPoint<1>>,
 }
 
 impl CurveApproxSegment {
-    /// Reverse the order of the approximation
+    /// Reverse the orientation of the approximation
     fn reverse(mut self) -> Self {
         self.points.reverse();
         self
