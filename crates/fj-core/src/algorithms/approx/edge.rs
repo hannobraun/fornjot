79bb93e4a7b664ca201461200fafd664ce3ca8d4
Hanno Braun <hanno@braun-odw.eu>
2023-08-18 12:18:03 +0200

    Update comment

--- crates/fj-core/src/algorithms/approx/edge.rs
+++ crates/fj-core/src/algorithms/approx/edge.rs
@@ -43,33 +43,32 @@ impl Approx for (&Edge, &Surface) {
         let first = ApproxPoint::new(position_surface, position_global);
 
         let points = {
-            // We cache approximated `HalfEdge`s using the `Curve`s they
-            // reference and their boundary on that curve as the key. That bakes
-            // in the undesirable assumption that all coincident `HalfEdge`s are
-            // also congruent. Let me explain.
+            // We cache approximated `Edge`s using the `Curve`s they reference
+            // and their boundary on that curve as the key. That bakes in the
+            // undesirable assumption that all coincident `Edge`s are also
+            // congruent. Let me explain.
             //
-            // When two `HalfEdge`s are coincident, we need to make sure their
+            // When two `Edge`s are coincident, we need to make sure their
             // approximations are identical where they overlap. Otherwise, we'll
             // get an invalid triangle mesh in the end. Hence, we cache
             // approximations.
             //
             // Caching works like this: We check whether there already is a
             // cache entry for the curve/boundary. If there isn't, we create the
-            // 3D approximation from the 2D `HalfEdge`. Next time we check for a
-            // coincident `HalfEdge`, we'll find the cache and use that, getting
+            // 3D approximation from the 2D `Edge`. Next time we check for a
+            // coincident `Edge`, we'll find the cache and use that, getting
             // the exact same 3D approximation, instead of generating a slightly
-            // different one from the different 2D `HalfEdge`.
+            // different one from the different 2D `Edge`.
             //
-            // So what if we had two coincident `HalfEdge`s that aren't
-            // congruent? Meaning, they overlap partially, but not fully. Then
-            // obviously, they wouldn't refer to the same combination of curve
-            // and boundary. And since those are the key in our cache, those
-            // `HalfEdge`s would not share an approximation where they overlap,
-            // leading to exactly the problems that the cache is supposed to
-            // prevent.
+            // So what if we had two coincident `fEdge`s that aren't congruent?
+            // Meaning, they overlap partially, but not fully. Then obviously,
+            // they wouldn't refer to the same combination of curve and
+            // boundary. And since those are the key in our cache, those `Edge`s
+            // would not share an approximation where they overlap, leading to
+            // exactly the problems that the cache is supposed to prevent.
             //
             // As of this writing, it is a documented (but not validated)
-            // limitation, that coincident `HalfEdge`s must always be congruent.
+            // limitation, that coincident `Edge`s must always be congruent.
             // However, we're going to need to lift this limitation going
             // forward, as it is, well, too limiting. This means things here
             // will need to change.
