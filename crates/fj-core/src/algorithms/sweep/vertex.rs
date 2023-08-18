a83d3ca579188d69417b2cd2b95893331729a18f
Hanno Braun <hanno@braun-odw.eu>
2023-08-18 11:29:56 +0200

    Remove unused return value

--- crates/fj-core/src/algorithms/sweep/vertex.rs
+++ crates/fj-core/src/algorithms/sweep/vertex.rs
@@ -1,7 +1,7 @@
 use fj_math::Vector;
 
 use crate::{
-    objects::{Curve, GlobalEdge, Vertex},
+    objects::{Curve, Vertex},
     operations::Insert,
     services::Services,
     storage::Handle,
@@ -10,7 +10,7 @@ use crate::{
 use super::{Sweep, SweepCache};
 
 impl Sweep for Handle<Vertex> {
-    type Swept = (Handle<Curve>, Handle<GlobalEdge>, [Self; 2]);
+    type Swept = (Handle<Curve>, [Self; 2]);
 
     fn sweep_with_cache(
         self,
@@ -32,12 +32,6 @@ impl Sweep for Handle<Vertex> {
             .clone();
         let vertices = [a, b];
 
-        let global_edge = cache
-            .global_edges
-            .entry(self.id())
-            .or_insert_with(|| GlobalEdge::new().insert(services))
-            .clone();
-
-        (curve, global_edge, vertices)
+        (curve, vertices)
     }
 }
