69979a967b760e2a4bb86277660169f6505525d1
Hanno Braun <hanno@braun-odw.eu>
2023-09-06 10:57:49 +0200

    Use `CurveApprox` in `CurveApproxCache`
    This only makes things more complicated for now, but it paves the way
    for making curve approximation caching more flexible.

--- crates/fj-core/src/algorithms/approx/curve/cache.rs
+++ crates/fj-core/src/algorithms/approx/curve/cache.rs
@@ -8,15 +8,13 @@ use crate::{
     storage::{Handle, HandleWrapper},
 };
 
-use super::CurveApproxSegment;
+use super::{CurveApprox, CurveApproxSegment};
 
 /// Cache for curve approximations
 #[derive(Default)]
 pub struct CurveApproxCache {
-    inner: BTreeMap<
-        (HandleWrapper<Curve>, CurveBoundary<Point<1>>),
-        CurveApproxSegment,
-    >,
+    inner:
+        BTreeMap<(HandleWrapper<Curve>, CurveBoundary<Point<1>>), CurveApprox>,
 }
 
 impl CurveApproxCache {
@@ -32,12 +30,26 @@ impl CurveApproxCache {
         self.inner
             .get(&(curve.clone().into(), normalized_boundary))
             .cloned()
-            .map(|approx| {
+            .map(|mut approx| {
                 if was_already_normalized {
                     approx
                 } else {
-                    approx.reverse()
+                    for segment in &mut approx.segments {
+                        *segment = segment.clone().reverse();
+                    }
+                    approx
                 }
+            })
+            .map(|approx| {
+                let mut segments = approx.segments.into_iter();
+                let segment = segments
+                    .next()
+                    .expect("Empty approximations should not exist in cache");
+                assert!(
+                    segments.next().is_none(),
+                    "Cached approximations should have exactly 1 segment"
+                );
+                segment
             })
     }
 
@@ -49,8 +61,29 @@ impl CurveApproxCache {
     ) -> CurveApproxSegment {
         let new_segment = new_segment.normalize();
 
+        // We assume that curve approximation segments never overlap, so so we
+        // don't have to do any merging of this segment with a possibly existing
+        // approximation for this curve.
+        //
+        // For now, this is a valid assumption, as it matches the uses of this
+        // method, due to documented limitations elsewhere in the system.
+        let approx = CurveApprox {
+            segments: vec![new_segment.clone()],
+        };
+
         self.inner
-            .insert((curve.into(), new_segment.boundary), new_segment.clone())
+            .insert((curve.into(), new_segment.boundary), approx)
+            .map(|approx| {
+                let mut segments = approx.segments.into_iter();
+                let segment = segments
+                    .next()
+                    .expect("Empty approximations should not exist in cache");
+                assert!(
+                    segments.next().is_none(),
+                    "Cached approximations should have exactly 1 segment"
+                );
+                segment
+            })
             .unwrap_or(new_segment)
     }
 }
