89227311b4696442b56028e3c9b8f5804704ccf7
Hanno Braun <hanno@braun-odw.eu>
2023-09-22 12:23:05 +0200

    Update comment

--- crates/fj-core/src/objects/kinds/region.rs
+++ crates/fj-core/src/objects/kinds/region.rs
@@ -49,11 +49,8 @@ impl Region {
 
     /// Access all cycles of the region (both exterior and interior)
     pub fn all_cycles(&self) -> impl Iterator<Item = &Handle<Cycle>> {
-        // It would be nice to return `HandleIter` here, but there's no
-        // straight-forward way to chain it to another iterator, given it's
-        // current implementation.
-        //
-        // Maybe this can be addressed, once the need arises.
+        // It would be nice to return `&Handles` here, but I don't see a way for
+        // doing that here *and* in `interiors`.
         [self.exterior()].into_iter().chain(self.interiors())
     }
 
