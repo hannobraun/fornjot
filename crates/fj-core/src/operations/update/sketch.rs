ca98d714910edba2a289ef0e711ba5dfa948f873
Hanno Braun <hanno@braun-odw.eu>
2023-09-27 11:37:48 +0200

    Add `UpdateSketch::replace_region`

--- crates/fj-core/src/operations/update/sketch.rs
+++ crates/fj-core/src/operations/update/sketch.rs
@@ -22,6 +22,23 @@ pub trait UpdateSketch {
         handle: &Handle<Region>,
         update: impl FnOnce(&Handle<Region>) -> Handle<Region>,
     ) -> Self;
+
+    /// Replace a region of the sketch
+    ///
+    /// This is a more general version of [`UpdateSketch::update_region`] which
+    /// can replace a single edge with multiple others.
+    ///
+    /// # Panics
+    ///
+    /// Uses [`Handles::replace`] internally, and panics for the same reasons.
+    ///
+    /// [`Handles::replace`]: crate::objects::Handles::replace
+    #[must_use]
+    fn replace_region<const N: usize>(
+        &self,
+        handle: &Handle<Region>,
+        replace: impl FnOnce(&Handle<Region>) -> [Handle<Region>; N],
+    ) -> Self;
 }
 
 impl UpdateSketch for Sketch {
@@ -35,6 +52,15 @@ impl UpdateSketch for Sketch {
         update: impl FnOnce(&Handle<Region>) -> Handle<Region>,
     ) -> Self {
         let regions = self.regions().update(handle, update);
+        Sketch::new(regions)
+    }
+
+    fn replace_region<const N: usize>(
+        &self,
+        handle: &Handle<Region>,
+        replace: impl FnOnce(&Handle<Region>) -> [Handle<Region>; N],
+    ) -> Self {
+        let regions = self.regions().replace(handle, replace);
         Sketch::new(regions)
     }
 }
