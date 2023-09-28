45a663c588f6a5a1aad2196750c8152235ccf049
Hanno Braun <hanno@braun-odw.eu>
2023-09-28 11:05:46 +0200

    Implement half-edge splitting operation
    This works, but is hard to use correctly right now (see documentation
    about validity). I'm working on ways to make it easier to use, but I
    wanted to get this merged in the meantime. It's already been sitting
    around in a local branch for quite a while.

---
+++ crates/fj-core/src/operations/split/edge.rs
@@ -0,0 +1,54 @@
+use fj_math::Point;
+
+use crate::{
+    objects::{HalfEdge, Vertex},
+    operations::Insert,
+    services::Services,
+};
+
+/// Split a [`HalfEdge`] into two
+pub trait SplitHalfEdge {
+    /// Split the half-edge into two
+    ///
+    /// # Validity
+    ///
+    /// Within a valid shell, a [`HalfEdge`] must have an equal but opposite
+    /// sibling. This operation only splits a single half-edge, which in itself
+    /// will make a valid shell invalid.
+    ///
+    /// The caller is responsible for also split this half-edge's sibling, if
+    /// appropriate, to preserve validity.
+    #[must_use]
+    fn split(
+        &self,
+        point: impl Into<Point<1>>,
+        services: &mut Services,
+    ) -> [HalfEdge; 2];
+}
+
+impl SplitHalfEdge for HalfEdge {
+    fn split(
+        &self,
+        point: impl Into<Point<1>>,
+        services: &mut Services,
+    ) -> [HalfEdge; 2] {
+        let point = point.into();
+
+        let [start, end] = self.boundary().inner;
+
+        let a = HalfEdge::new(
+            self.path(),
+            [start, point],
+            self.curve().clone(),
+            self.start_vertex().clone(),
+        );
+        let b = HalfEdge::new(
+            self.path(),
+            [point, end],
+            self.curve().clone(),
+            Vertex::new().insert(services),
+        );
+
+        [a, b]
+    }
+}
