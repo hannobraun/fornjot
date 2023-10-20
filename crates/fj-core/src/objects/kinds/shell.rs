f12ae5747435293e9cc3f5126d6430586b662758
Hanno Braun <hanno@braun-odw.eu>
2023-10-20 11:01:52 +0200

    Add `Shell::are_siblings`

--- crates/fj-core/src/objects/kinds/shell.rs
+++ crates/fj-core/src/objects/kinds/shell.rs
@@ -1,5 +1,6 @@
 use crate::{
-    objects::{handles::Handles, Face},
+    objects::{handles::Handles, Face, HalfEdge},
+    queries::BoundingVerticesOfHalfEdge,
     storage::Handle,
 };
 
@@ -20,5 +21,29 @@ impl Shell {
     /// Access the faces of the shell
     pub fn faces(&self) -> &Handles<Face> {
         &self.faces
+    }
+
+    /// Indicate whether the provided half-edges are siblings
+    pub fn are_siblings(
+        &self,
+        a: &Handle<HalfEdge>,
+        b: &Handle<HalfEdge>,
+    ) -> bool {
+        let same_curve = a.curve().id() == b.curve().id();
+        let same_boundary = a.boundary() == b.boundary().reverse();
+        let same_vertices = {
+            let Some(a_vertices) = self.bounding_vertices_of_half_edge(a)
+            else {
+                return false;
+            };
+            let Some(b_vertices) = self.bounding_vertices_of_half_edge(b)
+            else {
+                return false;
+            };
+
+            a_vertices == b_vertices.reverse()
+        };
+
+        same_curve && same_boundary && same_vertices
     }
 }
