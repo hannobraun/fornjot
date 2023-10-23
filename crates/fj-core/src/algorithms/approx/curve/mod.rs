8b1667e1b8eaed123c4fea9eacb6927ae9dc4f14
Hanno Braun <hanno@braun-odw.eu>
2023-10-23 12:27:58 +0200

    Remove unused code

--- crates/fj-core/src/algorithms/approx/curve/mod.rs
+++ crates/fj-core/src/algorithms/approx/curve/mod.rs
@@ -1,11 +1,9 @@
 //! Curve approximation
 
 mod approx;
-mod cache;
 mod points;
 mod segment;
 
 pub use self::{
-    approx::CurveApprox, cache::CurveApproxCache, points::CurveApproxPoints,
-    segment::CurveApproxSegment,
+    approx::CurveApprox, points::CurveApproxPoints, segment::CurveApproxSegment,
 };
