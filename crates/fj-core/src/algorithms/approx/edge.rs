527f9b32cd2264054906e9a0cdc23c387384b3c0
Hanno Braun <hanno@braun-odw.eu>
2023-10-23 13:37:15 +0200

    Move `approx_curve` to `approx::curve`

--- crates/fj-core/src/algorithms/approx/edge.rs
+++ crates/fj-core/src/algorithms/approx/edge.rs
@@ -5,15 +5,10 @@
 //! approximations are usually used to build cycle approximations, and this way,
 //! the caller doesn't have to deal with duplicate vertices.
 
-use fj_math::Point;
-
-use crate::{
-    geometry::{CurveBoundary, GlobalPath, SurfacePath},
-    objects::{HalfEdge, Surface},
-};
+use crate::objects::{HalfEdge, Surface};
 
 use super::{
-    curve::{CurveApprox, CurveApproxCache},
+    curve::{approx_curve, CurveApproxCache},
     vertex::VertexApproxCache,
     Approx, ApproxPoint, Tolerance,
 };
@@ -88,82 +83,6 @@ impl Approx for (&HalfEdge, &Surface) {
 pub struct HalfEdgeApprox {
     /// The points that approximate the half-edge
     pub points: Vec<ApproxPoint<2>>,
-}
-
-fn approx_curve(
-    path: &SurfacePath,
-    surface: &Surface,
-    boundary: CurveBoundary<Point<1>>,
-    tolerance: impl Into<Tolerance>,
-) -> CurveApprox {
-    // There are different cases of varying complexity. Circles are the hard
-    // part here, as they need to be approximated, while lines don't need to be.
-    //
-    // This will probably all be unified eventually, as `SurfacePath` and
-    // `GlobalPath` grow APIs that are better suited to implementing this code
-    // in a more abstract way.
-    let points = match (path, surface.geometry().u) {
-        (SurfacePath::Circle(_), GlobalPath::Circle(_)) => {
-            todo!(
-                "Approximating a circle on a curved surface not supported yet."
-            )
-        }
-        (SurfacePath::Circle(_), GlobalPath::Line(_)) => {
-            (path, boundary)
-                .approx_with_cache(tolerance, &mut ())
-                .into_iter()
-                .map(|(point_curve, point_surface)| {
-                    // We're throwing away `point_surface` here, which is a bit
-                    // weird, as we're recomputing it later (outside of this
-                    // function).
-                    //
-                    // It should be fine though:
-                    //
-                    // 1. We're throwing this version away, so there's no danger
-                    //    of inconsistency between this and the later version.
-                    // 2. This version should have been computed using the same
-                    //    path and parameters and the later version will be, so
-                    //    they should be the same anyway.
-                    // 3. Not all other cases handled in this function have a
-                    //    surface point available, so it needs to be computed
-                    //    later anyway, in the general case.
-
-                    let point_global = surface
-                        .geometry()
-                        .point_from_surface_coords(point_surface);
-                    (point_curve, point_global)
-                })
-                .collect()
-        }
-        (SurfacePath::Line(line), _) => {
-            let range_u =
-                CurveBoundary::from(boundary.inner.map(|point_curve| {
-                    [path.point_from_path_coords(point_curve).u]
-                }));
-
-            let approx_u = (surface.geometry().u, range_u)
-                .approx_with_cache(tolerance, &mut ());
-
-            let mut points = Vec::new();
-            for (u, _) in approx_u {
-                let t = (u.t - line.origin().u) / line.direction().u;
-                let point_surface = path.point_from_path_coords([t]);
-                let point_global =
-                    surface.geometry().point_from_surface_coords(point_surface);
-                points.push((u, point_global));
-            }
-
-            points
-        }
-    };
-
-    let points = points
-        .into_iter()
-        .map(|(point_curve, point_global)| {
-            ApproxPoint::new(point_curve, point_global)
-        })
-        .collect();
-    CurveApprox { points }
 }
 
 /// Cache for edge approximations
