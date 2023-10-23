cf1bf07be499d6acaef7330e10d8fbfd8b68f8dd
Hanno Braun <hanno@braun-odw.eu>
2023-10-23 12:42:22 +0200

    Bypass `CurveApproxSegment` in curve approximation

--- crates/fj-core/src/algorithms/approx/edge.rs
+++ crates/fj-core/src/algorithms/approx/edge.rs
@@ -15,10 +15,7 @@ use crate::{
     storage::{Handle, HandleWrapper},
 };
 
-use super::{
-    curve::{CurveApproxPoints, CurveApproxSegment},
-    Approx, ApproxPoint, Tolerance,
-};
+use super::{curve::CurveApproxPoints, Approx, ApproxPoint, Tolerance};
 
 impl Approx for (&HalfEdge, &Surface) {
     type Approximation = HalfEdgeApprox;
@@ -66,10 +63,10 @@ impl Approx for (&HalfEdge, &Surface) {
                     cache.insert_curve_approx(
                         edge.curve().clone(),
                         edge.boundary(),
-                        segment.points.clone(),
+                        segment.clone(),
                     );
 
-                    segment.points
+                    segment
                 }
             };
 
@@ -100,7 +97,7 @@ fn approx_curve(
     surface: &Surface,
     boundary: CurveBoundary<Point<1>>,
     tolerance: impl Into<Tolerance>,
-) -> CurveApproxSegment {
+) -> CurveApproxPoints {
     // There are different cases of varying complexity. Circles are the hard
     // part here, as they need to be approximated, while lines don't need to be.
     //
@@ -168,10 +165,7 @@ fn approx_curve(
             ApproxPoint::new(point_curve, point_global)
         })
         .collect();
-    CurveApproxSegment {
-        boundary,
-        points: CurveApproxPoints { inner: points },
-    }
+    CurveApproxPoints { inner: points }
 }
 
 /// Cache for edge approximations
