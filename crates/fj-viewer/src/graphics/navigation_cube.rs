9f1a29da60bcd4f898b66eda5e1300a966a37363
Hanno Braun <hanno@braun-odw.eu>
2023-10-30 19:17:33 +0100

    Upgrade to wgpu 0.18.0

--- crates/fj-viewer/src/graphics/navigation_cube.rs
+++ crates/fj-viewer/src/graphics/navigation_cube.rs
@@ -178,7 +178,7 @@ impl NavigationCubeRenderer {
                     resolve_target: None,
                     ops: wgpu::Operations {
                         load: wgpu::LoadOp::Load,
-                        store: true,
+                        store: wgpu::StoreOp::Store,
                     },
                 })],
                 ..Default::default()
--- crates/fj-viewer/src/graphics/renderer.rs
+++ crates/fj-viewer/src/graphics/renderer.rs
@@ -280,7 +280,7 @@ impl Renderer {
                             ops: wgpu::Operations {
                                 load: wgpu::LoadOp::Clear(wgpu::Color::WHITE),
                                 // Not necessary, due to MSAA being enabled.
-                                store: false,
+                                store: wgpu::StoreOp::Discard,
                             },
                         },
                     )],
@@ -289,7 +289,7 @@ impl Renderer {
                             view: &self.depth_view,
                             depth_ops: Some(wgpu::Operations {
                                 load: wgpu::LoadOp::Clear(1.0),
-                                store: true,
+                                store: wgpu::StoreOp::Store,
                             }),
                             stencil_ops: None,
                         },
