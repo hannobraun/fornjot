5093bf7f831a0003028258aee52d7d5908438023
Hanno Braun <hanno@braun-odw.eu>
2023-11-13 12:33:04 +0100

    Add `SplitEdge`

---
+++ crates/fj-core/src/operations/split/edge.rs
@@ -0,0 +1,61 @@
+use fj_math::Point;
+
+use crate::{
+    objects::{HalfEdge, Shell},
+    operations::{
+        insert::Insert, replace::ReplaceHalfEdge, split::SplitHalfEdge,
+        update::UpdateHalfEdge,
+    },
+    queries::SiblingOfHalfEdge,
+    services::Services,
+    storage::Handle,
+};
+
+/// Split a pair of [`HalfEdge`]s into two
+pub trait SplitEdge {
+    /// Split the provided [`HalfEdge`], as well as its sibling, into two
+    ///
+    /// # Panics
+    ///
+    /// Panics, if the provided half-edge is not a part of this shell.
+    #[must_use]
+    fn split_edge(
+        &self,
+        half_edge: &Handle<HalfEdge>,
+        point: impl Into<Point<1>>,
+        services: &mut Services,
+    ) -> Self;
+}
+
+impl SplitEdge for Shell {
+    fn split_edge(
+        &self,
+        half_edge: &Handle<HalfEdge>,
+        point: impl Into<Point<1>>,
+        services: &mut Services,
+    ) -> Self {
+        let point = point.into();
+
+        let sibling = self
+            .get_sibling_of(half_edge)
+            .expect("Expected half-edge and its sibling to be part of shell");
+
+        let [half_edge_a, half_edge_b] = half_edge
+            .split_half_edge(point, services)
+            .map(|half_edge| half_edge.insert(services));
+
+        let [sibling_a, sibling_b] = sibling.split_half_edge(point, services);
+        let sibling_b = sibling_b
+            .update_start_vertex(|_| half_edge_b.start_vertex().clone());
+
+        self.replace_half_edge(half_edge, [half_edge_a, half_edge_b], services)
+            .into_inner()
+            .replace_half_edge(
+                &sibling,
+                [sibling_a, sibling_b]
+                    .map(|half_edge| half_edge.insert(services)),
+                services,
+            )
+            .into_inner()
+    }
+}
--- crates/fj-core/src/operations/split/mod.rs
+++ crates/fj-core/src/operations/split/mod.rs
@@ -3,6 +3,7 @@
 //! See [`SplitHalfEdge`], which is currently the only trait in this module, for
 //! more information.
 
+mod edge;
 mod half_edge;
 
-pub use self::half_edge::SplitHalfEdge;
+pub use self::{edge::SplitEdge, half_edge::SplitHalfEdge};
