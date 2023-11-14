1794371f0f3809abf4b11300cd93037155521a2b
Hanno Braun <hanno@braun-odw.eu>
2023-11-14 10:57:44 +0100

    Update documentation of `operations::replace`

--- crates/fj-core/src/operations/split/mod.rs
+++ crates/fj-core/src/operations/split/mod.rs
@@ -1,7 +1,9 @@
 //! # Operations to split objects
 //!
-//! See [`SplitHalfEdge`], which is currently the only trait in this module, for
-//! more information.
+//! Splitting means removing an object, replacing it with to new ones that fill
+//! the same space. This often makes sense, when you want to modify only part of
+//! an object. In such a case, you can split off the part you want to modify,
+//! leaving the rest unchanged.
 
 mod edge;
 mod half_edge;
