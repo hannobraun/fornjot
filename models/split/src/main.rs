533132aff331b5a6a24bc6ff9319971991f7b391
Hanno Braun <hanno@braun-odw.eu>
2023-11-15 14:33:16 +0100

    Use more interesting split coordinates in model
    `0.5`, what was used before, is a bit of an edge case that could work,
    even if some of the code is buggy. Using a less special value is a
    better test case.

--- models/split/src/main.rs
+++ models/split/src/main.rs
@@ -2,7 +2,7 @@ use fj::{core::services::Services, handle_model};
 
 fn main() -> fj::Result {
     let mut services = Services::new();
-    let model = split::model(1.0, 0.5, &mut services);
+    let model = split::model(1.0, 0.2, &mut services);
     handle_model(model, services)?;
     Ok(())
 }
