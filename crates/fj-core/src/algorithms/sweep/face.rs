d1f58f3f1d6a81c40818ed25a7889f1122335959
Hanno Braun <hanno@braun-odw.eu>
2023-11-16 11:08:35 +0100

    Document use of terms in face sweeping code

--- crates/fj-core/src/algorithms/sweep/face.rs
+++ crates/fj-core/src/algorithms/sweep/face.rs
@@ -24,6 +24,19 @@ impl Sweep for Handle<Face> {
         cache: &mut SweepCache,
         services: &mut Services,
     ) -> Self::Swept {
+        // Please note that this function uses the words "bottom" and "top" in a
+        // specific sense:
+        //
+        // - "Bottom" refers to the origin of the sweep. The bottom face is the
+        //   original face, or a face in the same place.
+        // - "Top" refers to the location of the face that was created by
+        //   translating the bottom face along the path.
+        // - "Side" refers to new faces created in between bottom and top.
+        //
+        // These words are specifically *not* meant in the sense of z-axis
+        // locations, and depending on the direction of `path`, the two meanings
+        // might actually be opposite.
+
         let path = path.into();
 
         let mut faces = Vec::new();
