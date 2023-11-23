d923ec8694409df87e8581eeaa3be12ce3246c99
Hanno Braun <hanno@braun-odw.eu>
2023-11-23 16:19:37 +0100

    Remove unused code

--- crates/fj-core/src/operations/sweep/mod.rs
+++ crates/fj-core/src/operations/sweep/mod.rs
@@ -13,41 +13,12 @@ pub use self::{
 
 use std::collections::BTreeMap;
 
-use fj_math::Vector;
-
 use crate::{
     objects::{Curve, Vertex},
-    services::Services,
     storage::{Handle, ObjectId},
 };
 
-/// Sweep an object along a path to create another object
-pub trait Sweep: Sized {
-    /// The object that is created by sweeping the implementing object
-    type Swept;
-
-    /// Sweep the object along the given path
-    fn sweep(
-        self,
-        path: impl Into<Vector<3>>,
-        services: &mut Services,
-    ) -> Self::Swept {
-        let mut cache = SweepCache::default();
-        self.sweep_with_cache(path, &mut cache, services)
-    }
-
-    /// Sweep the object along the given path, using the provided cache
-    fn sweep_with_cache(
-        self,
-        path: impl Into<Vector<3>>,
-        cache: &mut SweepCache,
-        services: &mut Services,
-    ) -> Self::Swept;
-}
-
 /// A cache used for sweeping
-///
-/// See [`Sweep`].
 #[derive(Default)]
 pub struct SweepCache {
     /// Cache for curves
