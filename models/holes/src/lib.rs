061a58d9109f9a13b2a0a22d3e0cf3c9624e9eae
Hanno Braun <hanno@braun-odw.eu>
2023-12-06 13:01:34 +0100

    Add `holes` model

---
+++ models/holes/src/lib.rs
@@ -0,0 +1,24 @@
+use fj::core::{
+    objects::Solid,
+    operations::{holes::AddHole, insert::Insert, update::UpdateSolid},
+    services::Services,
+    storage::Handle,
+};
+
+pub fn model(services: &mut Services) -> Handle<Solid> {
+    let cuboid = cuboid::model(1., 1., 1., services);
+
+    let shell = cuboid.shells().first();
+    let bottom_face = shell.faces().first();
+
+    let hole_position = [0., 0.];
+    let hole_path = [0., 0., 0.5];
+
+    cuboid
+        .update_shell(shell, |shell| {
+            shell
+                .add_blind_hole(bottom_face, hole_position, hole_path, services)
+                .insert(services)
+        })
+        .insert(services)
+}
