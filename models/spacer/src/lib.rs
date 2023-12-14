faf5fbb882bcfe550b73896ce26a1a6539d7f853
Hanno Braun <hanno@braun-odw.eu>
2023-12-14 13:00:36 +0100

    Inline redundant variable

--- models/spacer/src/lib.rs
+++ models/spacer/src/lib.rs
@@ -23,15 +23,18 @@ pub fn model(
     let surface = services.objects.surfaces.xy_plane();
     let path = Vector::from([0., 0., height]);
 
-    let sketch = Sketch::empty().add_region(
-        Region::circle(Point::origin(), outer, services)
-            .add_interiors([Cycle::circle(Point::origin(), inner, services)
+    Sketch::empty()
+        .add_region(
+            Region::circle(Point::origin(), outer, services)
+                .add_interiors([Cycle::circle(
+                    Point::origin(),
+                    inner,
+                    services,
+                )
                 .reverse(services)
                 .insert(services)])
-            .insert(services),
-    );
-
-    sketch
+                .insert(services),
+        )
         .sweep_sketch(surface, path, services)
         .insert(services)
 }
