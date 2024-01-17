854e56139ab88517e651b38699ada33e1b8aa152
Hanno Braun <hanno@braun-odw.eu>
2024-01-17 10:37:54 +0100

    Implement `SetColor` for `Region`

--- crates/fj-core/src/operations/presentation.rs
+++ crates/fj-core/src/operations/presentation.rs
@@ -2,8 +2,20 @@
 
 use fj_interop::Color;
 
+use crate::objects::Region;
+
 /// Set the color of an object
 pub trait SetColor {
     /// Set the color of the object
     fn set_color(&self, color: impl Into<Color>) -> Self;
+}
+
+impl SetColor for Region {
+    fn set_color(&self, color: impl Into<Color>) -> Self {
+        Region::new(
+            self.exterior().clone(),
+            self.interiors().into_iter().cloned(),
+            Some(color.into()),
+        )
+    }
 }
