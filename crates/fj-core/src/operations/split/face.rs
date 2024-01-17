2ad4085f32f075a3c3f9fbe41bc9ffcea5f0bf67
Hanno Braun <hanno@braun-odw.eu>
2024-01-17 10:50:01 +0100

    Preserve color when splitting face

--- crates/fj-core/src/operations/split/face.rs
+++ crates/fj-core/src/operations/split/face.rs
@@ -7,6 +7,7 @@ use crate::{
     operations::{
         build::{BuildFace, BuildHalfEdge},
         insert::Insert,
+        presentation::SetColor,
         split::SplitEdge,
         update::{
             UpdateCycle, UpdateFace, UpdateHalfEdge, UpdateRegion, UpdateShell,
@@ -131,14 +132,18 @@ impl SplitFace for Shell {
             services,
         )
         .update_region(|region| {
-            region
-                .update_exterior(|cycle| {
-                    cycle
-                        .add_half_edges(half_edges_b_to_c_inclusive)
-                        .add_half_edges([dividing_half_edge_c_to_b])
-                        .insert(services)
-                })
-                .insert(services)
+            let mut region = region.update_exterior(|cycle| {
+                cycle
+                    .add_half_edges(half_edges_b_to_c_inclusive)
+                    .add_half_edges([dividing_half_edge_c_to_b])
+                    .insert(services)
+            });
+
+            if let Some(color) = face.region().color() {
+                region = region.set_color(color);
+            }
+
+            region.insert(services)
         })
         .insert(services);
 
@@ -152,14 +157,18 @@ impl SplitFace for Shell {
             services,
         )
         .update_region(|region| {
-            region
-                .update_exterior(|cycle| {
-                    cycle
-                        .add_half_edges(half_edges_d_to_a_inclusive)
-                        .add_half_edges([dividing_half_edge_a_to_d])
-                        .insert(services)
-                })
-                .insert(services)
+            let mut region = region.update_exterior(|cycle| {
+                cycle
+                    .add_half_edges(half_edges_d_to_a_inclusive)
+                    .add_half_edges([dividing_half_edge_a_to_d])
+                    .insert(services)
+            });
+
+            if let Some(color) = face.region().color() {
+                region = region.set_color(color);
+            }
+
+            region.insert(services)
         })
         .insert(services);
 
