2bfb010dba6b619276ad9020a26645476863d25b
Hanno Braun <hanno@braun-odw.eu>
2024-01-25 13:20:58 +0100

    Implement `SetColor` for `Handle<Region>` instead
    This is required to extract appearance into a separate layer, as
    associating attributes to objects can only work, if we have a unique
    `Handle` to that object.

--- crates/fj-core/src/operations/presentation.rs
+++ crates/fj-core/src/operations/presentation.rs
@@ -2,16 +2,19 @@
 
 use fj_interop::Color;
 
-use crate::objects::Region;
+use crate::{
+    objects::{IsObject, Region},
+    storage::Handle,
+};
 
 /// Set the color of an object
-pub trait SetColor {
+pub trait SetColor: IsObject {
     /// Set the color of the object
-    fn set_color(&self, color: impl Into<Color>) -> Self;
+    fn set_color(&self, color: impl Into<Color>) -> Self::BareObject;
 }
 
-impl SetColor for Region {
-    fn set_color(&self, color: impl Into<Color>) -> Self {
+impl SetColor for Handle<Region> {
+    fn set_color(&self, color: impl Into<Color>) -> Self::BareObject {
         Region::new(
             self.exterior().clone(),
             self.interiors().into_iter().cloned(),
