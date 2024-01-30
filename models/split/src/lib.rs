7234a05d41069c65275c095006f39e399d1947ef
Hanno Braun <hanno@braun-odw.eu>
2024-01-30 10:54:49 +0100

    Return bare `Solid` from `split` model

--- models/split/src/lib.rs
+++ models/split/src/lib.rs
@@ -5,31 +5,24 @@ use fj::core::{
         update::UpdateSolid,
     },
     services::Services,
-    storage::Handle,
 };
 
-pub fn model(
-    size: f64,
-    split_pos: f64,
-    services: &mut Services,
-) -> Handle<Solid> {
+pub fn model(size: f64, split_pos: f64, services: &mut Services) -> Solid {
     let cuboid = cuboid::model([size, size, size], services);
 
-    cuboid
-        .update_shell(cuboid.shells().only(), |shell| {
-            let face = shell.faces().first();
-            let cycle = face.region().exterior();
+    cuboid.update_shell(cuboid.shells().only(), |shell| {
+        let face = shell.faces().first();
+        let cycle = face.region().exterior();
 
-            let line = [
-                (cycle.half_edges().nth(0).unwrap(), [split_pos]),
-                (cycle.half_edges().nth(2).unwrap(), [split_pos]),
-            ];
+        let line = [
+            (cycle.half_edges().nth(0).unwrap(), [split_pos]),
+            (cycle.half_edges().nth(2).unwrap(), [split_pos]),
+        ];
 
-            let (shell, [face, _]) = shell.split_face(face, line, services);
+        let (shell, [face, _]) = shell.split_face(face, line, services);
 
-            shell
-                .sweep_face_of_shell(face, [0., 0., -size / 2.], services)
-                .insert(services)
-        })
-        .insert(services)
+        shell
+            .sweep_face_of_shell(face, [0., 0., -size / 2.], services)
+            .insert(services)
+    })
 }
