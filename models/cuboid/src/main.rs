d493424c04954f254ec55d1d0f5b39fc4fafe53b
Hanno Braun <hanno@braun-odw.eu>
2024-01-30 10:53:45 +0100

    Return bare `Solid` from `cuboid` model

--- models/cuboid/src/main.rs
+++ models/cuboid/src/main.rs
@@ -1,10 +1,8 @@
-use std::ops::Deref;
-
 use fj::{core::services::Services, handle_model};
 
 fn main() -> fj::Result {
     let mut services = Services::new();
     let model = cuboid::model([3., 2., 1.], &mut services);
-    handle_model(model.deref(), services)?;
+    handle_model(&model, services)?;
     Ok(())
 }
