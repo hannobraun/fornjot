d2b47bcb6a6581ad27903fad37623bf722ebaba0
Hanno Braun <hanno@braun-odw.eu>
2024-01-30 10:54:07 +0100

    Return bare `Solid` from `holes` model

--- models/holes/src/lib.rs
+++ models/holes/src/lib.rs
@@ -7,58 +7,52 @@ use fj::{
             update::UpdateSolid,
         },
         services::Services,
-        storage::Handle,
     },
     math::Scalar,
 };
 
-pub fn model(
-    radius: impl Into<Scalar>,
-    services: &mut Services,
-) -> Handle<Solid> {
+pub fn model(radius: impl Into<Scalar>, services: &mut Services) -> Solid {
     let radius = radius.into();
 
     let size = radius * 4.;
     let cuboid = cuboid::model([size * 2., size, size], services);
 
-    cuboid
-        .update_shell(cuboid.shells().only(), |shell| {
-            let bottom_face = shell.faces().first();
-            let offset = size / 2.;
-            let depth = size / 2.;
+    cuboid.update_shell(cuboid.shells().only(), |shell| {
+        let bottom_face = shell.faces().first();
+        let offset = size / 2.;
+        let depth = size / 2.;
 
-            let shell = shell.add_blind_hole(
-                HoleLocation {
-                    face: bottom_face,
-                    position: [-offset, Scalar::ZERO].into(),
-                },
+        let shell = shell.add_blind_hole(
+            HoleLocation {
+                face: bottom_face,
+                position: [-offset, Scalar::ZERO].into(),
+            },
+            radius,
+            [Scalar::ZERO, Scalar::ZERO, depth],
+            services,
+        );
+
+        let bottom_face = shell.faces().first();
+        let top_face = shell
+            .faces()
+            .nth(5)
+            .expect("Expected shell to have top face");
+
+        shell
+            .add_through_hole(
+                [
+                    HoleLocation {
+                        face: bottom_face,
+                        position: [offset, Scalar::ZERO].into(),
+                    },
+                    HoleLocation {
+                        face: top_face,
+                        position: [offset, Scalar::ZERO].into(),
+                    },
+                ],
                 radius,
-                [Scalar::ZERO, Scalar::ZERO, depth],
                 services,
-            );
-
-            let bottom_face = shell.faces().first();
-            let top_face = shell
-                .faces()
-                .nth(5)
-                .expect("Expected shell to have top face");
-
-            shell
-                .add_through_hole(
-                    [
-                        HoleLocation {
-                            face: bottom_face,
-                            position: [offset, Scalar::ZERO].into(),
-                        },
-                        HoleLocation {
-                            face: top_face,
-                            position: [offset, Scalar::ZERO].into(),
-                        },
-                    ],
-                    radius,
-                    services,
-                )
-                .insert(services)
-        })
-        .insert(services)
+            )
+            .insert(services)
+    })
 }
