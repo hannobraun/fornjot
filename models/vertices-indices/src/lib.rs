379965dfd4a7e5d2dc68e05a266450e276ae2155
Hanno Braun <hanno@braun-odw.eu>
2024-01-30 10:55:35 +0100

    Return bare `Solid` from `vertices-indices` model

--- models/vertices-indices/src/lib.rs
+++ models/vertices-indices/src/lib.rs
@@ -6,16 +6,13 @@ use fj::core::{
         update::UpdateSolid,
     },
     services::Services,
-    storage::Handle,
 };
 
-pub fn model(services: &mut Services) -> Handle<Solid> {
-    Solid::empty()
-        .add_shells([Shell::from_vertices_and_indices(
-            [[0., 0., 0.], [1., 0., 0.], [0., 1., 0.], [0., 0., 1.]],
-            [[2, 1, 0], [0, 1, 3], [1, 2, 3], [2, 0, 3]],
-            services,
-        )
-        .insert(services)])
-        .insert(services)
+pub fn model(services: &mut Services) -> Solid {
+    Solid::empty().add_shells([Shell::from_vertices_and_indices(
+        [[0., 0., 0.], [1., 0., 0.], [0., 1., 0.], [0., 0., 1.]],
+        [[2, 1, 0], [0, 1, 3], [1, 2, 3], [2, 0, 3]],
+        services,
+    )
+    .insert(services)])
 }
--- models/vertices-indices/src/main.rs
+++ models/vertices-indices/src/main.rs
@@ -1,10 +1,8 @@
-use std::ops::Deref;
-
 use fj::{core::services::Services, handle_model};
 
 fn main() -> fj::Result {
     let mut services = Services::new();
     let model = vertices_indices::model(&mut services);
-    handle_model(model.deref(), services)?;
+    handle_model(&model, services)?;
     Ok(())
 }
