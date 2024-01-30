8fbdc70f336987873fa1f076e351330dfb589e20
Hanno Braun <hanno@braun-odw.eu>
2024-01-30 13:22:15 +0100

    Update documentation of `IsObject`

--- crates/fj-core/src/objects/is_object.rs
+++ crates/fj-core/src/objects/is_object.rs
@@ -6,9 +6,9 @@ use super::{
 
 /// A trait implemented for all object types
 ///
-/// This trait is implemented for both `T` and `Handle<T>`, where `T` is the
-/// type of any bare object. The `BareObject` associated type provides access to
-/// the bare object type.
+/// This trait is implemented for both bare objects (`T`) and stored objects
+/// (`Handle<T>`). The `BareObject` associated type provides access to the bare
+/// object type.
 ///
 /// This is a piece of infrastructure that is useful for other traits, which
 /// would otherwise have to duplicate its functionality. Users are unlikely to
