aa6dafaea3d95d91f2bfe5b5cbf2df9d13ebfe72
Hanno Braun <hanno@braun-odw.eu>
2024-02-02 13:52:31 +0100

    Make use of `fj::core::Instance` in models

--- models/cuboid/src/main.rs
+++ models/cuboid/src/main.rs
@@ -1,8 +1,8 @@
-use fj::{core::services::Services, handle_model};
+use fj::handle_model;
 
 fn main() -> fj::Result {
-    let mut services = Services::new();
-    let model = cuboid::model([3., 2., 1.], &mut services);
-    handle_model(&model, services)?;
+    let mut core = fj::core::Instance::new();
+    let model = cuboid::model([3., 2., 1.], &mut core.services);
+    handle_model(&model, core.services)?;
     Ok(())
 }
