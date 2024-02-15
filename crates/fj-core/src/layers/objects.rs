421bdb96a738b5e496babd7271493092848a4281
Hanno Braun <hanno@braun-odw.eu>
2024-02-15 12:18:09 +0100

    Remove redundant command

--- crates/fj-core/src/layers/objects.rs
+++ crates/fj-core/src/layers/objects.rs
@@ -15,33 +15,11 @@ impl Layer<Objects> {
         validation: &mut Layer<Validation>,
     ) {
         let mut events = Vec::new();
-        self.process(ObjectsCommand::InsertObject { object }, &mut events);
+        self.process(InsertObject { object }, &mut events);
 
         for event in events {
             validation.process(event, &mut Vec::new());
         }
-    }
-}
-
-/// Command for `Layer<Objects>`
-#[derive(Debug)]
-pub enum ObjectsCommand {
-    /// Insert an object into the stores
-    ///
-    /// This is the one primitive operation that all other operations are built
-    /// upon.
-    InsertObject {
-        /// The object to insert
-        object: AnyObject<AboutToBeStored>,
-    },
-}
-
-impl Command<Objects> for ObjectsCommand {
-    type Event = InsertObject;
-
-    fn decide(self, _: &Objects, events: &mut Vec<Self::Event>) {
-        let ObjectsCommand::InsertObject { object } = self;
-        events.push(InsertObject { object });
     }
 }
 
@@ -52,6 +30,14 @@ impl Command<Objects> for ObjectsCommand {
 pub struct InsertObject {
     /// The object to insert
     pub object: AnyObject<AboutToBeStored>,
+}
+
+impl Command<Objects> for InsertObject {
+    type Event = InsertObject;
+
+    fn decide(self, _: &Objects, events: &mut Vec<Self::Event>) {
+        events.push(self);
+    }
 }
 
 impl Event<Objects> for InsertObject {
