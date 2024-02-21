9ba504a35d343427321ca316bbdc60a4d865dff4
Hanno Braun <hanno@braun-odw.eu>
2024-02-21 12:09:13 +0100

    Add `validation` module

---
+++ crates/fj-core/src/validation/mod.rs
@@ -0,0 +1,11 @@
+//! # Infrastructure for validating objects
+//!
+//! ## Implementation Note
+//!
+//! This is a new module whose goal is to replace [`crate::validate`]. While
+//! this transition is ongoing, both modules will be incomplete.
+//!
+//! Issues that track the transition:
+//!
+//! - <https://github.com/hannobraun/fornjot/issues/1713>
+//! - <https://github.com/hannobraun/fornjot/issues/2157>
