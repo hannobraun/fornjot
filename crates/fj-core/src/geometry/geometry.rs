65dffcbc4f53453797f0575f3f480c8963b05029
Hanno Braun <hanno@braun-odw.eu>
2024-02-28 13:15:33 +0100

    Make basis plane geometry easier to access

--- crates/fj-core/src/geometry/geometry.rs
+++ crates/fj-core/src/geometry/geometry.rs
@@ -12,6 +12,10 @@ use super::{GlobalPath, SurfaceGeometry};
 /// Geometric data that is associated with topological objects
 pub struct Geometry {
     surface: BTreeMap<HandleWrapper<Surface>, SurfaceGeometry>,
+
+    xy_plane: Handle<Surface>,
+    xz_plane: Handle<Surface>,
+    yz_plane: Handle<Surface>,
 }
 
 impl Geometry {
@@ -19,24 +23,28 @@ impl Geometry {
     pub fn new(objects: &Objects) -> Self {
         let mut self_ = Self {
             surface: BTreeMap::new(),
+
+            xy_plane: objects.surfaces.xy_plane(),
+            xz_plane: objects.surfaces.xz_plane(),
+            yz_plane: objects.surfaces.yz_plane(),
         };
 
         self_.define_surface_inner(
-            objects.surfaces.xy_plane(),
+            self_.xy_plane.clone(),
             SurfaceGeometry {
                 u: GlobalPath::x_axis(),
                 v: Vector::unit_y(),
             },
         );
         self_.define_surface_inner(
-            objects.surfaces.xz_plane(),
+            self_.xz_plane.clone(),
             SurfaceGeometry {
                 u: GlobalPath::x_axis(),
                 v: Vector::unit_z(),
             },
         );
         self_.define_surface_inner(
-            objects.surfaces.yz_plane(),
+            self_.yz_plane.clone(),
             SurfaceGeometry {
                 u: GlobalPath::y_axis(),
                 v: Vector::unit_z(),
@@ -64,5 +72,20 @@ impl Geometry {
             .get(&surface.clone().into())
             .copied()
             .expect("Expected geometry of surface to be defined")
+    }
+
+    /// Access the geometry of the xy-plane
+    pub fn xy_plane(&self) -> SurfaceGeometry {
+        self.of_surface(&self.xy_plane)
+    }
+
+    /// Access the geometry of the xz-plane
+    pub fn xz_plane(&self) -> SurfaceGeometry {
+        self.of_surface(&self.xz_plane)
+    }
+
+    /// Access the geometry of the yz-plane
+    pub fn yz_plane(&self) -> SurfaceGeometry {
+        self.of_surface(&self.yz_plane)
     }
 }
