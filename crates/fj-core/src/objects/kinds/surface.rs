5b66aa7862600bb58bab5b3b9f0571626c0162d1
Hanno Braun <hanno@braun-odw.eu>
2024-03-13 14:48:04 +0100

    Update documentation of `Surface`

--- crates/fj-core/src/objects/kinds/surface.rs
+++ crates/fj-core/src/objects/kinds/surface.rs
@@ -1,4 +1,19 @@
 /// A two-dimensional shape
+///
+///
+/// ## Equality
+///
+/// `Surface` contains no data and exists purely to be referenced via a
+/// `Handle`, where `Handle::id` can be used to compare different instances of
+/// it.
+///
+/// If `Surface` had `Eq`/`PartialEq` implementations, it containing no data
+/// would mean that all instances of `Surface` would be considered equal. This
+/// would be very error-prone.
+///
+/// If you need to reference a `Surface` from a struct that needs to derive
+/// `Eq`/`Ord`/..., you can use `HandleWrapper<Vertex>` to do that. It will
+/// use `Handle::id` to provide those `Eq`/`Ord`/... implementations.
 #[derive(Clone, Copy, Debug, Default, Hash)]
 pub struct Surface {}
 
