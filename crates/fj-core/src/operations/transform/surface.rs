aa665fb4c3a4d87fd55103b8ca0af34f0c02c79d
Hanno Braun <hanno@braun-odw.eu>
2024-03-13 14:50:46 +0100

    Refactor to improve clarity

--- crates/fj-core/src/operations/transform/surface.rs
+++ crates/fj-core/src/operations/transform/surface.rs
@@ -16,10 +16,10 @@ impl TransformObject for Handle<Surface> {
         cache
             .entry(self)
             .or_insert_with(|| {
-                let geometry =
-                    core.layers.geometry.of_surface(self).transform(transform);
                 let surface = Surface::new().insert(core);
 
+                let geometry =
+                    core.layers.geometry.of_surface(self).transform(transform);
                 core.layers
                     .geometry
                     .define_surface(surface.clone(), geometry);
