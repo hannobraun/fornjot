4d8d57728bafc65b1e1e449d8448d36cbec77952
Hanno Braun <hanno@braun-odw.eu>
2024-03-20 13:59:36 +0100

    Refactor to prepare for follow-on change

--- crates/fj-core/src/operations/build/shell.rs
+++ crates/fj-core/src/operations/build/shell.rs
@@ -91,13 +91,14 @@ pub trait BuildShell {
                         .zip_ext([[a, b], [b, c], [c, a]])
                         .zip_ext(curves_and_boundaries)
                         .map(|((vertex, positions), (curve, boundary))| {
-                            HalfEdge::line_segment(
+                            let half_edge = HalfEdge::line_segment(
                                 positions,
                                 Some(boundary.reverse().inner),
                                 core,
-                            )
-                            .update_start_vertex(|_, _| vertex, core)
-                            .update_curve(|_, _| curve, core)
+                            );
+                            half_edge
+                                .update_start_vertex(|_, _| vertex, core)
+                                .update_curve(|_, _| curve, core)
                         })
                 };
 
