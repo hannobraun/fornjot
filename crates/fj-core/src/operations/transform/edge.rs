b528430be3eee8665d14db69bc83e2690f8681ac
Hanno Braun <hanno@braun-odw.eu>
2024-03-22 23:31:34 +0100

    Inline redundant variable

--- crates/fj-core/src/operations/transform/edge.rs
+++ crates/fj-core/src/operations/transform/edge.rs
@@ -16,7 +16,6 @@ impl TransformObject for Handle<HalfEdge> {
     ) -> Self {
         // Don't need to transform the path, as that's defined in surface
         // coordinates.
-        let path = core.layers.geometry.of_half_edge(self).path;
         let boundary = self.boundary();
         let curve = self
             .curve()
@@ -30,9 +29,12 @@ impl TransformObject for Handle<HalfEdge> {
         let half_edge =
             HalfEdge::new(boundary, curve, start_vertex).insert(core);
 
-        core.layers
-            .geometry
-            .define_half_edge(half_edge.clone(), HalfEdgeGeometry { path });
+        core.layers.geometry.define_half_edge(
+            half_edge.clone(),
+            HalfEdgeGeometry {
+                path: core.layers.geometry.of_half_edge(self).path,
+            },
+        );
 
         half_edge
     }
