6b036c69ff6e28915f4a515a66dda0010b0ec370
Hanno Braun <hanno@braun-odw.eu>
2024-03-22 23:42:07 +0100

    Read boundary from geometry layer in reverse code

--- crates/fj-core/src/operations/reverse/edge.rs
+++ crates/fj-core/src/operations/reverse/edge.rs
@@ -12,10 +12,10 @@ impl ReverseCurveCoordinateSystems for Handle<HalfEdge> {
     fn reverse_curve_coordinate_systems(&self, core: &mut Core) -> Self {
         let mut geometry = core.layers.geometry.of_half_edge(self);
         geometry.path = geometry.path.reverse();
-        let boundary = self.boundary().reverse();
+        geometry.boundary = geometry.boundary.reverse();
 
         let half_edge = HalfEdge::new(
-            boundary,
+            geometry.boundary,
             self.curve().clone(),
             self.start_vertex().clone(),
         )
@@ -26,7 +26,7 @@ impl ReverseCurveCoordinateSystems for Handle<HalfEdge> {
             half_edge.clone(),
             HalfEdgeGeometry {
                 path: geometry.path,
-                boundary,
+                boundary: geometry.boundary,
             },
         );
 
