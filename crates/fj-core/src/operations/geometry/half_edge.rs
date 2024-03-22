77cba2b04ad7cd96a13dce15a0cbdf24d1caf6cf
Hanno Braun <hanno@braun-odw.eu>
2024-03-22 15:43:27 +0100

    Remove unused code

--- crates/fj-core/src/operations/geometry/half_edge.rs
+++ crates/fj-core/src/operations/geometry/half_edge.rs
@@ -1,12 +1,8 @@
-use fj_math::Point;
-
 use crate::{
-    geometry::{CurveBoundary, Geometry, HalfEdgeGeometry, SurfacePath},
+    geometry::{Geometry, HalfEdgeGeometry, SurfacePath},
     layers::Layer,
     objects::HalfEdge,
-    operations::insert::Insert,
     storage::Handle,
-    Core,
 };
 
 /// Update the geometry of a [`HalfEdge`]
@@ -16,22 +12,6 @@ pub trait UpdateHalfEdgeGeometry {
         self,
         path: SurfacePath,
         geometry: &mut Layer<Geometry>,
-    ) -> Self;
-
-    /// Update the path of the half-edge
-    #[must_use]
-    fn update_path(
-        &self,
-        update: impl FnOnce(SurfacePath) -> SurfacePath,
-        core: &mut Core,
-    ) -> Self;
-
-    /// Update the boundary of the half-edge
-    #[must_use]
-    fn update_boundary(
-        &self,
-        update: impl FnOnce(CurveBoundary<Point<1>>) -> CurveBoundary<Point<1>>,
-        core: &mut Core,
     ) -> Self;
 }
 
@@ -43,41 +23,5 @@ impl UpdateHalfEdgeGeometry for Handle<HalfEdge> {
     ) -> Self {
         geometry.define_half_edge(self.clone(), HalfEdgeGeometry { path });
         self
-    }
-
-    fn update_path(
-        &self,
-        update: impl FnOnce(SurfacePath) -> SurfacePath,
-        core: &mut Core,
-    ) -> Self {
-        let path = update(core.layers.geometry.of_half_edge(self).path);
-
-        let half_edge = HalfEdge::new(
-            path,
-            self.boundary(),
-            self.curve().clone(),
-            self.start_vertex().clone(),
-        )
-        .insert(core);
-
-        core.layers
-            .geometry
-            .define_half_edge(half_edge.clone(), HalfEdgeGeometry { path });
-
-        half_edge
-    }
-
-    fn update_boundary(
-        &self,
-        update: impl FnOnce(CurveBoundary<Point<1>>) -> CurveBoundary<Point<1>>,
-        core: &mut Core,
-    ) -> Self {
-        HalfEdge::new(
-            core.layers.geometry.of_half_edge(self).path,
-            update(self.boundary()),
-            self.curve().clone(),
-            self.start_vertex().clone(),
-        )
-        .insert(core)
     }
 }
