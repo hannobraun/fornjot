b94f3e46ddd7cd54b71f427c26bc13bc530ecc4d
Hanno Braun <hanno@braun-odw.eu>
2024-03-25 12:48:56 +0100

    Add `CurveGeom`

--- crates/fj-core/src/geometry/mod.rs
+++ crates/fj-core/src/geometry/mod.rs
@@ -1,6 +1,7 @@
 //! Types that are tied to objects, but aren't objects themselves
 
 mod boundary;
+mod curve;
 mod geometry;
 mod half_edge;
 mod path;
@@ -8,6 +9,7 @@ mod surface;
 
 pub use self::{
     boundary::{CurveBoundary, CurveBoundaryElement},
+    curve::{CurveGeom, LocalCurveGeom},
     geometry::Geometry,
     half_edge::HalfEdgeGeom,
     path::{GlobalPath, SurfacePath},
