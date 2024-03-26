8207077338cea0f6ad933dc375742ae86842714b
Hanno Braun <hanno@braun-odw.eu>
2024-03-26 12:37:28 +0100

    Make variable name more specific

--- crates/fj-core/src/operations/reverse/edge.rs
+++ crates/fj-core/src/operations/reverse/edge.rs
@@ -9,9 +9,9 @@ use super::ReverseCurveCoordinateSystems;
 
 impl ReverseCurveCoordinateSystems for Handle<HalfEdge> {
     fn reverse_curve_coordinate_systems(&self, core: &mut Core) -> Self {
-        let mut geometry = *core.layers.geometry.of_half_edge(self);
-        geometry.path = geometry.path.reverse();
-        geometry.boundary = geometry.boundary.reverse();
+        let mut half_edge_geom = *core.layers.geometry.of_half_edge(self);
+        half_edge_geom.path = half_edge_geom.path.reverse();
+        half_edge_geom.boundary = half_edge_geom.boundary.reverse();
 
         let half_edge =
             HalfEdge::new(self.curve().clone(), self.start_vertex().clone())
@@ -20,7 +20,7 @@ impl ReverseCurveCoordinateSystems for Handle<HalfEdge> {
 
         core.layers
             .geometry
-            .define_half_edge(half_edge.clone(), geometry);
+            .define_half_edge(half_edge.clone(), half_edge_geom);
 
         half_edge
     }
