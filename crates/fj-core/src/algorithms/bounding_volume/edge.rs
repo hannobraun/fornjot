85f57a93993e8a371a3d2be68f6ba626c2287a03
Hanno Braun <hanno@braun-odw.eu>
2024-03-26 11:26:42 +0100

    Rename variable to prepare for follow-on change

--- crates/fj-core/src/algorithms/bounding_volume/edge.rs
+++ crates/fj-core/src/algorithms/bounding_volume/edge.rs
@@ -10,9 +10,9 @@ impl super::BoundingVolume<2> for &Handle<HalfEdge> {
     fn aabb(self, geometry: &Geometry) -> Option<Aabb<2>> {
         let half_edge = self;
 
-        let geometry = geometry.of_half_edge(half_edge);
+        let half_edge = geometry.of_half_edge(half_edge);
 
-        match geometry.path {
+        match half_edge.path {
             SurfacePath::Circle(circle) => {
                 // Just calculate the AABB of the whole circle. This is not the
                 // most precise, but it should do for now.
@@ -26,8 +26,8 @@ impl super::BoundingVolume<2> for &Handle<HalfEdge> {
                 })
             }
             SurfacePath::Line(_) => {
-                let points = geometry.boundary.inner.map(|point_curve| {
-                    geometry.path.point_from_path_coords(point_curve)
+                let points = half_edge.boundary.inner.map(|point_curve| {
+                    half_edge.path.point_from_path_coords(point_curve)
                 });
 
                 Some(Aabb::<2>::from_points(points))
