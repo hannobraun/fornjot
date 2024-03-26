33803d37271040ff791df316143ad5381ee60578
Hanno Braun <hanno@braun-odw.eu>
2024-03-26 12:15:17 +0100

    Refactor to prepare for follow-on change

--- crates/fj-core/src/algorithms/bounding_volume/cycle.rs
+++ crates/fj-core/src/algorithms/bounding_volume/cycle.rs
@@ -4,9 +4,11 @@ use crate::{geometry::Geometry, topology::Cycle};
 
 impl super::BoundingVolume<2> for &Cycle {
     fn aabb(self, geometry: &Geometry) -> Option<Aabb<2>> {
+        let cycle = self;
+
         let mut aabb: Option<Aabb<2>> = None;
 
-        for edge in self.half_edges() {
+        for edge in cycle.half_edges() {
             let new_aabb =
                 edge.aabb(geometry).expect("`Edge` can always compute AABB");
             aabb = Some(aabb.map_or(new_aabb, |aabb| aabb.merged(&new_aabb)));
