2aaf431ac0cd022ae1cb7ad4b9aabadac38a9bfe
Hanno Braun <hanno@braun-odw.eu>
2024-03-26 11:20:11 +0100

    Provide surface to `Cycle`'s `BoundingVolume` impl

--- crates/fj-core/src/algorithms/bounding_volume/cycle.rs
+++ crates/fj-core/src/algorithms/bounding_volume/cycle.rs
@@ -1,10 +1,14 @@
 use fj_math::Aabb;
 
-use crate::{geometry::Geometry, topology::Cycle};
+use crate::{
+    geometry::Geometry,
+    storage::Handle,
+    topology::{Cycle, Surface},
+};
 
-impl super::BoundingVolume<2> for &Cycle {
+impl super::BoundingVolume<2> for (&Cycle, &Handle<Surface>) {
     fn aabb(self, geometry: &Geometry) -> Option<Aabb<2>> {
-        let cycle = self;
+        let (cycle, _) = self;
 
         let mut aabb: Option<Aabb<2>> = None;
 
--- crates/fj-core/src/algorithms/bounding_volume/face.rs
+++ crates/fj-core/src/algorithms/bounding_volume/face.rs
@@ -1,3 +1,5 @@
+use std::ops::Deref;
+
 use fj_math::Aabb;
 
 use crate::{
@@ -7,27 +9,30 @@ use crate::{
 
 impl super::BoundingVolume<3> for &Face {
     fn aabb(self, geometry: &Geometry) -> Option<Aabb<3>> {
-        self.region().exterior().aabb(geometry).map(|aabb2| {
-            let surface = geometry.of_surface(self.surface());
+        (self.region().exterior().deref(), self.surface())
+            .aabb(geometry)
+            .map(|aabb2| {
+                let surface = geometry.of_surface(self.surface());
 
-            match surface.u {
-                GlobalPath::Circle(circle) => {
-                    // This is not the most precise way to calculate the AABB,
-                    // doing it for the whole circle, but it should do.
+                match surface.u {
+                    GlobalPath::Circle(circle) => {
+                        // This is not the most precise way to calculate the
+                        // AABB, doing it for the whole circle, but it should
+                        // do.
 
-                    let aabb_bottom = circle.aabb();
-                    let aabb_top = Aabb {
-                        min: aabb_bottom.min + surface.v,
-                        max: aabb_bottom.max + surface.v,
-                    };
+                        let aabb_bottom = circle.aabb();
+                        let aabb_top = Aabb {
+                            min: aabb_bottom.min + surface.v,
+                            max: aabb_bottom.max + surface.v,
+                        };
 
-                    aabb_bottom.merged(&aabb_top)
+                        aabb_bottom.merged(&aabb_top)
+                    }
+                    GlobalPath::Line(_) => Aabb {
+                        min: surface.point_from_surface_coords(aabb2.min),
+                        max: surface.point_from_surface_coords(aabb2.max),
+                    },
                 }
-                GlobalPath::Line(_) => Aabb {
-                    min: surface.point_from_surface_coords(aabb2.min),
-                    max: surface.point_from_surface_coords(aabb2.max),
-                },
-            }
-        })
+            })
     }
 }
