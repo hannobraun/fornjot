23af296ce93a9838aebd5a46c3b7cdd726da718f
Hanno Braun <hanno@braun-odw.eu>
2024-04-30 12:40:05 +0200

    Remove obsolete comment

--- crates/fj-core/src/operations/transform/curve.rs
+++ crates/fj-core/src/operations/transform/curve.rs
@@ -13,9 +13,6 @@ impl TransformObject for Handle<Curve> {
         core: &mut Core,
         cache: &mut TransformCache,
     ) -> Self {
-        // There's nothing to actually transform here, as `Curve` holds no data.
-        // We still need this implementation though, as a new `Curve` object
-        // must be created to represent the new and transformed curve.
         cache
             .entry(self)
             .or_insert_with(|| {
