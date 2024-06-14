74168aa240249accd44f1bbae9131d0fd8320d6a
Hanno Braun <hello@hannobraun.com>
2024-06-14 21:26:51 +0200

    Correctly define geometry of transformed curve

--- crates/fj-core/src/operations/transform/curve.rs
+++ crates/fj-core/src/operations/transform/curve.rs
@@ -1,7 +1,7 @@
 use fj_math::Transform;
 
 use crate::{
-    operations::{geometry::UpdateCurveGeometry, insert::Insert},
+    operations::insert::Insert,
     storage::Handle,
     topology::{Curve, Surface},
     Core,
@@ -14,23 +14,40 @@ impl TransformObject for (&Handle<Curve>, &Handle<Surface>) {
 
     fn transform_with_cache(
         self,
-        _: &Transform,
+        transform: &Transform,
         core: &mut Core,
         cache: &mut TransformCache,
     ) -> Self::Transformed {
-        let (curve, _) = self;
+        let (curve, surface) = self;
 
-        cache
+        // We don't actually need to transform the curve, as its geometry is
+        // locally defined on a surface. We need to transform that surface
+        // though, or what we do here will have no consequence.
+        //
+        // If this transformation is only one element in the transformation of
+        // a whole object graph, using the cache here ensures that the surface
+        // doesn't get transformed multiple times.
+        let transformed_surface = cache
+            .entry(surface)
+            .or_insert_with(|| surface.transform(transform, core))
+            .clone();
+        let transformed_curve = cache
             .entry(curve)
-            .or_insert_with(|| {
-                // We don't actually need to transform the curve, as its
-                // geometry is locally defined on a surface. We need to set that
-                // geometry for the new object though, that we created here to
-                // represent the transformed curve.
-                Curve::new()
-                    .insert(core)
-                    .copy_geometry_from(curve, &mut core.layers.geometry)
-            })
-            .clone()
+            .or_insert_with(|| Curve::new().insert(core))
+            .clone();
+
+        core.layers.geometry.define_curve(
+            transformed_curve.clone(),
+            transformed_surface,
+            core.layers
+                .geometry
+                .of_curve(curve)
+                .unwrap()
+                .local_on(surface)
+                .unwrap()
+                .clone(),
+        );
+
+        transformed_curve
     }
 }
