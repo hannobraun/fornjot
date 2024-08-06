5beb8e8a54055847e3f13692817a57f4f2816c1a
Hanno Braun <hello@hannobraun.com>
2024-08-06 19:15:37 +0200

    Prepare for follow-on change

--- crates/fj-core/src/core.rs
+++ crates/fj-core/src/core.rs
@@ -7,7 +7,6 @@ use crate::{layers::Layers, validation::ValidationConfig};
 /// An instance of the Fornjot core
 ///
 /// This is the main entry point to `fj-core`'s API.
-#[derive(Default)]
 pub struct Core {
     /// The layers of data that make up the state of a core instance
     pub layers: Layers,
@@ -16,12 +15,20 @@ pub struct Core {
 impl Core {
     /// Construct an instance of `Instance`
     pub fn new() -> Self {
-        Self::default()
+        Self {
+            layers: Layers::default(),
+        }
     }
 
     /// Construct an instance of `Instance`, using the provided configuration
     pub fn with_validation_config(config: ValidationConfig) -> Self {
         let layers = Layers::with_validation_config(config);
         Self { layers }
+    }
+}
+
+impl Default for Core {
+    fn default() -> Self {
+        Self::new()
     }
 }
