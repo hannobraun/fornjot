86e9ad1ca6078a0b362c4b0a09a840e8a716282d
Hanno Braun <hello@hannobraun.com>
2024-08-12 21:49:12 +0200

    Prepare for follow-on change

--- crates/fj-core/src/validation/config.rs
+++ crates/fj-core/src/validation/config.rs
@@ -44,16 +44,16 @@ impl ValidationConfig {
     pub fn from_tolerance(tolerance: impl Into<Tolerance>) -> Self {
         let tolerance = tolerance.into();
 
+        // This value was chosen pretty arbitrarily. Seems small enough to catch
+        // errors. If it turns out it's too small (because it produces false
+        // positives due to floating-point accuracy issues), we can adjust it.
+        let identical_max_distance = Scalar::from_f64(5e-14);
+
         Self {
             panic_on_error: false,
             tolerance,
             distinct_min_distance: Scalar::from_f64(5e-7), // 0.5 µm,
-
-            // This value was chosen pretty arbitrarily. Seems small enough to
-            // catch errors. If it turns out it's too small (because it produces
-            // false positives due to floating-point accuracy issues), we can
-            // adjust it.
-            identical_max_distance: Scalar::from_f64(5e-14),
+            identical_max_distance,
         }
     }
 }
