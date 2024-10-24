b96cc3ff05ff927107c709e2f5da05b83f4f1366
Hanno Braun <hello@hannobraun.com>
2024-10-24 19:02:19 +0200

    Update doc comment

--- crates/fj-core/src/geometry/repr/mod.rs
+++ crates/fj-core/src/geometry/repr/mod.rs
@@ -1,11 +1,4 @@
-//! # Various geometric utilities
-//!
-//! ## Implementation Note
-//!
-//! This module collects functionality that doesn't (yet) fit anywhere else.
-//! Whatever's in here isn't expected to stay here permanently. Rather, this
-//! module provides an easy place to put new things, before it's clear what to
-//! do with them long-term.
+//! # The uniform intermediate representations of geometry
 
 pub mod polyline;
 pub mod tri_mesh;
