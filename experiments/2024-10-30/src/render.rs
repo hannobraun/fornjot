3c0ff8bffc824c8a32960a36bcb5c5edee84a831
Hanno Braun <hello@hannobraun.com>
2024-10-30 01:44:03 +0100

    Create event loop

--- experiments/2024-10-30/src/render.rs
+++ experiments/2024-10-30/src/render.rs
@@ -1,5 +1,38 @@
+use winit::{
+    application::ApplicationHandler,
+    event::WindowEvent,
+    event_loop::{ActiveEventLoop, EventLoop},
+    window::{Window, WindowAttributes, WindowId},
+};
+
 use crate::mesh::Mesh;
 
 pub fn render(_: &Mesh) -> anyhow::Result<()> {
+    let event_loop = EventLoop::new()?;
+
+    let mut app = App { window: None };
+    event_loop.run_app(&mut app)?;
+
     Ok(())
+}
+
+struct App {
+    window: Option<Window>,
+}
+
+impl ApplicationHandler for App {
+    fn resumed(&mut self, event_loop: &ActiveEventLoop) {
+        let window = event_loop
+            .create_window(WindowAttributes::default())
+            .unwrap();
+        self.window = Some(window);
+    }
+
+    fn window_event(
+        &mut self,
+        _: &ActiveEventLoop,
+        _: WindowId,
+        _: WindowEvent,
+    ) {
+    }
 }
