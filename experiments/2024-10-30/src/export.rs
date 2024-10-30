2e630af798a6b0e275730137b7a79fd67eaeef10
Hanno Braun <hello@hannobraun.com>
2024-10-30 01:31:42 +0100

    Prepare for follow-on change

--- experiments/2024-10-30/src/export.rs
+++ experiments/2024-10-30/src/export.rs
@@ -5,13 +5,15 @@ use crate::mesh::Mesh;
 pub fn export(mesh: Mesh) -> anyhow::Result<()> {
     let vertices = mesh
         .vertices
-        .into_iter()
+        .iter()
+        .copied()
         .map(|[x, y, z]| threemf::model::Vertex { x, y, z })
         .collect();
 
     let triangles = mesh
         .triangles
-        .into_iter()
+        .iter()
+        .copied()
         .map(|[v1, v2, v3]| threemf::model::Triangle { v1, v2, v3 })
         .collect();
 
