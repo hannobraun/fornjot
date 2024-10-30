a5f2d7199565323a03bed4bf36afa529080a1d39
Hanno Braun <hello@hannobraun.com>
2024-10-30 01:24:19 +0100

    Start organizing into modules

---
+++ experiments/2024-10-30/src/model.rs
@@ -0,0 +1,60 @@
+use std::fs::File;
+
+pub fn model() -> anyhow::Result<()> {
+    let vertices = [
+        [-0.5, -0.5, -0.5], // 0
+        [0.5, -0.5, -0.5],  // 1
+        [-0.5, 0.5, -0.5],  // 2
+        [0.5, 0.5, -0.5],   // 3
+        [-0.5, -0.5, 0.5],  // 4
+        [0.5, -0.5, 0.5],   // 5
+        [-0.5, 0.5, 0.5],   // 6
+        [0.5, 0.5, 0.5],    // 7
+    ];
+
+    let triangles = [
+        [0, 4, 6], // left
+        [0, 6, 2],
+        [1, 3, 7], // right
+        [1, 7, 5],
+        [0, 1, 5], // front
+        [0, 5, 4],
+        [2, 7, 3], // back
+        [2, 6, 7],
+        [0, 2, 1], // bottom
+        [1, 2, 3],
+        [4, 5, 7], // top
+        [4, 7, 6],
+    ];
+
+    export(vertices, triangles)?;
+
+    Ok(())
+}
+
+fn export(
+    vertices: impl IntoIterator<Item = [f64; 3]>,
+    triangles: impl IntoIterator<Item = [usize; 3]>,
+) -> anyhow::Result<()> {
+    let vertices = vertices
+        .into_iter()
+        .map(|[x, y, z]| threemf::model::Vertex { x, y, z })
+        .collect();
+
+    let triangles = triangles
+        .into_iter()
+        .map(|[v1, v2, v3]| threemf::model::Triangle { v1, v2, v3 })
+        .collect();
+
+    let mesh = threemf::Mesh {
+        vertices: threemf::model::Vertices { vertex: vertices },
+        triangles: threemf::model::Triangles {
+            triangle: triangles,
+        },
+    };
+
+    let output = File::create("output.3mf")?;
+    threemf::write(output, mesh)?;
+
+    Ok(())
+}
