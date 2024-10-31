6842a6bf821286267bb83d8600b5fa9786738d87
Hanno Braun <hello@hannobraun.com>
2024-10-31 22:10:51 +0100

    Simplify

--- experiments/2024-10-30/src/render.rs
+++ experiments/2024-10-30/src/render.rs
@@ -97,18 +97,9 @@ impl Renderer {
                             array_stride: size_of::<Vertex>()
                                 as wgpu::BufferAddress,
                             step_mode: wgpu::VertexStepMode::Vertex,
-                            attributes: &[
-                                wgpu::VertexAttribute {
-                                    format: wgpu::VertexFormat::Float32x3,
-                                    offset: 0,
-                                    shader_location: 0,
-                                },
-                                wgpu::VertexAttribute {
-                                    format: wgpu::VertexFormat::Float32x3,
-                                    offset: size_of::<[f32; 3]>()
-                                        as wgpu::BufferAddress,
-                                    shader_location: 1,
-                                },
+                            attributes: &wgpu::vertex_attr_array![
+                                0 => Float32x3,
+                                1 => Float32x3,
                             ],
                         }],
                     },
