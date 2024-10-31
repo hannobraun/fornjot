924e9a03d4da9128096606532bae8ceea4b7d92b
Hanno Braun <hello@hannobraun.com>
2024-10-31 21:24:40 +0100

    Add `Mesh::push_triangle`

--- experiments/2024-10-30/src/mesh.rs
+++ experiments/2024-10-30/src/mesh.rs
@@ -8,4 +8,8 @@ impl Mesh {
     pub fn push_vertex(&mut self, vertex: [f32; 3]) {
         self.vertices.push(vertex);
     }
+
+    pub fn push_triangle(&mut self, triangle: [u32; 3]) {
+        self.triangles.push(triangle);
+    }
 }
--- experiments/2024-10-30/src/model.rs
+++ experiments/2024-10-30/src/model.rs
@@ -29,7 +29,7 @@ pub fn model() -> anyhow::Result<Mesh> {
         [4, 5, 7], // top
         [4, 7, 6],
     ]
-    .map(|triangle| mesh.triangles.push(triangle));
+    .map(|triangle| mesh.push_triangle(triangle));
 
     Ok(mesh)
 }
