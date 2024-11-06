38c79002ac03f11fbe3ce7013b1d490afb25835a
Hanno Braun <hello@hannobraun.com>
2024-11-06 18:46:22 +0100

    Prepare for follow-on change

--- experiments/2024-10-30/src/render.rs
+++ experiments/2024-10-30/src/render.rs
@@ -277,7 +277,7 @@ impl Renderer {
             render_pass.set_pipeline(&self.pipeline);
             render_pass.set_bind_group(0, &self.bind_group, &[]);
             render_pass.draw_indexed(
-                0..mesh.triangles().len() as u32 * 3,
+                0..mesh_triangles.len() as u32 * 3,
                 0,
                 0..1,
             );
