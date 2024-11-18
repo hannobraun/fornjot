5549e9ac24dcc6f0c1fad31e50e516ded6eab4c8
Hanno Braun <hello@hannobraun.com>
2024-11-18 20:23:02 +0100

    Derive `Copy` for `MouseButton`

--- crates/fj-viewer/src/input/mod.rs
+++ crates/fj-viewer/src/input/mod.rs
@@ -5,7 +5,7 @@ mod event;
 pub use self::event::InputEvent;
 
 /// # Any mouse button that is relevant to the operation of this crate
-#[derive(Eq, PartialEq)]
+#[derive(Clone, Copy, Eq, PartialEq)]
 pub enum MouseButton {
     /// # The left mouse button
     Left,
