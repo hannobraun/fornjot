33a3185710756d69502db36ef9da619e20494a49
Hanno Braun <hello@hannobraun.com>
2024-11-21 19:51:02 +0100

    Prepare to improve vertex rendering

--- experiments/2024-10-30/src/render/shaders/mod.rs
+++ experiments/2024-10-30/src/render/shaders/mod.rs
@@ -109,3 +109,15 @@ impl Vertex for TrianglesVertex {
         1 => Float32x3,
     ];
 }
+
+#[derive(Clone, Copy, bytemuck::Pod, bytemuck::Zeroable)]
+#[repr(C)]
+pub struct VerticesVertex {
+    pub position: [f32; 3],
+}
+
+impl Vertex for VerticesVertex {
+    const ATTRIBUTES: &[wgpu::VertexAttribute] = &wgpu::vertex_attr_array![
+        0 => Float32x3,
+    ];
+}
