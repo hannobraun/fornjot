8323ad826d11baffe54e531b63f653c22623ec22
Hanno Braun <hello@hannobraun.com>
2024-11-25 21:45:25 +0100

    Render text

--- experiments/2024-10-30/src/render/text.rs
+++ experiments/2024-10-30/src/render/text.rs
@@ -11,6 +11,7 @@ impl TextRenderer {
         surface_config: &wgpu::SurfaceConfiguration,
     ) -> Self {
         let cache = glyphon::Cache::new(device);
+        let mut swash_cache = glyphon::SwashCache::new();
 
         let mut text_atlas = glyphon::TextAtlas::new(
             device,
@@ -28,12 +29,59 @@ impl TextRenderer {
             },
         );
 
-        let text_renderer = glyphon::TextRenderer::new(
+        let mut text_renderer = glyphon::TextRenderer::new(
             &mut text_atlas,
             device,
             wgpu::MultisampleState::default(),
-            None,
+            Some(wgpu::DepthStencilState {
+                format: wgpu::TextureFormat::Depth32Float,
+                depth_write_enabled: true,
+                depth_compare: wgpu::CompareFunction::Less,
+                stencil: wgpu::StencilState::default(),
+                bias: wgpu::DepthBiasState::default(),
+            }),
         );
+
+        let mut font_system = glyphon::FontSystem::new();
+
+        let mut buffer = glyphon::Buffer::new(
+            &mut font_system,
+            glyphon::Metrics {
+                font_size: 16.,
+                line_height: 16.,
+            },
+        );
+        buffer.set_text(
+            &mut font_system,
+            "Hello, world!",
+            glyphon::Attrs::new(),
+            glyphon::Shaping::Advanced,
+        );
+
+        text_renderer
+            .prepare(
+                device,
+                queue,
+                &mut font_system,
+                &mut text_atlas,
+                &viewport,
+                [glyphon::TextArea {
+                    buffer: &buffer,
+                    left: 0.,
+                    top: 0.,
+                    scale: 1.,
+                    bounds: glyphon::TextBounds {
+                        left: 0,
+                        top: 0,
+                        right: surface_config.width as i32,
+                        bottom: surface_config.height as i32,
+                    },
+                    default_color: glyphon::Color::rgb(0, 0, 0),
+                    custom_glyphs: &[],
+                }],
+                &mut swash_cache,
+            )
+            .unwrap();
 
         Self {
             text_atlas,
