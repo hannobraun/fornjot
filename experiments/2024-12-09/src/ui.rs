7a654f2a70589c4c0fd77282be9b08bea84428d8
Hanno Braun <hello@hannobraun.com>
2024-12-10 19:09:16 +0100

    Simplify

--- experiments/2024-12-09/src/ui.rs
+++ experiments/2024-12-09/src/ui.rs
@@ -28,9 +28,7 @@ impl OperationView {
 
     pub fn select_next(&mut self) {
         if let Some(selected) = self.selected {
-            if selected < self.last_index() {
-                self.selected = Some(selected + 1);
-            }
+            self.selected = Some(usize::min(selected + 1, self.last_index()));
         }
     }
 
