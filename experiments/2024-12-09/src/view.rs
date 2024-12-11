3f20187ad004f400debf711ad154d6b4585b6963
Hanno Braun <hello@hannobraun.com>
2024-12-11 21:32:50 +0100

    Prepare for follow-on change

--- experiments/2024-12-09/src/view.rs
+++ experiments/2024-12-09/src/view.rs
@@ -55,12 +55,13 @@ impl OperationView {
         selected: bool,
         indent_level: usize,
     ) -> Box<dyn Iterator<Item = (&Self, bool, usize)> + '_> {
-        Box::new(iter::once((self, selected, indent_level)).chain(
-            self.children.iter().enumerate().flat_map(move |(i, view)| {
+        let self_ = iter::once((self, selected, indent_level));
+        Box::new(self_.chain(self.children.iter().enumerate().flat_map(
+            move |(i, view)| {
                 let selected = Some(i) == self.selected;
                 view.operations_inner(selected, indent_level + 1)
-            }),
-        ))
+            },
+        )))
     }
 
     fn last_index(&self) -> usize {
