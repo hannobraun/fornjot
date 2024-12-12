788af1edc33e192d910c26849e9ea7366b531a71
Hanno Braun <hello@hannobraun.com>
2024-12-12 23:17:26 +0100

    Group related code

--- experiments/2024-12-09/src/app.rs
+++ experiments/2024-12-09/src/app.rs
@@ -99,11 +99,11 @@ impl ApplicationHandler for App {
                 }
 
                 match logical_key {
-                    Key::Named(NamedKey::ArrowDown) => {
-                        self.view.parent_of_selected_mut().select_next();
-                    }
                     Key::Named(NamedKey::ArrowRight) => {
                         self.view.selected_mut().select_last();
+                    }
+                    Key::Named(NamedKey::ArrowDown) => {
+                        self.view.parent_of_selected_mut().select_next();
                     }
                     Key::Named(NamedKey::ArrowUp) => {
                         self.view.parent_of_selected_mut().select_previous();
