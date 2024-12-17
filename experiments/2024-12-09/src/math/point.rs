71a0bb7a338025c5b3f8b960516c3f9196852ca0
Hanno Braun <hello@hannobraun.com>
2024-12-17 20:33:14 +0100

    Make `Point`/`Vector` addition generic

--- experiments/2024-12-09/src/math/point.rs
+++ experiments/2024-12-09/src/math/point.rs
@@ -18,9 +18,9 @@ where
     }
 }
 
-impl<T> ops::Add<T> for Point<3>
+impl<T, const D: usize> ops::Add<T> for Point<D>
 where
-    T: Into<Vector<3>>,
+    T: Into<Vector<D>>,
 {
     type Output = Self;
 
