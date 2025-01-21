306cae95b9150e1bb0eb2e2fc63a32a1273e1ad5
Hanno Braun <hello@hannobraun.com>
2025-01-21 20:24:54 +0100

    Update name of function parameter

--- experiments/2024-12-09/src/export.rs
+++ experiments/2024-12-09/src/export.rs
@@ -2,8 +2,8 @@ use std::{collections::BTreeMap, fs::File};
 
 use crate::geometry::Operation;
 
-pub fn export(shape: &dyn Operation) -> anyhow::Result<()> {
-    let tri_mesh = shape.tri_mesh();
+pub fn export(op: &dyn Operation) -> anyhow::Result<()> {
+    let tri_mesh = op.tri_mesh();
 
     let mut indices_by_vertex = BTreeMap::new();
 
