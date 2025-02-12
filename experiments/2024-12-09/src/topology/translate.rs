c2bf6871718136c0ac7be1f28a02041c21a9ff78
Hanno Braun <hello@hannobraun.com>
2025-02-12 20:45:02 +0100

    Prepare to add `Translate` operation

--- experiments/2024-12-09/src/topology/translate.rs
+++ experiments/2024-12-09/src/topology/translate.rs
@@ -3,17 +3,17 @@ use crate::{geometry::Handle, math::Vector};
 use super::face::Face;
 
 pub trait TranslateExt {
-    fn translate(self, offset: impl Into<Vector<3>>) -> Face;
+    fn translate(self, offset: impl Into<Vector<3>>) -> Handle<Face>;
 }
 
 impl TranslateExt for Handle<Face> {
-    fn translate(self, offset: impl Into<Vector<3>>) -> Face {
+    fn translate(self, offset: impl Into<Vector<3>>) -> Handle<Face> {
         let offset = offset.into();
 
-        Face::new(
+        Handle::new(Face::new(
             self.surface().translate(offset),
             self.vertices()
                 .map(|vertex| Handle::new(vertex.translate(offset))),
-        )
+        ))
     }
 }
