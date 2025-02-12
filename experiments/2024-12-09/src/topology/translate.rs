ab0308ad804bb695b0cd62ea8b5b062ecc50baef
Hanno Braun <hello@hannobraun.com>
2025-02-12 20:52:41 +0100

    Make `TranslateExt` implementation more flexible

--- experiments/2024-12-09/src/topology/translate.rs
+++ experiments/2024-12-09/src/topology/translate.rs
@@ -11,13 +11,17 @@ pub trait TranslateExt {
     fn translate(self, offset: impl Into<Vector<3>>) -> Translate;
 }
 
-impl TranslateExt for Handle<Face> {
+impl<T> TranslateExt for &T
+where
+    T: OperationOutput<Face>,
+{
     fn translate(self, offset: impl Into<Vector<3>>) -> Translate {
         let offset = offset.into();
 
         let output = Handle::new(Face::new(
-            self.surface().translate(offset),
-            self.vertices()
+            self.output().surface().translate(offset),
+            self.output()
+                .vertices()
                 .map(|vertex| Handle::new(vertex.translate(offset))),
         ));
 
