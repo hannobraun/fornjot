c25d1167e0bc33e038c1b4916c40844c4f8fca35
Hanno Braun <hello@hannobraun.com>
2025-02-12 20:32:26 +0100

    Prepare to add `Flip` operation

--- experiments/2024-12-09/src/topology/flip.rs
+++ experiments/2024-12-09/src/topology/flip.rs
@@ -3,11 +3,11 @@ use crate::geometry::Handle;
 use super::face::Face;
 
 pub trait FlipExt {
-    fn flip(self) -> Face;
+    fn flip(self) -> Handle<Face>;
 }
 
 impl FlipExt for Handle<Face> {
-    fn flip(self) -> Face {
-        Face::new(self.surface().flip(), self.vertices().cloned())
+    fn flip(self) -> Handle<Face> {
+        Handle::new(Face::new(self.surface().flip(), self.vertices().cloned()))
     }
 }
