80caef1119dc3f75a6e034c75ac4f2ae6872864a
Hanno Braun <hello@hannobraun.com>
2025-02-13 19:07:49 +0100

    Move `HandleAny` to dedicated module

---
+++ experiments/2024-12-09/src/operation/handle_any.rs
@@ -0,0 +1,39 @@
+use std::{fmt, rc::Rc};
+
+use crate::geometry::TriMesh;
+
+use super::{Operation, OperationOutput};
+
+#[derive(Clone)]
+pub struct HandleAny {
+    pub(super) inner: Rc<dyn Operation>,
+}
+
+impl HandleAny {
+    pub fn new(op: impl Operation + 'static) -> Self {
+        Self { inner: Rc::new(op) }
+    }
+}
+
+impl Operation for HandleAny {
+    fn display(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.inner.display(f)?;
+        write!(f, " ({:?})", Rc::as_ptr(&self.inner))?;
+
+        Ok(())
+    }
+
+    fn tri_mesh(&self) -> TriMesh {
+        self.inner.tri_mesh()
+    }
+
+    fn children(&self) -> Vec<HandleAny> {
+        self.inner.children()
+    }
+}
+
+impl OperationOutput for HandleAny {
+    fn output(&self) -> &Self {
+        self
+    }
+}
--- experiments/2024-12-09/src/operation/mod.rs
+++ experiments/2024-12-09/src/operation/mod.rs
@@ -1,45 +1,9 @@
 mod handle;
+mod handle_any;
 mod traits;
 
 pub use self::{
     handle::Handle,
+    handle_any::HandleAny,
     traits::{Operation, OperationOutput},
 };
-
-use std::{fmt, rc::Rc};
-
-use crate::geometry::TriMesh;
-
-#[derive(Clone)]
-pub struct HandleAny {
-    inner: Rc<dyn Operation>,
-}
-
-impl HandleAny {
-    pub fn new(op: impl Operation + 'static) -> Self {
-        Self { inner: Rc::new(op) }
-    }
-}
-
-impl Operation for HandleAny {
-    fn display(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        self.inner.display(f)?;
-        write!(f, " ({:?})", Rc::as_ptr(&self.inner))?;
-
-        Ok(())
-    }
-
-    fn tri_mesh(&self) -> TriMesh {
-        self.inner.tri_mesh()
-    }
-
-    fn children(&self) -> Vec<HandleAny> {
-        self.inner.children()
-    }
-}
-
-impl OperationOutput for HandleAny {
-    fn output(&self) -> &Self {
-        self
-    }
-}
