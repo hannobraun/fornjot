c530f1520da7d158f632b59606a0e944273165ea
Hanno Braun <hello@hannobraun.com>
2025-02-18 20:20:03 +0100

    Simplify return type of `TranslateExt::translate`

--- experiments/2024-12-09/src/topology/translate.rs
+++ experiments/2024-12-09/src/topology/translate.rs
@@ -6,18 +6,18 @@ use crate::{
 use super::face::Face;
 
 pub trait TranslateExt {
-    fn translate(self, offset: impl Into<Vector<3>>) -> Handle<Face>;
+    fn translate(self, offset: impl Into<Vector<3>>) -> Face;
 }
 
 impl TranslateExt for &Face {
-    fn translate(self, offset: impl Into<Vector<3>>) -> Handle<Face> {
+    fn translate(self, offset: impl Into<Vector<3>>) -> Face {
         let offset = offset.into();
 
-        Handle::new(Face::new(
+        Face::new(
             self.output().surface().translate(offset),
             self.output()
                 .vertices()
                 .map(|vertex| Handle::new(vertex.output().translate(offset))),
-        ))
+        )
     }
 }
