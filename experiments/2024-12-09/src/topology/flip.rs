72de73c3641785de32c9ab1641c079047fa190ad
Hanno Braun <hello@hannobraun.com>
2025-02-18 20:17:12 +0100

    Remove unused code

--- experiments/2024-12-09/src/topology/flip.rs
+++ experiments/2024-12-09/src/topology/flip.rs
@@ -1,10 +1,4 @@
-use std::fmt;
-
-use crate::{
-    geometry::TriMesh,
-    operation::{Handle, HandleAny, Operation, OperationOutput},
-    topology::face::Face,
-};
+use crate::{operation::Handle, topology::face::Face};
 
 pub trait FlipExt {
     fn flip(self) -> Handle<Face>;
@@ -13,29 +7,5 @@ pub trait FlipExt {
 impl FlipExt for &Face {
     fn flip(self) -> Handle<Face> {
         Handle::new(Face::new(self.surface().flip(), self.vertices().cloned()))
-    }
-}
-
-pub struct Flip {
-    pub output: Handle<Face>,
-}
-
-impl Operation for Flip {
-    fn display(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        write!(f, "Flip")
-    }
-
-    fn tri_mesh(&self) -> TriMesh {
-        self.output.tri_mesh()
-    }
-
-    fn children(&self) -> Vec<HandleAny> {
-        vec![self.output.to_any()]
-    }
-}
-
-impl OperationOutput<Face> for Flip {
-    fn output(&self) -> &Face {
-        self.output.output()
     }
 }
