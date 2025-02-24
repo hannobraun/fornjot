58902150baa20802ff450482157a866d9d22f2f1
Hanno Braun <hello@hannobraun.com>
2025-02-24 19:19:00 +0100

    Make `TranslateExt` more flexible

--- experiments/2024-12-09/src/operations/translate.rs
+++ experiments/2024-12-09/src/operations/translate.rs
@@ -1,11 +1,11 @@
 use crate::{math::Vector, object::Handle, topology::face::Face};
 
 pub trait TranslateExt {
-    fn translate(self, offset: impl Into<Vector<3>>) -> Face;
+    fn translate(self, offset: impl Into<Vector<3>>) -> Self;
 }
 
 impl TranslateExt for Face {
-    fn translate(self, offset: impl Into<Vector<3>>) -> Face {
+    fn translate(self, offset: impl Into<Vector<3>>) -> Self {
         let offset = offset.into();
 
         Face::new(
