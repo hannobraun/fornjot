0b57144e3629d424dfe19310cb3ea3676c598367
Hanno Braun <hello@hannobraun.com>
2025-02-24 19:16:58 +0100

    Implement `Object` for `HalfEdge`

--- experiments/2024-12-09/src/topology/half_edge.rs
+++ experiments/2024-12-09/src/topology/half_edge.rs
@@ -1,4 +1,9 @@
-use crate::object::Handle;
+use std::fmt;
+
+use crate::{
+    geometry::TriMesh,
+    object::{Handle, HandleAny, Object},
+};
 
 use super::vertex::Vertex;
 
@@ -13,5 +18,19 @@ impl HalfEdge {
 
     pub fn start(&self) -> &Handle<Vertex> {
         &self.start
+    }
+}
+
+impl Object for HalfEdge {
+    fn display(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        write!(f, "HalfEdge")
+    }
+
+    fn tri_mesh(&self) -> TriMesh {
+        TriMesh::new()
+    }
+
+    fn children(&self) -> Vec<HandleAny> {
+        vec![self.start.to_any()]
     }
 }
