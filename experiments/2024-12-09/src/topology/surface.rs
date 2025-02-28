e593aa976010531e089bc8906091c4ca2b950a77
Hanno Braun <hello@hannobraun.com>
2025-02-28 21:10:37 +0100

    Prepare for follow-on change

--- experiments/2024-12-09/src/topology/surface.rs
+++ experiments/2024-12-09/src/topology/surface.rs
@@ -1,6 +1,15 @@
+use std::fmt;
+
 use crate::math::Plane;
 
-#[derive(Debug)]
 pub struct Surface {
     pub geometry: Plane,
+}
+
+impl fmt::Debug for Surface {
+    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
+        f.debug_struct("Surface")
+            .field("geometry", &self.geometry)
+            .finish()
+    }
 }
