3af81be9556c6d4c0655a99740cd592d230f1688
Hanno Braun <hello@hannobraun.com>
2025-03-21 19:45:27 +0100

    Remove unused code

--- crates/fj-interop/src/ext.rs
+++ crates/fj-interop/src/ext.rs
@@ -2,11 +2,6 @@
 
 /// Extension trait for arrays
 pub trait ArrayExt<T, const N: usize> {
-    /// Stable replacement for `each_mut`
-    ///
-    /// <https://doc.rust-lang.org/std/primitive.array.html#method.each_mut>
-    fn each_mut_ext(&mut self) -> [&mut T; N];
-
     /// Stable replacement for `try_map`
     ///
     /// <https://doc.rust-lang.org/std/primitive.array.html#method.try_map>
@@ -21,11 +16,6 @@ pub trait ArrayExt<T, const N: usize> {
 }
 
 impl<T> ArrayExt<T, 2> for [T; 2] {
-    fn each_mut_ext(&mut self) -> [&mut T; 2] {
-        let [a, b] = self;
-        [a, b]
-    }
-
     fn try_map_ext<F, U, E>(self, f: F) -> Result<[U; 2], E>
     where
         F: FnMut(T) -> Result<U, E>,
@@ -42,11 +32,6 @@ impl<T> ArrayExt<T, 2> for [T; 2] {
 }
 
 impl<T> ArrayExt<T, 3> for [T; 3] {
-    fn each_mut_ext(&mut self) -> [&mut T; 3] {
-        let [a, b, c] = self;
-        [a, b, c]
-    }
-
     fn try_map_ext<F, U, E>(self, f: F) -> Result<[U; 3], E>
     where
         F: FnMut(T) -> Result<U, E>,
@@ -63,11 +48,6 @@ impl<T> ArrayExt<T, 3> for [T; 3] {
 }
 
 impl<T> ArrayExt<T, 4> for [T; 4] {
-    fn each_mut_ext(&mut self) -> [&mut T; 4] {
-        let [a, b, c, d] = self;
-        [a, b, c, d]
-    }
-
     fn try_map_ext<F, U, E>(self, f: F) -> Result<[U; 4], E>
     where
         F: FnMut(T) -> Result<U, E>,
