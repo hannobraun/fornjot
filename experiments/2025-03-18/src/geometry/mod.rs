8b6a34d7aa410af37b8f181a5adce29223d2b3fd
Hanno Braun <hello@hannobraun.com>
2025-03-21 22:59:16 +0100

    Remove redundant module

--- experiments/2025-03-18/src/geometry/mod.rs
+++ experiments/2025-03-18/src/geometry/mod.rs
@@ -1,11 +1,11 @@
 mod sketch;
 mod surface;
 mod tri_mesh;
-mod triangle;
 
 pub use self::{
     sketch::Sketch,
     surface::SurfaceGeometry,
     tri_mesh::{MeshTriangle, ToTriMesh, TriMesh},
-    triangle::Triangle,
 };
+
+pub use fj_math::Triangle;
