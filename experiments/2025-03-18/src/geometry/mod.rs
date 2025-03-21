0a94adf4ea7bfd0025c4baa80cbd9b3c5cafdc0f
Hanno Braun <hello@hannobraun.com>
2025-03-21 23:10:12 +0100

    Remove redundant re-export

--- experiments/2025-03-18/src/geometry/mod.rs
+++ experiments/2025-03-18/src/geometry/mod.rs
@@ -2,8 +2,6 @@ mod sketch;
 mod surface;
 mod tri_mesh;
 
-pub use self::{
-    sketch::Sketch,
-    surface::SurfaceGeometry,
-    tri_mesh::{ToTriMesh, TriMesh},
-};
+pub use self::{sketch::Sketch, surface::SurfaceGeometry, tri_mesh::ToTriMesh};
+
+pub use fj_interop::TriMesh;
--- experiments/2025-03-18/src/geometry/tri_mesh.rs
+++ experiments/2025-03-18/src/geometry/tri_mesh.rs
@@ -1,4 +1,4 @@
-pub use fj_interop::TriMesh;
+use fj_interop::TriMesh;
 
 pub trait ToTriMesh {
     fn to_tri_mesh(&self) -> TriMesh;
