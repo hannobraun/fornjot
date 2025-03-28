e86b14ef0cdc9343f0a082ba80704093e22d137c
Hanno Braun <hello@hannobraun.com>
2025-03-28 20:04:26 +0100

    Prepare to display intermediate result

--- experiments/2025-03-18/src/main.rs
+++ experiments/2025-03-18/src/main.rs
@@ -9,12 +9,13 @@ mod operations;
 mod topology;
 
 fn main() -> anyhow::Result<()> {
-    let tri_mesh = model::model();
+    let tri_mesh = fj_viewer::make_viewer_and_spawn_thread(|viewer| {
+        let tri_mesh = model::model();
+        viewer.display(tri_mesh.clone());
+        tri_mesh
+    })?;
 
     fj_export::export(&tri_mesh, "output.3mf")?;
-    fj_viewer::make_viewer_and_spawn_thread(|viewer| {
-        viewer.display(tri_mesh);
-    })?;
 
     Ok(())
 }
