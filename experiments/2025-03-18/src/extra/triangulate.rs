ec1cf342e35ed5faf76cd35a947f6a4f1ed3e25e
Hanno Braun <hello@hannobraun.com>
2025-04-03 13:54:49 +0200

    Update formatting

--- experiments/2025-03-18/src/extra/triangulate.rs
+++ experiments/2025-03-18/src/extra/triangulate.rs
@@ -47,21 +47,21 @@ fn half_edges_to_points(face: &Face, target: &mut Vec<TriangulationPoint>) {
         let point_global = half_edge.start.point;
 
         // Here, we project a 3D point (from the vertex) into the face's
-        // surface, creating a 2D point. Through the surface, this 2D point
-        // has a position in 3D space.
+        // surface, creating a 2D point. Through the surface, this 2D point has
+        // a position in 3D space.
         //
         // But this position isn't necessarily going to be the same as the
         // position of the original 3D point, due to numerical inaccuracy.
         //
-        // This doesn't matter. Neither does the fact, that other faces
-        // might share the same vertices and project them into their own
-        // surfaces, creating more redundancy.
+        // This doesn't matter. Neither does the fact, that other faces might
+        // share the same vertices and project them into their own surfaces,
+        // creating more redundancy.
         //
         // The reason that it doesn't, is that we're using the projected 2D
-        // points _only_ for this local triangulation. Once that tells us
-        // how the different 3D points must connect, we use the original 3D
-        // points to build those triangles. We never convert the 2D points
-        // back into 3D.
+        // points _only_ for this local triangulation. Once that tells us how
+        // the different 3D points must connect, we use the original 3D points
+        // to build those triangles. We never convert the 2D points back into
+        // 3D.
         let point_surface = face.surface.geometry.project_point(point_global);
 
         TriangulationPoint {
