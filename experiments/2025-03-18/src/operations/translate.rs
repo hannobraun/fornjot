9de2ff48aaa424ff2b610a0339814554f7e0c7ab
Hanno Braun <hello@hannobraun.com>
2025-04-04 11:58:03 +0200

    Add placeholder impl for translating curves

--- experiments/2025-03-18/src/operations/translate.rs
+++ experiments/2025-03-18/src/operations/translate.rs
@@ -3,12 +3,27 @@ use fj_math::Vector;
 use crate::{
     handle::Handle,
     topology::{
-        face::Face, half_edge::HalfEdge, surface::Surface, vertex::Vertex,
+        curve::Curve, face::Face, half_edge::HalfEdge, surface::Surface,
+        vertex::Vertex,
     },
 };
 
 pub trait TranslateExt {
     fn translate(&self, offset: impl Into<Vector<3>>) -> Self;
+}
+
+impl TranslateExt for Curve {
+    fn translate(&self, _: impl Into<Vector<3>>) -> Self {
+        // Right now, `Curve` is a placeholder, and there's no need to do
+        // anything here. It's possible, that it will end up being defined
+        // locally on a surface, and then this will remain too.
+        //
+        // The other scenario is that curves end up being defined globally, in
+        // 3D space, which means we have to do the actual translation here.
+        // We'll see how it shakes out.
+        let Curve {} = self;
+        Curve {}
+    }
 }
 
 impl TranslateExt for Face {
@@ -29,11 +44,11 @@ impl TranslateExt for HalfEdge {
     fn translate(&self, offset: impl Into<Vector<3>>) -> Self {
         let offset = offset.into();
 
-        let curve = self.curve.clone();
+        let curve = self.curve.translate(offset);
         let start = self.start.translate(offset);
 
         HalfEdge {
-            curve,
+            curve: Handle::new(curve),
             start: Handle::new(start),
             is_internal: self.is_internal,
         }
