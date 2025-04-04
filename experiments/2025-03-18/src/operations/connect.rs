a2acf031b08b8f04a53ee2fcaacff6ac977f3526
Hanno Braun <hello@hannobraun.com>
2025-04-04 12:32:09 +0200

    Make name of variable more explicit

--- experiments/2025-03-18/src/operations/connect.rs
+++ experiments/2025-03-18/src/operations/connect.rs
@@ -54,7 +54,7 @@ fn build_connecting_faces(bottom: &Face, top: &Face) -> Vec<Handle<Face>> {
     bottom
         .half_edges_with_end_vertex()
         .zip(top.half_edges_with_end_vertex())
-        .map(|((bottom_a, bottom_b), (d, c))| {
+        .map(|((bottom_a, bottom_b), (d, top_a))| {
             let is_internal = match [bottom_a.is_internal, d.is_internal] {
                 [true, true] => true,
                 [false, false] => false,
@@ -68,12 +68,13 @@ fn build_connecting_faces(bottom: &Face, top: &Face) -> Vec<Handle<Face>> {
 
             let surface = Handle::new(Surface {
                 geometry: Box::new(Plane::from_points(
-                    [&bottom_a.start, bottom_b, c].map(|vertex| vertex.point),
+                    [&bottom_a.start, bottom_b, top_a]
+                        .map(|vertex| vertex.point),
                 )),
             });
             let face = Face::new(
                 surface,
-                [&bottom_a.start, bottom_b, c, &d.start].map(|vertex| {
+                [&bottom_a.start, bottom_b, top_a, &d.start].map(|vertex| {
                     Handle::new(HalfEdge {
                         curve: Handle::new(Curve {}),
                         start: vertex.clone(),
