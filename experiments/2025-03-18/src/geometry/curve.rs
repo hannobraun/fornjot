6e805dd02e3b16e77e0343ff47a3656e8735963a
Hanno Braun <hello@hannobraun.com>
2025-04-07 13:09:02 +0200

    Remove redundant placeholder implementation

--- experiments/2025-03-18/src/geometry/curve.rs
+++ experiments/2025-03-18/src/geometry/curve.rs
@@ -4,12 +4,6 @@ pub trait CurveGeometry {
     fn translate(&self, offset: Vector<3>) -> Box<dyn CurveGeometry>;
 }
 
-impl CurveGeometry for () {
-    fn translate(&self, _: Vector<3>) -> Box<dyn CurveGeometry> {
-        Box::new(())
-    }
-}
-
 impl CurveGeometry for Line<3> {
     fn translate(&self, offset: Vector<3>) -> Box<dyn CurveGeometry> {
         let translated = self.transform(&Transform::translation(offset));
