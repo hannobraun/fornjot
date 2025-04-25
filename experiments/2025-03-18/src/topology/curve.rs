d5cb2ce685d2565df29cff5a85ee2b4ff77a7ac6
Hanno Braun <hello@hannobraun.com>
2025-04-25 11:51:11 +0200

    Split expression into simpler ones

--- experiments/2025-03-18/src/topology/curve.rs
+++ experiments/2025-03-18/src/topology/curve.rs
@@ -9,9 +9,7 @@ pub struct Curve {
 impl Curve {
     pub fn line_from_vertices(vertices: [&Vertex; 2]) -> Self {
         let points = vertices.map(|vertex| vertex.point);
-
-        Self {
-            geometry: AnchoredCurve::line_from_points(points),
-        }
+        let geometry = AnchoredCurve::line_from_points(points);
+        Self { geometry }
     }
 }
