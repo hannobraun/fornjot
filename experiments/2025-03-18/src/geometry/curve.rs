f2fe5d42f93fbeaab4dddf6d5d51002ccfe48a7f
Hanno Braun <hello@hannobraun.com>
2025-04-28 14:01:33 +0200

    Simplify

--- experiments/2025-03-18/src/geometry/curve.rs
+++ experiments/2025-03-18/src/geometry/curve.rs
@@ -124,13 +124,13 @@ impl CurveGeometry for Line {
     }
 
     fn vector_from_local_point(&self, point: Point<1>) -> Vector<3> {
-        let line = *self;
+        let line = self;
 
         line.vector_from_local_point(point)
     }
 
     fn project_vector(&self, vector: Vector<3>) -> Point<1> {
-        let line = *self;
+        let line = self;
 
         line.project_vector(vector)
     }
