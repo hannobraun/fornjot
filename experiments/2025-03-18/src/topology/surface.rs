aa4159cdfccccfd946ba388b32be2efa09227e91
Hanno Braun <hello@hannobraun.com>
2025-05-07 13:19:02 +0200

    Improve `Debug` output of `topology::Surface`

--- experiments/2025-03-18/src/topology/surface.rs
+++ experiments/2025-03-18/src/topology/surface.rs
@@ -1,15 +1,6 @@
-use std::fmt;
-
 use crate::geometry::SurfaceGeometry;
 
+#[derive(Debug)]
 pub struct Surface {
     pub geometry: Box<dyn SurfaceGeometry>,
-}
-
-impl fmt::Debug for Surface {
-    fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.debug_struct("Surface")
-            .field("geometry", &"Box<dyn SurfaceGeometry>")
-            .finish()
-    }
 }
