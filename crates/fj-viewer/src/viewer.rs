e83540ffdb2c9957f3d05da455e2a408306215a7
Hanno Braun <hello@hannobraun.com>
2025-05-16 12:29:50 +0200

    Update name of function

--- crates/fj-viewer/src/viewer.rs
+++ crates/fj-viewer/src/viewer.rs
@@ -64,7 +64,7 @@ pub struct Viewer {
 
 impl Viewer {
     /// # Display a triangle mesh in a new window
-    pub fn display_mesh(&self, tri_mesh: TriMesh) {
+    pub fn display_model(&self, tri_mesh: TriMesh) {
         // If there's an error, that means the display thread has closed down
         // and we're on our way to shutting down as well. I don't think there's
         // much we can do about that.
--- crates/fj/src/instance.rs
+++ crates/fj/src/instance.rs
@@ -89,7 +89,7 @@ impl Instance {
         }
 
         make_viewer_and_spawn_thread(|viewer| {
-            viewer.display_mesh(tri_mesh);
+            viewer.display_model(tri_mesh);
         })?;
 
         Ok(())
