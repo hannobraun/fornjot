0bcc5ff5f990b563664f2a18220a293e97032641
Hanno Braun <hello@hannobraun.com>
2025-06-06 08:08:53 +0200

    Prepare to support surface triangulation

--- experiments/2025-03-18/src/extra/triangulate/surface.rs
+++ experiments/2025-03-18/src/extra/triangulate/surface.rs
@@ -1,13 +1,16 @@
 use fj_interop::Tolerance;
 use fj_math::{Aabb, Point};
 
-use crate::topology::surface::Surface;
+use crate::{
+    extra::triangulate::{TriangulationPoint, delaunay::triangles},
+    topology::surface::Surface,
+};
 
 pub fn triangulate_surface(
     surface: &Surface,
     boundary: &Aabb<2>,
     _: impl Into<Tolerance>,
-) -> Vec<Point<2>> {
+) -> (Vec<Point<2>>, Vec<[TriangulationPoint; 3]>) {
     let mut points = surface.geometry.approximate(boundary);
 
     let boundary_points = {
@@ -25,5 +28,16 @@ pub fn triangulate_surface(
 
     points.extend(boundary_points);
 
-    points
+    let triangles = triangles(
+        [],
+        points.iter().copied().map(|point_surface| {
+            let point_global = surface.geometry.point_from_local(point_surface);
+            TriangulationPoint {
+                point_surface,
+                point_global,
+            }
+        }),
+    );
+
+    (points, triangles)
 }
