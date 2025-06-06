1d2a6d14588964215325fe2d266263c9d591e98a
Hanno Braun <hello@hannobraun.com>
2025-06-06 07:54:33 +0200

    Prepare to support surface triangulation

--- experiments/2025-03-18/src/extra/triangulate/surface.rs
+++ experiments/2025-03-18/src/extra/triangulate/surface.rs
@@ -8,5 +8,22 @@ pub fn triangulate_surface(
     boundary: &Aabb<2>,
     _: impl Into<Tolerance>,
 ) -> Vec<Point<2>> {
-    surface.geometry.approximate(boundary)
+    let mut points = surface.geometry.approximate(boundary);
+
+    let boundary_points = {
+        let [[min_u, min_v], [max_u, max_v]] =
+            [boundary.min, boundary.max].map(|point| point.coords.components);
+
+        [
+            [min_u, min_v],
+            [min_u, max_v],
+            [max_u, min_v],
+            [max_u, max_v],
+        ]
+        .map(Point::from)
+    };
+
+    points.extend(boundary_points);
+
+    points
 }
