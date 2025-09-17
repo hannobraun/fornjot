6596a06513534a6cd8da05c35a3696897fe00326
Hanno Braun <hello@hannobraun.com>
2025-09-17 13:26:11 +0200

    Document `HalfEdgeApprox`

--- experiments/2025-03-18/src/approx/half_edge.rs
+++ experiments/2025-03-18/src/approx/half_edge.rs
@@ -1,5 +1,14 @@
 use fj_math::Point;
 
+/// # The approximation of a half-edge
+///
+/// The approximation of a half-edge is the approximation of its curve within
+/// the boundary defined by the half-edge's start and end vertices, plus the
+/// position of the start vertex.
+///
+/// By including the start vertex and not the end vertex, a whole chain of
+/// half-edges can be approximated by simply appending the approximations of
+/// each half-edge, without the necessity of any deduplication of points.
 pub struct HalfEdgeApprox {
     pub points: Vec<Point<3>>,
 }
