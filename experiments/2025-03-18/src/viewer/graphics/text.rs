40d736b118bf1c4a45b2c6c444de56e05d972caa
Hanno Braun <hello@hannobraun.com>
2025-09-22 11:06:02 +0200

    Implement parts of text rendering
    This doesn't do anything yet, but it all seems to be stuff that needs to
    happen for text to show up on the screen.

--- experiments/2025-03-18/src/viewer/graphics/text.rs
+++ experiments/2025-03-18/src/viewer/graphics/text.rs
@@ -1,18 +1,52 @@
-pub struct TextRenderer {}
+pub struct TextRenderer {
+    text_atlas: glyphon::TextAtlas,
+    viewport: glyphon::Viewport,
+    text_renderer: glyphon::TextRenderer,
+}
 
 impl TextRenderer {
     pub fn new(
-        _: &wgpu::Device,
-        _: &wgpu::Queue,
-        _: wgpu::TextureFormat,
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
+        color_format: wgpu::TextureFormat,
     ) -> Self {
-        Self {}
+        let cache = glyphon::Cache::new(device);
+
+        let mut text_atlas =
+            glyphon::TextAtlas::new(device, queue, &cache, color_format);
+
+        let viewport = glyphon::Viewport::new(device, &cache);
+
+        let multisample_state = wgpu::MultisampleState {
+            count: 1,
+            mask: !0,
+            alpha_to_coverage_enabled: false,
+        };
+        let depth_stencil = None;
+        let text_renderer = glyphon::TextRenderer::new(
+            &mut text_atlas,
+            device,
+            multisample_state,
+            depth_stencil,
+        );
+
+        Self {
+            text_atlas,
+            viewport,
+            text_renderer,
+        }
     }
 
     pub fn draw(
         &mut self,
-        _: &mut wgpu::RenderPass,
+        render_pass: &mut wgpu::RenderPass,
     ) -> Result<(), glyphon::RenderError> {
+        self.text_renderer.render(
+            &self.text_atlas,
+            &self.viewport,
+            render_pass,
+        )?;
+
         Ok(())
     }
 }
