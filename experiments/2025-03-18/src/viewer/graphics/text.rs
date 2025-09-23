465291385d00692cb50c17e77c681d98f9f600df
Hanno Braun <hello@hannobraun.com>
2025-09-23 11:03:31 +0200

    Implement more parts of text rendering

--- experiments/2025-03-18/src/viewer/graphics/text.rs
+++ experiments/2025-03-18/src/viewer/graphics/text.rs
@@ -1,6 +1,10 @@
+use glyphon::FontSystem;
+
 pub struct TextRenderer {
+    font_system: glyphon::FontSystem,
     text_atlas: glyphon::TextAtlas,
     viewport: glyphon::Viewport,
+    swash_cache: glyphon::SwashCache,
     text_renderer: glyphon::TextRenderer,
 }
 
@@ -12,9 +16,11 @@ impl TextRenderer {
     ) -> Self {
         let cache = glyphon::Cache::new(device);
 
+        let font_system = FontSystem::new();
         let mut text_atlas =
             glyphon::TextAtlas::new(device, queue, &cache, color_format);
         let viewport = glyphon::Viewport::new(device, &cache);
+        let swash_cache = glyphon::SwashCache::new();
 
         let multisample_state = wgpu::MultisampleState {
             count: 1,
@@ -30,18 +36,29 @@ impl TextRenderer {
         );
 
         Self {
+            font_system,
             text_atlas,
             viewport,
+            swash_cache,
             text_renderer,
         }
     }
 
     pub fn draw(
         &mut self,
-        _: &wgpu::Device,
-        _: &wgpu::Queue,
+        device: &wgpu::Device,
+        queue: &wgpu::Queue,
         render_pass: &mut wgpu::RenderPass,
     ) -> Result<(), TextDrawError> {
+        self.text_renderer.prepare(
+            device,
+            queue,
+            &mut self.font_system,
+            &mut self.text_atlas,
+            &self.viewport,
+            [],
+            &mut self.swash_cache,
+        )?;
         self.text_renderer.render(
             &self.text_atlas,
             &self.viewport,
@@ -54,6 +71,9 @@ impl TextRenderer {
 
 #[derive(Debug, thiserror::Error)]
 pub enum TextDrawError {
+    #[error(transparent)]
+    Prepare(#[from] glyphon::PrepareError),
+
     #[error(transparent)]
     Render(#[from] glyphon::RenderError),
 }
