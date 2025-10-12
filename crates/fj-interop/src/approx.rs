40487bebbbf2a03f586bf8b0167b13ed409c5b42
Hanno Braun <hello@hannobraun.com>
2025-10-12 11:09:06 +0200

    Amend comment

--- crates/fj-interop/src/approx.rs
+++ crates/fj-interop/src/approx.rs
@@ -46,6 +46,9 @@ impl CircleApproxParams {
 
         // We can't generate a point exactly at the boundaries of the range as
         // part of the approximation. Make sure we stay inside the range.
+        //
+        // `min` and `max` are in units of the increment, so adding or
+        // subtracting `1` adds or substracts one increment.
         let min = min.floor() + 1.;
         let max = max.ceil() - 1.;
 
