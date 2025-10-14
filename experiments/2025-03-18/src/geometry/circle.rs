c7b58e84f9e22bd1f305def285157554a0b9e054
Hanno Braun <hello@hannobraun.com>
2025-10-14 13:20:12 +0200

    Copy `CircleApproxParams` from `fj-interop`
    I'm currently experimenting with some changes I'd like to make on it.
    This will be much more complicated to do with a type shared between
    experimental and mainline code.

--- experiments/2025-03-18/src/geometry/circle.rs
+++ experiments/2025-03-18/src/geometry/circle.rs
@@ -1,5 +1,7 @@
-use fj_interop::{CircleApproxParams, Tolerance};
-use fj_math::{Point, Scalar, Vector};
+use std::iter;
+
+use fj_interop::Tolerance;
+use fj_math::{Point, Scalar, Sign, Vector};
 
 use crate::{approx::curve::CurveApprox, geometry::curve::CurveGeometry};
 
@@ -71,6 +73,73 @@ impl CurveGeometry for Circle {
             .collect();
 
         CurveApprox { curvature }
+    }
+}
+
+/// # Approximation parameters for a circle
+#[derive(Debug)]
+pub struct CircleApproxParams {
+    increment: Scalar,
+}
+
+impl CircleApproxParams {
+    /// # Compute the approximation parameters for a given circle and tolerance
+    pub fn new(
+        radius: impl Into<Scalar>,
+        tolerance: impl Into<Tolerance>,
+    ) -> Self {
+        let num_vertices_to_approx_full_circle = Scalar::max(
+            Scalar::PI
+                / (Scalar::ONE - (tolerance.into().inner() / radius)).acos(),
+            3.,
+        )
+        .ceil();
+
+        let increment = Scalar::TAU / num_vertices_to_approx_full_circle;
+
+        Self { increment }
+    }
+
+    /// # Generate points to approximate the circle within a given boundary
+    pub fn approx_circle(
+        &self,
+        boundary: [Point<1>; 2],
+    ) -> impl Iterator<Item = Point<1>> + '_ {
+        // The boundary, in units of the increment.
+        let [a, b] = boundary.map(|point| point.t / self.increment);
+
+        let direction = (b - a).sign();
+        let [min, max] = if a < b { [a, b] } else { [b, a] };
+
+        // We can't generate a point exactly at the boundaries of the range as
+        // part of the approximation. Make sure we stay inside the range.
+        //
+        // `min` and `max` are in units of the increment, so adding or
+        // subtracting `1` adds or subtracts one increment.
+        let min = min.floor() + 1.;
+        let max = max.ceil() - 1.;
+
+        let [start, end] = match direction {
+            Sign::Negative => [max, min],
+            Sign::Positive | Sign::Zero => [min, max],
+        };
+
+        let mut i = start;
+        iter::from_fn(move || {
+            let is_finished = match direction {
+                Sign::Negative => i < end,
+                Sign::Positive | Sign::Zero => i > end,
+            };
+
+            if is_finished {
+                return None;
+            }
+
+            let t = self.increment * i;
+            i += direction.to_scalar();
+
+            Some(Point::from([t]))
+        })
     }
 }
 
