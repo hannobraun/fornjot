009eed8e8af729431220650587d7c7496a2a27fb
Hanno Braun <hello@hannobraun.com>
2025-11-09 10:34:00 +0100

    Add `Aabb`

--- experiments/2025-11-07/src/math.rs
+++ experiments/2025-11-07/src/math.rs
@@ -7,3 +7,9 @@ pub struct Point<const N: usize> {
 pub struct Scalar {
     pub value: f64,
 }
+
+/// # An axis-aligned bounding box
+pub struct Aabb<const D: usize> {
+    pub min: Point<D>,
+    pub max: Point<D>,
+}
