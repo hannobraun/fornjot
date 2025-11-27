86cd7c4d04f54db4ca84bc1decf4d7a4e4025c4d
Hanno Braun <hello@hannobraun.com>
2025-11-27 13:03:54 +0100

    Prepare to define model

--- experiments/2025-11-07/src/main.rs
+++ experiments/2025-11-07/src/main.rs
@@ -1,7 +1,10 @@
-use fj::math::Point;
+use fj_interop::TriMesh;
 
 fn main() -> anyhow::Result<()> {
-    let point = Point::from([0.]);
-    println!("{point:?}");
+    let tri_mesh =
+        fj::viewer::make_viewer_and_spawn_thread(|_viewer| TriMesh::new())?;
+
+    fj::export::export(tri_mesh.external_triangles(), "output.3mf")?;
+
     Ok(())
 }
