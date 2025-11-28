34ca41f2449f855437023850c475c34be592fb42
Hanno Braun <hello@hannobraun.com>
2025-11-28 11:25:11 +0100

    Initialize `DebugWindow` instance

--- experiments/2025-11-07/src/main.rs
+++ experiments/2025-11-07/src/main.rs
@@ -1,8 +1,11 @@
+use fj::debug::DEBUG_WINDOW;
 use fj_interop::TriMesh;
 
 fn main() -> anyhow::Result<()> {
-    let tri_mesh =
-        fj::viewer::make_viewer_and_spawn_thread(|_viewer| TriMesh::new())?;
+    let tri_mesh = fj::viewer::make_viewer_and_spawn_thread(|viewer| {
+        DEBUG_WINDOW.initialize(&viewer);
+        TriMesh::new()
+    })?;
 
     fj::export::export(tri_mesh.external_triangles(), "output.3mf")?;
 
