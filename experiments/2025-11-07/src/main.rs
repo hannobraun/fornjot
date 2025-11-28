4f0ebbc845cd935c84b5a04fddefdb7a50b7203d
Hanno Braun <hello@hannobraun.com>
2025-11-28 11:29:05 +0100

    Define triangle

--- experiments/2025-11-07/src/main.rs
+++ experiments/2025-11-07/src/main.rs
@@ -1,5 +1,6 @@
 use fj::{debug::DEBUG_WINDOW, viewer::ViewerHandle};
-use fj_interop::TriMesh;
+use fj_interop::{Color, MeshTriangle, TriMesh};
+use fj_math::Triangle;
 
 fn main() -> anyhow::Result<()> {
     let tri_mesh = fj::viewer::make_viewer_and_spawn_thread(|viewer| {
@@ -13,5 +14,15 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn model(_: &ViewerHandle) -> TriMesh {
-    TriMesh::new()
+    TriMesh {
+        triangles: vec![MeshTriangle {
+            inner: Triangle::from_points([
+                [0., 0., 0.],
+                [1., 0., 0.],
+                [0., 1., 0.],
+            ]),
+            is_internal: false,
+            color: Color::default(),
+        }],
+    }
 }
