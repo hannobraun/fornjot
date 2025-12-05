fd6680fe3887a4a713de8af75d7348fb0e854d03
Hanno Braun <hello@hannobraun.com>
2025-12-05 12:55:16 +0100

    Prepare to add geometry incrementally

--- experiments/2025-12-03/src/main.rs
+++ experiments/2025-12-03/src/main.rs
@@ -14,37 +14,36 @@ fn main() -> anyhow::Result<()> {
 }
 
 fn model() -> TriMesh {
-    let vertices = [
-        [0., 0., 0.], // v0
-        [0., 0., 1.], // v1
-        [0., 1., 0.], // v2
-        [0., 1., 1.], // v3
-        [1., 0., 0.], // v4
-        [1., 0., 1.], // v5
-        [1., 1., 0.], // v6
-        [1., 1., 1.], // v7
-    ];
+    let mut vertices = Vec::new();
+    let mut triangles = Vec::new();
 
-    let triangles = [
-        // front
-        [0, 4, 5], // t0
-        [0, 5, 1], // t1
-        // right
-        [4, 6, 7], // t2
-        [4, 7, 5], // t3
-        // back
-        [6, 2, 3], // t4
-        [6, 3, 7], // t5
-        // left
-        [2, 0, 1], // t6
-        [2, 1, 3], // t7
-        // bottom
-        [2, 6, 4], // t8
-        [2, 4, 0], // t9
-        // top
-        [1, 5, 7], // t10
-        [1, 7, 3], // t11
-    ];
+    vertices.push([0., 0., 0.]); // v0
+    vertices.push([0., 0., 1.]); // v1
+    vertices.push([0., 1., 0.]); // v2
+    vertices.push([0., 1., 1.]); // v3
+    vertices.push([1., 0., 0.]); // v4
+    vertices.push([1., 0., 1.]); // v5
+    vertices.push([1., 1., 0.]); // v6
+    vertices.push([1., 1., 1.]); // v7
+
+    // front
+    triangles.push([0, 4, 5]); // t0
+    triangles.push([0, 5, 1]); // t1
+    // right
+    triangles.push([4, 6, 7]); // t2
+    triangles.push([4, 7, 5]); // t3
+    // back
+    triangles.push([6, 2, 3]); // t4
+    triangles.push([6, 3, 7]); // t5
+    // left
+    triangles.push([2, 0, 1]); // t6
+    triangles.push([2, 1, 3]); // t7
+    // bottom
+    triangles.push([2, 6, 4]); // t8
+    triangles.push([2, 4, 0]); // t9
+    // top
+    triangles.push([1, 5, 7]); // t10
+    triangles.push([1, 7, 3]); // t11
 
     let mut tri_mesh = TriMesh::new();
 
