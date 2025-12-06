95c0e260061214f6f9a7ca1d7445346986f053de
Hanno Braun <hello@hannobraun.com>
2025-12-06 12:27:38 +0100

    Update name of function parameter

--- experiments/2025-12-03/src/main.rs
+++ experiments/2025-12-03/src/main.rs
@@ -122,18 +122,18 @@ pub fn sweep_edge_to_face(
     path: impl Into<Vector<3>>,
     vertices: &mut Store<Vertex>,
     triangles: &mut Store<Triangle>,
-    edges: &mut Store<HalfEdge>,
+    half_edges: &mut Store<HalfEdge>,
     faces: &mut Store<Face>,
 ) -> Index<Face> {
     let path = path.into();
 
-    let [v0, v1] = edges[e0].vertices;
+    let [v0, v1] = half_edges[e0].vertices;
 
     let [e3, e1] = [v0, v1]
-        .map(|vertex| sweep_vertex_to_edge(vertex, path, vertices, edges));
-    let [v3, v2] = [e3, e1].map(|edge| edges[edge].vertices[1]);
+        .map(|vertex| sweep_vertex_to_edge(vertex, path, vertices, half_edges));
+    let [v3, v2] = [e3, e1].map(|edge| half_edges[edge].vertices[1]);
 
-    let e2 = edges.push(HalfEdge { vertices: [v2, v3] });
+    let e2 = half_edges.push(HalfEdge { vertices: [v2, v3] });
     let _ = e2;
 
     triangles.push([v0, v1, v2]);
