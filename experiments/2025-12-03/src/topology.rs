460f2d59590782400ba03589c8c4d7701e798ffe
Hanno Braun <hello@hannobraun.com>
2025-12-06 12:48:10 +0100

    Derive `Ord` for topology types

--- experiments/2025-12-03/src/topology.rs
+++ experiments/2025-12-03/src/topology.rs
@@ -1,11 +1,11 @@
 use crate::{geometry::Vertex, store::Index};
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, Ord, PartialOrd, PartialEq)]
 pub struct HalfEdge {
     pub vertices: [Index<Vertex>; 2],
 }
 
-#[derive(Debug, Eq, PartialEq)]
+#[derive(Debug, Eq, Ord, PartialOrd, PartialEq)]
 pub struct Face {
     pub boundary: [Index<HalfEdge>; 4],
 }
