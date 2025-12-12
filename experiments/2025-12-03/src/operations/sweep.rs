9421c097b6446a337c66974b05e25126105e57d6
Hanno Braun <hello@hannobraun.com>
2025-12-12 13:56:52 +0100

    Group related code

--- experiments/2025-12-03/src/operations/sweep.rs
+++ experiments/2025-12-03/src/operations/sweep.rs
@@ -101,12 +101,11 @@ pub fn face_to_solid(
         top_edges_for_top.map(|e| reverse::half_edge(e, half_edges));
     let [e54, e65, e76, e47] = top_edges_for_sides;
 
-    let [e04, e15, e26, e37] = side_edges_going_up;
-
     let [e40, e51, e62, e73] =
         side_edges_going_up.map(|e| reverse::half_edge(e, half_edges));
 
     let [e01, e12, e23, e30] = bottom_edges_for_sides;
+    let [e04, e15, e26, e37] = side_edges_going_up;
 
     let f0154 = face::from_four_half_edges(
         [e01, e15, e54, e40],
