72a80cf5dd1535ef343c4aa75168ca8e60e0228f
Hanno Braun <hello@hannobraun.com>
2025-12-16 12:37:35 +0100

    Prepare to support other faces than tetragons

--- experiments/2025-12-03/src/operations/sketch.rs
+++ experiments/2025-12-03/src/operations/sketch.rs
@@ -36,21 +36,85 @@ pub fn from_four_half_edges(
     triangles: &mut Triangles,
     faces: &mut Faces,
 ) -> Index<Face> {
-    let [v0, v1b] = half_edges[e01].boundary;
-    let [v1, v2b] = half_edges[e12].boundary;
-    let [v2, v3b] = half_edges[e23].boundary;
-    let [v3, v0b] = half_edges[e30].boundary;
+    Sketch::new()
+        .push_half_edge(e01)
+        .push_half_edge(e12)
+        .push_half_edge(e23)
+        .push_half_edge(e30)
+        .build(vertices, half_edges, triangles, faces)
+}
 
-    assert_eq!(v0, v0b);
-    assert_eq!(v1, v1b);
-    assert_eq!(v2, v2b);
-    assert_eq!(v3, v3b);
+pub struct Sketch<const SIZE: usize> {
+    boundary: [Index<HalfEdge>; SIZE],
+}
 
-    let t012 = triangles.push([v0, v1, v2], vertices);
-    let t123 = triangles.push([v0, v2, v3], vertices);
+impl Sketch<0> {
+    pub fn new() -> Self {
+        Self { boundary: [] }
+    }
 
-    faces.push(Face {
-        boundary: vec![e01, e12, e23, e30],
-        triangles: vec![t012, t123],
-    })
+    pub fn push_half_edge(self, e01: Index<HalfEdge>) -> Sketch<1> {
+        let [] = self.boundary;
+        Sketch { boundary: [e01] }
+    }
+}
+
+impl Sketch<1> {
+    pub fn push_half_edge(self, e12: Index<HalfEdge>) -> Sketch<2> {
+        let [e01] = self.boundary;
+
+        Sketch {
+            boundary: [e01, e12],
+        }
+    }
+}
+
+impl Sketch<2> {
+    pub fn push_half_edge(self, e23: Index<HalfEdge>) -> Sketch<3> {
+        let [e01, e12] = self.boundary;
+
+        Sketch {
+            boundary: [e01, e12, e23],
+        }
+    }
+}
+
+impl Sketch<3> {
+    pub fn push_half_edge(self, e30: Index<HalfEdge>) -> Sketch<4> {
+        let [e01, e12, e23] = self.boundary;
+
+        Sketch {
+            boundary: [e01, e12, e23, e30],
+        }
+    }
+}
+
+impl Sketch<4> {
+    pub fn build(
+        self,
+        vertices: &Store<Vertex>,
+        half_edges: &Store<HalfEdge>,
+        triangles: &mut Triangles,
+        faces: &mut Faces,
+    ) -> Index<Face> {
+        let [e01, e12, e23, e30] = self.boundary;
+
+        let [v0, v1b] = half_edges[e01].boundary;
+        let [v1, v2b] = half_edges[e12].boundary;
+        let [v2, v3b] = half_edges[e23].boundary;
+        let [v3, v0b] = half_edges[e30].boundary;
+
+        assert_eq!(v0, v0b);
+        assert_eq!(v1, v1b);
+        assert_eq!(v2, v2b);
+        assert_eq!(v3, v3b);
+
+        let t012 = triangles.push([v0, v1, v2], vertices);
+        let t123 = triangles.push([v0, v2, v3], vertices);
+
+        faces.push(Face {
+            boundary: vec![e01, e12, e23, e30],
+            triangles: vec![t012, t123],
+        })
+    }
 }
