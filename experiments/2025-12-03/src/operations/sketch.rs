3fa00ebbbb12d8c788823cbd786ff61801aa7dd8
Hanno Braun <hello@hannobraun.com>
2025-12-27 10:57:16 +0100

    Prepare for follow-on change

--- experiments/2025-12-03/src/operations/sketch.rs
+++ experiments/2025-12-03/src/operations/sketch.rs
@@ -312,14 +312,14 @@ fn approx(
                 .flat_map(|(position, _, _)| [position].into_iter().copied()),
         ),
     );
-    let delaunay_points = positions_and_half_edges_and_approx.into_iter().map(
-        |(local, half_edge, _)| {
+    let delaunay_points = positions_and_half_edges_and_approx
+        .into_iter()
+        .flat_map(|(local, half_edge, _)| {
             let [_, vertex] = half_edges[half_edge].boundary;
             let global = vertices[vertex].point;
 
-            DelaunayPoint { local, global }
-        },
-    );
+            [DelaunayPoint { local, global }]
+        });
 
     delaunay(delaunay_points)
         .into_iter()
