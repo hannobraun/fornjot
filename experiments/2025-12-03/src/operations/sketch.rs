7944ac19a575a83af2972b402e5650733b8737c6
Hanno Braun <hello@hannobraun.com>
2026-01-01 14:15:25 +0100

    Relax type of function parameter

--- experiments/2025-12-03/src/operations/sketch.rs
+++ experiments/2025-12-03/src/operations/sketch.rs
@@ -107,7 +107,7 @@ impl Sketch {
             let next = self.segments[next_i];
 
             let (half_edge, approx) = current.to_half_edge_and_approx(
-                prev, next, &surface, half_edges, vertices,
+                &prev, next, &surface, half_edges, vertices,
             );
 
             positions_and_half_edges_and_approx.push((
@@ -151,7 +151,7 @@ struct SketchSegment {
 impl SketchSegment {
     pub fn to_half_edge_and_approx(
         self,
-        prev: SketchSegment,
+        prev: &SketchSegment,
         next: SketchSegment,
         surface: &Surface,
         half_edges: &mut Store<HalfEdge>,
