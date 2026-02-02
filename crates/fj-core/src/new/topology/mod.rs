dd07968ae3183c6341aa27ceab62b2f24e7865d1
Hanno Braun <hello@hannobraun.com>
2026-02-02 16:16:13 +0100

    Copy `topology` module to `fj-core`

---
+++ crates/fj-core/src/new/topology/mod.rs
@@ -0,0 +1,15 @@
+mod objects;
+mod store;
+
+pub use self::{
+    objects::{Face, HalfEdge, Solid, Vertex},
+    store::{Index, Store},
+};
+
+#[derive(Default)]
+pub struct Topology {
+    pub faces: Store<Face>,
+    pub half_edges: Store<HalfEdge>,
+    pub solids: Store<Solid>,
+    pub vertices: Store<Vertex>,
+}
