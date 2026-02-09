a7575768f0f19862946ee0941a2cdf3b5555180b
Hanno Braun <hello@hannobraun.com>
2026-02-09 13:34:22 +0100

    Document `approx` module

--- crates/fj-core/src/new/approx.rs
+++ crates/fj-core/src/new/approx.rs
@@ -1,3 +1,18 @@
+//! # Tools for creating approximations
+//!
+//! Fornjot can be viewed as a hybrid b-rep/mesh-based kernel. Topology is
+//! represented using typical b-rep primitives, but geometry is approximated
+//! with polylines and triangle meshes.
+//!
+//! Topological primitives and geometrical approximations exist side by side and
+//! approximations are built up together with the topological primitives. The
+//! tools provided by this module help doing that.
+//!
+//! This module is intended for internal use, as well as more advanced users of
+//! Fornjot. It is typically required to implement operations that create and
+//! modify shapes. More basic users would just use operations that others have
+//! implemented, never coming into contact with this module.
+
 use fj_math::{Point, Scalar};
 
 use crate::new::topology::{HalfEdge, Handle, Store, Vertex};
