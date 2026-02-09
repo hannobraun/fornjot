2c8103855b526c727d01e401950fbebb0c34b43a
Hanno Braun <hello@hannobraun.com>
2026-02-09 14:00:04 +0100

    Document `ApproxPoint`

--- crates/fj-core/src/new/approx.rs
+++ crates/fj-core/src/new/approx.rs
@@ -17,9 +17,30 @@ use fj_math::{Point, Scalar};
 
 use crate::new::topology::{HalfEdge, Handle, Store, Vertex};
 
+/// # A point in an approximation with both local and global representation
+///
+/// When creating an approximation, you often need to deal with local
+/// coordinates, either 1-dimensional on a curve or 2-dimensional on a surface.
+/// These local coordinates may be converted to global 3D coordinates later, or
+/// may correspond to 3D points that already exist.
+///
+/// Either way, storing a local point together with its corresponding global
+/// point is often advantageous or even necessary, and that's what this struct
+/// provides.
+///
+/// `ApproxPoint` is generic over the dimension of its local point. Typically,
+/// only `ApproxPoint<1>` and `ApproxPoint<2>` would be used.
+///
+/// `ApproxPoint<2>` [implements `spade::HasPosition`][`HasPosition`] and may be
+/// used together with [`spade`] for a Delaunay triangulation.
+///
+/// [`HasPosition`]: #impl-HasPosition-for-ApproxPoint<2>
 #[derive(Clone, Copy, Debug)]
 pub struct ApproxPoint<const D: usize> {
+    /// # The local form of the approximation point
     pub local: Point<D>,
+
+    /// # The global form of the approximation point
     pub global: Point<3>,
 }
 
