b5974b6a32c691aee659124877df5e83c0186828
Hanno Braun <hello@hannobraun.com>
2026-02-11 12:39:16 +0100

    Document `geometry`

--- crates/fj-core/src/new/geometry/mod.rs
+++ crates/fj-core/src/new/geometry/mod.rs
@@ -1,3 +1,15 @@
+//! # Geometrical primitives
+//!
+//! While [topology][`topology`] defines how parts of a shape relate to each
+//! other, geometry defines where those parts of a shape (and thus the shape
+//! overall) are located in space.
+//!
+//! Fornjot approximates geometry and embeds these approximations within the
+//! [topological primitives][`topology`]. This module thus plays a supporting
+//! role, by helping to create these approximations.
+//!
+//! [`topology`]: crate::new::topology
+
 mod curve;
 mod surface;
 
