29358d6ad6e4327ebaffff1fe148b8e284b8e9f7
Hanno Braun <hello@hannobraun.com>
2026-02-25 10:57:19 +0100

    Prepare for follow-on change

--- models/cuboid/src/lib.rs
+++ models/cuboid/src/lib.rs
@@ -17,13 +17,13 @@ use fj::core::{
 pub fn model(size: impl Into<Vector<3>>) -> TriMesh {
     let [x, y, z] = size.into().components;
 
-    let topology = Topology::new();
+    let mut topology = Topology::new();
     let Topology {
-        mut faces,
-        mut half_edges,
-        mut solids,
-        mut vertices,
-    } = topology;
+        faces,
+        half_edges,
+        solids,
+        vertices,
+    } = &mut topology;
 
     let mut sweep = Sweep::new();
 
@@ -37,18 +37,18 @@ pub fn model(size: impl Into<Vector<3>>) -> TriMesh {
                 origin: Point::from([0., 0., 0.]),
                 axes: [Vector::from([0., 1., 0.]), Vector::from([1., 0., 0.])],
             },
-            &mut vertices,
-            &mut half_edges,
-            &mut faces,
+            vertices,
+            half_edges,
+            faces,
         );
 
     let cuboid = sweep.face_to_solid(
         bottom,
         &Line::to([Scalar::ZERO, Scalar::ZERO, z]),
-        &mut vertices,
-        &mut half_edges,
-        &mut faces,
-        &mut solids,
+        vertices,
+        half_edges,
+        faces,
+        solids,
     );
 
     let triangles = solids[cuboid]
