f2bbcbdd4a964debe6773541f38e4d3a21f62b77
Hanno Braun <hello@hannobraun.com>
2026-04-14 22:11:18 +0200

    Fix documentation of `ValidationCheck`

--- crates/fj-core/src/new/validation/check.rs
+++ crates/fj-core/src/new/validation/check.rs
@@ -1,8 +1,10 @@
 /// # A specific validation check on a given primitive
 ///
 /// This trait is implemented once per validation check and primitive it applies
-/// to. `Self` is the primitive, while `T` identifies the validation check.
+/// to. It is implemented on a type that represents the validation check and
+/// carries information about a failure of that check. The type parameter `T` is
+/// the primitive that is being checked.
 pub trait ValidationCheck<T> {
-    /// # Run the validation check on the implementing primitive
+    /// # Run the validation check on the provided primitive
     fn check<'r>(primitive: &'r T) -> impl Iterator<Item = Self> + 'r;
 }
