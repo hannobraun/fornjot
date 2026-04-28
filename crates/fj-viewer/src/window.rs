60387eaee98d55edb6699c09270e3a46004068df
Hanno Braun <hello@hannobraun.com>
2026-04-28 21:08:18 +0200

    Update formatting in doc comments

--- crates/fj-viewer/src/window.rs
+++ crates/fj-viewer/src/window.rs
@@ -303,15 +303,15 @@ pub enum WindowError {
     RendererInit(#[from] RendererInitError),
 }
 
-/// Cursor position in normalized coordinates (-1 to +1)
+/// # Cursor position in normalized coordinates (-1 to +1)
 ///
 /// The center of the screen is at (0, 0). The aspect ratio is taken into
 /// account.
 #[derive(Clone, Copy, Debug)]
 pub struct NormalizedScreenPosition {
-    /// The x coordinate of the position [-1, 1]
+    /// # The x coordinate of the position [-1, 1]
     pub x: f64,
 
-    /// The y coordinate of the position [-1, 1]
+    /// # The y coordinate of the position [-1, 1]
     pub y: f64,
 }
