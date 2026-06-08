43ff2c7abdcb82e3a08c5c39230c7d0d954c3f9b
Hanno Braun <hello@hannobraun.com>
2026-06-08 11:13:46 +0200

    Upgrade dependency on `parry3d-f64` to `0.28.0`
    Close https://github.com/hannobraun/fornjot/issues/2633

--- crates/fj-core/src/math/triangle.rs
+++ crates/fj-core/src/math/triangle.rs
@@ -213,7 +213,15 @@ impl Triangle<2> {
 impl Triangle<3> {
     /// # Convert the triangle to a Parry triangle
     pub fn to_parry(self) -> parry3d_f64::shape::Triangle {
-        self.points.map(|point| point.to_na()).into()
+        self.points
+            .map(|point| {
+                point
+                    .coords
+                    .components
+                    .map(Scalar::into_f64)
+                    .into()
+            })
+            .into()
     }
 
     /// # Cast a ray against the Triangle
@@ -225,8 +233,12 @@ impl Triangle<3> {
         solid: bool,
     ) -> Option<Scalar> {
         let ray = Ray {
-            origin: origin.to_na(),
-            dir: dir.to_na(),
+            origin: origin
+                .coords
+                .components
+                .map(Scalar::into_f64)
+                .into(),
+            dir: dir.components.map(Scalar::into_f64).into(),
         };
 
         self.to_parry()
@@ -236,11 +248,12 @@ impl Triangle<3> {
 
     /// # Compute the triangle's normal
     pub fn normal(&self) -> Vector<3> {
-        self.to_parry()
+        let normal = self
+            .to_parry()
             .normal()
-            .expect("triangle is valid (validated on construction)")
-            .into_inner()
-            .into()
+            .expect("triangle is valid (validated on construction)");
+
+        Vector::from(normal.to_array())
     }
 }
 
