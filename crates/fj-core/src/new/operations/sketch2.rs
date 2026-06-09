d8534447c0d45e68cd67d24565e1fad32a271f31
Hanno Braun <hello@hannobraun.com>
2026-06-09 11:39:06 +0200

    Add `Sketch2::new`

--- crates/fj-core/src/new/operations/sketch2.rs
+++ crates/fj-core/src/new/operations/sketch2.rs
@@ -1,3 +1,10 @@
 /// # A new sketch operation
 #[derive(Default)]
 pub struct Sketch2 {}
+
+impl Sketch2 {
+    /// # Construct an empty sketch
+    pub fn new() -> Self {
+        Self::default()
+    }
+}
