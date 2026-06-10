3139fcb391d71074e91f7e7f3b268f3f4ca64357
Hanno Braun <hello@hannobraun.com>
2026-06-10 13:04:44 +0200

    Add `Sketch2::arc_to`

--- crates/fj-core/src/new/operations/sketch2.rs
+++ crates/fj-core/src/new/operations/sketch2.rs
@@ -1,5 +1,6 @@
 use crate::{
-    math::Point,
+    approx::{CircleApprox, Tolerance},
+    math::{Circle, NonZero, Point, Scalar},
     new::{
         approx::{ApproxHalfEdge, ApproxPoint, face_approx},
         geometry::Plane,
@@ -20,6 +21,32 @@ impl Sketch2 {
     /// # Construct an empty sketch
     pub fn new() -> Self {
         Self::default()
+    }
+
+    /// # Add an arc segment to a given point
+    ///
+    /// The arc is defined by a given radius, as well as a tolerance value that
+    /// defines how far the approximated representation of the arc is allowed to
+    /// deviate from the idealized arc.
+    ///
+    /// If the provided radius is positive, the resulting arc bulges to the
+    /// right, when viewed from the origin point of the arc. If the provided
+    /// radius is negative, the arc bulges to the left.
+    pub fn arc_to(
+        mut self,
+        radius: impl Into<Scalar>,
+        tolerance: impl Into<Tolerance>,
+        to: impl Into<Point<2>>,
+    ) -> Self {
+        self.segments.push(SketchSegment {
+            to: to.into(),
+            geometry: SketchSegmentGeometry::Arc {
+                radius: radius.into(),
+                tolerance: tolerance.into(),
+            },
+        });
+
+        self
     }
 
     /// # Add a line segment to a given point
@@ -112,6 +139,10 @@ struct SketchSegment {
 }
 
 enum SketchSegmentGeometry {
+    Arc {
+        radius: Scalar,
+        tolerance: Tolerance,
+    },
     Line,
 }
 
@@ -125,7 +156,78 @@ impl SketchSegmentGeometry {
         let _ = start;
         let _ = end;
 
-        let approx = match self {
+        let approx = match *self {
+            SketchSegmentGeometry::Arc { radius, tolerance } => {
+                let Some(start_to_end) = NonZero::new(end - start) else {
+                    panic!(
+                        "Sketches don't support arcs with identical start and \
+                        end points. These don't actually define a full circle, \
+                        as one might assume, as only one point and a signed \
+                        radius do not provide enough information to locate the \
+                        circle center.\n\
+                        \n\
+                        To create a circle, please construct two subsequent \
+                        half-circle arcs instead."
+                    );
+                };
+
+                let midpoint_towards_center =
+                    start_to_end.to_perpendicular().normalize()
+                        * radius.sign().to_scalar();
+
+                let distance_from_midpoint_to_center = {
+                    // We're computing the required distance from a right
+                    // triangle:
+                    //
+                    // - `a` (leg): `midpoint` to `end`
+                    // - `b` (leg): `midpoint` to circle center (the distance
+                    //   we're looking for)
+                    // - `c` (hypotenuse): `end` to circle center (which is
+                    //   `radius`)
+
+                    let a = start_to_end.magnitude() / 2.;
+                    let c = radius;
+
+                    let b_squared = c * c - a * a;
+
+                    if b_squared < Scalar::ZERO {
+                        panic!(
+                            "Radius of arc (`{radius}`) is too small: Must be \
+                            at least half the distance between start \
+                            (`{start:?}`) and end (`{end:?}`) points, or the \
+                            arc is not possible.",
+                            end = start_to_end,
+                        );
+                    }
+
+                    b_squared.sqrt()
+                };
+
+                let center = start
+                    + start_to_end.into_value() * 0.5
+                    + midpoint_towards_center
+                        * distance_from_midpoint_to_center;
+
+                // This only works if `surface` is a plane, which checks out for
+                // now.
+                let circle = {
+                    let a = start;
+                    let b = center + (a - center).to_perpendicular();
+
+                    Circle::new(center, a - center, b - center)
+                };
+
+                let approx = CircleApprox::new(radius, tolerance);
+
+                let start_local = circle.point_to_circle_coords(start);
+                let end_local = circle
+                    .point_to_circle_coords(start + start_to_end.into_value());
+
+                approx
+                    .points([start_local, end_local])
+                    .map(|local| circle.point_from_circle_coords(local))
+                    .collect()
+            }
             SketchSegmentGeometry::Line => Vec::new(),
         };
 
@@ -183,6 +285,29 @@ mod tests {
             &topology,
             1,
             [[0., 0., 0.], [1., 0., 0.], [0., 1., 0.]],
+        );
+        assert_eq!(half_face.orientation, Orientation::Nominal);
+    }
+
+    #[test]
+    fn circle() {
+        let mut topology = Topology::new();
+
+        let radius = 1.;
+        let tolerance = 0.35; // chosen to approximate circle in two triangles
+
+        let half_face = Sketch2::new()
+            .arc_to(radius, tolerance, [2., 0.])
+            .arc_to(radius, tolerance, [0., 0.])
+            .into_half_face(Plane::xy(), &mut topology);
+
+        assert_eq!(half_face.boundary.len(), 2);
+        check_connecting_vertices(&half_face, &topology);
+        check_approx(
+            &half_face,
+            &topology,
+            2,
+            [[0., 0., 0.], [1., -1., 0.], [2., 0., 0.], [1., 1., 0.]],
         );
         assert_eq!(half_face.orientation, Orientation::Nominal);
     }
